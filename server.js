efa83e24b641b94abe2bdb64a45d0fdd579591fa
Bjorn <bjossijohnson@gmail.com>
2020-01-23 23:31:26 +0100

    Bjorn's version

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ import cors from 'cors'
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
-// 
+//
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
+ import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
@@ -24,7 +24,29 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Welcome to the BOOKS API!')
+})
+
+app.get('/books', (req, res) => {
+  const minRating = req.query.minRating
+
+  if (minRating) {
+    const booksByMinimumRating = booksData.filter(book => book.average_rating >= minRating)
+    if (booksByMinimumRating.length > 0) {
+      res.json(booksByMinimumRating)
+    } else {
+    res.send("No books found with the minimum rating of " + minRating + " Min[0] to Max[5]")
+    }
+  }
+
+  res.json(booksData)
+})
+
+app.get('/isbn/:isbn', (req, res) => {
+  const isbn = req.params.isbn
+  console.log(isbn)
+  const bookByIsbn = booksData.find(book => book.isbn === +isbn)
+  res.json(bookByIsbn)
 })
 
 // Start the server
