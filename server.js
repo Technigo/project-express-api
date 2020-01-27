d636c80e65b8b86718c637bfdb0888daa1cfcc15
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-01-27 12:36:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,107 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import netflixData from './data/netflix-titles.json';
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+  res.send('Hello world');
+});
+
+app.get('/movies', (req, res) => {
+  const queryPage = +req.query.page;
+  const itemsPerPage = 20;
+  const startIndex = queryPage * itemsPerPage - itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const queryYear = +req.query.year;
+  const queryDuration = +req.query.duration;
+  let queryActor = req.query.actor;
+  let movies = netflixData;
+
+  if (queryYear) {
+    movies = movies.filter(movie => movie.release_year === queryYear);
+  }
+
+  if (queryActor) {
+    movies = movies.filter(movie => {
+      let actors = movie.cast.split(', ');
+
+      // Convert actor names to lowercase
+      actors = actors.map(actor => {
+        return actor.toLowerCase();
+      });
+
+      // Check if current movie contains value in queryActor
+      const result = actors.filter(item => {
+        let regexp = new RegExp(
+          '\\b(' + queryActor.toLowerCase() + ')\\b',
+          'gi'
+        );
+        const match = item.match(regexp);
+        console.log(item.match(regexp));
+
+        if (match) {
+          return item;
+        } else {
+          return;
+        }
+      });
+
+      // Return current movie if actor is found
+      if (result.length > 0) {
+        return movie;
+      }
+    });
+  }
+
+  if (queryDuration) {
+    movies = movies.filter(movie => {
+      const duration = movie.duration.split(' ');
+      if (
+        +duration[0] === queryDuration &&
+        duration[1].toLowerCase() !== 'seasons'
+      ) {
+        return movie;
+      }
+    });
+  }
+
+  if (queryPage) {
+    console.log('Page: ', queryPage);
+    console.log('startIndex: ', startIndex);
+    console.log('endIndex: ', endIndex);
+    movies = movies.slice(startIndex, endIndex);
+    console.log('Sliced array length: ', movies.length);
+  }
+  console.log('Array length: ', movies.length);
+  if (movies.length > 0) {
+    res.json(movies);
+  } else {
+    res.status(404).send('No movies found.');
+  }
+});
+
+app.get('/movies/:id', (req, res) => {
+  const id = req.params.id;
+  // console.log({ year });
+  const movie = netflixData.find(item => item.show_id === +id);
+
+  if (movie) {
+    res.json(movie);
+  } else {
+    res.status(404).send(`No movie found with id ${id}.`);
+  }
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
