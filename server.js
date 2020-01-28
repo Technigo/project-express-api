110c9fc51c5b6bbb8d27c471fc55e2e6de219ccc
Michel Elmoznino Laufer <mitchel_85@hotmail.com>
2020-01-28 18:02:45 +0100

    Created routes to return a collection of results and to return a single result

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import books from './data/books.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
-//
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
@@ -22,9 +13,41 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+// Start defining the routes
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+// Return all the books (objects) from the array in books.json
+app.get('/books', (req, res) => {
+  console.log(books)
+  res.json(books)
+})
+
+// Return an individual book (an object), using :id as a placeholder for the bookID number
+app.get('/books/:id', (req, res) => {
+  const id = req.params.id
+  const bookId = books.filter((item) => item.bookID === +id)
+  res.json(bookId)
+})
+
+// Return all the books (objects) from the array in books.json sorted by average rating (from the highest to lowest)
+app.get('/books/rating', (req, res) => {
+  const booksByRating = books.sort((a, b) => -(parseFloat(a.average_rating)-parseFloat(b.average_rating)))
+  res.json(booksByRating)
+})
+
+// Return all the books (objects) from the array in books.json sorted by number of pages (from highest to lowest)
+app.get('/books/pages', (req, res) => {
+  const booksByPages = books.sort((a, b) => -(parseFloat(a.num_pages)-parseFloat(b.num_pages)))
+  res.json(booksByPages)
+})
+
+// Return the books with a specific language code, using :language as a placeholder for language code
+app.get('/books/language/:language', (req, res) => {
+  const language = req.params.language
+  const booksByLanguage = books.filter((item) => item.language_code === language)
+  res.json(booksByLanguage)
 })
 
 // Start the server
