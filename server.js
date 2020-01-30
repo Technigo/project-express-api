b4d60d9f2544e160d8e35a5a5ecbeffb6c230a2e
Jenfi <jennifer_fick@hotmail.com>
2020-01-30 16:39:25 +0100

    after deply

--- server.js
+++ server.js
@@ -4,27 +4,6 @@ import cors from 'cors'
 import netflixData from './data/netflix-titles.json'
 
 
-// {
-//   "show_id": 81193313,
-//   "title": "Chocolate",
-//   "director": "",
-//   "cast": "Ha Ji-won, Yoon Kye-sang, Jang Seung-jo, Kang Bu-ja, Lee Jae-ryong, Min Jin-woong, Kim Won-hae, Yoo Teo",
-//   "country": "South Korea",
-//   "date_added": "November 30, 2019",
-//   "release_year": 2019,
-//   "rating": "TV-14",
-//   "duration": "1 Season",
-//   "listed_in": "International TV Shows, Korean TV Shows, Romantic TV Shows",
-//   "description": "Brought together by meaningful meals in the past and present, a doctor and a chef are reacquainted when they begin working at a hospice ward.",
-//   "type": "TV Show"
-// },
-
-
-//Endpoints som jag ska anv:
-// TV show (från "type")
-// Movie (från "type")
-
-
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //   PORT=9000 npm start
@@ -55,7 +34,7 @@ app.get('/shows', (req, res) => { //req = request-object, res = result-object,
 
 app.get('/shows/:id', (req, res) => {
   const id = req.params.id
-  console.log({ id })
+  // console.log({ id })
   const showsId = netflixData.filter((shows) => shows.type === "TV Show" && shows.show_id === +id)
   res.json(showsId)
 })
