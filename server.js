2ade22a2baf137705c90be82fa916e274295dd02
Emmie Nilsson <emmie.nilsson@gmail.com>
2020-01-30 21:05:24 +0100

    Made country and seasons queries instead of params. Moved country and seasons into shows route, and country into movies route. Refactored code for checking for seasons.

--- server.js
+++ server.js
@@ -3,95 +3,88 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import netflixData from './data/netflix-titles.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
 app.get('/', (req, res) => {
   res.send(netflixData)
 })
 
-
 // Shows routes
 app.get('/shows', (req, res) => {
+  const seasonsSearchString = req.query.seasons
+  const countrySearchString = req.query.country
 
-  // Filter shows
-  const shows = netflixData.filter((item) => item.type === "TV Show")
+  let filteredShows = netflixData.filter((item) => item.type === "TV Show")
 
-  // Send response
-  res.json(shows)
+  if (seasonsSearchString) {
+    filteredShows = netflixData.filter((item) => {
+
+      // Replace all characters that are not digits from the duration string
+      const showSeasons = item.duration.replace(/\D+/, '')
+
+      return item.type === "TV Show" && showSeasons === seasonsSearchString
+
+    })
+  }
+
+  if (countrySearchString) {
+    filteredShows = netflixData.filter((item) => {
+
+      const formattedCountry = item.country.toLowerCase()
+      const formattedSearchString = countrySearchString.toLowerCase()
+
+      return item.type === "TV Show" && formattedCountry.includes(formattedSearchString)
+
+    })
+  }
+
+  res.json(filteredShows)
 
 })
 
 app.get('/shows/:show', (req, res) => {
   const showId = req.params.show
 
-  // Filter shows
   const show = netflixData.filter((item) => item.show_id === +showId && item.type === "TV Show")
 
-  // Send response
   res.json(show)
 
-})
-
-app.get('/shows/seasons/:season', (req, res) => {
-  const season = req.params.season
-
-  const seasons = netflixData.filter((item) => {
-    if (season === "1") {
-      return item.duration === `${season} Season`
-    } else {
-      return item.duration === `${season} Seasons`
-    }
-  })
-
-  res.json(seasons)
 })
 
 // Movies routes
 
 app.get('/movies', (req, res) => {
+  const countrySearchString = req.query.country
 
-  // Filter movies
-  const movies = netflixData.filter((item) => item.type === "Movie")
+  let filteredMovies = netflixData.filter((item) => item.type === "Movie")
 
-  //Send response
-  res.json(movies)
+  if (countrySearchString) {
+    filteredMovies = netflixData.filter((item) => {
+
+      const formattedCountry = item.country.toLowerCase()
+      const formattedSearchString = countrySearchString.toLowerCase()
+
+      return item.type === "Movie" && formattedCountry.includes(formattedSearchString)
+
+    })
+  }
+
+  res.json(filteredMovies)
 })
 
 app.get('/movies/:movie', (req, res) => {
   const movieId = req.params.movie
 
-  // Filter movies
   const movie = netflixData.filter((item) => item.show_id === +movieId && item.type === "Movie")
 
-  // Send response
   res.json(movie)
+
 })
 
-
-// Countries route
-app.get('/countries/:country', (req, res) => {
-  const country = req.params.country
-
-  const showCountry = netflixData.filter((item) => {
-    const countries = item.country.split(',')
-
-    return countries.includes(country)
-  })
-
-  res.json(showCountry)
-})
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
