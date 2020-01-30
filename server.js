3e315a12881e6e3ae49baa76d2558bcec6770eef
Agnes Ketesdi <agnes.ketesdi@gmail.com>
2020-01-30 23:15:01 +0100

    added some data

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import books from './data/books.json'
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // 
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
@@ -15,7 +15,7 @@ import cors from 'cors'
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 5000
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
@@ -26,6 +26,27 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+// Returning all the books (objects) from the array
+
+app.get('/books', (req, res) => {
+  res.json(books)
+})
+
+// Return an individual book, using :id as a placeholder for the bookID number
+app.get('/books/:id', (req, res) => {
+  const id = req.params.id
+  const bookId = books.filter((item) => item.bookID === +id)
+  res.json(bookId)
+})
+
+// Returning the books with a specific language code
+app.get('/books/language/:language', (req, res) => {
+  const language = req.params.language
+  const booksLanguageCode = books.filter((item) => item.language_code === language)
+  res.json(booksLanguageCode)
+})
+
 
 // Start the server
 app.listen(port, () => {
