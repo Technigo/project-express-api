9e4b11e0384bd5eaf3b2e69309355a6f07dc7cb3
Jenfi <jennifer_fick@hotmail.com>
2020-01-30 14:55:19 +0100

    first commit, testing

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ import cors from 'cors'
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // 
+// {
+//   "show_id": 81193313,
+//   "title": "Chocolate",
+//   "director": "",
+//   "cast": "Ha Ji-won, Yoon Kye-sang, Jang Seung-jo, Kang Bu-ja, Lee Jae-ryong, Min Jin-woong, Kim Won-hae, Yoo Teo",
+//   "country": "South Korea",
+//   "date_added": "November 30, 2019",
+//   "release_year": 2019,
+//   "rating": "TV-14",
+//   "duration": "1 Season",
+//   "listed_in": "International TV Shows, Korean TV Shows, Romantic TV Shows",
+//   "description": "Brought together by meaningful meals in the past and present, a doctor and a chef are reacquainted when they begin working at a hospice ward.",
+//   "type": "TV Show"
+// },
+
+
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
 // import booksData from './data/books.json'
