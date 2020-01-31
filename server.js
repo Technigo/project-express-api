22ea1bf61a2c9d17aa1a2626be294c2b843a4c12
Petra B <petrabrinck@icloud.com>
2020-01-31 12:53:13 +0100

    they work separately but I am still writing the wrong text in localhost when combining them

--- server.js
+++ server.js
@@ -6,13 +6,6 @@ import netflixData from './data/netflix-titles.json'
 console.log(netflixData.length)
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -31,7 +24,6 @@ app.get('/', (req, res) => {
 
 // app.get('/types', (req, res) => {
 //   res.json(netflixData)
-
 // })
 
 // router  to finding specific year //
@@ -44,25 +36,76 @@ app.get('/', (req, res) => {
 //   let releasesFromYear = netflixData.filter((item) => item.release_year === +year)
 // res.json(releasesFromYear)
 
-
 //   if (showCountry) {
 //     releasesFromYear = releasesFromYear.filter((item) => item.country)
 //   }
 //   res.json(releasesFromYear)
 // })
 
-//// TEST ///// 
+// The Crossfit example: const show = athletesData.filter(item => +item.competitorid === +showId)
+// due to that everything in that API was a string.
 
 // path variable/path parameter
+// shows one specific id // http://localhost:8080/shows/80115338
+app.get('/shows/:id', (req, res) => {
+  const showId = req.params.id
+  const show = netflixData.filter(item => item.show_id === +showId)
+  console.log("id path parameter")
+  res.json(show)
+})
+
+// Filter based on title AND/OR country 
 app.get('/shows', (req, res) => {
-  // query parameter 
+  // Query parameter
+  const titleSearchString = req.query.title
+  const countrySearchString = req.query.country
+
+  let filteredShows = netflixData;
+
+  if (titleSearchString) {
+    filteredShows = filteredShows.filter(item => {
+      const itemTitle = item.title.toString()
+      return itemTitle.includes(titleSearchString)
+    })
+  }
+
+  if (countrySearchString) {
+    filteredShows = filteredShows.filter(item => {
+      const itemCountry = item.country.toString()
+      return itemCountry.includes(countrySearchString)
+    })
+  }
+
+  res.json(filteredShows);
+})
+//  THIS ID WORKS: http://localhost:8080/shows/80141270 (BONUS FAM/SWEDEN)
+// THESE TITLES WORKS: http://localhost:8080/shows?title=Orange
+//  http://localhost:8080/shows?title=Little
+
+// THESE COUNTRIES WORK: http://localhost:8080/shows?country=Sweden
+// http://localhost:8080/shows?country=Denmark
+
+
+//  res.json(show)
+//   return showId
+// })
+
+//http://localhost:8080/country/Sweden
+// app.get('/country/:country', (req, res) => {
+//   const country = req.params.country
+//   const fromCountry = netflixData.filter((item) => item.country === country)
+//   res.json(fromCountry)
+// })
+
+//Filter based on title OR Country or decription
+app.get('/shows', (req, res) => {
+  //query parameter 
   const searchString = req.query.search
 
   let filteredShows = netflixData
-  // const searchString: any
 
   if (searchString) {
-    // Filter once 
+    // Filter once on multiple fields
     filteredShows = filteredShows.filter(item => {
       const itemTitle = item.title.toString()
       const itemCountry = item.country.toString()
@@ -73,16 +116,8 @@ app.get('/shows', (req, res) => {
         itemDescription.includes(searchString)
     })
   }
-
-  res.json(filteredShows)
-})
-
-// http://localhost:8080/country/Sweden
-// app.get('/country/:country', (req, res) => {
-//   const country = req.params.country
-//   const fromCountry = netflixData.filter((item) => item.country === country)
-//   res.json(fromCountry)
-// })
+  res.json(filteredShows);
+});
 
 // Start the server
 app.listen(port, () => {
