bc0f4722c2c446f15a2a1ddc86d47ce33f90a8d4
SaraBorg <50766676+SaraBorg@users.noreply.github.com>
2020-01-31 13:56:25 +0100

    First route finding ID, message when not found

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import booksData from './data/books.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
+/* {
+  "bookID": 1,
+  "title": "Harry Potter and the Half-Blood Prince (Harry Potter  #6)",
+  "authors": "J.K. Rowling-Mary GrandPré",
+  "average_rating": 4.56,
+  "isbn": 439785960,
+  "isbn13": 9780439785969,
+  "language_code": "eng",
+  "num_pages": 652,
+  "ratings_count": 1944099,
+  "text_reviews_count": 26249
+}, */
+
+
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 5000
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
@@ -26,6 +30,29 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+app.get('/book', (req, res) => {
+  res.json(booksData)
+})
+
+app.get('/books/:id', (req, res) => {
+  const bookId = req.params.id
+  const book = booksData.find((item) => item.bookID === +bookId)
+  if (book) {
+    res.json(book);
+  } else {
+    res.status(404).send("No book found")
+  }
+})
+
+app.get('/title', (req, res) => {
+  const title = req.params.title
+  console.log({ title })
+  const bookTitle = booksData.filter((item) => item.title === title)
+
+  res.json(bookTitle)
+})
+
 
 // Start the server
 app.listen(port, () => {
