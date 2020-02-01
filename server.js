99bb010e2a27d0da58feae29fe410ba3fbf6d9ee
SanSjo <susanna.sjoberg@gmail.com>
2020-02-01 21:51:35 +0800

    add another route with path and query parameters. Change name on routes

--- server.js
+++ server.js
@@ -2,14 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-//
-// import goldenGlobesData from './data/golden-globes.json';
-// import avocadoSalesData from './data/avocado-sales.json'
-import booksData from './data/books.json';
 import netflixData from './data/netflix-titles.json';
-// import topMusicData from './data/top-music.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
@@ -22,90 +15,54 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world');
-});
+// Two routes here
 
-app.get('/netflix', (req, res) => {
-  res.json(netflixData);
-});
-
-app.get('/shows/:id', (req, res) => {
-  const id = req.params.id;
-  console.log({ id });
-  const showId = netflixData.filter(item => item.show_id === +id);
-  console.log('id path parameter');
-  res.json(showId);
-});
-
-//This works
-//Filter based on title OR country OR description
+//First route filter once on multiple fields based on title OR country OR desription OR genre
 app.get('/api', (req, res) => {
   // query parameter
   const searchString = req.query.search;
   let filteredApi = netflixData;
 
   if (searchString) {
-    //Filter once on multiple fields
     filteredApi = filteredApi.filter(item => {
       const itemTitle = item.title.toString();
       const itemCountry = item.country.toString();
       const itemDescription = item.description.toString();
-      const itemType = item.type.toString();
+      const itemGenre = item.listed_in.toString();
       return (
         itemTitle.includes(searchString.toLowerCase()) ||
         itemCountry.includes(searchString) ||
         itemDescription.includes(searchString) ||
-        itemType.includes(searchString)
+        itemGenre.includes(searchString)
       );
     });
   }
   res.json(filteredApi);
 });
 
-app.get('/shows', (req, res) => {
-  const titleSearch = req.query.title;
-  const showCountry = netflixData;
-
-  if (titleSearch) {
-    showCountry = showCountry.filter(item => {
-      return itemTitle.includes(titleSearch.toLowerCase());
-    });
-  }
-
-  res.json(showCountry);
-});
-
-// Is working
-app.get('/shows/:year', (req, res) => {
+// Second route has a path parameter to filter by year and two guery parameters to filter by genre OR/AND type.
+app.get('/api/:year', (req, res) => {
   const year = req.params.year;
-  const showGenre = req.query.genre;
+  const showGenre = req.query.listed_in;
   const showType = req.query.type;
 
   let releaseYear = netflixData.filter(item => item.release_year === +year);
 
   if (showType) {
-    releaseYear = releaseYear.filter(type =>
-      type.type.toLowerCase().includes(showType.toLowerCase())
-    );
+    releaseYear = releaseYear.filter(type => {
+      const typeItem = type.type.toLowerCase();
+      return typeItem.includes(showType.toLowerCase());
+    });
+  }
+
+  if (showGenre) {
+    releaseYear = releaseYear.filter(item => {
+      const genreItem = item.listed_in.toLowerCase();
+      return genreItem.includes(showGenre.toLowerCase());
+    });
   }
 
   res.json(releaseYear);
-});
-
-app.get('/books', (req, res) => {
-  const { page } = req.query; // "2"
-  const startIndex = +page * 20;
-
-  res.json(booksData.slice(startIndex, 20)); // only sends 20 item on one page.
-});
-
-app.get('/books/:id', (req, res) => {
-  const { id } = req.params;
-  console.log();
-  const findBook = booksData.find(book => book.bookId === +id);
-  res.json(findBook);
 });
 
 // Start the server
