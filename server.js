47c898a3c1bae57bfc4d3007360fba3e3ce384e3
AnnaTerelius <tereliusanna@hotmail.com>
2020-02-02 19:44:39 +0100

    uppdated searchstring title

--- server.js
+++ server.js
@@ -2,17 +2,11 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
+//deployed at: https://tereliusexpressdeployment.herokuapp.com/
 
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-//import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-//import booksData from './data/books.json'
 import netflixData from './data/netflix-titles.json'
 import { query } from 'express/lib/express'
-// import topMusicData from './data/top-music.json'
+
 
 // overridden when starting the server. For example:
 //
@@ -24,49 +18,57 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+// Start defining routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
+//get all films with the path "films"
 app.get('/films', (req, res) => {
   res.json(netflixData)
 })
 
+//get film frpm specific year with for example the path "year/2019"
 app.get('/year/:year', (req, res) => {
  const year = req.params.year
  const filmsFromYear = netflixData.filter((item) => item.release_year === +year)
  res.json(filmsFromYear)
 })
 
+//get specific film with for example the path "film/Chocolate"
 app.get('/film/:title', (req, res) => {
   const title = req.params.title
   const film = netflixData.filter((item) => item.title === title)
   res.json(film)
  })
 
+ //get the type of show with for example the path "Movie"
 app.get('/type/:type', (req, res) => {
   const type = req.params.type
   const typeOfFilm = netflixData.find((item) => item.type === type)
   res.json(typeOfFilm)
  })
 
+ //get the first 10 films with the path "firstTenFilms"
  app.get('/films/:firstTenFilms', (req, res) => {
   const firstTenFilms = netflixData.slice(0, 10)
-  res.json(firstTenFilms
-    )
+  res.json(firstTenFilms)
  })
 
- //http://localhost:8080/shows?search=choc  this is the searchstring for shows with the word 'choc' somewhere
- //const titleSearchString = req.query.search;
-  // let filteredShows = netflixData.filter(item => {
-     //const itemTitle = item.title.toString();
-     //return itemTitle.toLowerCase().includes(titleSearchString.toLowerCase())
+ //http://localhost:8080/show?search=choc  this is the searchstring for shows with the word 'choc' somewhere
+ app.get('/show', (req, res) => { 
+ const titleSearchString = req.query.search;
+   let filteredShow = netflixData.filter(item => {
+     const itemTitle = item.title.toString();
+     return itemTitle.toLowerCase().includes(titleSearchString.toLowerCase())
     
-  // });
-   //res.json(filteredShows);
- //});
+   });
+  
+   res.json(filteredShow);
+ });
 
+
+ //get the films filtered on title, country and description with query and for example searchstring "/shows?search=sweden"
  app.get('/shows', (req, res) => {
   const searchString = req.query.search;
     
