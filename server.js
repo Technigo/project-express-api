1e93b96166d0654ba940ba70aeb3f66587f489f4
Agnes Ketesdi <agnes.ketesdi@gmail.com>
2020-02-02 21:28:06 +0100

    final commit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ import books from './data/books.json'
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8000
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
@@ -27,17 +27,24 @@ app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-// Returning all the books (objects) from the array
-
+// Returning all the books (objects) from the array 
 app.get('/books', (req, res) => {
-  res.json(books)
+  const { page } = req.query
+  const startIndex = 20 * +page
+  res.json(books.slice(startIndex, startIndex + 20))
 })
 
+//request.params is an object containing properties to the named route
 // Return an individual book, using :id as a placeholder for the bookID number
 app.get('/books/:id', (req, res) => {
   const id = req.params.id
-  const bookId = books.filter((item) => item.bookID === +id)
+  const bookId = books.filter((item) => item.bookID === +id) // + means turning string into a number
   res.json(bookId)
+  if (bookId) {
+    res.json(bookId)
+  } else {
+    res.status(404).json({ message: 'No books found with this id.' })
+  }
 })
 
 // Returning the books with a specific language code
