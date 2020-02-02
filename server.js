262b192d02e1a814812b6733cca3b561c74c9418
Linda <isell.linda@gmail.com>
2020-02-02 19:21:33 +0100

    Fix pages

--- server.js
+++ server.js
@@ -1,32 +1,21 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
 import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
 
 
 // {
-//   "bookID": 1,
-//     "title": "Harry Potter and the Half-Blood Prince (Harry Potter  #6)",
-//       "authors": "J.K. Rowling-Mary GrandPré",
-//         "average_rating": 4.56,
-//           "isbn": 439785960,
-//             "isbn13": 9780439785969,
-//               "language_code": "eng",
-//                 "num_pages": 652,
-//                   "ratings_count": 1944099,
-//                     "text_reviews_count": 26249
+// "bookID": 1,
+// "title": "Harry Potter and the Half-Blood Prince (Harry Potter  #6)",
+// "authors": "J.K. Rowling-Mary GrandPré",
+// "average_rating": 4.56,
+// "isbn": 439785960,
+// "isbn13": 9780439785969,
+// "language_code": "eng",
+// "num_pages": 652,
+// "ratings_count": 1944099,
+// "text_reviews_count": 26249
 // },
 
 //   PORT=9000 npm start
@@ -40,7 +29,7 @@ app.use(bodyParser.json())
 
 // Start defining your routes here. Below: 
 app.get('/', (req, res) => {
-  res.send('Hello world - project express api')
+  res.send('Possible routes: /books, /language/:language, /rating/:rating')
 })
 
 //SHOW WELCOME 
@@ -49,11 +38,11 @@ app.get('/Welcome', (req, res) => {
 })
 
 //SHOW ALL DATA IN JSON
-app.get('/books', (req, res) => {
-  res.json(booksData)
-})
+// app.get('/books', (req, res) => {
+//   res.json(booksData)
+// })
 
-//SHOW A BOOK BASED ON ID (WHY NOT WORKING WITH .FILTER?)
+//SHOW A BOOK BASED ON ID
 app.get('/books/:id', (req, res) => {
   const bookId = req.params.id
   const book = booksData.find((item) => item.bookID === +bookId)
@@ -63,15 +52,13 @@ app.get('/books/:id', (req, res) => {
   } else {
     res.status(404).send(`No book found with id ${bookId}.`);
   }
-  console.log(bookId)
 })
 
-// Filter based on title AND/OR country 
+// Filter based on title AND/OR author. http://localhost:8080/books?title=harry
 app.get("/books", (req, res) => {
   //Query parameter
   const titleSearch = req.query.title
   const authorsSearch = req.query.authors
-
   let filteredBooks = booksData
 
   if (titleSearch) {
@@ -85,35 +72,65 @@ app.get("/books", (req, res) => {
       const authorsName = book.authors.toString()
       return authorsName.toLowerCase().includes(authorsSearch.toLowerCase())
     })
-    res.json(filteredBooks)
+  } if (filteredBooks.length === 0) {
+    res.status(404).send(`No books found`);
   } else {
-    res.status(404).send(`No book found with title: ${titleSearch}.`);
+    res.json(filteredBooks)
   }
-  console.log(filteredBooks)
-})
+}) //Why is not error message showing?
 
 //Filters on language_code.  http://localhost:8080/language/eng
 app.get('/language/:language', (req, res) => {
   const language = req.params.language
   const bookLanguage = booksData.filter((item) => item.language_code === language)
 
-  res.json(bookLanguage)
+  if (bookLanguage.length === 0) {
+    res.status(404).send(`No book found.`);
+
+  } else {
+    res.json(bookLanguage)
+    console.log(bookLanguage)
+  }
 })
 
+//Sort on avarage_rating.  http://localhost:8080/language/eng
+// app.get('/rating/:rating', (req, res) => {
+//   const ratingSearch = req.params.rating
+//   const bookRating = booksData.filter((item) => item.avarage_rating === rating)
+
+//   res.json(bookRating)
+// })
+
+app.get('/sort/rating', (req, res) => {
+  const booksByRating = booksData.sort((a, b) => -(parseFloat(a.average_rating) - parseFloat(b.average_rating)))
+  res.json({
+    text: "Sorted by average rating",
+    booksByRating
+  })
+})
+
+
 //PAGINATION. URL = http://localhost:8080/books1?page=1
-const PER_PAGE = 10
+const PER_PAGE = 20
 
 app.get('/books1', (req, res) => {
   const { page } = req.query
   const startIndex = PER_PAGE * +page
   const data = booksData.slice(startIndex, startIndex + PER_PAGE)
+  const pageCount = Math.ceil(booksData.length / 20)
 
-  res.json({
-    totalPages: Math.floor(booksData.length / PER_PAGE),
-    currentPage: +page,
-    data
-  })
+  if (page >= pageCount) {
+    res.status(404).send(`No books found`);
+
+  } else {
+    res.json({
+      totalPages: Math.floor(booksData.length / PER_PAGE),
+      currentPage: +page,
+      data
+    })
+  }
 })
+
 
 // Start the server
 app.listen(port, () => {
