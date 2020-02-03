1a6683447ef0b624cbcd2dcd604aaee93f360025
Johanna Luks Wærn <johanna@misswaern.se>
2020-02-03 20:20:57 +0100

    initial commit

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import sales from './data/avocado-sales.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+// {
+//   "id": 0,
+//   "date": "2015-12-27",
+//   "averagePrice": 1.33,
+//   "totalVolume": 64236.62,
+//   "totalBagsSold": 8696.87,
+//   "smallBagsSold": 8603.62,
+//   "largeBagsSold": 93.25,
+//   "xLargeBagsSold": 0,
+//   "region": "Albany"
+// },
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
@@ -23,8 +27,56 @@ app.use(cors())
 app.use(bodyParser.json())
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+
+app.get('', (req, res) => {
+  res.end('Learn about avocado sales. Jeeiij')
+})
+
+// /sales - all sales everywhere
+// /sales?totalBagsSoldMoreThan=1234 - all sales with more than 1234 sold bags
+app.get('/sales', (req, res) => {
+  const totalBagsSoldMoreThan = req.query.totalBagsSoldMoreThan
+  if (totalBagsSoldMoreThan) {
+    const matchingSales = sales.filter(
+      (sale) => sale.totalBagsSold >= totalBagsSoldMoreThan
+    )
+    res.json(matchingSales)
+  } else {
+    res.json(sales)
+  }
+})
+
+// /sales/5 - sale with id 5 in every region
+app.get('/sales/:id', (req, res) => {
+  const id = req.params.id
+  const saleIdInAllRegions = sales.find((sale) => sale.id === +id)
+  res.json(saleIdInAllRegions)
+})
+
+// /Albany/sales - all sales in Albany
+// /Albany/sales?totalBagsSoldMoreThan=8000 - alla sales i Albany med mer än 8000 sålda bags
+
+app.get('/:region/sales', (req, res) => {
+  const region = req.params.region
+  const salesInRegion = sales.filter(
+    (sale) => sale.region.toLowerCase() === region.toLowerCase()
+  )
+  res.json(salesInRegion)
+})
+
+// /Albany/sales/5 -  sale with id 5 in a specific region
+app.get('/:region/sales/:id', (req, res) => {
+  const region = req.params.region
+  const { id } = req.params
+  let specificSale = sales.filter(
+    (sale) => sale.region.toLowerCase() === region.toLowerCase()
+  )
+
+  if (specificSale) {
+    specificSale = specificSale.find((sale) => sale.id === +id)
+  }
+
+  res.json(specificSale)
 })
 
 // Start the server
