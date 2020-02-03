21a7aa12721741a153d468fc51a8b4021b4ee74a
horsefacegirl <51134090+horsefacegirl@users.noreply.github.com>
2020-02-03 16:48:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,27 +3,14 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import booksData from "./data/books.json";
 
-//     Example of json stucture
-//
-//     "bookID": 2,
-//     "title": "Harry Potter and the Order of the Phoenix (Harry Potter  #5)",
-//     "authors": "J.K. Rowling-Mary GrandPré",
-//     "average_rating": 4.49,
-//     "isbn": 439358078,
-//     "isbn13": 9780439358071,
-//     "language_code": "eng",
-//     "num_pages": 870,
-//     "ratings_count": 1996446,
-//     "text_reviews_count": 27613
-
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+// Middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(bodyParser.json());
 
-// Start defining your routes here
+// Routes
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
