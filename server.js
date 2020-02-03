4de28982ca6af91a715e84f6ac60d351bb298130
Jenfi <jennifer_fick@hotmail.com>
2020-02-03 14:06:57 +0100

    added query

--- server.js
+++ server.js
@@ -20,24 +20,53 @@ app.get('/', (req, res) => {
 })
 
 //BELOW SHOWS ALL TITLES
-app.get('/titles', (req, res) => { //req = request-object, res = result-object, 
+app.get('/all', (req, res) => { //req = request-object, res = result-object, 
   //Here we use the response object to stat building up the response to send back to the browser/the one requesting the data by GET
+  // const title = req.query.title
+  // let specificShowTitle = netflixData.filter((item) => item.title === title)
+
+  // if (title) {
+  //   specificShowTitle = specificShowTitle.filter((item) => item.title)  }
+
+
+
   res.json(netflixData)
+
+
 })
 
 //BELOW FILTERS TO SHOW ONLY THE TV-SHOWS
-app.get('/shows', (req, res) => { //req = request-object, res = result-object, 
+app.get('/shows/', (req, res) => { //req = request-object, res = result-object, 
   //Here we use the response object to stat building up the response to send back to the browser/the one requesting the data by GET
+
   const shows = netflixData.filter((item) => item.type === "TV Show")
+
+  //Jag vill kunna söka på specifik titel
+
   res.json(shows)
 })
 
 app.get('/shows/:id', (req, res) => {
   const id = req.params.id
-  // console.log({ id })
   const showsId = netflixData.filter((shows) => shows.type === "TV Show" && shows.show_id === +id)
+
+
   res.json(showsId)
 })
+
+app.get('/year/:year', (req, res) => {
+  console.log({ year })
+  const year = req.params.year
+  const anyTypeFromYear = netflixData.filter((item) => item.release_year === +year) //(=== year, alltså det year som jag sätter in som endpoint i webbläsaren)
+
+
+
+  res.json(anyTypeFromYear)
+})
+
+
+
+
 
 
 //BELOW FILTERS TO SHOW ONLY THE MOVIES
