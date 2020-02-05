51ce966035a011ffc1f95ce756da0815aca691c3
JoacimNilsen <joacimnilsen1986@gmail.com>
2020-02-05 17:39:27 +0100

    Added a route for starting letter of titles

--- server.js
+++ server.js
@@ -1,33 +1,50 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from "express"
+import bodyParser from "body-parser"
+import cors from "cors"
+import booksData from "./data/books.json"
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+// Routes
+app.get("/", (req, res) => {
+  res.send("Books API")
+});
+
+app.get("/books", (req, res) => {
+  res.json(booksData)
+});
+
+app.get("/books/:id", (req, res) => {
+  const bookId = req.params.id
+  const book = booksData.find(item => item.bookID.toString() === bookId)
+  res.json(book)
+})
+
+app.get("/language/:language", (req, res) => {
+  const language = req.params.language
+  const booksByLanguage = booksData.filter(item => item.language_code === language)
+  res.json(booksByLanguage)
+})
+
+app.get('/sort/rating', (req, res) => {
+  const sortByRating = booksData.sort((a, b) => -(parseFloat(a.average_rating)-parseFloat(b.average_rating)))
+  res.json(sortByRating)
+})
+
+app.get("/title/:title", (req, res) => {
+  const title = req.params.title
+  const titleLetter = booksData.filter(book => {
+    let bookTitle = book.title.toString()
+    return bookTitle.toLowerCase().charAt(0) === title.toLowerCase().charAt(0)
+  })
+  res.json(titleLetter)
 })
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
