d4e364cb01a1b38cb08693ebc08bb9fa8014229e
Jens_Nilsen <jensnilsen89@gmail.com>
2020-02-13 01:39:41 +0100

    project pretty much finished. could put more our to this and maybe get some error codes at least but for now its good

--- server.js
+++ server.js
@@ -2,22 +2,8 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import netflixData from './data/netflix-titles.json'
-
-// FIGURE OUT HOW TO MAKE item.nominee TO A STRING
-
-// console.log(netflixData.length)
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-//
 import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
 
-// import topMusicData from './data/top-music.json'
-
-// Defines the port the app will run on. Defaults to 8080, but can be
-// overridden when starting the server. For example:
-//
 // PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
@@ -28,7 +14,7 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello worlsssnpmd')
+  res.send('Golden Globe!')
 })
 
 app.get('/listlength', (req, res) => {
