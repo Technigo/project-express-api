a905f709fcf58c3dbb5062a1a762527f9bf4910f
ucha3i <jchabik@gmail.com>
2020-02-17 09:40:40 +0100

    working project

--- server.js
+++ server.js
@@ -1,106 +1,74 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import sales from './data/avocado-sales.json'
 
-
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
+// Defines the port the app will run on. Defaults to 8080, but can be
+// overridden when starting the server. For example:
+//
+//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
+// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello world')
+// Start defining your routes here
+app.get('', (req, res) => {
+  res.end('First backend project')
 })
 
-// Example of getting shows with limit
-// app.get('/shows', (req, res)=>{
-//   const limit = req.query.limit;
-//   let showsReturned = netflixData;
-//   if(limit){
-//     showsReturned = showsReturned.slice(0,limit);
-//   }
-//   res.json(showsReturned);
-// })
+// /sales - all sales everywhere
+// /sales?totalBagsSoldMoreThan=1234 - all sales with more than 1234 sold bags
+app.get('/sales', (req, res) => {
+  const totalBagsSoldMoreThan = req.query.totalBagsSoldMoreThan
+  if (totalBagsSoldMoreThan) {
+    const matchingSales = sales.filter(
+      (sale) => sale.totalBagsSold >= totalBagsSoldMoreThan
+    )
+    res.json(matchingSales)
+  } else {
+    res.json(sales)
+  }
+})
 
+// average price, sorted from low to high
+app.get('/salesByAveragePrice', (req, res) => {
+  res.json(sales.sort((a, b) => a.averagePrice - b.averagePrice))
+})
+//combine this route with the sales route if there is time
 
-// {
-//   "show_id": 81193313,
-//   "title": "Chocolate",
-//   "director": "",
-//   "cast": "Ha Ji-won, Yoon Kye-sang, Jang Seung-jo, Kang Bu-ja, Lee Jae-ryong, Min Jin-woong, Kim Won-hae, Yoo Teo",
-//   "country": "South Korea",
-//   "date_added": "November 30, 2019",
-//   "release_year": 2019,
-//   "rating": "TV-14",
-//   "duration": "1 Season",
-//   "listed_in": "International TV Shows, Korean TV Shows, Romantic TV Shows",
-//   "description": "Brought together by meaningful meals in the past and present, a doctor and a chef are reacquainted when they begin working at a hospice ward.",
-//   "type": "TV Show"
-//   }
+// /sales/5 - sale with id 5 in every region
+app.get('/sales/:id', (req, res) => {
+  const id = req.params.id
+  const saleIdInAllRegions = sales.filter((sale) => sale.id === +id)
+  res.json(saleIdInAllRegions)
+})
 
-// Path variable/path parameter
-app.get('/shows/:id', (req, res) => {
-  const showId = req.params.id;
-  const show = netflixData.filter(item => item.show_id === +showId);
-  console.log("id path parameter");
-  res.json(show);
-});
+// /Albany/sales - all sales in Albany
+app.get('/:region/sales', (req, res) => {
+  const region = req.params.region
+  const salesInRegion = sales.filter(
+    (sale) => sale.region.toLowerCase() === region.toLowerCase()
+  )
+  res.json(salesInRegion)
+})
 
-// Filter based on title AND/OR country 
-// app.get('/shows', (req, res) => {
-//   // Query parameter
-//   const titleSearchString = req.query.title;
-//   const countrySearchString = req.query.country;
+// /Albany/sales/5 -  sale with id 5 in a specific region
+app.get('/:region/sales/:id', (req, res) => {
+  const region = req.params.region
+  const id = req.params.id
+  let specificSale = sales.filter(
+    (sale) => sale.region.toLowerCase() === region.toLowerCase()
+  )
 
-//   let filteredShows = netflixData;
-
-//   if (titleSearchString) {
-//     filteredShows = filteredShows.filter(item => {
-//       const itemTitle = item.title.toString();
-//       return itemTitle.includes(titleSearchString)
-//     });
-//   }
-
-//   if (countrySearchString) {
-//     filteredShows = filteredShows.filter(item => {
-//       const itemCountry = item.country.toString();
-//       return itemCountry.includes(countrySearchString)
-//     });
-//   }
-
-//   res.json(filteredShows);
-// });
-
-// Filter based on title OR country OR description
-app.get('/shows', (req, res) => {
-  // Query parameter
-  const searchString = req.query.search;
-
-  let filteredShows = netflixData;
-
-  if (searchString) {
-    // Filter once on multiple fields
-    filteredShows = filteredShows.filter(item => {
-      const itemTitle = item.title.toString();
-      const itemCountry = item.country.toString();
-      const itemDescription = item.description.toString();
-      return itemTitle.includes(searchString) ||
-        itemCountry.includes(searchString) || 
-        itemDescription.includes(searchString);
-    });
+  if (specificSale) {
+    specificSale = specificSale.find((sale) => sale.id === +id)
   }
 
-
-  res.json(filteredShows);
-});
-
+  res.json(specificSale)
+})
 
 // Start the server
 app.listen(port, () => {
