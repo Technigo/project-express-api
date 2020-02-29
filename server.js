b6a855413ba763cc543f1e5e13435c01f4d67b3a
visua77 <nicznet@hotmail.com>
2020-02-29 19:44:52 +0100

    commited to server.js

--- server.js
+++ server.js
@@ -2,20 +2,22 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
+
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // 
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
 // import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
+import netflixData from './data/netflix-titles.json'
+//console.log(netflixData)
 // import topMusicData from './data/top-music.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8000
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
@@ -24,10 +26,32 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+    res.send('Hello world')
 })
+
+app.get('/shows', (req, res) => {
+    res.json(netflixData)
+})
+
+app.get('/shows/:id', (req, res) => {
+    const showId = req.params.id
+    const show = netflixData.filter((item) => +item.show_id === +showId)
+    res.json(show)
+
+})
+
+app.get('/year/:year', (req, res) => {
+    const year = req.params.year
+    const releaseYear = netflixData.filter((show) => show.release_year === +year)
+    res.json(releaseYear)
+
+})
+
+
+
+
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
+    console.log(`Server running on http://localhost:${port}`)
 })
