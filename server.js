1fbe447368b9719b989052ea7ee9a20ecca79fe9
Frida Eriksson <Frida.Eriksson@silf.se>
2020-04-30 16:04:39 +0200

    added books.json as data for route / to server.js

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import data from './data/books.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -23,8 +15,10 @@ app.use(cors())
 app.use(bodyParser.json())
 
 // Start defining your routes here
+
+// Route for array of all books
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.json(data)
 })
 
 // Start the server
