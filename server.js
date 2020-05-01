c6f13ee39cc90399a5222ca29db44aaf6fdc4fd2
Ruterryter <hanna.ruter@gmail.com>
2020-05-01 11:49:34 +0200

    sort by rating, filter by author

--- server.js
+++ server.js
@@ -2,30 +2,67 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
+
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // 
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
+import booksData from './data/books.json'
+//import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8088
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+//Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Endpoints:  /books, /books?sort=rating, /?author=x for search, /heavyreads, /toprated')
 })
+
+//list all the books in the api
+app.get('/books', (req, res) => {
+  res.json(booksData)
+})
+
+//Show one book using :id as a placeholder for the bookID number
+app.get('/books/:id', (req, res) => {
+  const { id } = req.params
+  const bookId = booksData.find((item) => item.bookID === +id)
+  if (bookId) {
+    res.json(bookId)
+  } else {
+    res.status(404).json({ message: 'Not found' })
+  }
+})
+
+//Sort books after rating highest to lowest
+app.get('/rating', (req, res) => {
+  const bestRated = booksData.sort((a, b) => b.average_rating - a.average_rating);
+  res.json(bestRated)
+})
+
+
+//Query by author
+app.get('/authors/:author', (req, res) => {
+  const author = req.params.author;
+  const booksByAuthor = booksData.filter((item) =>
+    item.authors.toLowerCase().includes(author)
+  );
+  res.json(booksByAuthor);
+});
+
+
+//Show the top 10 long books
+
 
 // Start the server
 app.listen(port, () => {
