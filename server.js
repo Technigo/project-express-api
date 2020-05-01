b7b43085789671e0681784bd6887890c22be4154
Ragna <ragnaulfars@gmail.com>
2020-05-01 16:05:39 +0200

    Working on endpoints

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import netflixData from './data/netflix-titles.json'
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -23,9 +24,61 @@ app.use(cors())
 app.use(bodyParser.json())
 
 // Start defining your routes here
+
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('What a time to be alive!')
 })
+
+app.get('/content', (req, res) => {
+  res.json(netflixData)
+})
+
+app.get('/movies', (req, res) => {
+  const areMovies = netflixData.filter((item) => item.type === "Movie")
+  res.json(areMovies)
+})
+
+app.get('/tv_shows', (req, res) => {
+  const areTVShows = netflixData.filter((item) => item.type === "TV Show")
+  res.json(areTVShows)
+})
+
+app.get('/year/:year', (req, res) => {
+  const year = req.params.year
+  console.log(year)
+  const contentFromYear = netflixData.filter((item) => item.release_year === +year)
+  res.json(contentFromYear)
+})
+
+
+app.get('/title/:title', (req, res) => {
+  const title = req.params.title
+  console.log(title)
+  const showTitle = netflixData.filter((item) => item.show_id === +title)
+  console.log(showTitle)
+  res.json(showTitle)
+})
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello world')
+// })
+
+// app.get('/nominations', (req, res) => {
+//   res.json(goldenGlobesData)
+// })
+
+// app.get('/year/:year', (req, res) => {
+//   const year = req.params.year
+//   const showWon = req.query.win
+//   let nominationsFromYear = goldenGlobesData.filter((item) => item.year_award === +year)
+
+//   if (showWon) {
+//     nominationsFromYear = nominationsFromYear.filter((item) => item.win)
+//   }
+
+//   res.json(nominationsFromYear)
+// })
 
 // Start the server
 app.listen(port, () => {
