b2d913b8236227042b915a0f913e8f648583c47f
Frida Eriksson <Frida.Eriksson@silf.se>
2020-05-03 12:52:07 +0200

    added error for when the list of books is empty, moved index variables, added comments

--- server.js
+++ server.js
@@ -23,28 +23,37 @@ app.get('/', (req, res) => {
   res.send(listEndpoints(app))
 })
 
-// Route for array of all books with pagination as default, possible to use queries to choose page, sort based on rating, find books by author or title 
+// Route for array of all books with pagination as default
 app.get('/books', (req, res) => {
   const { page, sort, author, title } = req.query
   let booksList = booksData
-  const startIndex = 10 * (+page - 1) || 0
-  const endIndex = startIndex + 10
 
+  // sort books by rating using query, returns sorted booksList
   if (sort === 'rating_dsc') {
     booksList = booksList.sort((a, b) => (b.average_rating - a.average_rating))
   } else if (sort === 'rating_asc') {
     booksList = booksList.sort((a, b) => (a.average_rating - b.average_rating))
   }
 
+  // find books by author using query, returns filtered booksList
   if (author) {
     booksList = booksList.filter((item) => item.authors.toString().toLowerCase().includes(author.toLowerCase()))
   }
 
+  // find books based on words in title using query, returns filtered booksList
   if (title) {
     booksList = booksList.filter((item) => item.title.toString().toLowerCase().includes(title.toLowerCase()))
   }
 
+  // slice used for pagination
+  const startIndex = 10 * (+page - 1) || 0
+  const endIndex = startIndex + 10
   const booksListPaginated = booksList.slice(startIndex, endIndex)
+
+  // 404 when there are no books in the list
+  if (booksListPaginated.length === 0) {
+    res.status(404).json({ error: 'No books found, please try a different query' })
+  }
 
   res.json(booksListPaginated)
 })
@@ -54,6 +63,7 @@ app.get('/books/:id', (req, res) => {
   const { id } = req.params
   const book = booksData.find((item) => item.bookID === +id)
 
+  // 404 when there is no book with path id
   if (!book) {
     res.status(404).json({ error: 'There is no book with that id' })
   }
