4abce386b5b2f06671c5553556b4cdd4509a0b4e
crushoftheyear <kajsalovisaholmstrom@gmail.com>
2020-05-04 17:42:42 +0200

    Sorting & title search in /books

--- server.js
+++ server.js
@@ -19,9 +19,21 @@ app.get('/', (req, res) => {
   res.json('Hello world')
 })
 
+
+// All books
 app.get('/books', (req, res) => {
+  const titleSearch = req.query.title
   const numPages = req.query.pages
+  const sort = req.query.sort
   let allBooks = data
+
+  // Filter on title
+  if (titleSearch) {
+    allBooks = allBooks.filter((item) => {
+      const bookTitle = item.title.toString() // Not sure why .toString() is neccessary
+      return bookTitle.toLowerCase().includes(titleSearch.toLowerCase())
+    })
+  }
 
   // Filter on number of pages
   if (numPages) {
@@ -29,10 +41,19 @@ app.get('/books', (req, res) => {
       Math.floor(item.num_pages / 100) * 100 === +numPages) // Round down to nearest 100
   }
 
+  // Sorting
+  if (sort) {
+    if (sort === 'rating') { // Sort on best rating
+      allBooks = allBooks.sort((a, b) => b.average_rating - a.average_rating);
+    } else if (sort === 'pages') { // Sort on number of pages
+      allBooks = allBooks.sort((a, b) => b.num_pages - a.num_pages);
+    }
+  }
+
   res.json(allBooks)
 })
 
-
+// Single book by id
 app.get('/books/:id', (req, res) => {
   const bookId = req.params.id
   const book = data.filter((item) => item.bookID === +bookId)
@@ -46,6 +67,7 @@ app.get('/books/:id', (req, res) => {
   }
 })
 
+// Books by author
 app.get('/authors/:author', (req, res) => {
   const author = req.params.author
   const showRating = req.query.rating
@@ -56,7 +78,7 @@ app.get('/authors/:author', (req, res) => {
   // Filter on rating
   if (showRating) {
     booksByAuthor = booksByAuthor.filter((item) =>
-      item.average_rating.toFixed(1) === showRating
+      item.average_rating.toFixed() === showRating
     )
   }
 
