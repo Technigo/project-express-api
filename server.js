aaa02f0ca1ec658963701dfe0cd8772a2005c588
Johanna Åstrand <johanna@johannas-mbp.mynet>
2020-05-04 20:39:36 +0200

    changed the data to netflix data, set up enpoint for titles, id and release year

--- server.js
+++ server.js
@@ -1,50 +1,65 @@
+
+
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-import goldenGlobesData from './data/golden-globes.json'
+// // If you're using one of our datasets, uncomment the appropriate import below
+// // to get started!
+// // 
+// import goldenGlobesData from './data/golden-globes.json'
 
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+// // import avocadoSalesData from './data/avocado-sales.json'
+// // import booksData from './data/books.json'
+import netflixData from './data/netflix-titles.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+console.log(netflixData.length)
+// // import topMusicData from './data/top-music.json'
+
+// // Defines the port the app will run on. Defaults to 8080, but can be 
+// // overridden when starting the server. For example:
+// //
+// //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
+// // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+// // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Netflix data')
 })
 
-app.get('/nominations', (req, res) => {
-  res.json(goldenGlobesData)
+app.get('/titles', (req, res) => {
+  res.json(netflixData)
+})
+
+app.get('/titles/:id', (req, res) => {
+  const id = req.params.id
+  const title = netflixData.filter((item) => item.show_id === +id)
+  res.json(title)
 })
 
 app.get('/year/:year', (req, res) => {
   const year = req.params.year
-  const showWon = req.query.won
-  let nominationsFromYear = goldenGlobesData.filter((item) => item.year_award === +year)
-
-  if (showWon) {
-    nominationsFromYear = nominationsFromYear.filter((item) => item.win)
-  }
-
-  res.json(nominationsFromYear)
+  const releasesFromYear = netflixData.filter((item) => item.release_year === +year)
+  res.json(releasesFromYear)
 })
 
-// Start the server
+app.get('/type/:movie', (req, res) => {
+  const isMovie = netflixData.filter((item) => item.type === 'Movie')
+  res.json(isMovie)
+})
+
+app.get('/type/:tvshow', (req, res) => {
+  const isTvShow = netflixData.filter((item) => item.type === 'TV Show')
+  res.json(isTvShow)
+})
+
+
+// // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
