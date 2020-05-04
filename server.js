5aa020e0d115561dbc7039f4479c097a132a5861
camilla <camilla.gausland@gmail.com>
2020-05-04 14:59:46 +0200

    server file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // 
-// import goldenGlobesData from './data/golden-globes.json'
+ import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
 // import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
@@ -14,6 +14,9 @@ import cors from 'cors'
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
+
+
+
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
@@ -25,6 +28,23 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/nominations', (req, res) => {
+  res.json(goldenGlobesData)
+})
+
+app.get('/year/:year', (req, res) => {
+  const year = req.params.year
+  const showWon = req.query.won
+  
+  let nominationsFromYear = goldenGlobesData.filter((item ) => item.year_award === +year)
+
+  if (showWon) {
+    nominationsFromYear = nominationsFromYear.filter((item) => item.win)
+  }
+
+  res.json(nominationsFromYear)
 })
 
 // Start the server
