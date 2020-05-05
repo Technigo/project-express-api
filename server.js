4fe27176e715827a79b74ba9ddc5cf5c8b313a7b
annatakman <annatakman@gmail.com>
2020-05-05 15:47:21 +0200

    added more queries

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
 import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+// overridden when starting the server. For example: PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -23,17 +12,54 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+// Home page
 app.get('/', (req, res) => {
-  res.send('This is an API with 1375 Netflix titles. Have fun exploring them!')
+  res.send('This is an API with 1375 Netflix titles. Have fun exploring it!')
 })
 
-// Gets all objects in the API
+// Gets all titles in the API
 app.get('/titles', (req, res) => {
-  res.json(netflixData)
+
+  // Queries for type, country and director:
+  let titles = netflixData
+  const showType = req.query.type
+  const showCountry = req.query.country
+  const showDirector = req.query.director
+
+  if (showType) {
+    titles = titles.filter((item) => item.type.toLowerCase() === showType.toLowerCase())
+  } if (showCountry) {
+    titles = titles.filter((item) => item.country.toLowerCase().includes(showCountry.toLocaleLowerCase()))
+  } if (showDirector) {
+    titles = titles.filter((item) => item.director.toLowerCase().includes(showDirector.toLowerCase()))
+  }
+  res.json(titles)
 })
 
-// Gets an object with a specific id
+// Gets everything a specific director has directed
+app.get("/directors/:director", (req, res) => {
+  const director = req.params.director
+  const titlesWithDirector = netflixData.filter((item) =>
+    item.director.toLowerCase().includes(director.toLowerCase())
+  );
+  res.json(titlesWithDirector);
+});
+
+// Gets titles released a specific year
+app.get('/releaseyear/:year', (req, res) => {
+  const year = req.params.year
+
+  // Query for type (TV show or movie):
+  const showType = req.query.type
+  let titlesFromYear = netflixData.filter((item) => item.release_year === + year)
+
+  if (showType) {
+    titlesFromYear = titlesFromYear.filter((item) => item.type.toLowerCase() === showType.toLowerCase())
+  }
+  res.json(titlesFromYear)
+})
+
+// Gets the title with a specific id
 app.get('/titles/:id', (req, res) => {
   const id = req.params.id
   const titleId = netflixData.find((item) => item.show_id === +id)
@@ -42,20 +68,6 @@ app.get('/titles/:id', (req, res) => {
   } else {
     res.status(404).send('Sorry, no such title was found.')
   }
-})
-
-// Gets objects released specific year
-app.get('/releaseyear/:year', (req, res) => {
-  const year = req.params.year
-
-  // Query for type:
-  const showType = req.query.type
-  let titlesFromYear = netflixData.filter((item) => item.release_year === + year)
-
-  if (showType) {
-    titlesFromYear = titlesFromYear.filter((item) => item.type.toLowerCase() === showType.toLowerCase())
-  }
-  res.json(titlesFromYear)
 })
 
 // Start the server
