db174858d4bf31b829e7842bec51595d8a7d9ece
William Jensen <william.riis93@gmail.com>
2020-05-05 13:19:47 +0200

    tweaked search function

--- server.js
+++ server.js
@@ -64,10 +64,10 @@ app.get('/books', (req, res) => {
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
-  // const firstResult = booksData.filter((book) => book.authors.toLowerCase().replace(' ', '_').includes(keyword))
-  // const secondResult = booksData.filter((book) => book.title.toLowerCase().replace(' ', '_').includes(keyword))
-  // const finalResult = firstResult.concat(secondResult)
-  res.json({ keyword: keyword })
+  const firstResult = booksData.filter((book) => book.authors.toLowerCase().replace(' ', '_').includes(keyword))
+  const secondResult = booksData.filter((book) => book.title.toLowerCase().replace(' ', '_').includes(keyword))
+  const finalResult = firstResult.concat(secondResult)
+  res.json(finalResult)
 })
 
 app.get('/books/:id', (req, res) => {
