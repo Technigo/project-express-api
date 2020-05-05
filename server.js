4dc4d2b4349ef00c2744ab979310ae23c06394dd
Jihyun An <jihyunan42@gmail.com>
2020-05-05 09:55:06 +0200

    added two endpoints and query for won

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import goldenGlobesData from './data/golden-globes.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+console.log(goldenGlobesData.length)
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -26,7 +15,25 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+app.get('/nominations', (req, res) => {
+  res.json(goldenGlobesData)
+})
+app.get('/year/:year', (req, res) => {
+  const year = req.params.year
+  const showWon = req.query.won
+  console.log({ year })
+  let nominationsFromYear = goldenGlobesData.filter((item) => item.year_award === +year)
+  if (showWon) {
+    nominationsFromYear = nominationsFromYear.filter((item) => item.win)
+  }
+  res.json(nominationsFromYear)
+})
 
+app.get('/nominations:index', (req, res) => {
+  const { index } = req.params
+  console.log(index)
+  res.send(goldenGlobesData[index])
+})
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
