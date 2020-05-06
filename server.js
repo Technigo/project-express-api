1d95d0abf2fa76ce9eb0ba4357db728701b6e3ab
annatakman <annatakman@gmail.com>
2020-05-06 12:32:59 +0200

    added pages at year endpoint

--- server.js
+++ server.js
@@ -13,10 +13,16 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+// const listEndpoints = require('express-list-endpoints')
+
 // Home page
 app.get('/', (req, res) => {
   res.send('This is an API with 1375 Netflix titles. Have fun exploring it! Possible routes: /titles, /titles/:id, /directors/:director, /releaseyear/:year')
 })
+
+// app.get('/', (req, res) => {
+//   res.send(listEndpoints(app))
+// })
 
 // Endpoint, returns the title with a specific id
 app.get('/titles/:id', (req, res) => {
@@ -49,11 +55,8 @@ app.get('/titles', (req, res) => {
     pageSearch = pageCount
   }
 
-  // Queries for type and country:
-  const showType = req.query.type
-  const showCountry = req.query.country
-
   // Query for type: filter and return only TV shows or only movies 
+  const showType = req.query.type
   if (showType) {
     titles = titles.filter((item) => item.type.toLowerCase() === showType.toLowerCase())
     const pageCount = titles.length / 10
@@ -70,6 +73,7 @@ app.get('/titles', (req, res) => {
   }
 
   // Query for country: filter and return only titles from the specified country
+  const showCountry = req.query.country
   if (showCountry) {
     titles = titles.filter((item) => item.country.toLowerCase().includes(showCountry.toLocaleLowerCase()))
     const pageCount = titles.length / 10
@@ -85,27 +89,57 @@ app.get('/titles', (req, res) => {
     }
   }
 
+  // Query for director: filter and return only titles from the specified director
+  const showDirector = req.query.director
+  if (showDirector) {
+    titles = titles.filter((item) => item.director.toLowerCase().includes(showDirector.toLocaleLowerCase()))
+    const pageCount = titles.length / 10
+    if (!pageSearch) {
+      pageSearch = 1
+    }
+    else if (pageSearch > pageCount) {
+      pageSearch = pageCount
+    } if (titles.length === 0) {
+      res.status(404).send(`Couldn't find any titles from ${showDirector}`)
+    } else {
+      res.json(titles.slice(pageSearch * 10 - 10, pageSearch * 10))
+    }
+  }
+
   //Slice the data, begin on the page written in query
   res.json(titles.slice(pageSearch * 10 - 10, pageSearch * 10))
 })
 
-// Endpoint, returns titles released a specific year (no pages)
+// Endpoint, returns titles released a specific year
 app.get('/year/:year', (req, res) => {
   const year = req.params.year
-  const titlesFromYear = netflixData.filter((item) =>
-    item.release_year === + year)
-  res.json(titlesFromYear)
+  let pageSearch = req.query.page
+  const titlesFromYear = netflixData.filter((item) => item.release_year === + year)
+  const pageCount = titlesFromYear.length / 10
+
+  if (!pageSearch) {
+    pageSearch = 1
+  }
+  else if (pageSearch > pageCount) {
+    pageSearch = pageCount
+  }
+  if (titlesFromYear.length === 0) {
+    res.status(404).send(`Couldn't find any boardgame from year ${year}`)
+  } else {
+    res.json(titlesFromYear.slice(pageSearch * 10 - 10, pageSearch * 10))
+  }
 })
 
 
-// Endpoint, returns everything a specific director has directed (no pages)
-app.get("/directors/:director", (req, res) => {
-  const director = req.params.director
-  const titlesWithDirector = netflixData.filter((item) =>
-    item.director.toLowerCase().includes(director.toLowerCase())
-  )
-  res.json(titlesWithDirector)
-})
+// // Endpoint, returns everything a specific director has directed (no pages)
+// //Commented this out as I decided to make a query for director instead
+// app.get("/directors/:director", (req, res) => {
+//   const director = req.params.director
+//   const titlesWithDirector = netflixData.filter((item) =>
+//     item.director.toLowerCase().includes(director.toLowerCase())
+//   )
+//   res.json(titlesWithDirector)
+// })
 
 
 // Start the server
