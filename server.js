16f695b9d4a6961edfdda4cf053091de0f926652
Mathias Thorström <thisen@gmail.com>
2020-05-06 15:12:31 +0200

    added new routs and querys

--- server.js
+++ server.js
@@ -27,26 +27,64 @@ app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-// Add new GET route to get all books
+// Get all books
 app.get('/books', (req, res) => {
   res.json(booksData)
 })
 
-// Filter by ID
-// How to handle if the ID doesnt exist?
-app.get('/books/:id', (req, res) => {
+// Filter books by minPages or maxPages
+// Query: ?minPages=NUM or ?maxPage=NUM
+app.get('/books/pages', (req, res) => {
+  const minPages = req.query.minPages 
+  const maxPages = req.query.maxPages 
+  const booksByMinPages = booksData.filter((item) => item.num_pages >= minPages)
+  const booksByMaxPages = booksData.filter((item) => item.num_pages <= maxPages)
+
+  if(minPages) {
+    res.json(booksByMinPages)
+  } 
+  if(maxPages) {
+    res.json(booksByMaxPages)
+  }
+  
+  res.json(booksData)
+})
+
+// Filter books by ID
+app.get('/books/id/:id', (req, res) => {
   const id = req.params.id
   let booksById = booksData.filter((item) => item.bookID === +id)
-  res.json(booksById)
+
+  if( booksById.length > 0 ) {
+    res.json(booksById)
+  } else {
+    res.status(404).json({ message: `Error, no book with id: ${id} found!` })
+  }
 })
 
-// Show average rating over x
-app.get('/rating/:rank', (req, res) => {
-  const rank = req.params.rank
-  let booksByRating = booksData.filter((item) => item.average_rating >= rank)
-  res.json(booksByRating)
+// Filter books by ISBN
+app.get('/books/isbn/:isbn', (req, res) => {
+  const isbn = req.params.isbn
+  let booksByIsbn = booksData.filter((item) => item.isbn === +isbn)
+
+  if( booksByIsbn.length > 0 ){
+    res.json(booksByIsbn)
+  } else {
+    res.status(404).json({ message: `Error, no book with isbn: ${isbn} found!` })
+  }
 })
 
+// Show all books with average rating over x
+app.get('/rating/:minRating', (req, res) => {
+  const minRating = req.params.minRating
+  let booksByRating = booksData.filter((item) => item.average_rating >= minRating)
+
+  if(booksByRating.length > 0) {
+    res.json(booksByRating)
+  } else {
+    res.status(404).json({ message: `Error, no books found with min rating of ${minRating}` })
+  }
+})
 
 // Start the server
 app.listen(port, () => {
