bc1e2f5f436db8642f4e7e98c44fa1a895ec006c
johannaraden <51335476+johannaraden@users.noreply.github.com>
2020-05-06 14:53:32 +0200

    added multiple routes, added filter method, slice method to create pages

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+// Using my own data set that shows the dogs of Zurich, Switzerland
+
+import dogs from './data/dogs-in-zurich.json'
+
+// Explaning the different values
+// ALTER -> Age
+// GESCHLECHT -> Gender
+// STADTKREIS -> City Quarter or District
+// RASSE1 -> Dog's Primary Breed
+// RASSE2 -> Dog's Secondary Breed
+// GEBURTSJAHR_HUND -> Dog's Year of Birth
+// GESCHLECHT_HUND -> Dog's Gender
+// HUNDEFARBE -> Dog's Color
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -22,10 +27,48 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+// My routes
+app.get('/about', (req, res) => {
+  res.send("Hello! this project is about the dogs living in Zurich, Switzerland. Explaning the different values. ALTER -> Age. GESCHLECHT -> Gender. STADTKREIS -> City Quarter or District. RASSE1 -> Dog's Primary Breed. RASSE2 -> Dog's Secondary Breed. GEBURTSJAHR_HUND -> Dog's Year of Birth. GESCHLECHT_HUND -> Dog's Gender. HUNDEFARBE -> Dog's Color")
 })
+
+//Route in plural that shows the whole object with all dogs. Usage of slice-method
+app.get('/dogs', (req, res) => {
+  const page = req.query.page ?? 0;
+  const pageSize = 20;
+  const startIndex = page * pageSize;
+  const endIndex = startIndex + pageSize;
+  const dogsPerPage = dogs.slice(startIndex,endIndex)
+  const returnObject = {
+    pageSize: pageSize,
+    page: page,
+    numDogs: dogs.length,
+    result: dogsPerPage,
+    }
+  res.send(returnObject)
+})
+
+//Routes that shows dogs from a certain year of birth or a certain dog race. Usage of filter method. 
+
+app.get('/year/:year', (req, res) => {
+  const year = req.params.year
+  const birthYear = dogs.filter((item) => item.GEBURTSJAHR_HUND === +year)
+  res.json(birthYear)
+})
+
+app.get('/race/:race', (req, res) => {
+  const race = req.params.race
+  // const specRace = req.query.specRace
+  const dogRace = dogs.filter((item) => item.RASSE1 === race)
+
+  // if statement to choose dogs from certain birth year from this race.
+  // if() {
+
+  // }
+  res.json(dogRace)
+})
+
+//find-method
 
 // Start the server
 app.listen(port, () => {
