786c176cf66e44e8c30a29e11b08d5a63f3daa13
L-Lisa <lisaojeland@gmail.com>
2020-05-07 22:57:56 +0200

    basic requirements

--- server.js
+++ server.js
@@ -1,33 +1,48 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import avocadoSalesData from './data/avocado-sales.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
+
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Happy world')
 })
 
-// Start the server
+app.get("/avocados", (req, res) => {
+  const page = req.query.page ?? 0
+  const pageSize = req.query.pageSize ?? 20
+  const startIndex = page * pageSize
+  const endIndex = startIndex + +pageSize
+
+  const salesForPage = avocadoSalesData.slice(startIndex, endIndex)
+
+  const returnObject = {
+    pageSize: pageSize,
+    page: page,
+    maxPages: parseInt(avocadoSalesData.length / pageSize),
+    results: salesForPage,
+  }
+
+  res.send(returnObject)
+})
+
+
+app.get("/avocados/:id", (req, res) => {
+  const { id } = req.params
+  const findById = avocadoSalesData.find(
+    (avocado) => avocado.id == id)
+
+  res.send(findById)
+})
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
