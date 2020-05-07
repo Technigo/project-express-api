f869479a695faf050fd0a90544ed5496c3595003
ebbabw <ebbabergholtz@gmail.com>
2020-05-07 15:01:29 +0200

    Got Routes and error message to work

--- server.js
+++ server.js
@@ -2,14 +2,9 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
-// import the api
+// Imported API
 import netflixData from './data/netflix-titles.json'
 
-
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -17,36 +12,48 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+// Routes 
+
+//(Start page)
 app.get('/', (req, res) => {
-  res.send('View these endpoints /netflixtitles /year/:year')
+  res.send('View these endpoints /netflixtitles /year/:year/&=(movie or tv show)')
 })
 
+//(All netflixtitles)
 app.get('/netflixtitles', (req, res) => {
   res.json(netflixData)
 })
 
-// `/songs` - Returns an array of songs
-
-// `/songs/5` - Returns a single song whose ID is '5'
-
-
-
+//(filter on year)
 app.get('/year/:year', (req, res) => {
 
   const year = req.params.year 
   const showType = req.query.type
   let netflixReleaseYear = netflixData.filter((item) => +item.release_year === +year)
 
-  if (year > 2020) {
+   if (year && netflixReleaseYear.length === 0) {
     res.status(404).send('No netflix releses for this year but stay tuned');
   }
-   
+
+  //(filter on movie and tv shows relesed on diffrent years)
   if (showType) {
-    netflixReleaseYear = netflixReleaseYear.filter((item) => item.type.toLowerCase() === showType.toLowerCase());
-  } else if (showType != null) {
-    res.status(404).send(`No ${showType} relesed year`);
-  }
+    netflixReleaseYear = netflixReleaseYear.filter((item) =>
+      item.type.toLowerCase() === showType.toLowerCase());
+}
+
+if (showType && netflixReleaseYear.length === 0) {
+    res.status(404).send(`No ${showType} relesed this year`);
+}
+
+  // if (year > 2020) {
+  //   res.status(404).send('No netflix releses for this year but stay tuned');
+  // }
+   
+  // if (showType) {
+  //   netflixReleaseYear = netflixReleaseYear.filter((item) => item.type.toLowerCase() === showType.toLowerCase());
+  // } else if (showType === null) {
+  //   res.status(404).send(`No ${showType} relesed this year`);
+  // }
 
   
   res.json(netflixReleaseYear)
