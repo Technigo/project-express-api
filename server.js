418077881c77c64190f500403316515799ab0d53
jenniferot <56264114+Jenniferot@users.noreply.github.com>
2020-05-07 22:46:52 +0200

    added query

--- server.js
+++ server.js
@@ -3,18 +3,9 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import data from './data.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
-//
+
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
@@ -25,26 +16,58 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello Hello world')
+  res.send('Endpoints = /shows & /titles/title & year/year & cast/cast & genre/listed_in & country/country&type=Movie or TV Show')
 })
 
-app.get('/nominations', (req, res) => {
+app.get('/shows', (req, res) => {
   res.json(data)
+})
+
+app.get('/titles/:title', (req, res) => {
+  const title = req.params.title
+
+  let showTitle = data.filter((item) => item.title.toString().toLowerCase() === title.toLowerCase())
+  if (showTitle.length > 0) {
+    res.json(showTitle)
+  } else {
+    res.status(404).json({ message: `${title} not found` })
+  }
 })
 
 app.get('/year/:year', (req, res) => {
   const year = req.params.year
-  const showWon = req.query.won
-  let nominationsFromYear = data.filter((item) => item.year_award === +year)
+  const showsFromYear = data.filter((item) => item.release_year === +year)
+  res.json(showsFromYear)
+})
 
-  if (showWon) {
-    nominationsFromYear = nominationsFromYear.filter((item) => item.win)
+app.get('/cast/:cast', (req, res) => {
+  const cast = req.params.cast
+  const showsCast = data.filter((item) => item.cast.toLowerCase().includes(cast.toLowerCase()))
+  res.json(showsCast)
+})
+
+app.get('/genre/:listed_in', (req, res) => {
+  const genre = req.params.listed_in
+  const showsGenre = data.filter((item) => item.listed_in.toLowerCase().includes(genre.toLowerCase()))
+  res.json(showsGenre)
+})
+
+app.get('/country/:country', (req, res) => {
+  const country = req.params.country
+  const showType = req.query.type
+
+  let netflixShowsFromCountry = data.filter((item) => item.country.toLowerCase() === country.toLowerCase())
+
+  if (showType) {
+    netflixShowsFromCountry = netflixShowsFromCountry.filter((item) => item.type.toLowerCase().replace(' ', '_') === showType.toLowerCase().replace(' ', '_'))
   }
-
-  res.json(nominationsFromYear)
+  res.json(netflixShowsFromCountry)
 })
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
+
+
+
