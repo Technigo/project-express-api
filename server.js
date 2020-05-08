1d9a16cc52d0f2bc645aac5fe89c8fdb219409fb
Louise Eriksson <louise@Louises-MacBook-Air.local>
2020-05-08 09:20:00 +0200

    added /pages, /ceremonies, /category

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
 import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -24,12 +12,53 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hi! For all the data: http://localhost:8080/nominations, filter by year of the nomination: http://localhost:8080/year/2010, only the winner for a specific year: http://localhost:8080/year/2013?win=true, filter by ceremony: http://localhost:8080/nominations/ceremony/67')
 })
+
+
+// not sure if this page-query works as it should
+
+app.get('/page', (req, res) => {
+  const PER_PAGE = 20
+  const page = req.query.page
+  const startIndex = PER_PAGE * +page
+  const data = goldenGlobesData.slice(startIndex, startIndex + PER_PAGE)
+  const pageCount = Math.ceil(goldenGlobesData.length / 20)
+
+  if (page >= pageCount) {
+    res.status(404).send(`The page ${page} is not found, the last page is ${pageCount}.`);
+  } else {
+    res.json({
+      totalPages: Math.floor(goldenGlobesData.length / PER_PAGE),
+      currentPage: +page,
+      data
+    })
+  }
+})
+
+// All the data
+// http://localhost:8080/nominations
 
 app.get('/nominations', (req, res) => {
   res.json(goldenGlobesData)
 })
+
+// // FILTER ON ID or similar
+// //http://localhost:8080/books/1
+// app.get('/books/:id', (req, res) => {
+//   const bookId = req.params.id
+//   const book = booksData.find((item) => item.bookID === +bookId)
+
+//   if (book) {
+//     res.json(book);
+//   } else {
+//     res.status(404).send('Not found')
+//   }
+// })
+
+
+// The winners per selected year 
+// http://localhost:8080/year/2010
 
 app.get('/year/:year', (req, res) => {
   const year = req.params.year
@@ -43,7 +72,57 @@ app.get('/year/:year', (req, res) => {
   res.json(nominationsFromYear)
 })
 
-// Start the server
+
+// filer on ceremony
+// http://localhost:8080/nominations/ceremony/67
+
+app.get('/ceremony/', (req, res) => {
+  const searchCeremony = req.query.ceremony
+  let filteredNominations = ceremonyData
+
+  if (searchCeremony) {
+    filteredNominations = filteredNominations.filter(item => {
+      const ceremony = item.authors.toString()
+      return ceremony.includes(searchCeremony)
+    })
+  }
+
+  if (filteredNominations.length === 0) {
+    res.status(404).send('Ceremony not found, try again!')
+  } else {
+    res.json(filteredNominations)
+  }
+})
+
+
+// http://localhost:8080/category/Best Television Series - Musical or Comedy
+
+app.get('/category/:category', (req, res) => {
+  const category = req.params.category
+  const nominationCategory = goldenGlobesData.filter((item) => item.category === category)
+
+  if (nominationCategory.length === 0) {
+    res.status(404).send(`Not found. Your entry: ${category} is not valid.`)
+  } else {
+    res.json(nominationCategory)
+  }
+})
+
+// sort on title including f.ex. /Harry and then show all the harry potter titles etc
+
+// app.get('/title', (req, res) => {
+//   const title = req.query.title
+//   const titleIncludes = goldenGlobesData.filter((item) => item.title.includes(title))
+
+//   res.json(titleIncludes)
+// })
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
+
+
+
+
+
+
