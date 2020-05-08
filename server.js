26bf30b5ab7fdb61407a9163c850e7255fc08598
aasa72 <asa.andersson10@comhem.se>
2020-05-08 23:12:40 +0200

    initial project

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import netflixData from './data/netflix-titles.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -25,6 +17,39 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+//show all data on all shows
+app.get('/shows', (req, res) => {
+  res.json(netflixData)
+})
+//show list of all titles
+app.get('/titles', (req, res) => {
+  const listTitles = netflixData.map((titles) => titles.title)
+  res.json(listTitles)
+})
+
+//show all data on one specific title
+app.get('/titles/:title', (req, res) => {
+  const title = req.params.title
+  console.log({ title })
+  const showName = netflixData.filter((name) => name.title === title)
+  res.json(showName)
+})
+
+//filter on release year
+app.get('/year/:year', (req, res) => {
+  const year = req.params.year
+  console.log({ year })
+  const releaseYear = netflixData.filter((item) => item.release_year === +year )
+  res.json(releaseYear)
+})
+
+//filter on country
+app.get('/countries/:country', (req, res) => {
+  const country =req.params.country
+  const countryOfOrigin = netflixData.filter ((place) => place.country === country)
+  res.json(countryOfOrigin)
 })
 
 // Start the server
