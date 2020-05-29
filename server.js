f1a6e28ec0e6fb67b0a8f2608f27bffe63a5b228
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-05-29 15:42:49 +0200

    added error handling

--- server.js
+++ server.js
@@ -14,52 +14,86 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Routes
+// Homepage
 app.get('/', (req, res) => {
-  res.send('Hello')
+  res.send('Netflix titles')
 })
 
-app.get('/titles', (req, res) => {
-  const titles = jsonData
-  res.send({ numTitles: titles.length, titles })
-})
-
+// One title
 app.get('/titles/:id', (req, res) => {
   const id = req.params.id
   const title = jsonData.find(item => item.show_id === +id)
-  res.send(title)
+  if (title === undefined) {
+    console.log('empty')
+    res.status(400).send(`There's no matching title`)
+  } else {
+    res.send(title)
+  }
 })
 
-// Pagination
-app.get('/titles/page', (req, res) => {
-  const page = req.query.page || 1
-  const limit = req.query.limit || 10
-
-  const startIndex = (page - 1) * limit
-  const endIndex = page * limit
-
-  const results = {
-    maxPages: parseInt(jsonData.length / limit)
-  }
-
-  if (endIndex < jsonData.length) {
-    results.next = {
-      page: +page + 1,
-      limit: limit
-    }
-  }
-
-  if (startIndex > 0) {
-    results.previous = {
-      page: +page - 1,
-      limit: limit
-    }
-  }
-
-  results.titles = jsonData.slice(startIndex, endIndex)
-
-  res.send({ numTitles: results.titles.length, results })
+// All titles
+app.get('/titles', paginatedResult(jsonData), (req, res) => {
+  res.send(res.paginatedResult)
 })
+
+// Filtering movies with params
+app.get('/movies', (req, res) => {
+  const movies = jsonData.filter(item => item.type === "Movie")
+  res.send(movies)
+})
+
+app.get('/movies/filter', (req, res) => {
+  const year = req.query.year
+  const movies = jsonData.filter(item => item.release_year === +year)
+  res.send(movies)
+})
+
+// Filtering tv shows with params
+app.get('/tv-shows', (req, res) => {
+  const shows = jsonData.filter(item => item.type === "TV Show")
+  res.send(shows)
+})
+
+app.get('/tv-shows/filter', (req, res) => {
+  const year = req.query.year
+  const shows = jsonData.filter(item => item.release_year === +year)
+  res.send(shows)
+})
+
+// Function for pagination
+function paginatedResult(model) {
+  return (req, res, next) => {
+    const page = req.query.page || 1
+    const limit = req.query.limit || 10
+
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+
+    const results = {
+      maxPages: parseInt(model.length / limit)
+
+    }
+
+    if (endIndex < model.length) {
+      results.next = {
+        page: +page + 1,
+        limit: limit
+      }
+    }
+
+    if (startIndex > 0) {
+      results.previous = {
+        page: +page - 1,
+        limit: limit
+      }
+    }
+
+    results.titles = model.slice(startIndex, endIndex)
+
+    res.paginatedResult = results
+    next()
+  }
+}
 
 // Start the server
 app.listen(port, () => {
