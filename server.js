67d29f224de9736488f43d933bb60705bd44e256
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-12-06 19:48:52 +0100

    Renamed paths, add one extra query filter

--- server.js
+++ server.js
@@ -17,14 +17,25 @@ app.get('/', (req, res) => {
   res.send("Hello world, this is Vanessa's books API!")
 });
 
-//Books: path /books - narrow down the data to the top 50 books from the 
+// Books: path /books - narrow down the data to the first 50 books from the 
 // booksData object
 app.get('/books', (req, res) => {
-  res.json(booksData.slice(0, 50))
+  let fiftyBooks = booksData.slice(0, 50);
+
+  // Set a filter via query parameter - path example: localhost:8080/books?reviewersChoice=true
+  // This query will show only books with more than 10,000 text reviews
+  const reviewersChoice = req.query.reviewersChoice;
+
+  if (reviewersChoice) {
+    fiftyBooks = fiftyBooks.filter((item) => item.text_reviews_count > 10000);
+  };
+
+  res.json(fiftyBooks);
 });
 
-// Show a single book based on the ID: example path /books/7
-app.get('/books/:bookID', (req, res) => {
+// Show a single book based on the ID: example path /books/book/7
+// Example path: localhost:8080/books/book/347
+app.get('/books/book/:bookID', (req, res) => {
   const bookID = req.params.bookID;
   const singleBook = booksData.find((item => item.bookID === +bookID));
 
@@ -35,12 +46,12 @@ app.get('/books/:bookID', (req, res) => {
   res.json(singleBook);
 });
 
-// Show 20 Books with rating higher than 4: path /top-rated
-app.get('/top-rated', (req, res) => {
+// Show 20 Books with rating higher than 4: path /books/top-rated
+app.get('/books/top-rated', (req, res) => {
   const topRatedBooks = booksData.filter((item) => item.average_rating >= 4);
   let firstTwentyTopBooks = topRatedBooks.slice(0, 20);
 
-  // Set a filter via query parameter - path example: localhost:8080/top-rated?quickRead=true
+  // Set a filter via query parameter - path example: localhost:8080/books/top-rated?quickRead=true
   // From the Top Rated Books, which ones are a quick read? Meaning they have less than 600 pages
   // If there is a quickRead query in the URL, respond with only the books with less than 600
   // pages from the firstTwentyTopBooks array
