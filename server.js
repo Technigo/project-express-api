bd87da0bd2b4305a37d8a6beb6f1c0569f1c1c20
Emelie Svensson <emeliesv@live.se>
2020-12-06 21:28:46 +0100

    Created first routes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import cors from 'cors'
 // 
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
+import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
@@ -25,6 +25,48 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+//This will send a get-request to /books. 
+//It will use a callbackfunction that will
+//pass two arguments: the request and the response.
+//in the request we could ask for what headers the browser send
+//or if it's a postrequest we could get the data of the post-request body.
+//Then we have the response object.
+//This is ued to start building up the response to send back to the browser.
+//We can use the response to add stuff to it (?!).
+
+app.get('/books', (req, res) => {
+  res.json(booksData)
+})
+
+//This route will return books with a specifik written language
+app.get('/lang/:lang', (req, res) => {
+  const lang = req.params.lang
+  const writtenLang = booksData.filter((item) => item.language_code === lang)
+  res.json(writtenLang)
+})
+
+//This route will return all books not written in the specified language
+app.get('/otherlang/:notlang', (req, res) => {
+  const notlang = req.params.notlang
+  const otherlang = booksData.filter((item) => item.language_code != notlang)
+  res.json(otherlang)
+})
+
+//This returns single result of booksearch per ID.
+app.get('/books/:book', (req, res) => {
+  const book = req.params.book
+  const result = booksData.filter((item) => item.bookID == book)
+  res.json(result)
+})
+
+//This route will return a single book depening on search for title
+//Currently using the bookID, but should use title in frontend-search
+app.get('/books/:title', (req, res) => {
+  const title = req.params.title
+  const result = booksData.filter((item) => item.bookID == title)
+  res.json(result)
 })
 
 // Start the server
