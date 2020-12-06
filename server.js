0fc00761563aa4489158a43769b786adbaf39aab
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-12-06 19:11:49 +0100

    Created Top Rated endpoint with quick read query filter

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/books', (req, res) => {
   res.json(booksData.slice(0, 50))
 });
 
-// Show a single book based on the ID: path /books/7
+// Show a single book based on the ID: example path /books/7
 app.get('/books/:bookID', (req, res) => {
   const bookID = req.params.bookID;
   const singleBook = booksData.find((item => item.bookID === +bookID));
@@ -35,6 +35,23 @@ app.get('/books/:bookID', (req, res) => {
   res.json(singleBook);
 });
 
+// Show 20 Books with rating higher than 4: path /top-rated
+app.get('/top-rated', (req, res) => {
+  const topRatedBooks = booksData.filter((item) => item.average_rating >= 4);
+  let firstTwentyTopBooks = topRatedBooks.slice(0, 20);
+
+  // Set a filter via query parameter - path example: localhost:8080/top-rated?quickRead=true
+  // From the Top Rated Books, which ones are a quick read? Meaning they have less than 600 pages
+  // If there is a quickRead query in the URL, respond with only the books with less than 600
+  // pages from the firstTwentyTopBooks array
+  const quickRead = req.query.quickRead;
+
+  if (quickRead) {
+    firstTwentyTopBooks = firstTwentyTopBooks.filter((item) => item.num_pages <= 600);
+  };
+
+  res.json(firstTwentyTopBooks);
+});
 
 // Start the server
 app.listen(port, () => {
