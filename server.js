cba0234b24d35083727e0f03cb0be0e48ed5f771
Therese Lindén <theresemlinden@gmail.com>
2020-12-06 21:21:33 +0100

    added bookID endpoint,  added pagination to display 20 books per page

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ import cors from 'cors'
 // 
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
+import booksData from './data/books.json'
+console.log(`number of books: ${booksData.length}`)
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
@@ -26,6 +27,61 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+//to get all books presented in json
+//Pagination 
+  // 1) How many things are we going to return per page?  
+  // 2) How do we know which page to return? 
+  // to main things we need to know to create of subset of the data we are returning.
+app.get('/books', (req, res) => {
+  const { page } = req.query  
+  console.log(`page: ${page}`)
+  let booksList = booksData;
+  console.log(`books = ${booksList}`)
+  const pageSize = 20   //calculate the start index will be 0 (0 * 20)
+  const startIndex = page * pageSize //calculate and bound the end index will be 20 (0 + 20)
+  const endIndex = startIndex + pageSize
+  const booksListForPage = booksList.slice(startIndex, endIndex)
+  const returnObject = { books: booksListForPage.length, booksListForPage }
+
+  if (booksListForPage.length === 0) {
+    res
+    .status(404)
+    .send({ error: 'Sorry, no books where found, please try a different query' })
+  }
+
+  //to see this on localhost type: http://localhost:8080/books/?page=3 
+  //change page=3 to any number depending on page you want to view
+  res.json(returnObject)
+
+  //STARTED OFF WITH THIS
+  // let booksList = booksData
+  // res.json(booksList)
+ 
+   // if (average_rate) {
+  //   bookListRatings = bookListRatings.filter((item) =>
+  //     item.title.toLowerCase().includes(title.toLowerCase())
+  //   );
+ 
+})
+
+  
+
+/*** to get one book presented in json based on book id ***/
+// Displays book with specific ID
+app.get("/books/:id", (req, res) => {
+  const id = req.params.id;
+  const bookWithId = booksData.find((item) => item.bookID === +id);
+
+  if (!bookWithId) {
+    res
+      .status(404)
+      .send({ error: `No book with id: "${id}" found` });
+  }
+
+  res.json(bookWithId);
+});
+
 
 // Start the server
 app.listen(port, () => {
