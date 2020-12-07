445ecbbd298bf8cf17cea1ebad9415d75f07c7b4
Karin Nordkvist <kontakt@karinnordkvist.se>
2020-12-07 23:17:53 +0100

    Tried to implement second category

--- server.js
+++ server.js
@@ -4,47 +4,59 @@ import cors from 'cors';
 
 import tedData from './data/ted.json';
 
-// Defines the port the app will run on. Defaults to 8080, but can be
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8088;
+const port = process.env.PORT || 8080;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(bodyParser.json());
 
-// Start defining your routes here
+// ----------------------------------------------------------------
+
+// All tasks
 app.get('/', (req, res) => {
   res.json(tedData);
 });
 
-app.get('/talks/:url', (req, res) => {
-  const paramString = req.params.url;
-  const url = `https://www.ted.com/talks/${paramString}`;
+// To get single talk
+app.get('/talks/:slug', (req, res) => {
+  const slug = req.params.slug;
+  const url = `https://www.ted.com/talks/${slug}`;
   const filteredByUrl = tedData.filter((talk) => talk.url === url);
   res.json(filteredByUrl);
 });
 
-// app.get('/:year', (req, res) => {
-//   const year = req.params.year;
-//   const filteredByYearOfEvent = tedData.filter(
-//     (talk) => talk.event === `TED${year}`
-//   );
-//   res.json(filteredByYearOfEvent);
-// });
+// Sort by yearly event
+app.get('/events/:year', (req, res) => {
+  const year = req.params.year;
+  const filteredByYearOfEvent = tedData.filter(
+    (talk) => talk.event === `TED${year}`
+  );
+  res.json(filteredByYearOfEvent);
+});
 
+// Sort by category
 app.get('/:category', (req, res) => {
   const category = req.params.category;
   const filteredByCategory = tedData.filter((talk) =>
     talk.tags.includes(category)
   );
-  res.json(filteredByCategory);
+
+  // To add a second category
+  const secondCategory = req.query.category;
+  console.log(secondCategory);
+  const addedCategory = filteredByCategory.filter((talk) =>
+    talk.tags.includes(secondCategory)
+  );
+
+  if (secondCategory !== undefined) {
+    res.json(filteredByCategory);
+  } else {
+    res.json(addedCategory);
+  }
 });
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
-  console.log('Hej Karin! igen');
 });
