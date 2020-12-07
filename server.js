2342707355212b4da5dfab1cca9c95064ac8c3ea
emma-tech <emma.urman@gmail.com>
2020-12-07 17:22:58 +0100

    Added routes for all books, id, author and rating.

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import booksData from './data/books.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -25,6 +18,32 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+//Route for all books
+app.get('/books', (req, res) => {
+  res.json(booksData)
+})
+
+//Route for finding a book by its id
+app.get('/books/:id', (req, res) => {
+  const id = req.params.id
+  const bookByID = booksData.find(item => item.bookID === +id);
+  res.json(bookByID)
+})
+
+//Route for finding books by author
+app.get('/authors/:author', (req, res) => {
+  const author = req.params.author
+  const booksByAuthor = booksData.filter(item => item.authors === author);
+  res.json(booksByAuthor);
+})
+
+//Route for finding books by rating
+app.get('/rating/:rating', (req, res) => {
+  const rating = req.params.rating;
+  let booksByRating = booksData.filter(item => item.average_rating <= +rating);
+  res.json(booksByRating)
 })
 
 // Start the server
