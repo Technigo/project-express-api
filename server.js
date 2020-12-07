601916ac79a381a7129e49657657aceb82350ea5
Corneliaryas <hello@corneliaryas.com>
2020-12-07 21:09:14 +0100

    Route to show the 20 top rated books

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Welcome to Books API')
+  res.send('Welcome to Books API, type in /books for an API of all the books we have in the library')
 })
 
 
@@ -44,9 +44,14 @@ app.get('/books', (req, res) => {
   }
 })
 
+app.get('/books/top-rated', (req, res) => {
+  const sortedOnRating = booksData.sort((a, b) => b.average_rating - a.average_rating)
+  const topRated = sortedOnRating.slice(0,20)
+  res.json(topRated)
+})
+
 app.get('/books/:id', (req, res) => {
   const {id} = req.params;
-  console.log(id)
   res.json(booksData.find(item => item.bookID === +id))
 })
 
