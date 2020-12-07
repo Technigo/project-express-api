20f268b89952e0218a220786a1ca1ded4a66a01c
Moa <63230455+mhammarback@users.noreply.github.com>
2020-12-07 14:39:48 +0100

    blue level, golden globes

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import data from './data/golden-globes.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -22,12 +10,23 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+//routes 
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-// Start the server
+app.get('/nominations', (req, res) => {
+  res.json(data)
+})
+
+app.get('/nominations/:nominee', (req, res) => {
+  const nominee = req.params.nominee
+  const oneNominee = data.filter((item) => item.nominee === nominee)
+
+  res.json(oneNominee)
+})
+
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
