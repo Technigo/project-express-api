202cd75b1ee5d858b7a672df189fe470c3ebc7ac
emma-tech <emma.urman@gmail.com>
2020-12-07 20:13:19 +0100

    Updated readme, updated endpoint/route for showing one book based on id, added route/endpoint for showing a top 10 list, updated the route/endpoint with authors to use include() for making it easier to find books based on author. Limited the data returned by using slice().

--- README.md
+++ README.md
@@ -3,9 +3,14 @@
 The focus for this weeks project is to start learning backend by creating our very first API using Express. 
 
 ## The problem/How I solved it
-The task is to create an API that should have at least a couple of RESTful endpoints, which returns either an array of data or a single item. Since we're not using databases yet, we got a set of hard-coded data stored as a JSON files in the project. The API endpoints created use this file along with array methods such as .find(), .filter(), and .slice() to select, filter, or limit the items returned in the responses.
+The task was to create an API that would have a couple of RESTful endpoints, which return either an array of data or a single item. Since we're not using databases yet we got a set of hard-coded data stored as a JSON files to use in this project. I decided to use the file with a list of book reviews. 
 
+The API endpoints I've created use the data from this file, along with array methods such as .find(), .filter(), and .slice() to select, filter, or limit the items returned in the responses.
+
+I started out with listing all the books in the data, but then decided to use .slice() to minimize the number of books returned. I've also created routes/endpoints to make it possible for:
+- showing one single book based on id,
+- showing the top 10 books with highest rating,
+- filtering books by author.
 
 ## View it live
-
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+Link to deployed project: 
--- server.js
+++ server.js
@@ -15,36 +15,58 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+// Routes
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('👋 Welcome to my fist API with books and book reviews 📚🦉')
 })
 
-//Route for all books
+//Showing the first 100 books of all the books in the data
 app.get('/books', (req, res) => {
-  res.json(booksData)
+  let selectedBooks = booksData.slice(0, 100)
+  res.json(selectedBooks)
 })
 
-//Route for finding a book by its id
-app.get('/books/:id', (req, res) => {
+//Finding a single book by its id
+app.get('/books/book/:id', (req, res) => {
   const id = req.params.id
   const bookByID = booksData.find(item => item.bookID === +id);
+
+  if (!bookByID) {
+    res.send('Error: Could not find any book with that ID. Try another ID!')
+  }
   res.json(bookByID)
 })
 
-//Route for finding books by author
-app.get('/authors/:author', (req, res) => {
+//Filtering books by author
+app.get('/books/authors/:author', (req, res) => {
   const author = req.params.author
-  const booksByAuthor = booksData.filter(item => item.authors === author);
+  const booksByAuthor = booksData.filter(item => item.authors.includes(author));
+
+  if (booksByAuthor.length === 0) {
+    res.send('Error: Could not find any books by that author. Try again!')
+  }
   res.json(booksByAuthor);
 })
 
-//Route for finding books by rating
-app.get('/rating/:rating', (req, res) => {
-  const rating = req.params.rating;
-  let booksByRating = booksData.filter(item => item.average_rating <= +rating);
-  res.json(booksByRating)
+// Finding the top 10 books based on rating
+app.get('/books/toplist', (req, res) => {
+  const highRating = booksData.filter(item => item.average_rating >= 4)
+  let topTenBooks = highRating.slice(0,10)
+
+  res.json(topTenBooks);
 })
+
+/* Filtering books by title -- this doesn't work, but author does... ?
+app.get('/books/title/:title', (req, res) => {
+  const title = req.params.title
+  const booksByTitle = booksData.filter(item => item.title.includes(title));
+  
+  if (booksByTitle.length === 0) {
+    res.send('Error: Could not find any books with that title. Try again!')
+  }
+  res.json(booksByTitle);
+})
+*/
 
 // Start the server
 app.listen(port, () => {
