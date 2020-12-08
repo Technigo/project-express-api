d2d312ffc44ee7fc38c9f7812180c4fa154ebc79
Henrike Wiemker <65461017+HenrikeW@users.noreply.github.com>
2020-12-08 16:19:55 +0100

    completed minimum requirements

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import books from './data/books.json';
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -15,19 +16,38 @@ import cors from 'cors'
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+  res.send('Hello world');
+});
+
+// Get all the books in the list
+app.get('/books', (req, res) => {
+  res.json(books);
+});
+
+// Get all books in a specific language
+app.get('/language/:language', (req, res) => {
+  const language = req.params.language;
+  const booksInLanguage = books.filter((book) => book.language_code === language);
+  res.json(booksInLanguage);
+});
+
+// Get a specific book usig the ID
+app.get('/id/:id', (req, res) => {
+  const id = req.params.id;
+  const book = books.find((book) => book.bookID === +id);
+  res.json(book);
+});
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
-})
+});
