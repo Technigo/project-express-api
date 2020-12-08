d2cb1b56efe02f06ac04fbe3e5391e63290715f2
Anna <hasselstromanna@gmail.com>
2020-12-08 16:48:18 +0100

    Add api.

--- server.js
+++ server.js
@@ -1,31 +1,46 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import goldenGlobesData from './data/golden-globes.json'
+//console.log(goldenGlobesData)
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+// overridden when starting the server. For example: PORT=9000 npm start
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get('/', (request, response) => {
+  response.send('Hello world again')
+});
+
+// Define the method - get. Give it a path - nominations. Handle the
+// response
+// Get all data and send as a json response to client
+app.get('/nominations', (request, response) => {
+  response.json(goldenGlobesData)
+});
+
+// 1 year is the path, 2 year is a placeholder for a variable
+app.get('/year/:year', (request, response) => {
+  const year = request.params.year
+  const showWon = request.query.win
+  console.log(showWon)
+  //console.log({ year })
+  // /year/2010
+  let nominationsFromYear = goldenGlobesData.filter((item) => item.year_award === +year)
+
+  // /year/2011?win=true
+  if (showWon) {
+    nominationsFromYear = nominationsFromYear.filter((item) => item.win)
+  }
+  response.json(nominationsFromYear)
+});
 
 // Start the server
 app.listen(port, () => {
