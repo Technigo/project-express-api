aa875fe1236e57bc9b1a64e9a744a3cf1ba93e41
jonna ru <jonna.ru@icloud.com>
2020-12-08 22:38:22 +0100

    2 routes, endpoint returning collection, endpoint returningsingle result + RESTful

--- server.js
+++ server.js
@@ -1,33 +1,48 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import booksData from "./data/books.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (req, res) => {
+  res.send("Hello wo00000rld");
+});
+
+// Books
+
+app.get("/books", (req, res) => {
+  const { author } = req.query;
+
+  let filteredBooks = booksData;
+
+  if (author) {
+    filteredBooks = filteredBooks.filter((book) =>
+      book.authors.includes(author)
+    );
+  }
+
+  res.json(filteredBooks);
+});
+
+app.get("/books/:id", (req, res) => {
+  const id = req.params.id;
+  const bookId = booksData.find((book) => book.bookID === +id);
+
+  res.json(bookId);
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
