4dea208a1ad9c7fc61e1d72967e1a487e7dd84c5
Nadia Mazzoni <Nadia@Nadias-MBP.lan>
2020-12-09 13:33:02 +0100

    1st commit: 2 endpoints (all+single) incl err.mess

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import netflixData from './data/netflix-titles.json'
+// console.log(netflixData.length);
 
+const ERROR_ID_NOT_FOUND = {error: 'Id not found'};
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // 
@@ -24,8 +27,27 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello Api endpoints')
 })
+
+//the entire array of obejcts / http://localhost:8080/shows/
+app.get('/shows', (req, res) => {
+  res.json(netflixData);  
+})
+
+//point to single object id / http://localhost:8080/shows/81193313
+app.get('/shows/:id', (req, res) => {
+  const { id } = req.params.id;
+  let showId = netflixData.find((item) => item.show_id === +id);
+  // console.log(showId);
+
+  if(showId) {
+    res.json(showId);
+  } else {
+    res.status(404).json(ERROR_ID_NOT_FOUND)
+  }
+})
+
 
 // Start the server
 app.listen(port, () => {
