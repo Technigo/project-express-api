5c8e94d95b7e8f3163ef9088854a94739dcc0ebc
Sara Stjernberg <sara_stjernberg@hotmail.com>
2020-12-09 13:55:44 +0100

    added different paths

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import booksData from './data/books.json'
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -26,6 +27,51 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+//get all data
+app.get('/books', (req, res) => {
+  res.json(booksData)
+})
+
+//get a book bases on its id.
+app.get('/books/:id', (req, res) => {
+  const bookId = req.params.id
+  const book = booksData.find(book => book.bookID === +bookId)
+  if (!book) {
+    res.send('Error: Could not find any book with that Id!')
+  }
+  res.json(book)
+})
+
+//sort books based on author
+app.get('/books/authors/:author', (req, res) => {
+  const author = req.params.author
+  const booksByAuthor = booksData.filter(book => book.authors === author)
+  if (booksByAuthor.length === 0) {
+    res.send('Error: Could not find any books by that author!')
+  }
+  res.json(booksByAuthor)
+})
+
+//get a book based on title
+app.get('/books/titles/:title', (req, res) => {
+  const book = req.params.title
+  const bookTitle = booksData.filter(item => item.title === book)
+  if (bookTitle.length === 0) {
+    res.send('Error: Could not find any book with that title!')
+  }
+  res.json(bookTitle);
+})
+
+//sort books by rating 
+app.get('/books/toplist/:nr', (req, res) => {
+  const nrOfBooks = req.params.nr;
+  const highRatedBooks = [...booksData];
+  highRatedBooks.sort((a, b) => b.average_rating - a.average_rating)
+  const topList = highRatedBooks.slice(0, nrOfBooks);
+  res.json(topList)
+})
+
 
 // Start the server
 app.listen(port, () => {
