50751b4826470e2c6fdf8317a697214eca39d6b8
Moa Blomkvist <moa@birgerochblom.se>
2020-12-09 23:52:13 +0100

    naming

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ console.log(netflixData.length)
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8083
+const port = process.env.PORT || 8084
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
@@ -28,22 +28,33 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
-})
+});
 
-app.get('/production', (req, res) => {
+// Return a collection (array) of elements
+app.get('/productions', (req, res) => {
   res.json(netflixData)
-})
+});
 
-app.get('/year/:year', (req, res) => {
-  const year = req.params.year
-  console.log({ year })
+// Single production
+//app.get('/productions/:id', (req, res) => {
+  //const id = req.params.id;
+
+  //const production = netflixData.find((production) => production.show_id === +id);
+
+  //res.json(production)
+//});
+
+// Filter on release-year
+app.get('/release/:year', (req, res) => {
+  const year = req.params.year;
   
-  const netflixProductionsFromYear = netflixData.filter((item) => item.release_year === +year)
-  res.json(netflixProductionsFromYear)
-})
+  const netflixReleaseYear = netflixData.filter((item) => item.release_year === +year);
+
+  res.json(netflixReleaseYear)
+});
 
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
-})
+});
