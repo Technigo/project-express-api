9675074ab9fda9301ff51efd3b4e314c7b32b460
Gabbi-89 <gabriellabolin@hotmail.com>
2020-12-09 18:58:39 +0100

    Filtering with query added

--- server.js
+++ server.js
@@ -18,20 +18,52 @@ app.use(bodyParser.json());
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello, welcome to Gabriella´s API with books!')
+  res.send('Hello, welcome to Gabriella´s API with books! Possible endpoints to use: /books (for seeing all books), /books/book/:id, (to search by Book ID) /books/author/:author (to search by author) and /books/top-rated (to see the 20 highest rated books)')
 });
 
 // The first endpoint, returns a collection of books
 app.get('/books', (req, res) => {
+  res.json(books)
+});
+
+// Filter query
+app.get('/books/filter', (req, res) => {
+  // Slice to set the 50 first books
+  let filteredBooks = books.slice(0, 50);
   
   // Set a filter with query parameter to filter on
   // only the books with average_rating > 4.0
-  const highRating = req.query.highRating;
-  if (highRating) {
-    books = books.filter((item) => item.average_rating > 4.0);
+  // Access this query by '/books?average_rating=high'
+  const { average_rating, page } = req.query;
+  if (average_rating === 'high') {
+    filteredBooks = filteredBooks.filter((item) => item.average_rating > 4.0);
   };
+  
+  // Set query for which site to see, with 50 on each.
+  // Total 500 books divided on 10 pages
+  if (page === '1') {
+    filteredBooks = books.slice(0, 50)
+  } else if (page === '2') {
+    filteredBooks = books.slice(51, 100)
+  } else if (page === '3') {
+    filteredBooks = books.slice(101, 150)
+  } else if (page === '4') {
+    filteredBooks = books.slice(151, 200)
+  } else if (page === '5') {
+    filteredBooks = books.slice(201, 250)
+  } else if (page === '6') {
+    filteredBooks = books.slice(251, 300)
+  } else if (page === '7') {
+    filteredBooks = books.slice(301, 350)
+  } else if (page === '8') {
+    filteredBooks = books.slice(351, 400)
+  } else if (page === '9') {
+    filteredBooks = books.slice(401, 450)
+  } else if (page === '10') {
+    filteredBooks = books.slice(451, 500)
+  }
 
-  res.json(books)
+  res.json(filteredBooks)
 });
 
 // The second endpoint, returns a single book based on bookID
@@ -68,8 +100,13 @@ app.get('/books/top-rated', (req, res) => {
 });
 
 // Empty/dummy endpoints
+app.get('/books/isbn/:isbn', (req, res) => {
+// To search by isbn number
+  res.send('This endpoint does not have any content yet!')
+});
+
 app.get('/books/lesspages', (req, res) => {
-  res.send('This endpoint does not exist yet!')
+  res.send('This endpoint does not have any content yet!')
 });
 
 // Start the server
