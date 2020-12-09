259740dd96f2acc07d3b1737b5bb135b06426769
ELLEN BJORKMAN <ELLEN881008@HOTMAIL.SE>
2020-12-09 12:02:10 +0100

    Adds new structure to the different endpoints

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import cors from 'cors'
 
 import booksData from './data/books.json'
 
-
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -18,57 +17,51 @@ app.get('/', (req, res) => {
   res.send('Hello world, Im here now!')
 })
 
-//listing all books. Title is a possible param.
+//returning all books. Anda query param to search on title.
 //to String is needed since item.title is an object.
 app.get('/books', (req, res) => {
-  const title = req.query.title
+  const { title, author } = req.query
+  let filteredBooks = booksData
 
-  if (title){
-    let filteredBooksTitle = booksData.filter(item => item.title.toString().toLowerCase().includes(title.toLowerCase()))
-    if (filteredBooksTitle.length > 0) {
-      res.json(filteredBooksTitle)
-    } else{
-      res.status(404).json({ message: 'No such title found' })
-    }
-  } else {
-    res.json(booksData)
+  if (title) {
+    filteredBooks = booksData.filter(item => item.title.toString().toLowerCase().includes(title.toLowerCase())) 
+  } 
+  if (author) {
+    filteredBooks = booksData.filter(item => item.authors.toString().toLowerCase().includes(author.toLowerCase()))
+  }
+
+  if (filteredBooks.length > 0) {
+    res.json(filteredBooks)
+  } else{
+    res.status(404).json({ message: 'No such book found' })
   }
 })
 
-//listing books from a specific Author
-app.get('/:author', (req, res) => {
-  console.log(author)
-  const author = req.params.author
 
-
-  const booksFromAuthor = booksData.filter((item) => item.authors === author)
-
-  res.json(booksFromAuthor)
-  
-})
-
-//returning a single book filtered on id
+//returning a single book from searching id
 // using find instead of filter since I only want one result
 app.get('/books/:id', (req, res) => {
-  const id = req.params.id
-  const matchingId = booksData.find((item) => item.bookID === +id)
+  const { id } = req.params
+  const book = booksData.find((item) => item.bookID === +id)
 
-  if (matchingId) {
-    res.json(matchingId)
+  if (book) {
+    res.json(book)
   } else {
-    res.status(404).json({ message: 'No such Id found' })
+    res.status(404).json({ message: 'No such book found' })
   }
-  
 })
 
+  
 
-/*app.get('/:rating', (req, res) => {
+/*app.get('/books/:rating', (req, res) => {
   const rating = req.params.rating
   const highRatingArray = booksData.filter((item) => item.average_rating > 4)
-  console.log(ratingArray)
+  console.log(hihgRatingArray)
   //const highrating = ratingArray.sort((a,b) => a-b)
-  res.json(ratingArray)
+  res.json(highRatingArray)
 })*/
+
+/*const sortedBooksOnRating = booksData.sort(function (a, b) { return b.average_rating - a.average_rating })*/
 
 // Start the server
 app.listen(port, () => {
