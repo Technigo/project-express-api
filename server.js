c230e0de342818aa844312ef005aedcfee755f3c
LINDA HINTZE <lindahintze@hotmail.com>
2020-12-09 20:47:50 +0100

    add include to first enpoint to be able to search for substring

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-import express, { response } from 'express'
+import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-
-import data from './data/christmas_billboard.json'
 
 import harryData from './data/harry-potter-characters.json'
 
@@ -21,22 +19,20 @@ app.get('/', (request, response) => {
   response.send('Hello world')
 })
 
-// THIS END POINT WILL SHOW ALL THE DATA + FILTERING ON NAME AVAILABLE
+// This endpoint shows all characters + will let you filter on name with query parameter
 app.get('/characters', (request, response) => {
   const { name } = request.query
   if (name) {
-    const filteredName = harryData.filter(character => character.name.toLocaleLowerCase() === name)
+    const filteredName = harryData.filter(character => 
+      character.name.toLocaleLowerCase() && 
+      character.name.toLocaleLowerCase().includes(name))
     response.json(filteredName)
   } else {
     response.json(harryData)
   }
 })
 
-// ALIVE/DEAD
-// MALE/FEMALE
-
-
-// THIS END POINT WILL SHOW DATA FOR A UNIQUE ID
+// This endpoint shows one character from a unique id
 app.get('/characters/id/:id', (request, response) => {
   const { id } = request.params
   const showId = harryData.find((item) => item.id === +id)
@@ -48,25 +44,25 @@ app.get('/characters/id/:id', (request, response) => {
   }
 })
 
-// This enpoint will show all names of the characters, sorted from A-Z
+// This enpoint shows all names of the characters, sorted from A-Z
 app.get('/characters/names', (request, response) => {
   const names = harryData.map(name => name.name).sort()
   response.json(names)
 })
 
-// THIS END POINT WILL SHOW DATA FROM EACH HOUSE (Gryffindor, Slytherin, Ravenclaw, Hufflepuff)
+// This endpoint shows all characters from a specific house
 app.get('/characters/:house', (request, response) => {
   const { house } = request.params
-  const showHouse = harryData.filter((item) => item.house.toLocaleLowerCase() === house)
+  const houses = harryData.filter((item) => item.house.toLocaleLowerCase() === house)
 
-  if (showHouse.length === 0) {
+  if (houses.length === 0) {
     response.status(404).json(ERROR_CHARACTERS_NOT_FOUND)
   } else {
-    response.json(showHouse)
+    response.json(houses)
   }
 })
 
-// END POINT TO SHOW DIFFERENT GENDERS IN EACH HOUSE
+// This endpoint shows different genders in a specific house
 app.get('/characters/:house/genders/:gender', (request, response) => {
   const { house, gender } = request.params
   const genders = harryData.filter(character => 
@@ -79,13 +75,6 @@ app.get('/characters/:house/genders/:gender', (request, response) => {
   } else {
     response.json(genders)
   }
-})
-
-
-// THIS ENPOINT WILL ONLY SHOW DATA FROM THE TOP 20 SONGS
-app.get('/songs/top-20', (request, response) => {
-  const songsTopTen = data.filter((item) => item.week_position >= 1 && item.week_position <= 20)
-  response.json(songsTopTen)
 })
 
 // Start the server
