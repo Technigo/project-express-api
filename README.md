6ab83f1192f74553d994ee9bd47d47476a51aadf
Emelie Svensson <emeliesv@live.se>
2020-12-10 15:31:17 +0100

    started working on sortingfunction

--- README.md
+++ README.md
@@ -1,13 +1,35 @@
 # Express API Project
 
-Replace this readme with your own information about your project. 
+This is a project where I've created my first API using the Express server.
 
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+The API is about books and uses mainly the get-method but also has a post-method.
+It has a few different paths and also uses query for further filtration.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+I started out reaching for these goals:
+- The API should have at least 2 routes. Try to push yourself to do more, though!
+- A minimum of one endpoint to return a collection of results (array of elements)
+- A minimum of one endpoint to return a single result (single element).
+- The API should be RESTful
+
+Then I also implemented the following:
+- On routes which return a single item, handle when the item doesn't exist and return some useful data in the response.
+- Accept filters via query parameters to filter the data you return from endpoints which return an array of data.
+
+I started working on dummy-enpoints as well, but then I made the decision to actually create the post-method in one of the paths instead. This was a bit complex, since I had to figure out how to update a file and add objects to that array without using a database. The solution was to install the file-system dependencies and use it to read and write to local files.
+
+## Next up
+If I had more time, I'd like to have written a documentation about this API. However, I might updated the project with a documentation later on.
+
+## Tech
+* Node.js
+* Javascript
+* File-system library
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+<a href='https://project-api-books-emelie.herokuapp.com'>Project Express API</a>
+
+## Author
+* <a href="https://github.com/emeliesv">Emelie Svensson</a>
--- server.js
+++ server.js
@@ -10,14 +10,15 @@ const app = express()
 const fs = require('file-system')
 
 const ERROR_MSG = {
+  status_code: 404,
   error: 'no books found!'
 }
 
 const INPUT_ERROR = {
+  status_code: 400,
   error: 'invalid input!'
 }
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
@@ -75,7 +76,6 @@ const validateBookInput = (bookDefinition, input) => {
   return result
 }
 
-// Start defining your routes here
 app.get('/', (req, res) => {
   res.send(endpoints(app))
 })
@@ -83,19 +83,38 @@ app.get('/', (req, res) => {
 app.get('/books', (req, res) => {
   const language = req.query.lang
   const otherlanguages = req.query.otherlang
+  const sortedAscending = req.query.sorted_acending
 
   if (language) {
-    let booksInChosenLang = booksData.filter((item) => item.language_code === language)
+    const booksInChosenLang = booksData.filter((item) => item.language_code === language)
     if (booksInChosenLang.length === 0) {
       res.status(404).json(ERROR_MSG)
     } res.json(booksInChosenLang)
   } else if (otherlanguages) {
-    let booksInOtherLang = booksData.filter((item) => item.language_code != language)
+    const booksInOtherLang = booksData.filter((item) => item.language_code != language)
     if (booksInOtherLang.length === 0) {
       res.status(404).json(ERROR_MSG)
     } res.json(booksInOtherLang)
-  } res.json(booksData)
+  } else if (sortedAscending){
+    const sortedBooks = booksData.sort((a, b) => { return a.num_pages - b.num_pages })
+    const sortedPage = sortedBooks.slice(0, sortedAscending)
+    res.json(sortedPage)
+  }res.json(booksData)
+})
 
+app.post('/books', (req, res) => {
+  const input = req.body
+  const validationResult = validateBookInput(bookDefinition, input)
+  const invalids = validationResult.filter((object) => !object.isValid)
+
+  if (invalids.length === 0) {
+    const result = JSON.parse(fs.readFileSync('./data/books.json'))
+    result.push(input)
+    const resultJSON = JSON.stringify(result)
+    fs.writeFileSync('./data/books.json', resultJSON)
+    res.status(201).json(validationResult)
+  } else
+      res.status(400).json(INPUT_ERROR)
 })
 
 app.get('/books/:book', (req, res) => {
@@ -107,22 +126,6 @@ app.get('/books/:book', (req, res) => {
   res.json(result)
 })
 
-app.post('/books', (req, res) => {
-  const input = req.body
-  const validationResult = validateBookInput(bookDefinition, input)
-  const invalids = validationResult.filter((object) => !object.isValid)
-  
-  if (invalids.length === 0) {
-    const result = JSON.parse(fs.readFileSync('./data/books.json'))
-    result.push(input)
-    const resultJSON = JSON.stringify(result)
-    fs.writeFileSync('./data/books.json', resultJSON)
-    res.status(200).json(validationResult)
-  } else
-      res.status(400).json(INPUT_ERROR)
-})
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
