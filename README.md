ac55126869466cdc289927e494ba09f88674d404
JESSICA PANDITHA <JESSICA.PANDITHA@GMAIL.COM>
2020-12-10 23:46:32 +0100

    fixed id

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # Express API Project
 
-Replace this readme with your own information about your project. 
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+This project was an introduction to back-end and was about creating our own APIs using Express.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+I chose data already given about Netfllix movies and TV-shows. Then I started with the routes and naming the data as a pluralized route, titles in this case. Then I added different endpoints such as id, year where I used path parameters and then I used query parameter for searching based on whether it is a movie or a TV-show. Filter method was used to get a collection of data based on year and type and find method was used to get a specific TV show/movie based on its unique ID. If I had more time I would have added more endpoints and sorted on more criteria and would have tried to add frontend as well. Express, Node.js, Babel dependencies were used in this project.
 
 ## View it live
 
--- server.js
+++ server.js
@@ -4,38 +4,30 @@ import cors from 'cors';
 
 import netflixData from './data/netflix-titles.json';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
-//
+
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
+const ERROR = { error: 'Request not found'};
 
-// Add middlewares to enable cors and json body parsing
+//Middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(bodyParser.json());
 
 
-// Start defining your routes here. request is incoming and response is outgoing.
+// Routes. Request is incoming and Response is outgoing.
 app.get('/', (request, response) => {
   response.send('Hello fellow user! Here is a Netflix API where you can find data about Movies and TV shows on Netflix. Enjoy!');
-})
+});
 
 //Here we are using query parameter so you can search only movies or only tv-shows like this 
 //:localhost:8080/titles?type=Movie
 app.get('/titles', (request, response) => {
   const { type } = request.query;
   if (type) {
-    const filteredTitles = netflixData.filter(title => title.type === type);
+    const filteredTitles = netflixData.filter((title) => title.type === type);
     response.json(filteredTitles);
   } else  {
     response.json(netflixData);
@@ -44,12 +36,37 @@ app.get('/titles', (request, response) => {
 
 
 //Here we are using path parameter to get only one title that has the exact ID
-app.get('titles/:id', (request, response) => {
+app.get('/titles/id/:id', (request, response) => {
   const { id } = request.params;
-  console.log(request.params);
-  const title = netflixData.find(title => +title.show_id === +id);
-  response.json(title);
+  // console.log(id);
+  const title = netflixData.find(title => title.show_id === +id);
+  if (title === 0) {
+    response.status(404).json(ERROR);
+    } else {
+    response.json(title);
+    }
 });
+
+//Here we are using path parameter to get all titles for a specific release year
+app.get('/titles/year/:year', (request, response) => {
+  const { year } = request.params;
+  // console.log(year);
+  const titlesOfYear = netflixData.filter(title => title.release_year === +year);
+  response.json(titlesOfYear);
+});
+
+//Here we are using path parameter to get all movies for a specific release year
+// app.get('/titles', (request, response) => {
+//   const { type } = request.query;
+//   if (type) {
+//     const filteredTitles = netflixData.filter((title) => title.type === type);
+//     response.json(filteredTitles);
+//   } else  {
+//     response.json(netflixData);
+//   }
+// });
+
+//Here we are using path parameter to get all TV-shows for a specific release year
 
 
 // Start the server
