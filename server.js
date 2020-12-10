6343b9f586de5116a40d311f23e5a21aab879d4e
Ylva Nilsson <ylva.z.nilsson@teliacompany.com>
2020-12-10 09:05:32 +0100

    cleaning up query filter by author and title plus a single result by book id

--- server.js
+++ server.js
@@ -2,15 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-//
-// import goldenGlobesData from './data/golden-globes.json';
-// import avocadoSalesData from './data/avocado-sales.json'
 import booksData from './data/books.json';
 console.log(`Number of books: ${booksData.length}`);
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
@@ -25,46 +18,66 @@ app.use(bodyParser.json());
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello API');
+  if (!res) {
+    res
+      .status(404)
+      .send({ error: 'Sorry, seems like there is an issue, try agian later!' });
+  } else res.send('Hello API');
 });
 
 // A Collection of results - An array with book elements
-// that can be filtered by title or author
+// that can be filtered by title or author.
+// Returns an object with the fields booksData, filteredAuthors and filteredTitles
+// plus number of books listed.
 app.get('/books', (req, res) => {
   const { author, title } = req.query;
-
+  console.log(`The author value is: ${author}`);
   console.log(`The title value is: ${title}`);
-  console.log(`The authors value is: ${author}`);
 
-  // Query parameter filters for author and title
+  let booksList = booksData;
+  const totalNumberOfBooks = booksList.length;
+
+  // Query parameter to filter by author
   if (author) {
-    const filteredAuthors = booksData.filter((item) =>
+    booksList = booksList.filter((item) =>
       item.authors.toLowerCase().includes(author.toLowerCase())
     );
-    res.json(filteredAuthors);
-  } else if (title) {
-    const filteredTitles = booksData.filter((item) =>
+  }
+
+  // Query parameter to filter by title
+  if (title) {
+    booksList = booksList.filter((item) =>
       item.title.toString().toLowerCase().includes(title.toLowerCase())
     );
-    res.json(filteredTitles);
-  } else {
-    res.json(booksData);
   }
+
+  const returnObject = {
+    totalNumberOfBooks: totalNumberOfBooks,
+    numberOfBooks: booksList.length,
+    results: booksList
+  };
+
+  // If the result is zero, status set to 404 and returning a useful data in the response
+  if (booksList.length === 0) {
+    res.status(404).send({
+      error: 'Sorry, no books where found, please try a different query.'
+    });
+  }
+
+  res.json(returnObject);
 });
 
 // A Single result - A single book element find by id
 app.get('/books/:id', (req, res) => {
-  const id = req.params.id;
-  console.log(id);
+  const { id } = req.params;
+  console.log(`The id value is: ${id}`);
 
-  const book = booksData.find((item) => item.bookID === +id);
+  const bookId = booksData.find((item) => item.bookID === +id);
 
   // If the book doesn't exist, status set to 404 and returning a useful data in the response
-  if (!book) {
-    res.status(404).send({ error: `No book with id: ${id} found` });
-  }
-
-  res.json(book);
+  if (!bookId) {
+    res.status(404).send({ error: `No book with id: ${id} found.` });
+  } else res.json(bookId);
 });
 
 // Start the server
