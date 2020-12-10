01ac10c0f786b7322605ceef7e22dce0fc11630e
Rebkova <rebkova@gmail.com>
2020-12-10 22:04:49 +0100

    title search using query selector working

--- server.js
+++ server.js
@@ -6,14 +6,6 @@ import booksData from './data/books.json'
 console.log(`Book nr: ${booksData.length}`)
 
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -25,20 +17,46 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// --- ALL the routes ---
+// --- ALL THE ROUTES ---
 
-app.get('/', (request /* incoming */, res /* outgoing */) => {
+//the basic endpoint
+app.get('/', (request /* incoming */, response /* outgoing */) => {
   //response object -> used for building the response we send to the browser
-  res.send('Good morning, Rebeka! Best of luck with this week\'s project')
+  response.send('Good morning, Rebeka! Best of luck with this week\'s project')
 })
 
-//all the books
-app.get("/books", (req, res) => {
-  res.json(booksData)
+//search by TITLE using query selectors
+//localhost8080/books?title=Harry
+app.get("/books", (request, response) => {
+
+  const titleSearch = request.query.title
+
+  if (titleSearch === undefined) {
+    response.json(booksData)
+  }
+  else if (titleSearch !== undefined) {
+    const titleLowerCase = titleSearch.toLowerCase()
+    const titleArray = booksData.filter(book => book.title.toString().toLowerCase().includes(titleLowerCase))
+
+    if (titleArray.length > 0) {
+      response.json(titleArray)
+    }
+    else if (titleArray.length === 0) {
+      response.json(`Ups, no keyword called "${titleLowerCase}" in a title.`)
+    }
+  }
+})
+
+
+//all books sorted asc by bookId
+app.get("/books", (request, response) => {
+  //WHY DOESN'T THIS WORK AS A PAGE?
+  // response.send({ all_books: booksData.length })
+  response.json(booksData)
 })
 
 //specific book (one object)
-app.get("/id/:isbn13", (req, res) => {
+app.get("/id/:isbn13", (request, response) => {
   const bookId = req.params.isbn13
   console.log({ bookId })
 
@@ -46,7 +64,8 @@ app.get("/id/:isbn13", (req, res) => {
   //find a book based on its isbn13
   const bookISBN13 = booksData.find(item => item.isbn13 === +bookId)
 
-  res.json(bookISBN13)
+  response.json(bookISBN13)
+
   //if the book is not found error
   //sending back the data 9780060920081 (The lost continent)
 
@@ -56,19 +75,15 @@ app.get("/id/:isbn13", (req, res) => {
   // response.status(404).json({ error: `No books with ISBN13 were found.` });
   // }
   // else {
-  //   res.json(bookISBN13)
+  //   response.json(bookISBN13)
   // }
 
 })
 
-//filter books based on average rating
-app.get("/books/rating", (req, res) => {
+//sort books based on average rating (descending)
+app.get("/books/rating", (request, response) => {
 
-  //sorting the numbers 
-  const ratingArray = booksData.sort(compare)
-  console.log(`Rating array: ${ratingArray}`)
-
-  function compare(a, b) {
+  const compare = (a, b) => {
     if (a.average_rating > b.average_rating) {
       return -1
     }
@@ -80,10 +95,13 @@ app.get("/books/rating", (req, res) => {
     }
   }
 
-  // const sortedArray = booksData.sort(compare)
-  // console.log(`soret array; ${sortedArray}`)
-  // res.json(sortedArray)
+  //sorting the numbers 
+  const sortedRatingArray = booksData.sort(compare)
+
+  response.json(sortedRatingArray)
 })
+
+
 
 //empty endpoint (to be added in the future)
 // app.get("/books/:thumbnail", (req, res)) {
