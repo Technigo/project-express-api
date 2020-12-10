df8b4df88ae700f2035f4c1e84cc0d5d9330e19c
Gabbi-89 <gabriellabolin@hotmail.com>
2020-12-10 08:24:12 +0100

    Sorting added on top rated books and Read me written

--- README.md
+++ README.md
@@ -1,16 +1,38 @@
 # Express API Project
 
-This project was to create my own API using Express.
-
-
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+In this project I have built my first backend project where I created my own API using Node and Express. The API have some RESTful endpoints where some return an array of data and some a single item.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+I did not use any database in this project to I used a hard-coded set of data, stored as a JSON file in the project folder. To make different choices about what to show in the different endpoint I used the methods `.find()`, `.filter()`, `.sort()` and `.slice()`. 
+
+In my project I use a JSON file with 500 book reviews. First I created one endpoint retrieving all data. Then I made sorting on book ID and author(s). For the routes retrieving one single books or books from a specific author(s) I added error messages if the user choose an ID or author that are not existing. 
+
+I did also an endpoint showing the 20 books with highest rating by using `.filter()`, `.sort()` and `.slice()` methods. 
+Later I added query parameters to filter out the books with an average rating above 4.0 and a `.slice()`method showing 50 books per side wehre the user can choose with query parameter which side to show (between 1 and 10).
+
+If I get more time I will add error messages if a non existing endpoint is entered. I will also build a frontend site which uses the API to show the data. I would also like to learn how to create useful documentation for my endpoints and to present it in a good way. 
+
+## Learning Objectives
+
+- How to build an API in Node using Express
+
+- How to create routes in Express
+
+- How to deploy Node projects
+
+- How to make RESTful API's
+
+- How to manipulate data in JavaScript by selectiong, filtering and limiting arrays
+
+## Tech
+
+- Node
+
+- API's 
+
+- Express
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
-Link to deployed project: 
+Link to my API start page: https://gabriella-books-api.herokuapp.com/
--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/books', (req, res) => {
 app.get('/books/filter', (req, res) => {
   // Slice to set the 50 first books
   let filteredBooks = books.slice(0, 50);
-  
+
   // Set a filter with query parameter to filter on
   // only the books with average_rating > 4.0
   // Access this query by '/books?average_rating=high'
@@ -38,7 +38,7 @@ app.get('/books/filter', (req, res) => {
   if (average_rating === 'high') {
     filteredBooks = filteredBooks.filter((item) => item.average_rating > 4.0);
   };
-  
+
   // Set query for which site to see, with 50 on each.
   // Total 500 books divided on 10 pages
   if (page === '1') {
@@ -70,7 +70,7 @@ app.get('/books/filter', (req, res) => {
 app.get('/books/book/:id', (req, res) => {
   const bookID = req.params.id;
   const bookById = books.find((item) => item.bookID === +bookID);
-  
+
   // If bookID wasn't found
   if (!bookById) {
     res.send("Sorry, we cannot find any book with that ID :( Please try another ID!");
@@ -83,7 +83,7 @@ app.get('/books/book/:id', (req, res) => {
 app.get('/books/author/:author', (req, res) => {
   const authors = req.params.author;
   const booksFromAuthors = books.filter((item) => item.authors === authors);
-  
+
   // If authors is not found
   if (booksFromAuthors.length === 0) {
     res.send("Sorry, we cannot find any books from that author in our list. Try by entering the name in another way or search for another author.")
@@ -94,18 +94,20 @@ app.get('/books/author/:author', (req, res) => {
 
 // Top rated books, the 20 highest 
 app.get('/books/top-rated', (req, res) => {
-  const topRated = books.filter((item) => item.average_rating >=4);
-  const top20Books = topRated.slice(0, 20);
+  const topRated = books.filter((item) => item.average_rating >= 4);
+  const sortedBooks = topRated.sort((a, b) => b.average_rating - a.average_rating);
+  const top20Books = sortedBooks.slice(0, 20);
   res.json(top20Books);
 });
 
-// Empty/dummy endpoints
+// Empty/dummy endpoints to create and use later on if wanted
 app.get('/books/isbn/:isbn', (req, res) => {
-// To search by isbn number
+  // To search by isbn number
   res.send('This endpoint does not have any content yet!')
 });
 
-app.get('/books/lesspages', (req, res) => {
+// To search on books with few pages
+app.get('/books/fewpages', (req, res) => {
   res.send('This endpoint does not have any content yet!')
 });
 
