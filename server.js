53b9a1db20b45d39901c414b8ef94ab0118c9a6d
Ingela Löfgren <ingela.lofgren@gmail.com>
2020-12-10 19:41:57 +0100

    Added genre

--- server.js
+++ server.js
@@ -28,15 +28,23 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Netflix movies & TV shows')
 })
 
-//shows the whole array of Netflix shows and movies
+
+// a) Shows the whole array of Netflix shows and movies
 app.get('/shows', (req, res) => {
   res.json(netflixData)
 })
 
-//Shows all objects (movies and tv-shows) with the release date from a specific year
+
+// b) Shows a list of all the Netlix titles
+app.get('/shows/titles', (req, res) => {
+  res.json(netflixData.map(item => item.title))
+})
+
+
+// c) Shows all objects (movies and tv-shows) with the release date from a specific year
 app.get('/shows/year/:year', (req, res) => {
   const year = req.params.year
   const releaseYear = netflixData.filter((item) => item.release_year === +year)
@@ -44,13 +52,13 @@ app.get('/shows/year/:year', (req, res) => {
  //this shows an error message if there are no movies or shows from a specific year
   if (releaseYear.length === 0) {
     res.status(404).json(ERROR_SHOWS_NOT_FOUND)
-
   } else {
-  res.json(releaseYear)
+    res.json(releaseYear)
   }
 })
 
-//shows one object with a specific id
+
+// d) shows one object with a specific id
 app.get('/shows/id/:id', (req, res) => {
   const id = req.params.id
   const netflixId = netflixData.find((item) => item.show_id === +id)
@@ -58,13 +66,13 @@ app.get('/shows/id/:id', (req, res) => {
   //If the id is false then return an error message
   if (!netflixId) {
     res.status(404).json(ERROR_SHOWS_NOT_FOUND)
-
   } else {
     res.json(netflixId)
   }
 })
 
-//shows movies and shows from a specific country 
+
+// e) shows movies and shows from a specific country 
 app.get('/shows/country/:country', (req, res) => {
   const country = req.params.country
   const netflixCountry = netflixData.filter((item) => item.country === country)
@@ -77,24 +85,48 @@ app.get('/shows/country/:country', (req, res) => {
   }
 })
 
-/* app.get('/shows/:year/type/:movie', (req, res) => {
-    const { year } = req.params
-    const { type } = req.query
 
-    let filteredShows = netflixData
+// f) Shows movies and tv-shows from a specified genre. If you search for e.g Comedies you will also get results where many different genres are listed for the same movie/show. It also works to write comedies (lowercase c) or even only part of a word like comed.
+app.get('/shows/genre/:genre', (req, res) => {
+  const genre = req.params.genre
+  const showsGenre = netflixData.filter((item) => item.listed_in.toLowerCase().includes(genre.toLowerCase()))
 
-    filteredShows = filteredShows.filter(
-      (test) => +test.release_year === +year 
-    )
-    
-    filteredShows = filteredShows.filter(
-      (test) => test.type === country
-    )
-    res.json(filteredShows)
-}) 
- */
+  //shows error message if the specified word isn't included within a genre.
+  if (showsGenre.length === 0) {
+    res.status(404).json(ERROR_SHOWS_NOT_FOUND)
+  } else { 
+    res.json(showsGenre)
+  }
+  })
 
- //Two params are used to return movies that are of the type "movie" and from a specified year.
+
+// g) Similar to the one above but here you will only get the movies/shows which is defined with only one genre, for example Comedies(written exactly like that). If you write comedies (with lowercase c you will get an error message)
+app.get('/genre/:genre', (req, res) => {
+  const genre = req.params.genre
+  const movieGenre = netflixData.filter((item) => item.listed_in === genre)
+
+//shows an error message if a genre doesn't exist or is misspelled. 
+  if (movieGenre.length === 0) {
+    res.status(404).json(ERROR_SHOWS_NOT_FOUND)
+  } else { 
+    res.json(movieGenre)
+}
+})
+
+
+// h) Here we can query a movie/show title. We can use the query method since several movies/shows can have the same name. We have to write like this in the browser: localhost:8080/shows/title?title=xxx
+app.get('/shows/title', (req, res) => {
+  const title  = req.query.title
+  if (title) {
+    const filteredTitle = netflixData.filter((item) => item.title === title)
+    res.json(filteredTitle)
+  } else {
+    res.json(netflixData)
+  }
+})
+
+
+ // i) Two params are used to return movies that are of the type "movie" and from a specified year.
 app.get('/shows/:year/type/:type', (req, res) => {
   const { year, type } = req.params;
   const yearType = netflixData.filter((yearType) => {
@@ -108,9 +140,9 @@ app.get('/shows/:year/type/:type', (req, res) => {
   } 
 })
 
- //DUMMY ENDPOINT: Create some empty/dummy endpoints which could contain more complex operations in the future.  Find good names for them
- app.get('/shows/:maxduration', (req, res) => {
-//Suggestion:Here we could do a conditional that all movies with the maximum duration of 90 min should be shown.
+ // j) DUMMY ENDPOINT: Create some empty/dummy endpoints which could contain more complex operations in the future.  Find good names for them. (RED LEVEL)
+ app.get('/shows/maxduration/:maxduration', (req, res) => {
+//Suggestion:Here we could do a conditional that all movies with the maximum duration of 90 min should be shown. First filter out all movies and then use and if statement to show movies with a maximum of 90 min.
   res.json()
  })
 
