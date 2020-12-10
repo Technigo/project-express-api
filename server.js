d9d1f1fb07a45c7d440472a329a8ffe5202027dc
Karin Nordkvist <kontakt@karinnordkvist.se>
2020-12-10 16:36:44 +0100

    Added 404s

--- server.js
+++ server.js
@@ -6,39 +6,60 @@ import data from './data/ted.json';
 
 const port = process.env.PORT || 8080;
 const app = express();
+const listEndpoints = require('express-list-endpoints');
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(bodyParser.json());
 
-// ----------------------------------------------------------------
+// --------------------------------------------------------------------------------
 
-// ALL TASKS
 app.get('/', (req, res) => {
-  // res.json(data);
-  // TODO: Add a list of all endpoints here
+  res.send('Welcome to TED-talks by Karin');
 });
 
-// INITIAL RESULT = ALL TALKS
+app.get('/endpoints', (req, res) => {
+  res.send(listEndpoints(app));
+});
+
+// ALL TALKS
 app.get('/talks/', (req, res) => {
-  // TALKS BY SPEAKER
+  // FILTERED BY SPEAKER  ---------------------------------------------------------
   if (req.query.speaker) {
     const speaker = req.query.speaker;
     const filteredBySpeaker = data.filter((talk) =>
       talk.main_speaker.includes(speaker)
     );
-    res.json(filteredBySpeaker);
+
+    if (filteredBySpeaker.length === 0) {
+      res
+        .status(404)
+        .send(
+          `Sorry, couldn't find any talks by ${req.query.speaker}. Try again! (or find the whole list of speakers under /speakers)`
+        );
+    } else {
+      res.json(filteredBySpeaker);
+    }
   } else if (req.query.category) {
-    // TALKS BY ONE OR MORE CATEGORIES
-    // If there is one category in the query
+    // FILTERED BY ONE OR MORE CATEGORIES ----------------------------------------
+    // If there is one category in the query -------
     if (typeof req.query.category === 'string') {
       const category = req.query.category;
       const filteredByCategory = data.filter((talk) =>
         talk.tags.includes(category)
       );
-      res.json(filteredByCategory);
+
+      if (filteredByCategory.length === 0) {
+        res
+          .status(404)
+          .send(
+            `Sorry, couldn't find any talks matching your search. Try again!`
+          );
+      } else {
+        res.json(filteredByCategory);
+      }
     } else {
-      // If there are multiple categories in the query
+      // If there are multiple categories in the query -------
       const categories = req.query.category;
 
       // Filter talks by checking whether the category/categories that the user wrote
@@ -54,39 +75,73 @@ app.get('/talks/', (req, res) => {
           return true;
         }
       });
-      res.json(multipleCategories);
+      if (multipleCategories.length === 0) {
+        res
+          .status(404)
+          .send(
+            `Sorry, couldn't find any talks matching your search. Try again!`
+          );
+      } else {
+        res.json(multipleCategories);
+      }
     }
   } else if (req.query.event) {
-    // TALKS  BY EVENT
+    // FILTERED BY EVENT ---------------------------------------------------------
     const event = req.query.event;
     const filteredByEvent = data.filter((talk) => talk.event === event);
-    res.json(filteredByEvent);
+
+    if (filteredByEvent.length === 0) {
+      res
+        .status(404)
+        .send(
+          `Sorry, couldn't find any talks matching your search. Try again!`
+        );
+    } else {
+      res.json(filteredByEvent);
+    }
   } else {
     res.json(data);
   }
 });
 
-// SINGLE TALK
+// SINGLE TALK ---------------------------------------------------------
 app.get('/talks/:id', (req, res) => {
   const id = req.params.id;
   const filteredById = data.filter((talk) => talk.id === id);
-  res.json(filteredById);
+
+  if (filteredById.length === 0) {
+    res
+      .status(404)
+      .send(`Sorry, couldn't find any talks matching that id. Try again!`);
+  } else {
+    res.json(filteredById);
+  }
 });
 
-// SPEAKERS
+// ALL SPEAKERS ---------------------------------------------------------
 app.get('/speakers', (req, res) => {
   const allSpeakers = data.map((talk) => talk.main_speaker);
   // New set to remove all dublicates
   const allSpeakersSet = Array.from(new Set(allSpeakers)).sort();
-  res.json(allSpeakersSet);
+
+  if (allSpeakersSet.length === 0) {
+    res.status(404).send(`Sorry, couldn't find any speakers. Try again!`);
+  } else {
+    res.json(allSpeakersSet);
+  }
 });
 
-// EVENTS
+// ALL EVENTS ---------------------------------------------------------
 app.get('/events', (req, res) => {
   const allEvents = data.map((talk) => talk.event);
   // New set to remove all dublicates
   const allEventsSet = Array.from(new Set(allEvents)).sort();
-  res.json(allEventsSet);
+
+  if (allEventsSet.length === 0) {
+    res.status(404).send(`Sorry, couldn't find any events. Try again!`);
+  } else {
+    res.json(allEventsSet);
+  }
 });
 
 // Start the server
