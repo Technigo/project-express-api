dc83726ff69afb0e3ee4ac15a1b2d75ca8652d3e
ClaireCaudwell <clairecaudwell@gmail.com>
2020-12-10 12:17:07 +0100

    Updated server.js: 1. Updated first endpoint books to books/search to imply that this endpoint can be used to search in the frontend. Returning books/search will return the entire array of data and then added 4 query parameters which can be used to search for books by specific author, title, language and average rating. 2. Also added som error responses if the user enters an author, title, language or rating that doesn't exsist to the endpoint. 3. Then created the second endpoint that returns one element from the data set. This is id and has the same idea with the error message if the id doesn't match with the ones in the array then an error will be returned.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express, { response } from 'express';
+import bodyParser, { json } from 'body-parser';
 import cors from 'cors';
 
-// Data I want to use to create end points for?
+// Data I want to use to create end points with
 import booksData from './data/books.json';
 
 //Instance of express
@@ -20,33 +20,78 @@ const port = process.env.PORT || 8081;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Start defining your routes here
-// Handler for what should happen when your routes are matched, res
-// First end point getting the entire booksData arrat
-app.get('/books', (req, res) => {
-  res.json(booksData);  
+/* ---- First endpoint ----
+Endpoint that returns the whole array of data and also for books based on author, title, language or average rating when using the query paramater. 
+Each if statement is checked and if true will either return the results or an error message as the array returned is empty (e.g. if the user has entered an incorrect author, title, langauage etc).
+
+Example routes:
+http://localhost:8081/books/search - Returns the whole data set/array
+http://localhost:8081/books/search?author=Rowling - Searches for books based on author
+http://localhost:8081/books/search?title=Harry - Searches for books based on title
+http://localhost:8081/books/search?language=eng  - Searches for books based on language
+eng, en-GB, en-US, spa, fre, ger, ara, por, grc, mul
+http://localhost:8081/books/search?averagerating=4.5 - Searches for books based on avergage rating.
+
+request, response is callback function
+*/
+app.get('/books/search', (request, response) => {
+  const {author, title, language, averagerating} = request.query;
+
+  if (author) {
+    const authorResults = booksData.filter((item) => item.authors.toLocaleLowerCase().includes(author.toLocaleLowerCase()));
+    if(authorResults.length === 0) {
+      response.status(404).json("Sorry that author doesn't exist.");
+    } else {
+      response.json(authorResults);
+    }
+  }
+  
+  if (title) {
+    const titleResults = booksData.filter((item) => item.title.toString().toLocaleLowerCase().includes(title.toLocaleLowerCase()));
+    if(titleResults.length === 0) {
+      response.status(404).json("Sorry that book title doesn't exist.");
+    } else {
+      response.json(titleResults);
+    }
+  }
+
+  if (language) {
+    const languageResults = booksData.filter((item) => item.language_code === language);
+    if(languageResults.length === 0) {
+      response.status(404).json("Sorry we don't have any books in that language.");
+    } else {
+      response.json(languageResults);
+    }
+  }
+
+  // "+" on averagerating turns the string in to a number.
+  if (averagerating) {
+    const aveRatingResults = booksData.filter((item) => item.average_rating === +averagerating);
+    if(aveRatingResults.length === 0) {
+      response.status(404).json("Sorry, that rating doesn't exist.");
+    } else {
+      response.json(aveRatingResults);
+    }
+  } 
+  response.json(booksData);
 });
 
-
-// Second endpoint getting a specific book based on the id added to the endpoint. :id is a variable that will be the value the user has specified on the endpoint e.g. 3.
-// req, res is callback function
-// Use params to get the id/value the user has specified and put that into the bookId variable. We then use that when finding the id in the array by comparing the bookID number in the array with the id the user has specified on the endpoint. 
-// "+" on bookId makes the string in to a number.
-// Then that bookID element from the erray is translated into json so it's readable on the endpoint.
-app.get('/books/:id', (req, res) => {
-  const bookId = req.params.id;
-  const specificBook = booksData.find((item) => item.bookID === +bookId);
-  res.json(specificBook);  
-});
-
-app.get('/books/:author', (req, res) => {
-  const bookAuthor = req.params.author;                
-  console.log({bookAuthor});
-  const booksByAuthor = booksData.filter((item) => item.authors.includes(bookAuthor));
-  res.json(booksByAuthor);  
+/* ---- Second endpoint ----
+Getting a specific book based on the id added to the endpoint. 
+:id is a variable that will be the value the user has specified on the endpoint e.g. 3. Use params to get the id/value the user has specified in endpoint and put that into the bookId variable. We then use that when finding the id in the array by comparing the bookID number in the array with the id the user has specified on the endpoint. 
+If statement for if the length of the specificBook variable/array is 0 (i.e. the author doesn't exist) then return the error message, else translate the bookID element from the array into json so the data it's readable on the endpoint.*/
+//http://localhost:8081/books/10
+app.get('/books/:id', (request, response) => {
+  const bookID  = request.params.id;
+  const specificBook = booksData.find(item => item.bookID === +bookID);
+  
+  if (!specificBook) {
+    response.status(404).json("ID doesn't correspond to any book in our API");    
+  }   
+  response.json(specificBook);    
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
+  console.log(`Server is running!`)
 });
