653b683008fde0d87d3f3a1d895ec02df70c25a1
Annika Melin <melin.anki>
2020-12-11 16:43:57 +0100

    improved readability of the code

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from 'cors'
 
 import booksData from './data/books'
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8083
 const app = express()
 
 // Added middlewares to enable cors and json body parsing
@@ -17,7 +17,6 @@ app.get('/', (request, response) => { // root endpoint
 
 app.get('/books', (request, response) => {
 
-  // how to structure this code in books?
   const language = request.query.language
   const title = request.query.title
   const author = request.query.author
@@ -25,10 +24,14 @@ app.get('/books', (request, response) => {
   const shortest = request.query.shortest
 
   if (author && language) {
-    const filteredBooksOnAuthorAndLanguage = booksData.filter(item => item.authors.toLowerCase().split("-").includes(author.toLowerCase()) || item.authors.toLowerCase().replace("-", " ").split(" ").includes(author.toLowerCase()) && item.language_code === language)
-    // where to do manipulations on data and input? reuse long manipulations? naming for data and input? manipulatedAuthorData? manipulatedAuthorInput?
+    const filteredBooksOnAuthorAndLanguage = booksData.filter(item => {
+      return (
+        item.authors.toLowerCase().split("-").includes(author.toLowerCase()) ||
+        item.authors.toLowerCase().replace("-", " ").split(" ").includes(author.toLowerCase()) && item.language_code === language
+      )
+    })
     if (filteredBooksOnAuthorAndLanguage.length > 0) {
-      return response.json(filteredBooksOnAuthorAndLanguage)
+      return response.json(filteredBooksOnAuthorAndLanguage.slice(0, 20))
     } else {
       return response.status(404).json({ error: "No such combination" })
     }
@@ -37,37 +40,38 @@ app.get('/books', (request, response) => {
   if (language) {
     const filteredBooksOnLanguage = booksData.filter(item => item.language_code === language)
     if (filteredBooksOnLanguage.length > 0) {
-      return response.json(filteredBooksOnLanguage)
+      return response.json(filteredBooksOnLanguage.slice(0, 20))
     } else if (filteredBooksOnLanguage.length === 0) {
       return response.status(404).json({ error: "What is that language?" })
     }
   }
 
   if (title) {
-    const filteredBooksOnTitle = booksData.filter(item => item.title.toString().toLowerCase().replace(/[^a-zA-Z0-9-']/g, " ").split(" ").includes(title.toLowerCase()))
-    // can only search for one word in a title right now, as it turns each word in a title into an array item
-
-    // This will make all words searchable, but then also include titles like "CliffsNotes on Tolstoy's Anna Karenina" when searching for "toy" - I do not want that
-    // item.title.toString().toLowerCase().includes(title.toLowerCase())
-
-    // Not working yet, but this is an attempt to make each title an array item and add it to the filter above with ||, to have user being able to search also on full title
-    // Object.entries(item.title.toString().toLowerCase().includes(title.toLowerCase()))
+    const filteredBooksOnTitle = booksData.filter(item => {
+      return (
+        item.title.toString().toLowerCase().replace(/[^a-zA-Z0-9-']/g, " ").split(" ").includes(title.toLowerCase()) || // matches against one word in the title
+        item.title.toString().toLowerCase().split().includes(title.toLowerCase())) // matches against full title
+    })
 
     // Not working, trying regex to have an exact match of the search word/words - this would be the best solution I think:
     // item.title.toString().toLowerCase().replace(/[^a-zA-Z0-9-']/g, " ").split(" ").match(`/^${title.toLowerCase()}$/`)
     // item.title.toString().toLowerCase().replace(/[^a-zA-Z0-9-']/g, " ").match(`/^\s${title.toLowerCase()}\s$/`))
 
     if (filteredBooksOnTitle.length > 0) {
-      return response.json(filteredBooksOnTitle)
+      return response.json(filteredBooksOnTitle.slice(0, 20))
     } else if (filteredBooksOnTitle.length === 0) {
       return response.status(404).json({ error: "No such title" })
     }
   }
 
   if (author) {
-    const filteredBooksOnAuthor = booksData.filter(item => item.authors.toLowerCase().split("-").includes(author.toLowerCase()) || item.authors.toLowerCase().replace("-", " ").split(" ").includes(author.toLowerCase()))
+    const filteredBooksOnAuthor = booksData.filter(item => {
+      return (
+        item.authors.toLowerCase().replace("-", " ").split(" ").includes(author.toLowerCase())) || // matches against one name in the author name
+        item.authors.toLowerCase().split("-").includes(author.toLowerCase()) // matches against full author name
+    })
     if (filteredBooksOnAuthor.length > 0) {
-      return response.json(filteredBooksOnAuthor)
+      return response.json(filteredBooksOnAuthor.slice(0, 20))
     } else if (filteredBooksOnAuthor.length === 0) {
       return response.status(404).json({ error: "No such author" })
     }
@@ -96,7 +100,7 @@ app.get('/books', (request, response) => {
   }
 
   else {
-    return response.json(booksData.slice(0, 20)) // some sorting?
+    return response.json(booksData.slice(0, 20))
   }
 })
 
@@ -138,3 +142,10 @@ app.get('/books/:id/authors', (request, response) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
+
+
+
+
+
+
+
