7e9ab7d59bf1b05621c6b9405ae8a30bf6dd7ef6
Annika Melin <melin.anki>
2020-12-11 12:47:07 +0100

    updated title filter

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from 'cors'
 
 import booksData from './data/books'
 
-const port = process.env.PORT || 8085
+const port = process.env.PORT || 8080
 const app = express()
 
 // Added middlewares to enable cors and json body parsing
@@ -17,19 +17,20 @@ app.get('/', (request, response) => { // root endpoint
 
 app.get('/books', (request, response) => {
 
-  // should all be delcared here, or just on top of their conditional to make the code more efficient?
+  // how to structure this code in books?
   const language = request.query.language
   const title = request.query.title
   const author = request.query.author
   const toprated = request.query.toprated
   const shortest = request.query.shortest
 
-  if (author && language) { // not working
-    const filteredBooksOnAuthorAndLanguage = booksData.filter(item => item.authors.toLowerCase().replace("-", " ").split(" ").includes(author.toLowerCase()) && item.language_code === language)
+  if (author && language) {
+    const filteredBooksOnAuthorAndLanguage = booksData.filter(item => item.authors.toLowerCase().split("-").includes(author.toLowerCase()) || item.authors.toLowerCase().replace("-", " ").split(" ").includes(author.toLowerCase()) && item.language_code === language)
+    // where to do manipulations on data and input? reuse long manipulations? naming for data and input? manipulatedAuthorData? manipulatedAuthorInput?
     if (filteredBooksOnAuthorAndLanguage.length > 0) {
       return response.json(filteredBooksOnAuthorAndLanguage)
     } else {
-      return response.status(404).json({ "error": "No such combination" })
+      return response.status(404).json({ error: "No such combination" })
     }
   }
 
@@ -38,31 +39,43 @@ app.get('/books', (request, response) => {
     if (filteredBooksOnLanguage.length > 0) {
       return response.json(filteredBooksOnLanguage)
     } else if (filteredBooksOnLanguage.length === 0) {
-      return response.status(404).json({ "error": "What is that language?" })
+      return response.status(404).json({ error: "What is that language?" })
     }
   }
 
   if (title) {
-    const filteredBooksOnTitle = booksData.filter(item => item.title.toString().toLowerCase().replace(/[^a-zA-Z0-9-']/g, " ").split(" ").includes(title.toLowerCase())) // my first regex!! 
+    const filteredBooksOnTitle = booksData.filter(item => item.title.toString().toLowerCase().replace(/[^a-zA-Z0-9-']/g, " ").split(" ").includes(title.toLowerCase()))
+    // can only search for one word in a title right now, as it turns each word in a title into an array item
+
+    // This will make all words searchable, but then also include titles like "CliffsNotes on Tolstoy's Anna Karenina" when searching for "toy" - I do not want that
+    // item.title.toString().toLowerCase().includes(title.toLowerCase())
+
+    // Not working yet, but this is an attempt to make each title an array item and add it to the filter above with ||, to have user being able to search also on full title
+    // Object.entries(item.title.toString().toLowerCase().includes(title.toLowerCase()))
+
+    // Not working, trying regex to have an exact match of the search word/words - this would be the best solution I think:
+    // item.title.toString().toLowerCase().replace(/[^a-zA-Z0-9-']/g, " ").split(" ").match(`/^${title.toLowerCase()}$/`)
+    // item.title.toString().toLowerCase().replace(/[^a-zA-Z0-9-']/g, " ").match(`/^\s${title.toLowerCase()}\s$/`))
+
     if (filteredBooksOnTitle.length > 0) {
       return response.json(filteredBooksOnTitle)
     } else if (filteredBooksOnTitle.length === 0) {
-      return response.status(404).json({ "error": "No such title" })
+      return response.status(404).json({ error: "No such title" })
     }
   }
 
   if (author) {
-    const filteredBooksOnAuthor = booksData.filter(item => item.authors.toLowerCase().replace("-", " ").split(" ").includes(author.toLowerCase()))
+    const filteredBooksOnAuthor = booksData.filter(item => item.authors.toLowerCase().split("-").includes(author.toLowerCase()) || item.authors.toLowerCase().replace("-", " ").split(" ").includes(author.toLowerCase()))
     if (filteredBooksOnAuthor.length > 0) {
       return response.json(filteredBooksOnAuthor)
     } else if (filteredBooksOnAuthor.length === 0) {
-      return response.status.json({ "error": "No such author" })
+      return response.status(404).json({ error: "No such author" })
     }
   }
 
   if (toprated) {
     if (+toprated > booksData.length) {
-      return response.status.json({ "error": "That is just too many books" })
+      return response.status(404).json({ error: "That is just too many books" })
     } else {
       const sortedBooksOnRating = booksData.sort(function (a, b) { return b.average_rating - a.average_rating })
       const topratedBooks = sortedBooksOnRating.slice(0, toprated)
@@ -74,7 +87,7 @@ app.get('/books', (request, response) => {
     const minPages = 20 // assuming pages less than 20 is an incorrect value
     const filteredBooksOnPages = booksData.filter(item => item.num_pages >= minPages)
     if (+shortest > filteredBooksOnPages.length) {
-      return response.status(404).json({ "error": "That is just too many books" })
+      return response.status(404).json({ error: "That is just too many books" })
     } else {
       const sortedBooksOnPages = filteredBooksOnPages.sort(function (a, b) { return a.num_pages - b.num_pages })
       const shortestBooks = sortedBooksOnPages.slice(0, shortest)
@@ -83,7 +96,7 @@ app.get('/books', (request, response) => {
   }
 
   else {
-    return response.json(booksData)
+    return response.json(booksData.slice(0, 20)) // some sorting?
   }
 })
 
@@ -93,7 +106,7 @@ app.get('/books/:id', (request, response) => {
   if (book) {
     return response.json(book)
   } else {
-    return response.status(404).json({ "error": "No such book" })
+    return response.status(404).json({ error: "No such book" })
   }
 })
 
@@ -105,7 +118,7 @@ app.get('/books/:id/title', (request, response) => {
   if (book) {
     return response.json(book.title)
   } else {
-    return response.status(404).json({ "error": "No such book" })
+    return response.status(404).json({ error: "No such book" })
   }
 })
 
@@ -117,7 +130,7 @@ app.get('/books/:id/authors', (request, response) => {
   if (book) {
     return response.json(book.authors.split("-"))
   } else {
-    return response.status(404).json({ "error": "No such book" })
+    return response.status(404).json({ error: "No such book" })
   }
 })
 
