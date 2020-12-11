a43f41f9a99a0075d27a67db4802ead22a7e9c80
Liza Minicangit config --global user.email liza.minican@gmail.com <liza.minican@gmail.com>
2020-12-11 10:48:26 +0100

    added 3 plural result endpoint and 1 single result

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
-import express from 'express'
+import express, { response } from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import avocadoSalesData from './data/avocado-sales.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+
+
+//console.log(avocadoSalesData.length);
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -22,10 +14,72 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+console.log("this is node working")
+
+
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+app.get('/', (request, response) => {
+  response.send('Hello, welcome to my api! Find some information about avocado sales here. ')
 })
+
+app.get('/sales', (request, response) => {
+  const { date } = request.query;
+  const { region } = request.query;
+  const { id } = request.query;
+
+  if (date) {
+    const salesByDate = avocadoSalesData.filter((item) => item.date === date);
+    response.json(salesByDate); 
+  } else if (region) {
+    const salesByRegion = avocadoSalesData.filter((item) => item.region === region);
+    response.json(salesByRegion); 
+  } else {
+    response.json(avocadoSalesData);
+  }
+});
+
+app.get('/sales/:region/ids/:id', (request,response) => {
+const {region, id} = request.params;
+const sale = avocadoSalesData.find(
+  (sale) => sale.region === region && +sale.id === +id
+);
+response.json(sale);
+});
+
+// app.get('/sales/:region/:date', (request,response) => {
+
+// });
+
+//Dummy endpoint - red level
+// app.get('/sales/:xlargebagssold', (request,response) => {
+// //This will return smth
+//response.send();
+// });
+
+
+//sales/region/id unique 
+//sales/averageprice asc
+//sales/averageprice_low <1
+//sales/averageprice_high >1
+
+// app.get('/sales/:region',(request,response) => {
+//   const region = request.params;
+//   const salesByRegion = avocadoSalesData.filter((item) => item.region === region);
+//   response.json(salesByRegion);
+// })
+// app.get('/region/:region',(request,response) => {
+//   const region = req.params.region;
+//   const salesByRegion = avocadoSalesData.filter((item) => item.region === region);
+//   response.json(salesByRegion);
+// })
+
+// app.get('/sorted_by_price', (req, res) => {
+//   const price = req.query.price;
+//   const sortedByPrice = avocadoSalesData.sort(function (a, b) { return b.price - a.price})
+//   res.json(sortedByPrice);
+// })
+
+// full-date       = date-fullyear "-" date-month "-" date-mday
 
 // Start the server
 app.listen(port, () => {
