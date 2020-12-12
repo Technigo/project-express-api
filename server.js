ee152a570c43e24855fed0b035518b03d4bf8c37
Hanna Skeppe <hannaskeppe@gmail.com>
2020-12-12 23:24:41 +0100

    Continued working on routes and query params

--- server.js
+++ server.js
@@ -33,6 +33,8 @@ const ERROR_MESSAGE_LANGUAGE_NOT_FOUND = { error: 'No books with that language-c
 app.use(cors())
 app.use(bodyParser.json())
 
+const myEndpoints = require('express-list-endpoints')
+
 // Start defining your routes here (endpoints)
 // req is incoming(we don't change it), the res is outgoing(we do change it)
 app.get('/', (req, res) => {
@@ -42,17 +44,67 @@ app.get('/', (req, res) => {
   //-Complicated operations
   //-Data lookup
   //-Third party API-requests
-
+  if (!res) {
+    res.status(404).json({ error : 'Error. No data found'})
+  }
+  res.send(myEndpoints(app))
   // simple: send something back without checking the request:
-  res.send('Hello world')
+  //res.send('Hello world')
 })
 
+// BOOKS-ENDPOINT(WITH PAGES QUERY-PARAM)
+//Add paging using query-params below (lecture no. 2 last year)
+//res.send or res.json??? What to use?
+//What's the difference of writing res.send(something) and res.json(something)?
+//--> https://medium.com/gist-for-js/use-of-res-json-vs-res-send-vs-res-end-in-express-b50688c0cddf
 //Gets all the books from books.json:
 //http://localhost:8080/books
 app.get('/books', (req, res) => {
-  res.json(booksData)
+  //res.json(booksData)
+  const { author, title } = req.query;
+  let booksList = booksData;
+  // QUERY-PARAMETERS:
+  // -Page (minValue 0, default 0): number to indicate what page to return.
+  // -PageSize (default: 20) : a number indicating how many results per page
+  // usage: localhost:8080/?page=2
+  const page = req.query.page ?? 0;
+  //console.log(`page = ${page}`)
+  // We can define pageSize like: http://localhost:8080/books/?page=49&pageSize=10
+  const pageSize = req.query.pageSize ?? 20;//The query param is a string
+  const totalNumberOfBooks = booksData.length;
+  const firstPageIndex = 0;
+  const startIndex = page * pageSize;
+  const endIndex = startIndex + +pageSize; // converts pageSize (string) to a number
+  //Creating pages using slice:
+  const bookPage = booksData.slice(startIndex, endIndex);
+  const returnObject = { 
+    totalNumberOfBooks: totalNumberOfBooks,
+    pageSize: pageSize,
+    page: page,
+    firstPageIndex: firstPageIndex,
+    lastPageIndex: parseInt(booksData.length/pageSize), 
+    results: bookPage 
+  };
+
+  // Se lecture https://technigo.wistia.com/medias/2o4ta7pwhd
+  // @44 mins forward about filtering results. How would I do that?
+  // Van's quich pseudo-example:
+
+  // let results = allResults;
+  // if (somequeryParameter) {
+  // results = results.filter(...something else based on one/more of query parameter)
+  //} else if (anotherqueryParameter) {
+    // results = results.filter(...something else based on one/more of query parameter)
+  //}
+
+  if (!page || !pageSize) {
+    res.json(booksData)
+  }
+  res.json(returnObject)
 })
 
+// LANGUAGE-ENDPOINT.
+// According to Maks friday lecture: language should be query param??
 // Gets books in a specified language (by language-code):
 //http://localhost:8080/books/language/fre 
 // language_codes: eng, fre, en-US, spa, en-GB, mul, ger
@@ -69,7 +121,7 @@ app.get('/books/language/:language', (req, res) => {
   res.status(404).json(ERROR_MESSAGE_LANGUAGE_NOT_FOUND)
 })
 
-//Get a single book by id:
+//ID-ENDPOINT. Get a single book by id:
 //books/id for example "id": 154, http://localhost:8080/books/id/154
 app.get('/books/id/:id', (req, res) => {
   const id = req.params.id
@@ -81,6 +133,7 @@ app.get('/books/id/:id', (req, res) => {
   res.json(singleBookId)
 })
 
+//ISBN-ENDPOINT.
 //Return single book by isbn/isbn13:
 //path: http://localhost:8080/books/isbn/"isbn"
 //OR: http://localhost:8080/books/isbn/"isbn13"
@@ -102,6 +155,7 @@ app.get('/books/isbn/:isbn', (req, res) => {
 
 //Search for a specific book title
 
+//BOOKS-BY-AUTHOR-ENDPOINT.
 //Get all books by a specific author ("authors")
 // maybe this should be a query param instead as I'm using includes-method?
 app.get('/books/author/:author', (req, res) => {
@@ -118,6 +172,13 @@ app.get('/books/author/:author', (req, res) => {
     }
   }
 })
+
+// Dummy endpoints:
+
+//Post request to add a book to the json
+
+// Put request to update average rating on book
+
 
 // Start the server
 app.listen(port, () => {
