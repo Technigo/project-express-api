9eca2f86dd9b30976a7ac95be646c8b7bfe4ffe8
Joel Harde <joelharde@Joels-MacBook-Pro.local>
2020-12-12 17:37:12 +0100

    Added error message when results are not found

--- server.js
+++ server.js
@@ -2,21 +2,10 @@ import express, { request, response } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-//
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from "./data/books.json";
-
 import data from "./data/netflix-titles.json";
-// import topMusicData from './data/top-music.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be
-// overridden when starting the server. For example:
-//
-
-const ERROR_SHOW_NOT_FOUND = { error: "No ranking results were found." };
+// Variable that will show an error message when no results are found
+const ERROR_SHOW_NOT_FOUND = { error: "No results were found." };
 
 // RESTful routes to the API
 
@@ -50,7 +39,11 @@ app.get("/shows/movies", (req, res) => {
 app.get("/shows/movies/:movies", (req, res) => {
   const { movies } = req.params;
   const movie = data.filter((item) => item.title == movies);
-  res.json(movie);
+  if (movie.length === 0) {
+    res.status(404).json(ERROR_SHOW_NOT_FOUND);
+  } else {
+    res.json(movie);
+  }
 });
 
 // Route to list all the tv shows in the show array
@@ -63,32 +56,34 @@ app.get("/shows/tvshows", (req, res) => {
 app.get("/shows/tvshows/:tvshows", (req, res) => {
   const { tvshows } = req.params;
   const tvShow = data.filter((item) => item.title == tvshows);
-  res.json(tvShow);
+  if (tvShow.length === 0) {
+    res.status(404).json(ERROR_SHOW_NOT_FOUND);
+  } else {
+    res.json(tvShow);
+  }
 });
 
 // This route makes it possible to search a specific country and list all shows from that country
 app.get("/shows/country/:country", (req, res) => {
   const { country } = req.params;
   const showCountry = data.filter((item) => item.country == country);
-  res.json(showCountry);
+  if (showCountry.length === 0) {
+    res.status(404).json(ERROR_SHOW_NOT_FOUND);
+  } else {
+    res.json(showCountry);
+  }
 });
 
 // This route makes it possible to search on the id and return a single object from the array
 app.get("/shows/:id", (req, res) => {
-  //let shows = data;
-
   const { id } = req.params;
   const showFound = data.find((show) => show.show_id === +id);
-  if (data) {
-    res.json(showFound);
-  } else {
+  if (showFound === false) {
     res.status(404).json(ERROR_SHOW_NOT_FOUND);
+  } else {
+    res.json(showFound);
   }
-  // const showId = data.filter((item) => item.show_id === +id);
-  // if (data) {
-  //   res.json(data);
-  // } else res.status(404).json({ error: `show id ${id} not found` });
-  //res.json(showId);
+
   console.log(`Req.params: ${JSON.stringify(req.params)}`);
 });
 
