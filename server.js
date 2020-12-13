d4932c31fc370fe8de51a15f3bf607b90c2b178b
Nadia Mazzoni <Nadia@Nadias-MBP.lan>
2020-12-13 13:40:18 +0100

    4th commit: added logic to queries, added suggested endpoints

--- server.js
+++ server.js
@@ -28,15 +28,15 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Api endpoints: /shows && /shows/:id && /shows/year/:year')
+  res.send('Api endpoints: /shows && /shows/:id && /shows/year/:year && ?type=movie || ?listed= Dramas')
 })
 
-//the entire array of objects / http://localhost:8080/shows/
+//the entire array of objects / http://localhost:8081/shows/
 app.get('/shows', (req, res) => {
   res.json(netflixData);  
 })
 
-//point to single object id / http://localhost:8080/shows/81193313
+//point to single object id / http://localhost:8081/shows/81193313
 app.get('/shows/:id/', (req, res) => {
   const { id } = req.params;
 
@@ -52,7 +52,8 @@ app.get('/shows/:id/', (req, res) => {
 })
 
 
-//point to year and filter type for that specific year / http://localhost:8081/shows/year/2019?type=movie
+//point to year and search type of show or "listed as" for that specific year /
+// http://localhost:8081/shows/year/2019?type=movie http://localhost:8081/shows/year/2019?type=tv%20show need to write reg ex / http://localhost:8081/shows/year/2019?listed=dramas
 app.get('/shows/year/:year/', (req, res) => {
   const { year } = req.params;
   const { type, listed } = req.query;
@@ -69,7 +70,7 @@ app.get('/shows/year/:year/', (req, res) => {
 
     if(listed) {
       showReleaseYear = showReleaseYear.filter(
-        (item) => item.listed_in === listed)
+        (item) => item.listed_in.toLowerCase() === listed.toLowerCase())
     }
 
   if(year && showReleaseYear.length === 0) {
@@ -78,9 +79,25 @@ app.get('/shows/year/:year/', (req, res) => {
 
   if(type && showReleaseYear.lenght === 0) {
     res.status(404).json(ERROR_DATA_NOT_FOUND);
+  }
+
+    if(listed && showReleaseYear.lenght === 0) {
+      res.status(404).json(ERROR_DATA_NOT_FOUND);
+
   } else {
     res.json(showReleaseYear)
   }
+
+//   Suggested endpoints: point to country and search for director or cast
+//   app.get('/shows/country/:origin', (req, res) => {
+//     const { origin } = req.params;
+    // const { director, cast } = req.query;
+
+    //point to all shows and return director
+    // app.get('/shows/:director', (req, res) => {
+    //   const { director } = req.params;
+    // })
+  
 });
 
 // Start the server
