cdeb581e36e012c2d2cdcb2fb4fe11f10d8beadc
Claudia Kjerrgren <ckjerrgren@gmail.com>
2020-12-13 16:46:15 +0100

    added filters

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 //My data for the Golden Globe endpoints  
 import goldenGlobes from './data/golden-globes.json';
 
-
 // For no results 
-const ERROR_NOMINEES_NOT_FOUND = {error : 'No nominee results found'}
+const ERROR_NOMINEES_NOT_FOUND = {error : 'No results found'}
 
-
-// This is where i create the endpoints 
 const app = express();
+ 
+// middlewares to anable cors and json nody parsing 
+app.use(cors())
+app.use(bodyParser.json())
 
+
+// My routes 
 app.get('/', ( request, response) => {
   response.send("Hello internet");
 });
@@ -19,10 +24,11 @@ app.get('/goldenglobes' , (request, response) => {
   response.json(goldenGlobes);
 });
 
+ //get Golden Globe data based on name of nominee (movie or actor)
 app.get('/goldenglobes/:name' , (request, response) => {
   const {name} = request.params;
-  //get Golden Globe data based on name of nominee (movie or actor)
-  const filterdGoldenGlobes = goldenGlobes.filter(goldenGlobes => goldenGlobes.nominee === name);
+  const filterdGoldenGlobes = goldenGlobes.filter(
+    goldenGlobes => goldenGlobes.nominee === name);
 
   if(filterdGoldenGlobes.length === 0) {
     response.status(404).json(ERROR_NOMINEES_NOT_FOUND);
@@ -31,19 +37,41 @@ app.get('/goldenglobes/:name' , (request, response) => {
   }
 });
 
-//who won a specific category a specifiic year 
-app.get('/goldenglobes/category/2009/win' , (request, response) => {
-  response.json(goldenGlobes);
+
+//Checks is a specific person or film  was nominated a specific year
+//Not great, because I think actors can be nominateed in several categorys 
+app.get('/goldenglobes/:name/:year', (request, response) => {
+  const {year, name} = request.params;
+  const nominatedYear = goldenGlobes.find(
+    (goldenGlobes) => +goldenGlobes.year_film === +year && goldenGlobes.nominee === name);
+
+    if(!nominatedYear) {
+      response.status(404).json(ERROR_NOMINEES_NOT_FOUND);
+    } else {
+      response.json(nominatedYear); 
+    }
 });
 
 
+// Filters on year 
+app.get('/years/:year', (request, response) => {
+  const year = request.params.year
+  const showWon = request.query.win
+  let goldenGlobesFromYear = goldenGlobes.filter((goldenGlobes) => goldenGlobes.year_award === +year)
 
-app.get('/goldenglobes/:year', ( request, response) => {
-  //Dummy response for year search - if you would like to filter it like that 
-});
+  if (showWon) {
+    goldenGlobesFromYear = goldenGlobesFromYear.filter((goldenGlobes) => goldenGlobes.win)
 
- // My port <3 
+  }
+
+  response.json(goldenGlobesFromYear)
+})
+
+// Filters on winners 
+
+
+
+
 app.listen(8080, () => {
-  console.log("the server is runnin");
-
+  console.log('Its all working')
 });
