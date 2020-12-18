291a2adceced5a07e15391cf57bc608595a73a0a
Annika Melin <melin.anki>
2020-12-18 17:05:21 +0100

    removed comments

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from 'cors'
 
 import booksData from './data/books'
 
-const port = process.env.PORT || 8087
+const port = process.env.PORT || 8086
 
 const app = express()
 
@@ -12,7 +12,7 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/', (request, response) => { // root endpoint
+app.get('/', (request, response) => {
   response.send("Welcome to Annika's books API")
 })
 
@@ -24,10 +24,54 @@ app.get('/books', (request, response) => {
   const toprated = request.query.toprated
   const shortest = request.query.shortest
 
-  if (author && language) {
+  if (title) {
+    const filteredBooksOnTitle = booksData.filter(item => {
+      return (
+        item.title.toString().toLowerCase().replace(/[^a-zA-Z0-9'-]/g, " ").split(" ").includes(title.toLowerCase()) || // will match against individual words in titel
+        item.title.toString().toLowerCase().split().includes(title.toLowerCase()) // will match against full title
+        // could use insteaad: RegExp("\\b" + title.toLowerCase() + "\\b").test(item.title.toString().toLowerCase())
+      )
+    })
+    if (filteredBooksOnTitle.length > 0) {
+      return response.json(filteredBooksOnTitle.slice(0, 20))
+    } else if (filteredBooksOnTitle.length === 0) {
+      return response.status(404).json({ error: "No such title" })
+    }
+  }
+
+  else if (author && !language) {
+    const filteredBooksOnAuthor = booksData.filter(item => {
+      return (
+        item.authors.toLowerCase().replace("-", " ").split(" ").includes(author.toLowerCase()) || // will match against one name in author name
+        item.authors.toLowerCase().split("-").includes(author.toLowerCase()) // will match against full author name
+        // could use instead: RegExp("\\b" + author.toLowerCase() + "\\b").test(item.authors.toLowerCase())
+      )
+    })
+    if (filteredBooksOnAuthor.length > 0) {
+      return response.json(filteredBooksOnAuthor.slice(0, 20))
+    } else if (filteredBooksOnAuthor.length === 0) {
+      return response.status(404).json({ error: "No such author" })
+    }
+  }
+
+  else if (language && !author) {
+    const filteredBooksOnLanguage = booksData.filter(item => {
+      return (item.language_code === language)
+    })
+    if (filteredBooksOnLanguage.length > 0) {
+      return response.json(filteredBooksOnLanguage.slice(0, 20))
+    } else if (filteredBooksOnLanguage.length === 0) {
+      return response.status(404).json({ error: "What is that language?" })
+    }
+  }
+
+  else if (author && language) {
     const filteredBooksOnAuthorAndLanguage = booksData.filter(item => {
       return (
-        RegExp("\\b" + author.toLowerCase() + "\\b").test(item.authors.toLowerCase())
+        (item.authors.toLowerCase().replace("-", " ").split(" ").includes(author.toLowerCase()) // will match against one name in author name
+          ||
+          item.authors.toLowerCase().split("-").includes(author.toLowerCase()))  // will match against full author name
+        // could use instead: RegExp("\\b" + author.toLowerCase() + "\\b").test(item.authors.toLowerCase())
         &&
         item.language_code === language
       )
@@ -39,44 +83,8 @@ app.get('/books', (request, response) => {
     }
   }
 
-  if (language) {
-    const filteredBooksOnLanguage = booksData.filter(item => {
-      return (item.language_code === language)
-    })
-    if (filteredBooksOnLanguage.length > 0) {
-      return response.json(filteredBooksOnLanguage.slice(0, 20))
-    } else if (filteredBooksOnLanguage.length === 0) {
-      return response.status(404).json({ error: "What is that language?" })
-    }
-  }
 
-  if (title) {
-    const filteredBooksOnTitle = booksData.filter(item => {
-      return (
-        RegExp("\\b" + title.toLowerCase() + "\\b").test(item.title.toString().toLowerCase())
-      )
-    })
-    if (filteredBooksOnTitle.length > 0) {
-      return response.json(filteredBooksOnTitle.slice(0, 20))
-    } else if (filteredBooksOnTitle.length === 0) {
-      return response.status(404).json({ error: "No such title" })
-    }
-  }
-
-  if (author) {
-    const filteredBooksOnAuthor = booksData.filter(item => {
-      return (
-        RegExp("\\b" + author.toLowerCase() + "\\b").test(item.authors.toLowerCase())
-      )
-    })
-    if (filteredBooksOnAuthor.length > 0) {
-      return response.json(filteredBooksOnAuthor.slice(0, 20))
-    } else if (filteredBooksOnAuthor.length === 0) {
-      return response.status(404).json({ error: "No such author" })
-    }
-  }
-
-  if (toprated) {
+  else if (toprated) {
     if (+toprated > booksData.length) {
       return response.status(404).json({ error: "That is just too many books" })
     } else {
@@ -88,8 +96,8 @@ app.get('/books', (request, response) => {
     }
   }
 
-  if (shortest) {
-    const minPages = 20 // assuming pages less than 20 is an incorrect value
+  else if (shortest) {
+    const minPages = 20 // business rule: pages less than 20 is an incorrect value
     const filteredBooksOnPages = booksData.filter(item => {
       return (item.num_pages >= minPages)
     })
