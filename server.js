46b20bc7d8db570cdca897678b3a2ec42038947e
AnnaHellkvist <51534839+AnnaHellkvist@users.noreply.github.com>
2020-12-22 14:40:48 +0100

    added queries in /books endpoint

--- server.js
+++ server.js
@@ -3,15 +3,6 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import booksData from './data/books.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -31,7 +22,33 @@ app.get('/', (request, response) => {
 
 // retrieves the whole books array
 app.get('/books', (request, response) => {
-  response.json(booksData);
+
+  const { author, toplist, rating, language } = request.query;
+
+  if (author) {
+    const booksByAuthor = booksData.filter((book) => book.authors === author)
+    response.json(booksByAuthor);
+
+  } else if (toplist) {
+    const sortedBooks = [...booksData];
+    sortedBooks.sort((a, b) => b.average_rating - a.average_rating)
+    const topList = sortedBooks.slice(0, toplist);
+
+    response.json(topList);
+
+  } else if (rating) {
+    if (rating < 5) {
+      const filteredBooks = booksData.filter((book) => Math.floor(book.average_rating) === +rating);
+      response.json(filteredBooks)
+    } else {
+      response.json({ error: 'No books with that rating were found' })
+    }
+  } else if (language) {
+    const filteredLanguage = booksData.filter((book) => book.language_code === language);
+    response.json(filteredLanguage);
+  } else {
+    response.json(booksData);
+  }
 })
 
 //retrieves one book based on ID
@@ -43,44 +60,6 @@ app.get('/books/:id', (request, response) => {
 })
 
 //------------------------------------------------------------------------------------------------------
-// QUERIES
-
-// /authors?author=Bill Bryson returns books by a choosen author
-app.get('/authors', (request, response) => {
-  const { author } = request.query;
-  const booksByAuthor = booksData.filter((book) => book.authors === author)
-
-  response.json(booksByAuthor);
-})
-
-// /top-list?list=20 retrieves the 20 books with highest rating
-app.get('/top-list', (request, response) => {
-  const { list } = request.query;
-  const sortedBooks = [...booksData];
-  sortedBooks.sort((a, b) => b.average_rating - a.average_rating)
-  const topList = sortedBooks.slice(0, list);
-
-  response.json(topList);
-})
-
-// /ratings?rating=4 shows books with rating between 4-5, retrieves filtered array based on rating
-app.get('/ratings', (request, response) => {
-  const { rating } = request.query;
-
-  if (rating < 5) {
-    const filteredBooks = booksData.filter((book) => Math.floor(book.average_rating) === +rating);
-    response.json(filteredBooks)
-  } else {
-    response.json({ error: 'No books with that rating were found' })
-  }
-})
-
-//Query for language ex. /language/lang=en-US
-app.get('/language', (request, response) => {
-  const { lang } = request.query;
-  const filteredLanguage = booksData.filter((book) => book.language_code === lang);
-  response.json(filteredLanguage);
-})
 
 // empty/dummy endpoints
 app.get('/books/:year', (request, response) => {
