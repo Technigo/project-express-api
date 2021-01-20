3656c31efe1fa6a860e622c93fe0fcf6a1db3b47
nathalie <nathalie-nordstrom@hotmail.com>
2021-01-20 17:37:11 +0100

    new api

---
+++ data/data.js
@@ -0,0 +1,24 @@
+[
+    {
+        name: 'broccoli',
+        timestamp: 2020 - 01 - 20,
+        rate: 3
+    },
+    {
+        name: 'carrot',
+        timestamp: 2020 - 01 - 20,
+        rate: 4
+    },
+    {
+        name: 'apple',
+        timestamp: 2020 - 01 - 20,
+        rate: 2
+    },
+    {
+        name: 'peas',
+        timestamp: 2020 - 01 - 20,
+        rate: 5
+    }
+
+
+]
--- server.js
+++ server.js
@@ -1,31 +1,71 @@
-import express, { response } from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from 'express'
 
-import data from './data/netflix-titles.json'
+const app = express();
 
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const users = [
+  {id:1 , name: 'Bob' , age:34},
+  {id:2 , name: 'Abe' , age:45},
+  {id:3 , name: 'Fabbe' , age:12},
+  {id:4 , name: 'Kabbe' , age:93},
+];
 
-app.use(cors())
-app.use(bodyParser.json())
+app.get('/', (request, response) => {
+  response.send("Hello world")
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get('/users', (request, response) => {
+  const { name } = request.query;
+  if (name) {
+    const filteredUsers = users.filter((user) => user.name === name);
+    response.json(filteredUsers);
+  } else {
+    response.json(users);
+  }
+});
 
-app.get('/year', (req, res) => {
-  res.json(data)
-})
-app.get('/years/:year', (req, res) => {
-  const year = req.params.year
-  const releaseYear = data.filter((item) => item.release_year === +year)
+app.get('/users/:id', (request, response) => {
+  const { id } = (request.params);
+  const user = users.find((user) => user.id === +id)
+  response.json(user);
+});
 
-    res.json(releaseYear)
-})
+app.listen(8080, () => {
+  console.log("hello");
+});
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+
+
+
+
+// import bodyParser from 'body-parser'
+// import cors from 'cors'
+
+// import data from './data/netflix-titles.json'
+
+// //   PORT=9000 npm start
+// const port = process.env.PORT || 8080
+// const app = express()
+
+// app.use(cors())
+// app.use(bodyParser.json())
+
+// app.get('/', (req, res) => {
+//   res.send('Hello world')
+// })
+
+// app.get('/year', (req, res) => {
+//   res.json(data)
+// })
+// app.get('/years/:year', (req, res) => {
+//   const year = req.params.year
+//   const releaseYear = data.filter((item) => item.release_year === +year)
+
+//     res.json(releaseYear)
+// })
+
+// // Start the server
+// app.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}`)
+// })
+
+
