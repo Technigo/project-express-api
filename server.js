d5113a013862b18862f11ae8e911899d8a22a045
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2021-03-25 13:09:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ import cors from 'cors'
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
-// 
+//
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
 // import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
