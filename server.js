0303f1132434521d586b5cc4401cf978252df956
sirihemsley <siri.hemsley@gmail.com>
2021-05-03 09:30:03 +0200

    Added some GET for the netflix data

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import data from './data/netflix-titles.json'
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -25,6 +26,29 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/shows', (req, res) => {
+  res.send(data)
+  // const showWon = req.query.won
+  // let nominationsFromYear = data.filter((item) => item.year_award === +year)
+
+  // if (showWon) {
+  //   nominationsFromYear = nominationsFromYear.filter((item) => item.win)
+    
+  // }
+})
+
+app.get('/shows/:country', (req, res) => {
+  const country = req.params
+  const showsFromCountry = data.filter((item) => item.country === country)
+  res.send(showsFromCountry)
+})
+
+app.get('/shows/:id', (req, res) => {
+  const id = req.params
+  const requestedShow = data.find((show) => show.show_id === +id)
+  res.send(requestedShow)
 })
 
 // Start the server
