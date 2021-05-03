f11152a433e285de2018a1aefa4b0f4e090f29f2
Mir Albertioni <fethullah.dincer@gmail.com>
2021-05-03 19:28:53 +0200

    work in progress

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ import cors from 'cors'
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // 
-// import goldenGlobesData from './data/golden-globes.json'
+import goldenGlobesData from './data/golden-globes.json'
+console.log(goldenGlobesData.length)
 // import avocadoSalesData from './data/avocado-sales.json'
 // import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
@@ -22,9 +23,29 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+// TO DO 
+// Du kan göra knappar så att ifall jag trycker på en knapp så får jag filmer i enlighet med år etc etc. att du ändå gör frontend delen
+
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/nominations', (req, res) => {
+  res.json(goldenGlobesData)
+})
+
+app.get('/year/:year', (req, res) => {
+  const year = req.params.year
+  const showWon = req.query.won
+  const nominationPerYear = goldenGlobesData.filter((item) => item.year_award === +year)
+
+  if(showWon) {
+    const onlyWon = nominationPerYear.filter((item) => item.win)
+    res.json(onlyWon)
+  }
+
+  res.json(nominationPerYear)
 })
 
 // Start the server
