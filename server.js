d05a70bbba0dbabeb8debc9ad73c6dc228aca2a0
Polkapetunian <70851837+Polkapetunian@users.noreply.github.com>
2021-05-03 20:27:15 +0200

    added data from kaggle.com, git 3 endpoints working: all plants, by id and by country

--- server.js
+++ server.js
@@ -1,30 +1,38 @@
-import express from 'express'
+import express, { response } from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import plantsData from './data/plants.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8999
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+// endpoint to get all power plants
+app.get('/nuclear-power-plants', (request, response) => {
+  response.json(plantsData)
+})
+//endpoint to get power plant by id
+app.get('/nuclear-power-plants/:id', (request, response) => {
+  const { id } = request.params
+  const plantById = plantsData.find (plant => plant.FID === +id)
+  if (!plantById) {
+    response.status(404).send(`There is no nuclear power plant with id number ${id} in the data!`)
+  }
+  response.json(plantById)
+})
+//endpoint to get power plant by country
+app.get('/nuclear-power-plants/countries/:country', (request, response) => {
+  const country = request.params.country
+  const plantsByCountry = plantsData.filter(plant => plant.Country === country)
+  response.json(plantsByCountry)
 })
 
 // Start the server
