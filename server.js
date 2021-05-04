3c18ad1b06b96dab65939e5314abc4e08e2e7097
tuttibalutti <emmajbartl@gmail.com>
2021-05-04 18:02:16 +0200

    created routes authors, ratings, pages

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import cors from 'cors'
 // 
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
+import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
@@ -26,6 +26,32 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+app.get('/books', (req, res) => {
+  res.json(booksData)
+})
+
+app.get('/authors', (req, res) => {
+  const authorsName = booksData.filter((book) => book.authors)
+  res.json(authorsName)
+})
+
+app.get('/ratings', (req, res) => {
+  const booksTopRated = booksData.filter((book) => book.average_rating >= 4.5)
+  booksTopRated.sort(function (a, b) {
+    return b.average_rating - a.average_rating
+  })
+  res.json(booksTopRated)
+})
+
+app.get('/pages', (req, res) => {
+  const booksUnderHundredPages = booksData.filter((book) => book.num_pages <= 100)
+  booksUnderHundredPages.sort(function (a, b) {
+    return a.num_pages - b.num_pages
+  })
+  res.json(booksUnderHundredPages)
+})
+
 
 // Start the server
 app.listen(port, () => {
