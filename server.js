fb00b2041548af48a042a003a2e1a368da81c03d
vladjnbykov <67795638+vladjnbykov@users.noreply.github.com>
2021-05-05 01:05:44 +0200

    testing first endpoint

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable */
+
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
@@ -8,7 +11,7 @@ import cors from 'cors'
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
 // import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
+import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
@@ -18,14 +21,29 @@ import cors from 'cors'
 const port = process.env.PORT || 8080
 const app = express()
 
+// console.log(booksData)
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+// app.get('/', (req, res) => {
+  // res.send('Hello world')
+//})
+
+app.get('/movies', (request, response) => {
+  
+  response.json(netflixData)
 })
+
+// endpoint to get one movie
+app.get('/movies/:id', (request, response) => {
+  const { id } = request.params
+  const movie = netflixData.find( movie => movie.show_id === +id)
+  response.json(movie)
+  
+})
+
 
 // Start the server
 app.listen(port, () => {
