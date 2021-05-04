9bda84def5fa3ba0a37bada5046657de30567afc
Pauan8 <pauline.j.andersson@vgregion.se>
2021-05-04 02:05:52 +0200

    adding message if there is no more pages with results

--- server.js
+++ server.js
@@ -16,13 +16,13 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.json(data);
+  res.json(data.slice(0, 100)); // on the homescreen 100 results shows
 });
 
 // slice depending on page query
-const sliceData = (array, num) => {
+const sliceData = (array, num, res) => {
   array = array.slice((num * 20), ((num + 1) * 20))
-  return array;
+  return array.length < 1 ? res.send("Sorry, no more pages") : array;
 }
 
 // function for routing
@@ -37,17 +37,11 @@ const routes = (name, array) => {
     array = data.filter((item) => {
       switch (name) {
         case "countries":
-          return (
-            item.country.toLowerCase().replace(/\s+/g, "") === countries
-          );
+          return (item.country.toLowerCase().replace(/\s+/g, "") === countries);
         case "types":
-          return (
-            item.type.toLowerCase().replace(/\s+/g, "") === types
-          );
+          return (item.type.toLowerCase().replace(/\s+/g, "") === types);
         case "title":
-          return (
-            item.title.toString().toLowerCase().replace(/\s+/g, "") === title
-          );
+          return (item.title.toString().toLowerCase().replace(/\s+/g, "") === title);
         default:
           return item.show_id === Number(id);
       }
@@ -56,18 +50,19 @@ const routes = (name, array) => {
     if (year) {
       array = array.filter((item) => item.release_year === Number(year));
     }
+
     if (genre) {
       array = array.filter((item) => item.listed_in.toLowerCase().includes(genre));
     }
 
     if (page) {
-      array = sliceData(array, Number(page)) // user can see more results with query page
+      array = sliceData(array, Number(page), res) // user can see more results with query page
     }
 
     // if no data is returned from filtering (no match with params/query) a message says so
     if (array.length === 0) {
       res.send(`
-        Sorry, no titles exist for this category 
+        Sorry, no more titles exist for this category 
         ${year ? `from year ${year}` : ""} 
         ${genre ? `in the genre ${genre}` : ""}
       `);
