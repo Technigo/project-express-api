0032f9a8c366bd7414ad408c6bf1564a345e52c2
Hannah Jesinkey <h.jesinkey@gmail.com>
2021-05-04 21:20:18 +0200

    2 working routes

--- server.js
+++ server.js
@@ -1,34 +1,41 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import netflixData from './data/netflix-titles.json';
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get('/movies', (req, res) => {
+  res.json(netflixData);
+});
+
+app.get('/movies/:title', (req, res) => {
+  const title = req.params.title;
+  console.log(title);
+  console.log(netflixData[0].title.toLowerCase());
+
+  // netflixData.forEach((item) => console.log(item.title.toLowerCase()));
+
+  let singleMovie = netflixData.filter(
+    (item) => item.title.toLowerCase() === title.toLowerCase()
+  );
+  console.log(singleMovie);
+  res.json(singleMovie);
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
