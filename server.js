fcebe52d1eebcc662ad0796ec8ac9cab68b598a9
annalenksjo <75678284+annalenksjo@users.noreply.github.com>
2021-05-05 11:19:08 +0200

    damians codealong

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
-import express from 'express'
+import express, { request } from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import booksData from './data/golden-globes.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+// PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -23,8 +15,26 @@ app.use(cors())
 app.use(bodyParser.json())
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+app.get('/', (request, response) => {
+  response.send('Hello worlds')
+})
+
+app.get('/nominations', (request, response) => {
+  response.json(booksData)
+})
+
+app.get('/year/:year', (request, response) => {
+  const year = request.params.year
+  const showWon = request.query.won
+  console.log(showWon)
+  //use let to be able to filter it again
+  let nominationsFromYear = booksData.filter((item) => item.year_award === +year)
+
+  if (showWon) {
+    nominationsFromYear = nominationsFromYear.filter((item => item.win))
+  }
+
+  response.json(nominationsFromYear)
 })
 
 // Start the server
