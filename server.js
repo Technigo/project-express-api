955f3268f242fe821ceadd90377bc9315f1a105b
annalenksjo <75678284+annalenksjo@users.noreply.github.com>
2021-05-05 20:56:20 +0200

    added some new end points

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-import express, { request } from 'express'
+import express, { request, response } from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
-import booksData from './data/golden-globes.json'
+import goldenGlobesData from './data/golden-globes.json'
+
+//Ideas: filtering on different decades like
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -19,22 +21,58 @@ app.get('/', (request, response) => {
   response.send('Hello worlds')
 })
 
+//The whole data set
 app.get('/nominations', (request, response) => {
-  response.json(booksData)
+  response.json(goldenGlobesData)
 })
 
-app.get('/year/:year', (request, response) => {
-  const year = request.params.year
+
+//Filter on a films award year & win true or false
+app.get('/awardyear/:awardYear', (request, response) => {
+  const awardYear = request.params.awardYear
+  //use query like ?won=true
   const showWon = request.query.won
-  console.log(showWon)
+  const showCategoryContains = request.query.categoryContains
   //use let to be able to filter it again
-  let nominationsFromYear = booksData.filter((item) => item.year_award === +year)
+  let nominationsFromYear = goldenGlobesData.filter((item) => item.year_award === +awardYear)
+
+  if (nominationsFromYear.length === 0) {
+    response.send(`There are no nominations from year ${awardYear} in the data file!`)
+  }
 
   if (showWon) {
     nominationsFromYear = nominationsFromYear.filter((item => item.win))
   }
 
+  //check if the category contains a specific word
+  // if (showCategoryContains != "") {
+  //   nominationsFromYear = nominationsFromYear.filter((item => item.category.includes(categoryContains)))
+  // }
+
   response.json(nominationsFromYear)
+})
+
+//Filter on film year with intervals
+app.get('/year/new_films', (request, response) => {
+
+  let newFilms = goldenGlobesData.filter((item => item.year_film > 2018))
+  response.json(newFilms)
+})
+
+app.get('/year/old_films', (request, response) => {
+  let oldFilms = goldenGlobesData.filter((item => item.year_film < 2011))
+  response.json(oldFilms)
+})
+
+//Filter on film title
+app.get('/films/:film', (request, response) => {
+  const film = request.params.film
+  let singleFilmNominations = goldenGlobesData.filter((item => item.film === film))
+
+  if (singleFilmNominations.length === 0) {
+    response.send(`The film title "${film}" does not exist in the data file!`)
+  }
+  response.json(singleFilmNominations)
 })
 
 // Start the server
