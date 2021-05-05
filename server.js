76e8de85ef1500834d8964b048fa87c857d541c3
Laima Duhovnaja <laima@macbook-air.mynet>
2021-05-05 17:53:42 +0200

    changed req query

--- server.js
+++ server.js
@@ -1,41 +1,43 @@
 import express from "express";
-import bodyParser from "body-parser";
 import cors from "cors";
+import listEndpoints from "express-list-endpoints";
 
-import topMusic from "./data/top-music.json";
+import songs from "./data/top-music.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Start defining your routes here
-app.get("/music", (req, res) => {
-  const { track_name } = req.query;
-  if (track_name) {
-    const filteredSongs = topMusic.filter(
-      (song) => song.trackName === track_name
-    );
+app.get("/", (req, res) => {
+  res.send(listEndpoints(songs));
+});
+
+app.get("/songs", (req, res) => {
+  const { artist_name } = req.query;
+  if (artist_name) {
+    const filteredSongs = songs.filter((song) => {
+      return (
+        song.artistName.toLowerCase().indexOf(artist_name.toLowerCase()) !== -1
+      );
+    });
     res.json(filteredSongs);
   } else {
-    res.json(topMusic);
+    res.json(songs);
   }
 });
 
-app.get("/music/:id", (req, res) => {
+app.get("/songs/:id", (req, res) => {
   const { id } = req.params;
-  const musics = topMusic.find((music) => music.id === +id);
+  const chosenSong = songs.find((song) => song.id === +id);
 
-  if (!musics) {
+  if (!chosenSong) {
     res.status(404).send(`No song with id number ${id}`);
   }
-  res.json(musics);
+  res.json(chosenSong);
 });
 
 // Start the server
