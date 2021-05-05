f4719763ceb1ad26c45527b60df465de23f530be
Your Name <isabellamarkskog@live.se>
2021-05-05 23:43:16 +0200

    Added two endpoints; title and top rating

--- server.js
+++ server.js
@@ -20,30 +20,44 @@ app.get('/books', (req, res) => {
   res.json(booksData);
 });
 
-// Books by author: path '/books/search'
-// Example path: '/books/search?author=J.K.Rowling'
-app.get('/books/search', (req, res) => {
-  const { author, title } = req.query; // maybe you can add more queries within the braces?
+// Books by author
+// Example path: '/books/authors?author=adams'
+app.get('/books/authors', (req, res) => {
+  const { author } = req.query; 
   const booksByAuthor = booksData.filter((book) => book.authors.toLowerCase().includes(author.toLowerCase()));
-  const booksByTitle = booksData.filter((bookTitle) => bookTitle.title.toLowerCase().includes(title.toLowerCase()));
 
   if (booksByAuthor.length === 0) {
     res.status(404).send(`Sorry, could not find any books by ${author}.`);
   }
 
+  res.json(booksByAuthor);
+});
+
+// Books by title
+// Example path: '/books/titles?title=bill'
+app.get('/books/titles', (req, res) => {
+  const { title } = req.query; 
+  const booksByTitle = booksData.filter((book) => book.title.toString().toLowerCase().includes(title.toLowerCase()));
+
   if (booksByTitle.length === 0) {
-    res.status(404).send(`Sorry, could not find any books with title ${title}.`);
+    res.status(404).send(`Sorry, could not find any books with the title "${title}".`);
   }
 
-  res.json(booksByAuthor);
   res.json(booksByTitle);
 });
 
-// Books by title: path '/books/search'
-// Example path: '/books/search?title='
-// app.get('/books/search')
+// Books by top rating
+// Example path: 'books/toprating?rating=4'
+app.get('/books/toprating', (req, res) => {
+  const { rating } = req.query;
+  const booksByTopRating = booksData.filter((book) => book.average_rating >= 4);
 
+  if (booksByTopRating.length === 0) {
+    res.status(404).send(`Sorry, could not find any books with the rating of ${rating}.`);
+  }
 
+  res.json(booksByTopRating);
+});
 
 // Book by id 
 // Example path: '/books/1' 
