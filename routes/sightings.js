2fe4f498f90cd5414293d5daf7556f96665bfc1c
Bellsibub <isabella.billgren@gmail.com>
2021-05-05 09:14:17 +0200

    added queries

--- routes/sightings.js
+++ routes/sightings.js
@@ -1,17 +1,21 @@
+// import moment from 'moment';
 import data from '../data/ufoSightings.json';
+import { filterBy } from '../utils/filters';
 
 export const list = (req, res) => {
+  let dataToSend = data;
   // queries
-  // srting
-  // orderby
+  dataToSend = filterBy(dataToSend, { ...req.query })
   // groupby
+  // srting
+  // orderby  
   res.send({
-    count: data.length,
-    items: data
+    count: dataToSend.length,
+    items: dataToSend
   });
 };
 export const view = (req, res) => {
   const { id } = req.params;
-  const item = data.find((d) => d.id === +id)
+  const item = data.find((d) => d.id === +id);
   res.send(item);
 };
---
+++ utils/filters.js
@@ -0,0 +1,28 @@
+import moment from 'moment';
+import { parseDate } from './dates';
+
+export const filterBy = (data, query) => {
+  // filter by year-like queries (only one!)
+  if ('year' in query) {
+    // filter by year
+    data = data.filter((item) => moment(parseDate(item.date)).year() === +query.year);
+  } else if ('yearRange' in query) {
+    const range = query.yearRange.split('-');
+    // filter by range of 2 year values
+    data = data.filter((item) => moment(parseDate(item.date)).isBetween(range[0], range[1]));
+  }
+
+  // filter by countries
+  if ('countries' in query) {
+    const countries = query.countries.split(',');
+    data = data.filter((item) => countries.includes(item.country));
+  }
+  
+  // filter by shape
+  if ('shapes' in query) {
+    const shapes = query.shapes.split(',');
+    data = data.filter((item) => shapes.includes(item.shape));
+  }
+
+  return data;
+};
