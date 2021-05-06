32bf532efc5dd1631ee8787a4be22bc209b45f7d
Erika Andersson Porath <erika.porath@hotmail.com>
2021-05-07 01:10:20 +0200

    cleaned up the code a little

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ import netflixData from './data/netflix-titles.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
-//
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
@@ -16,7 +15,7 @@ app.use(cors())
 app.use(bodyParser.json())
 
 
-// Start defining your routes here
+// == Defining routes ==
 
 //_ is a placeholder for req 
 app.get('/', (_, res) => {
@@ -77,7 +76,8 @@ app.get('/shows/id/:id', (req, res) => {
 })
 
 
-// Start the server
+// == Start the server ==
+
 app.listen(port, () => {
   // eslint-disable-next-line
   console.log(`Server running on http://localhost:${port}`)
