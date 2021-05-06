005e3ee4ee885fc32c70443f50912fc9a2da69ee
esteficodes <estefaniaquevedolusby@gmail.com>
2021-05-06 22:20:16 +0200

    api and endpoints

--- server.js
+++ server.js
@@ -1,34 +1,75 @@
-import express from 'express'
-import bodyParser from 'body-parser'
+import express from "express"
 import cors from 'cors'
+import bodyParser from 'body-parser'
+import booksData from './data/books.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
+//adding middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+//error messages
+
+const notFound = {
+  "code": 404,
+  "message": "Not Found",
+  "description": "Sorry, this resource does not exist."
+}
+
+//start defining routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-// Start the server
+//endpoint to get all books
+app.get('/books', (request, response) => {
+  const bookTitle = request.query.title
+  const author = request.query.author
+  let books = booksData.sort((a, b) => a.bookID - b.bookID)
+
+  if (author) {
+    books= books.filter((book) => book.authors.toLowerCase().includes(author)) //checks by author
+  }
+
+  if (bookTitle) {
+    books = books.filter((book) => typeof book.title === "string") //checks if book title is a string
+    books= books.filter((book) => book.title.toLowerCase().includes(bookTitle)) //
+  }
+
+  if (books.length === 0) {
+    res.status(404).json(notFound) //displays 404 message when no results
+  }
+
+  response.json(books)
+})
+
+//endpoint to get one book by id
+app.get('/books/:id', (request, response) => {
+  const id = +request.params.id
+  const bookById = booksData.find((book) => book.bookID === id) //checks Id
+
+  if (!bookById) {
+    response.status(404).json(notFound) //displays 404 error message 
+  }
+
+  response.json(bookById)
+})
+
+//endpoint to get the top 25 highest rated books with above 1000 ratings
+app.get("/top25", (request, res) => {
+  let highestRatingBooks = booksData.sort((a, b) => b.average_rating - a.average_rating) //to sort by highest rating
+
+  highestRatingBooks = highestRatingBooks.filter((book) => book.ratings_count >= 1000) //only results above 1000
+
+  const top25 = highestRatingBooks.slice(0, 25) //takes first 25
+  res.json(top25)
+})
+
+
+//start the server
 app.listen(port, () => {
-  // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
+  //eslint-disable-next-line
+ console.log(`Server 🚀 on http://localhost:${port}`)
 })
