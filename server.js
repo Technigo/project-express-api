7883aa34b65c7efaeeae292f0f970a5526da5b26
tuttibalutti <emmajbartl@gmail.com>
2021-05-06 23:56:01 +0200

    changed datasets to music, created track endpoint with query params to show either an array filtered by artistname or trackname. Added dummy endpoints, genre and id

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ import cors from 'cors'
 // 
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-import booksData from './data/books.json'
+//import booksData from './data/books.json'
+import listEndpoints from 'express-list-endpoints'
 // import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import topMusicData from './data/top-music.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -20,15 +21,90 @@ const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send(listEndpoints(app))
 })
 
-app.get('/books', (req, res) => {
+app.get('/top-music', (req, res) => {
+  res.json(topMusicData)
+})
+
+app.get('/top-music/:id', (req, res) => {
+  const { id } = req.params
+  const trackId = topMusicData.find(track => track.id === +id)
+
+  if (trackId) {
+    res.json({ data: trackId })   
+  } else {
+    res.status(404).json({ error: `Sorry, we didn't find id: ${id}` })
+  }
+
+  res.json(trackId)
+})
+
+app.get('/genre', (req, res) => {
+  const trackGenres = []
+  topMusicData.forEach(track => trackGenres.push(track.genre))
+
+  let uniqueGenres = [...new Set(trackGenres)]
+
+  res.json({ data: uniqueGenres })
+})
+
+app.get('/tracks', (req, res) => {
+  const { artistName } = req.query
+  const { trackName } = req.query
+
+  console.log(trackName)
+  
+  let filteredTracks = topMusicData
+
+  if (artistName) {
+    filteredTracks = filteredTracks.filter(track => {
+      const currentArtistName = track.artistName.toLowerCase()
+      return currentArtistName.includes(artistName.toLowerCase())
+    })
+  }
+
+  if (trackName) {
+    filteredTracks = filteredTracks.filter(track => {
+      const currentTrackName = track.trackName.toLowerCase()
+      return currentTrackName.includes(trackName.toLowerCase())
+    })
+  }
+
+  res.json({ data: filteredTracks })
+})
+
+app.get('/tracks/popular', (req, res) => {
+
+})
+
+app.get('/tracks/recommended', (req, res) => {
+  res.json('Wanna listen to recommended tracks? Coming soon!')
+})
+
+app.get('/tracks/trending', (req, res) => {
+  res.json("Wanna know the latest music trends? Hold on, we're working on it!" )
+})
+
+/*app.get('/books', (req, res) => {
   res.json(booksData)
+})
+
+app.get('/books/:isbn', (req, res) => {
+  const { isbn } = req.params
+  const findIsbn = booksData.find((book) => book.isbn === +isbn)
+
+  if (findIsbn) {
+    res.json({ data: findIsbn })   
+  } else {
+    res.status(404).json({ error: 'Not found' })
+  }
+   
 })
 
 app.get('/authors', (req, res) => {
@@ -50,7 +126,7 @@ app.get('/pages', (req, res) => {
     return a.num_pages - b.num_pages
   })
   res.json(booksUnderHundredPages)
-})
+})*/
 
 
 // Start the server
