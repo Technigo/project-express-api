b16f4732e5d4566bb074f884cd1d6a1ca2963f5e
Eleonor Frankenberg <elle@Eleonors-MacBook-Air.local>
2021-05-06 22:24:42 +0200

    made it possible to filter books below 500 pages

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Express API Project
 
-Replace this readme with your own information about your project. 
+Replace this readme with your own information about your project.
 
 Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
 
 ## The problem
+
+jag hade problem att göra ett error message om det inte fanns någon författare
 
 Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
 
--- server.js
+++ server.js
@@ -1,39 +1,68 @@
 import express from 'express'
-// import bodyParser from 'body-parser'
-// import cors from 'cors'
+ //import bodyParser from 'body-parser'
+ import cors from 'cors'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
+
  import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
-// app.use(cors())
-// app.use(bodyParser.json())
+app.use(cors())
+app.use(express.json())
 
-// Endpoint to get all the books
+// get all the books
 app.get('/books', (req, res) => {
-  const { author } = req.query
-
-  if(author) {
-    const authorList = booksData.filter(book => book.authors.toLowerCase().includes(author.toLowerCase()))
-    res.json(authorList)
-  }
   res.json(booksData)
+});
+
+
+//get books by author /books/authors?author= ...
+app.get('/books/authors', (req, res) => {
+  const { author } = req.query
+  const authorList = booksData.filter(book => book.authors.toLowerCase().includes(author.toLowerCase()))
+
+  if(authorList.length === 0) {
+    res.status(404).send(`Sorry, there is no books by ${author}`)
+  }
+  res.json(authorList)
+});
+
+
+//get books by title /books/titles?title= ...                    
+app.get('/books/titles', (req, res) => {
+  const { title } = req.query
+  const  booksTitles = booksData.filter(book => book.title.toString().toLowerCase().includes(title.toLowerCase()))
+
+  if (booksTitles.length === 0) {
+    res.status(404).send(`Sorry, there is no books with the title ${title}`)
+  }
+  res.json(booksTitles)
+});
+
+
+// get thin books /books/thin?pages= ...
+app.get('/books/thin', (req, res) => {
+  const { pages } = req.query
+  const thinBooks = booksData.filter(book => book.num_pages <= 500)
+
+  if(pages >= 501) {
+    res.status(404).send(`Sorry, could not find a thin book with ${pages} pages`)
+  } 
+
+  if(thinBooks === 0) {
+    res.status(404).send(`Sorry, could not find a book with ${pages} pages`)
+  }
+  res.json(thinBooks)
 })
 
-// Endpoint to get one book
+
+//else (booksByThickness <= 500 ? thinBook : thickBook) ?????
+
+
+
+// get one book by id /books/1
 app.get('/books/:id', (req, res) => {
   const { id } = req.params
   const book = booksData.find(book => book.bookID === +id)
@@ -42,10 +71,14 @@ app.get('/books/:id', (req, res) => {
     res.status(404).send(`No book with id number ${id}`)
   }
   res.json(book)
-})
+});
+
+
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
   console.log(`Server running on http://localhost:${port}`)
 })
+
+
