57d0f8d237fce33c7a6f0c23bc482a1e9b89d449
newmarkdev <newmarkdev@gmail.com>
2021-05-06 15:54:07 +0200

    added endpoints

--- server.js
+++ server.js
@@ -1,30 +1,34 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
+import listEndpoints from 'express-list-endpoints'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import avocadoSalesData from './data/avocado-sales.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
-
-// Add middlewares to enable cors and json body parsing
+// Add middleware to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+// _ acts as a placeholder for req 
+app.get('/', (_, res) => {
+  res.send(listEndpoints(app));
+})
+
+// Endpoint 1: All Data
+app.get('/avo-sales', (_, res) => {
+  res.send(avocadoSalesData);
+})
+
+// Endpoint 2: Avo sales by location 
+
+app.get('/avo-sales/regions', (req, res) => {
+  // Step 1 add Map method to create array of regions 
+  const allRegions = avocadoSalesData.map((avocadoSalesData) => { return avocadoSalesData.region })
+  // Step 2 Detect if location occurs more than one time (const reduced regions )
+  // forEach let location = "" 
+  // Google: detect multiple occurrences of the same array element 
+  res.send(allRegions);
 })
 
 // Start the server
