d1784e6af20346f1649a49b4b944214ae7d3aba4
Aepera <astridepersson@gmail.com>
2021-05-06 17:46:52 +0200

    Routes for filtering all movies and all TV series

--- server.js
+++ server.js
@@ -1,33 +1,51 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
+import listEndpoints from 'express-list-endpoints'
 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
 import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+// Defines the port the app will run on. Defaults to 8080
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
+// Middlewares 
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
-// Start defining your routes here
+// Route to return the entire json
 app.get('/', (req, res) => {
   res.json(netflixData)
+  res.send(listEndpoints(app))
 })
 
+// Route to return a single movie/tvshow object, when request is an exact Url
 app.get('/:title', (req, res) => {
   const { title } = req.params
-  const findShow = netflixData.find((show) => show.title === title)
-  res.json(findShow)
+  const findShow = netflixData.find((show) => show.title.toString().toLowerCase().replace(/\s+/g, '') === title.toLowerCase().replace(/\s+/g, ''))
+  
+  if (findShow) {
+    res.status(200).json({ title: findShow })
+  } else {
+    res.status(400).json({ error: 'Title not found, check your spelling or try another title' })
+  }
+})
+
+// Route to filter out all movies
+app.get('/movies/:movie', (req, res) => {
+  const { movie } = req.params
+
+  const allMovies = netflixData.filter((item) => item.type.toString().toLowerCase() === movie.toString().toLowerCase())
+
+  res.json(allMovies)
+})
+
+// Route to filter out all TV Series
+app.get('/tvshows/:tvshow', (req, res) => {
+  const { tvshow } = req.params
+
+  const allTVshows = netflixData.filter((item) => item.type.toString().toLowerCase().replace(/\s+/g, '') === tvshow.toString().toLowerCase().replace(/\s+/g, ''))
+
+  res.json(allTVshows)
 })
 
 // Start the server
