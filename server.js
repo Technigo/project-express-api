cdd1d24f57166596cf359559ce43e828ab0f3d96
Eleonor Frankenberg <elle@Eleonors-MacBook-Air.local>
2021-05-06 15:18:08 +0200

    made endpoints for books array, filter by author and to find books by id

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+// import bodyParser from 'body-parser'
+// import cors from 'cors'
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // 
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
+ import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
@@ -19,12 +19,29 @@ const port = process.env.PORT || 8080
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+// app.use(cors())
+// app.use(bodyParser.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+// Endpoint to get all the books
+app.get('/books', (req, res) => {
+  const { author } = req.query
+
+  if(author) {
+    const authorList = booksData.filter(book => book.authors.toLowerCase().includes(author.toLowerCase()))
+    res.json(authorList)
+  }
+  res.json(booksData)
+})
+
+// Endpoint to get one book
+app.get('/books/:id', (req, res) => {
+  const { id } = req.params
+  const book = booksData.find(book => book.bookID === +id)
+
+  if(!book) {
+    res.status(404).send(`No book with id number ${id}`)
+  }
+  res.json(book)
 })
 
 // Start the server
