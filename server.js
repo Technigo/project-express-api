3706da0e6426712bd3329d0eee0a3d17e6f637ca
Andrea Larsson <andrea.e.larsson@hotmail.se>
2021-05-06 20:51:00 +0200

    filter for author and title is working

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-// import listEndpoints from 'express-list-endpoints'
 import booksData from './data/books.json'
 
 // If you're using one of our datasets, uncomment the appropriate import below
@@ -9,7 +8,6 @@ import booksData from './data/books.json'
 // 
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
@@ -23,29 +21,36 @@ const app = express()
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
-// app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Welcome to Andreas book site. To get full list use the endpoint /books')
-  // res.send(listEndpoints(app))
+  res.send('Endpoints: /books, /books?author=collins, /books?title=titanic, /books/1, /books/languages/en-US')
 })
 
-// Get all data/books.
-app.get('/books', (req, res) => {
-  /* const { authors, title } = req.puery
+// Get all books and filter author and title.
+// http://localhost:8080/books?author=collins
+// http://localhost:8080/books?title=titanic
+app.get("/books", (req, res) => {
+  const { author, title } = req.query
   let books = booksData
 
-  if (authors) {
-    books = books.filter((item) => item.authors.toLowerCase().includes(authors.toLowerCase()))
+  if (author) {
+    books = booksData.filter((book) => book.authors.toLowerCase().includes(author.toLowerCase()))
   }
+
   if (title) {
-  books = books.filter((item) => item.title.toString().toLowerCase().includes(title.toLowerCase()))
-  } */
-  res.json(booksData)
+    books = booksData.filter((book) => book.title.toString().toLowerCase().includes(title.toLowerCase()))
+  } 
+
+  if (books.length) {
+    res.json(books)
+  } else {
+    res.status(404).json(`Sorry, there is no book that fits your requirment!`)
+  }
 })
 
-// Get a book based on its id.
+// Get a book based on id.
+// http://localhost:8080/books/1
 app.get('/books/:id', (req, res) => {
   const { id } = req.params
   const bookid = booksData.find((book) => book.bookID === +id)
@@ -54,26 +59,6 @@ app.get('/books/:id', (req, res) => {
     res.status(404).send(`There is no book with id number ${id} in the data!`)
   }
   res.json(bookid)
-})
-
-//  Get a book based on the title.
-// ERROR
-app.get('books/titles/:title', (req, res) => {
-  const { title } = req.params
-  // eslint-disable-next-line max-len
-  const bookTitle = booksData.filter((book) => book.title.toString().toLowerCase().includes(title.toString().toLowerCase()))
-  
-  res.json(bookTitle)
-})
-
-// Filter books based on authors
-//  ERROR
-app.get('/books/authors/:author', (req, res) => {
-  const { authors } = req.params
-  // eslint-disable-next-line max-len
-  const bookAuthor = booksData.filter((book) => book.authors.toLowerCase().includes(authors.toLowerCase()))
-
-  res.json({ data: bookAuthor })
 })
 
 // Filter books based on language
