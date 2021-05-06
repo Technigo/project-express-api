c2b9927b68af39421bbf9ee43c177071488534b1
Erika Andersson Porath <erika.porath@hotmail.com>
2021-05-07 01:01:11 +0200

    Added more filters and endpoints. Cannot get the endpoint for a single object to work

--- server.js
+++ server.js
@@ -18,17 +18,55 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 
+//_ is a placeholder for req 
+app.get('/', (_, res) => {
+  res.send(listEndpoints(app))
+})
+
 //Endpoint to get all the movies
 app.get('/shows', (req, res) => {
-  const { title } = req.query
+  const { title, cast, country, director } = req.query
+  
+  //Filters so the user can search the items in the query
   if (title) {
     const filteredTitleList = netflixData.filter(show => show.title.toString().includes(title))
     res.json(filteredTitleList)
+  } else if (cast) {
+    const filteredCastList = netflixData.filter(show => show.cast.toString().includes(cast))
+    res.json(filteredCastList)
+  } else if (country) {
+    const filteredCountryList = netflixData.filter(show => show.country.toString().includes(country))
+    res.json(filteredCountryList)
+  } else if (director) {
+    const filteredDirectorList = netflixData.filter(show => show.director.toString().includes(director))
+    res.json(filteredDirectorList)
   }
   res.json(netflixData)
 })
 
-//Endpoint to get one movie
+//Endpoint to get only movies
+app.get('/movies', (_, res) => {
+  const typeMovies = netflixData.filter(show => show.type === "Movie")
+  res.json(typeMovies)
+})
+
+//Endpoint to get only TV-shows
+app.get('/tv-shows', (_, res) => {
+  const typeTvShow = netflixData.filter(show => show.type === "TV Show")
+  res.json(typeTvShow)
+})
+
+//Endpoint to get one title
+app.get('/shows/:title', (req, res) => {
+  const { title } = req.params
+  const showTitle = netflixData.find(show => show.title === title)
+  if (!showTitle) {
+    res.status(404).send(`No show with the title ${title} excists`)
+  }
+  res.json(showTitle)
+})
+
+//Endpoint to get one movie by id
 app.get('/shows/:id', (req, res) => {
   const { id } = req.params
   const show = netflixData.find(show => show.show_id === +id)
@@ -37,6 +75,7 @@ app.get('/shows/:id', (req, res) => {
   }
   res.json(show)
 })
+
 
 // Start the server
 app.listen(port, () => {
