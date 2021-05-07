a2122dd480083e2db4c9b1abdc604101741daf0c
katliebig <liebig.kat@gmail.com>
2021-05-07 09:33:27 +0200

    code cleanup

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.use(bodyParser.json())
 // Error messages
 
 const notFound = {
-  "code": 404,
-  "message": "Not Found",
-  "description": "This resource does not exist."
+  code: 404,
+  message: "Not Found",
+  description: "This resource does not exist."
 }
 
 // Start defining your routes here
@@ -37,21 +37,25 @@ app.get('/', (req, res) => {
 
 // endpoint for all books
 app.get("/books", (req, res) => {
-  const bookTitle = req.query.title
-  const author = req.query.author
+  const { title } = req.query
+  const { author } = req.query
   let books = booksData.sort((a, b) => a.bookID - b.bookID)
 
-  if (bookTitle) {
-    books = books.filter((book) => typeof book.title === "string") // checks if book.title is a string (because some are not!)
-    books = books.filter((book) => book.title.toLowerCase().includes(bookTitle)) // checks for title
+  if (title) {
+    // checks if book.title is a string (because some are not!)
+    books = books.filter((book) => typeof book.title === "string")
+    // checks for title
+    books = books.filter((book) => book.title.toLowerCase().includes(title.toLowerCase()))
   }
 
   if (author) {
-    books = books.filter((book) => book.authors.toLowerCase().includes(author)) // checks for author
+    // checks for author
+    books = books.filter((book) => book.authors.toLowerCase().includes(author.toLowerCase()))
   }
 
   if (books.length === 0) {
-    res.status(404).json(notFound) // displays 404 error message if author or title search has no results
+    // displays 404 error message if author or title search has no results
+    res.status(404).json(notFound)
   }
 
   res.json(books)
@@ -63,34 +67,40 @@ app.get("/books/:id", (req, res) => {
   const bookById = booksData.find((book) => book.bookID === id) // checks for id
 
   if (!bookById) {
-    res.status(404).json(notFound) // displays 404 error message if id has no match
+    // displays 404 error message if id has no match
+    res.status(404).json(notFound)
   }
 
   res.json(bookById)
 })
 
-// endpoint for highest rated books, search query minRatingCount to filter for a minimum number of ratings
+// endpoint for highest rated books
+// search query minRatingCount to filter for a minimum number of ratings
 app.get("/highestRated", (req, res) => {
-  let highestRatedBooks = booksData.sort((a, b) => b.average_rating - a.average_rating) // sorts by highest rating
+  // sorts by highest rating
+  let highestRatedBooks = booksData.sort((a, b) => b.average_rating - a.average_rating)
 
-  const minRatingCount = req.query.minRatingCount
+  const { minRatingCount } = req.query
 
   if (minRatingCount) {
-    highestRatedBooks = highestRatedBooks.filter((book) => book.ratings_count >= minRatingCount) // includes only results above specified rating count
+    // includes only results above specified rating count
+    highestRatedBooks = highestRatedBooks.filter((book) => book.ratings_count >= minRatingCount)
   }
   res.json(highestRatedBooks)
 })
 
 // endpoint for top 50 highest rated books with above 1000 ratings
 app.get("/top50", (req, res) => {
-  let highestRatedBooks = booksData.sort((a, b) => b.average_rating - a.average_rating) // sorts by highest rating
+  // sorts by highest rating
+  let highestRatedBooks = booksData.sort((a, b) => b.average_rating - a.average_rating)
 
-  highestRatedBooks = highestRatedBooks.filter((book) => book.ratings_count >= 1000) // includes only results above 1000 rating counts
+  // includes only results above 1000 rating counts
+  highestRatedBooks = highestRatedBooks.filter((book) => book.ratings_count >= 1000)
 
-  const top50 = highestRatedBooks.slice(0, 50) // takes first 50
+  // takes first 50
+  const top50 = highestRatedBooks.slice(0, 50)
   res.json(top50)
 })
-
 
 // Start the server
 app.listen(port, () => {
