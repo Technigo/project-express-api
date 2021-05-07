d7ee88bdececb32ec3604ff2788a36afd8d484d8
Atefeh H.Takrami <ati.takrami@gmail.com>
2021-05-07 12:48:20 +0200

    add route

--- server.js
+++ server.js
@@ -1,16 +1,46 @@
 import express from "express";
-import booksData from "./data/books.json";
+import goldenGlobesData from "./data/golden-globes.json";
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 8080;
 const app = express();
 
-// endpoint to get all books
-app.get("/books", (req, res) => {
-  res.json(booksData);
+app.get("/", (request, response) => {
+  const dataLength = String(goldenGlobesData.length);
+  response.send(
+    `You can find ${dataLength} movies from Golden Globes ceremony here!`
+  );
 });
 
-// Start the server
+app.get("/golden-globes", (request, response) => {
+  const { ceremony, win, category } = request.query;
+  console.log(request.query);
+  let result;
+
+  result = goldenGlobesData.filter(
+    (movie) => movie.ceremony === Number(ceremony)
+  );
+
+  if (win) {
+    result = result.filter((movie) => movie.win);
+  }
+
+  if (category) {
+    result = result.filter((movie) => movie.category.includes(category));
+  }
+  response.json(result);
+});
+
+app.get("/golden-globes/:film", (request, response) => {
+  const { film } = request.params;
+  const filteredFilm = goldenGlobesData.filter((movie) => movie.film === film);
+
+  if (filteredFilm.length > 0) {
+    response.json(filteredFilm);
+  } else {
+    response.send(`There is not information about ${film}`);
+  }
+});
+
 app.listen(port, () => {
-  // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`);
+  console.log("Hello console the serer is now running");
 });
