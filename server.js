da2a320e3c72bfd6b2c20611cb2fdb56d6b2b149
Åsa Hildebrand <asahildebrand@yahoo.com>
2021-05-07 21:54:03 +0200

    Cleaned up code

--- server.js
+++ server.js
@@ -2,16 +2,11 @@ import express from 'express'
 import cors from 'cors'
 import listEndpoints from 'express-list-endpoints'
 
-import books from './data/books.json'
+import booksData from './data/books.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8083
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(express.json())
 
@@ -19,33 +14,28 @@ app.get('/', (req, res) => {
   res.send(listEndpoints(app))
 })
 
-
 // Endpoint that shows all books:
 app.get('/books', (req, res) => {
-  res.json(books)
+  res.json(booksData)
 })
 
-
-// Endpoint that shows all authors in alphabetic order (for ex: /books/authors/authors)
+// Endpoint that shows all authors in alphabetic order
 app.get("/books/authors/:authors/", (req, res) => {
-  const listAuthors = books.map((item) => item.authors).sort();
-  res.json(listAuthors);
-});
-
+  const listAuthors = booksData.map((item) => item.authors).sort()
+  res.json(listAuthors)
+})
 
 // Endpoint that shows books with specific rating (for ex: /books/rating/4.56)
 app.get('/books/rating/:rating', (req, res) => {
   const rating = req.params.rating
-  const avarageRating = books.filter((item) => item.average_rating === +rating)
+  const avarageRating = booksData.filter((item) => item.average_rating === +rating)
   res.json(avarageRating)
 })
-
 
 // Endpoint that shows book with specific id (for ex: /books/id/45)
 app.get('/books/id/:id', (req, res) => {
   const { id } = req.params
-
-  const getBook = books.find((book) => book.bookID === +id)
+  const getBook = booksData.find((book) => book.bookID === +id)
 
   if (!getBook) {
     res.status(404).json('Not found')
@@ -53,7 +43,6 @@ app.get('/books/id/:id', (req, res) => {
     res.json({ data: getBook })
   }
 })
-
 
 app.listen(port, () => {
   // eslint-disable-next-line
