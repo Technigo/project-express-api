08044aebc17c1b54d094f3edc554f8f7b989c8c8
Ylva Landoff Lindberg <ylvalandofflindberg@Ylvas-MBP-2.lan>
2021-05-07 15:24:51 +0200

    Tested and added many diffrent endpoints

--- server.js
+++ server.js
@@ -11,58 +11,71 @@ const app = express()
 app.use(cors())
 app.use(express.json());
 
+// dummy endpoint
 app.get('/', (req, res) => {
-  res.json('🖤 Music endpoint starts here! 🖤')
+  res.json('🖤 Music endpoint starts here! 🖤, List of all traks with info: /music. List of all music genres: /music/allgenres. Get all songs from an Artist no matter spelling: /music/artists/:artist(add name). Get all songs from a genre /music/pop (or rock or something else). All songs with same energy: /music/energy/ and a number. Sorted The 20 least popular songs, low to high: /music/popularity/lowest-20. Sorted The 20 most popular songs, hight to low; /music/popularity/best-20. ' )
 })
 
-// //all songs endpoint http://localhost:8080/music
-// app.get('/music', (req, res) => {
-//   const { song } = req.query
-//   if (song) {
-//     const musicList = topMusicData.filter(song => song.trackName.includes(song))
-//     res.json(musicList)
-//   } 
-//   res.json(topMusicData)
-// })
-
+//all songs endpoint http://localhost:8080/music
 app.get('/music', (req, res) => {
-
-  const { song, artist } = req.query
-
+  const { song } = req.query
   if (song) {
-    const songByTitle = topMusicData.filter(item => item.trackName.includes(song))
-    if (songByTitle.length === 0){
-      res.status(404).json(ERROR_MESSAGE_SONG)
-    } else {
-      res.json(songByTitle)
-    }
-  } else if (artist){
-    const artistName = topMusicData.filter((item) => {
-    return item.artistName.toLowerCase().indexOf(item.toLowerCase()) !== -1;
-})
-    res.json({ data: artistName });
-  } else {
-    res.json(topMusicData)
-  }
-  
+    const musicList = topMusicData.filter(song => song.trackName.includes(song))
+    res.json(musicList)
+  } 
+  res.json(topMusicData)
 })
 
 
-// //http://localhost:8080/music/artists/j
-// // Get all songs from an Artist No mather if its capital letters or full name, you get all songs fom artists with the same name.
-// app.get('/music/artists/:artist', (req, res) => {
-//   const { artist } = req.params
-//   const artistName = topMusicData.filter((song) => {
-//     return song.artistName.toLowerCase().indexOf(artist.toLowerCase()) !== -1;
+// app.get('/music/info', (req, res) => {
+
+//   const { song, artist } = req.query
+
+//   if (song) {
+//     const songByTitle = topMusicData.filter(song => song.trackName.includes(song))
+//     if (songByTitle.length === 0){
+//       res.status(404).json(ERROR_MESSAGE_SONG)
+//     } else {
+//       res.json(songByTitle)
+//     }
+//   } else if (artist){
+//     const artistName = topMusicData.filter((item) => {
+//     return item.artistName.toLowerCase().indexOf(artist.toLowerCase()) !== -1;
 // })
-// res.json({ data: artistName });
-// });
+//     res.json({ data: artistName });
+//   } 
+  
+// })
 
+// http://localhost:8080/music/info?artist=shaw
 
+app.get('/music/info', (req, res) => {
+  const { title, artist } = req.query;
+  let musicToSend = topMusicData;
+  
+  if (title) {
+    musicToSend = musicToSend
+    .filter(music => music.trackName.toLowerCase().includes(title.toLowerCase()));
+  } 
+  if (artist) {
+    musicToSend = musicToSend
+    .filter(music => music.artistName.toLowerCase().includes(artist.toLowerCase()));
+  }
+  res.json({ length: musicToSend.length, data: musicToSend})
+})
 
+//http://localhost:8080/music/artists/j
+// Get all songs from an Artist No mather if its capital letters or full name, you get all songs fom artists with the same name.
+app.get('/music/artists/:artist', (req, res) => {
+  const { artist } = req.params
+  const artistName = topMusicData.filter((song) => {
+    return song.artistName.toLowerCase().indexOf(artist.toLowerCase()) !== -1;
+})
+res.json({ data: artistName });
+});
 
   
-//dictionaryn of all kinds music genres
+//dictionary of all kinds of music genres
 //v1- path param
 app.get('/music/allgenres', (req, res) => {
   const genresDuplicated = topMusicData.map(item => item.genre);
@@ -102,7 +115,7 @@ app.get('/music/:id', (req, res) => {
   const { id } = req.params
   const song = topMusicData.find( song => song.id === +id)
   if (!song) {
-    res.status(404).send(`sorry, song id:{ id } can not be fund`)
+    res.status(404).send(`sorry, that song can not be fund`)
   }
   res.json(song)
 })
@@ -114,7 +127,7 @@ app.get('/music/genre/:genre', (req, res) => {
   res.json(song)
 })
 
-//http://localhost:8080/music/artists/j
+http://localhost:8080/music/artists/j
 // Get all songs from an Artist No mather if its capital letters or full name, you get all songs fom artists with the same name.
 app.get('/music/artists/:artist', (req, res) => {
   const { artist } = req.params
