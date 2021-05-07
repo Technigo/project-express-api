6f215cddc1eabef51e61b05b9e1e87edd2f1eb41
Tobias Hagberg <tbs.hagberg@gmail.com>
2021-05-07 15:08:21 +0200

    added queries to books path and an isbn param

--- server.js
+++ server.js
@@ -4,39 +4,65 @@ import cors from 'cors'
 import booksData from './data/books.json'
 import listEndpoints from 'express-list-endpoints'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
+
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
+// Middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(express.json())
 
-// Start defining your routes here
+// Routes
 app.get('/', (req, res) => {
   res.send(listEndpoints(app))
-  
-  /* (
-    `
-      This API provides detaliled information about books. <br>
-      Please specify an endpoint or a query. <br>
-      Available Endpoints: /books or /id/(id) <br>
-      Sample Query:
-    `) */
-})
-app.get('/books', (req, res) => {
-  res.json(booksData)
 })
 
-app.get('/books/:id', (req, res) => {
+app.get('/books', (req, res) => {
+  const { title, author, language_code } = req.query
+  let booksToSend = booksData
+
+  if (title) {
+    booksToSend = booksToSend
+      .filter(book => book.title.toLowerCase().indexOf(title.toLowerCase()) !== -1)
+  }
+
+  if (author) {
+    booksToSend = booksToSend
+      .filter(book => book.authors.toLowerCase().indexOf(author.toLowerCase()) !== -1)
+  }
+
+  if (language_code) {
+    booksToSend = booksToSend
+      .filter(book => book.language_code === language_code)
+  }
+
+  res.json({ length: booksToSend.length, data: booksToSend })
+})
+//sample queries
+// http://localhost:8080/books?author=adams
+// http://localhost:8080/books?language_code=en-US
+// http://localhost:8080/books?author=adams&language_code=en-US
+
+
+
+app.get('/books/id/:id', (req, res) => {
   const { id } = req.params
-  const queriedBook = booksData.find((book) => book.bookID === +id)
+  const bookId = booksData.find((book) => book.bookID === +id)
   
-  if (queriedBook) {
-    res.json({ data: queriedBook })
+  if (bookId) {
+    res.json({ data: bookId })
+  } else {
+    res.status(404).json({ error: 'Not found' })
+  }
+})
+
+app.get('/books/isbn/:isbn', (req, res) => {
+  const { isbn } = req.params
+  const bookIsbn = booksData.find((book) => book.isbn === +isbn)
+  
+  if (bookIsbn) {
+    res.json({ data: bookIsbn })
   } else {
     res.status(404).json({ error: 'Not found' })
   }
