22d859a423351d3d66659250ee37041ee19437f0
CarolineF3 <caroline.fahlgren@gmail.com>
2021-05-07 21:24:46 +0200

    blue level

--- server.js
+++ server.js
@@ -1,34 +1,62 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+import listEndpoints from "express-list-endpoints";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import netflixData from "./data/netflix-titles.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(express.json());
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+// Functions
+const divideShows = (showsArray, pageNumber) => {
+  showsArray = showsArray.slice(pageNumber * 10, (pageNumber + 1) * 10);
+  return showsArray;
+};
 
-// Start the server
+app.get("/", (req, res) => {
+  res.send(listEndpoints(app));
+});
+
+app.get("/shows", (req, res) => {
+  const { year, type, director, cast, page } = req.query;
+  let queriedShows = netflixData;
+
+  if (year) {
+    queriedShows = queriedShows.filter((show) => show.release_year === +year);
+  }
+  if (type) {
+    queriedShows = queriedShows.filter(
+      (show) => show.type.toLowerCase() === type.toLowerCase()
+    );
+  }
+  if (director) {
+    queriedShows = queriedShows.filter(
+      (show) => show.director.toLowerCase() === director.toLowerCase()
+    );
+  }
+  if (cast) {
+    queriedShows = queridShows.filter(
+      (show) => show.cast.toLowerCase() === cast.toLowerCase()
+    );
+  }
+  quiredShow
+    ? res.status(200).json({ data: divideShows(queriedShows, +page) })
+    : res.status(404).json({ error: "Not found" });
+});
+
+app.get("/shows/:id", (req, res) => {
+  const { id } = req.params;
+
+  const quiredShow = netflixData.find((show) => show.show_id === +id);
+  quiredShow
+    ? res.status(200).json({ data: quiredShow })
+    : res.status(404).json({ error: "Not found" });
+});
+
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
