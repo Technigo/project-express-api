cb2900c489875ee34994622277188b1f9b88703b
“[ida <[ida.t.sylven@gmail.com]>
2021-05-07 21:16:59 +0200

    Made comments and cleaned out code. Revised README file.

--- server.js
+++ server.js
@@ -1,40 +1,33 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
+// Importing data from Books API
 import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+// Defining the port the app will run on
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
+// Middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
-// Start defining your routes here
+// Routes
 
-// Endpoint to get all books
+// Endpoint to get all books or filter by author
+// http://localhost:8080/books
+// http://localhost:8080/books?author=tolkien
 app.get('/books', (req, res) => {
   const { author } = req.query
   if (author) {
-    const filteredBooks = booksData.filter(book => book.authors.includes(author))
-    res.json(filteredBooks)
+    const booksByAuthor = booksData.filter(book => book.authors.toLowerCase().includes(author.toLowerCase()))
+    res.json(booksByAuthor)
   }
   res.json(booksData)
 })
 
-// Endpoint to one book
+// Endpoint to one book by ID
+// http://localhost:8080/books/1
 app.get('/books/:id', (req, res) => {
   const { id } = req.params
   const book = booksData.find(book => book.bookID === +id)
@@ -44,7 +37,7 @@ app.get('/books/:id', (req, res) => {
   res.json(book)
 })
 
-// Start the server
+// Starting the server
 app.listen(port, () => {
   // eslint-disable-next-line
   console.log(`Server running on my http://localhost:${port}`)
