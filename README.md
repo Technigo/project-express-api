ec2e6e42a8a0594afb6d39ddc24335ec39838271
Erika Andersson Porath <erika.porath@hotmail.com>
2021-05-08 00:37:10 +0200

    added more filters to the other endpoints, as well as a not found status code. Found a better solution for the filters that allowed you to add more than one query at the time so switched to that. Also added express-list-endpoints to list all endpoints. Wrote the readme

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
-# Express API Project
+# Express API Project - Netflix Data
 
-Replace this readme with your own information about your project. 
+This project is built with Node.js and Express to practice making a REST-API, by using a json with data from Netflix.
 
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+I started by setting up the dependencies and enabling the json-file that I wanted to use. I ran the path in Postman to see how the data looked. I then decided to starting with creating an endpoint that would show all the data, in this case all the Netflix movies and TV-shows from the json. After that I sat up endpoints that filtered the two different types of shows, “Movie” and “TV-show”. I also sat up two endpoints so the user can use parameters to look for a specific title or an id. I then decided to add filters for the endpoints that shows a list of objects, so the user can search for specific items like a title or director by adding query strings. Finally I added status codes for when the filtered array ends up empty (no query result) and when the params are not matching any objects. I also went back once to change the logic for the filters because I found a cleaner solution that also allowed you to add more than one query, which did not work with my first solution. I also added a starting endpoint (by using express-list-endpoints) that lists all the different endpoints available. Lastly I deployed the project on Heroku. 
+
+I didn't have any blockers with this project, and if I got stuck somewhere I easily found a solution on StackOverflow, but I managed to figure out most of it by myself. If I had more time I would like to add some frontend to use the data, and probably more filters to make it searchable.
+
+**The available endpoints are:**
+
+/ - *lists all the endpoints*
+
+/shows – *show all the data (all the shows, available filters for title, cast, director, country)*
+
+/movies – *show only movies (available filters for title, cast, director, country)*
+
+/tv-shows – *show only TV-shows (available filters for title, cast, director, country)*
+
+/shows/id/:id – *able to add an id to look for a specific show*
+
+/shows/title/:title – *able to add a title too look for a specific show*
+
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+See the deployed project here: 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
+import listEndpoints from 'express-list-endpoints'
 
 import netflixData from './data/netflix-titles.json'
 
@@ -10,65 +10,126 @@ import netflixData from './data/netflix-titles.json'
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
+// Middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
 
 // == Defining routes ==
 
-//_ is a placeholder for req 
+// list all the endpoints (_ works like a placeholder for req)
 app.get('/', (_, res) => {
   res.send(listEndpoints(app))
 })
 
-//Endpoint to get all the movies
+// ** Endpoint to get all the shows **
+
 app.get('/shows', (req, res) => {
   const { title, cast, country, director } = req.query
+  let shows = netflixData
 
-  //Filters so the user can search the items in the query
+  // Filters so the user can search for items in the endpoint
   if (title) {
-    const filteredTitleList = netflixData.filter(show => show.title.toString().includes(title))
-    res.json(filteredTitleList)
-  } else if (cast) {
-    const filteredCastList = netflixData.filter(show => show.cast.toString().includes(cast))
-    res.json(filteredCastList)
-  } else if (country) {
-    const filteredCountryList = netflixData.filter(show => show.country.toString().includes(country))
-    res.json(filteredCountryList)
-  } else if (director) {
-    const filteredDirectorList = netflixData.filter(show => show.director.toString().includes(director))
-    res.json(filteredDirectorList)
+    shows = shows.filter(show => show.title.toString().includes(title))
   }
-  res.json(netflixData)
+  if (cast) {
+    shows = shows.filter(show => show.cast.toString().includes(cast))
+  }
+  if (country) {
+    shows = shows.filter(show => show.country.toString().includes(country))
+  }
+  if (director) {
+    shows = shows.filter(show => show.director.toString().includes(director))
+  }
+
+  // Show an error message if the returning array is empty (no search result)
+  if (shows.length === 0) {
+    res.status(404).send('Not found')
+
+  // Show all data
+  } else {
+    res.json(shows)
+  }
 })
 
-//Endpoint to get only movies
-app.get('/movies', (_, res) => {
-  const typeMovies = netflixData.filter(show => show.type === "Movie")
-  res.json(typeMovies)
+// ** Endpoint to get only movies **
+
+app.get('/movies', (req, res) => {
+  let movies = netflixData.filter(show => show.type === "Movie")
+  const { title, cast, country, director } = req.query
+
+  // Filters so the user can search for items in the movies endpoint
+  if (title) {
+    movies = movies.filter(show => show.title.toString().includes(title))
+  }
+  if (cast) {
+    movies = movies.filter(show => show.cast.toString().includes(cast))
+  }
+  if (country) {
+    movies = movies.filter(show => show.country.toString().includes(country))
+  }
+  if (director) {
+    movies = movies.filter(show => show.director.toString().includes(director))
+  }
+
+  // Show an error message if the returning array is empty (no search result)
+  if (movies.length === 0) {
+    res.status(404).send('Not found')
+
+  // Show all data for the movies
+  } else {
+    res.json(movies)
+  }
 })
 
-//Endpoint to get only TV-shows
-app.get('/tv-shows', (_, res) => {
-  const typeTvShow = netflixData.filter(show => show.type === "TV Show")
-  res.json(typeTvShow)
+// ** Endpoint to get only TV-shows **
+
+app.get('/tv-shows', (req, res) => {
+  let tvShows = netflixData.filter(show => show.type === "TV Show")
+  const { title, cast, country, director } = req.query
+
+  // Filters so the user can search for items in the tv-shows endpoint
+  if (title) {
+    tvShows = tvShows.filter(show => show.title.toString().includes(title))
+  }
+  if (cast) {
+    tvShows = tvShows.filter(show => show.cast.toString().includes(cast))
+  }
+  if (country) {
+    tvShows = tvShows.filter(show => show.country.toString().includes(country))
+  }
+  if (director) {
+    tvShows = tvShows.filter(show => show.director.toString().includes(director))
+  }
+
+  // Show an error message if the returning array is empty (no search result)
+  if (tvShows.length === 0) {
+    res.status(404).send('Not found')
+
+  // Show all data for the movies
+  } else {
+    res.json(tvShows)
+  }
 })
 
-//Endpoint to get one title
+// ** Endpoint to get only one show by title **
+
 app.get('/shows/title/:title', (req, res) => {
   const { title } = req.params
   const showTitle = netflixData.find(show => show.title === title)
+
   if (!showTitle) {
     res.status(404).send(`No show with the title ${title} exists`)
   }
   res.json(showTitle)
 })
 
-//Endpoint to get one movie by id
+// ** Endpoint to get only one show by id **
+
 app.get('/shows/id/:id', (req, res) => {
   const { id } = req.params
-  const show = netflixData.find(show => show.show_id === +id)
+  const show = netflixData.find(show => show.show_id === +id) 
+
   if (!show) {
     res.status(404).send(`No show with id number ${id} exists`)
   }
