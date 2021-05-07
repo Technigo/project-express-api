89aea7e58077519dcc26a2dca5b2c994461e59fb
Atefeh H.Takrami <ati.takrami@gmail.com>
2021-05-07 14:38:13 +0200

    set header

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "localhost:3000"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
