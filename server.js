6f2dbf31809768065eef454812096a30794fafe6
Åsa Hildebrand <asahildebrand@yahoo.com>
2021-05-07 17:40:07 +0200

    Fixed the endpoints

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from 'express'
-//import bodyParser from 'body-parser'
 import cors from 'cors'
 import listEndpoints from 'express-list-endpoints'
 
@@ -14,55 +13,48 @@ const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-//app.use(bodyParser.json())
 app.use(express.json())
 
-// Start defining your routes here
 app.get('/', (req, res) => {
   res.send(listEndpoints(app))
 })
 
-// Endpoint shows all books:
+
+// Endpoint that shows all books:
 app.get('/books', (req, res) => {
   res.json(books)
 })
 
-// Endpoint that shows book with specific id
-app.get('/books/:id', (req, res) => {
-  const { id } = req.params;
 
-  const getBook = books.find(book => book.bookID === +id)
+// Endpoint that shows all authors in alphabetic order (for ex: /books/authors/authors)
+app.get("/books/authors/:authors/", (req, res) => {
+  const listAuthors = books.map((item) => item.authors).sort();
+  res.json(listAuthors);
+});
 
-  if (getBook) {
-    res.json({ data: getBook })
+
+// Endpoint that shows books with specific rating (for ex: /books/rating/4.56)
+app.get('/books/rating/:rating', (req, res) => {
+  const rating = req.params.rating
+  const avarageRating = books.filter((item) => item.average_rating === +rating)
+  res.json(avarageRating)
+})
+
+
+// Endpoint that shows book with specific id (for ex: /books/id/45)
+app.get('/books/id/:id', (req, res) => {
+  const { id } = req.params
+
+  const getBook = books.find((book) => book.bookID === +id)
+
+  if (!getBook) {
+    res.status(404).json('Not found')
   } else {
-    res.status(404).json({ error: 'Not found' })
+    res.json({ data: getBook })
   }
 })
 
-// Endpoint that shows all authors in alphabetic order
-app.get("/books/authors/", (req, res) => {
-  const allAuthors = books.map((item) => item.authors).sort();
-  res.json(allAuthors);
-});
 
-/*
-app.get('/books/:authors', (req, res) => {
-  const authors = req.params.authors
-  const authorsFromList = books.filter((item) => item.authors === authors)
-  res.json(authorsFromList)
-})*/
-
-app.get('/books', (req, res) => {
-  const { author } = req.query
-
-  const queriedBooks = books.filter(book => {
-    return book.author.toLowerCase().indexOf(author.toLowerCase()) !== -1
-  })
-
-  res.json({ data: queriedBooks })
-})
-// Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
   console.log(`Server running on http://localhost:${port}`)
