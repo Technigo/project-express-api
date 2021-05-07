3aa5efcdbdbae00c29b8b623ee75641c12826c62
Anna Lindgren <annalindgren@MacBook-Pro-som-tillhor-Anna.local>
2021-05-07 12:23:21 +0200

    changed data source and added different endpoints and queries

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
@@ -8,14 +9,14 @@ import cors from 'cors'
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
 // import booksData from './data/books.json'
-import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+// import netflixData from './data/netflix-titles.json'
+import topMusicData from './data/top-music.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8086
+const port = process.env.PORT || 8090
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
@@ -23,21 +24,37 @@ app.use(cors())
 app.use(bodyParser.json())
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.json('Hello You')
+
+// Endpoint to get all music
+app.get('/music', (req, res) => {
+  // get artist from query
+  const { artist, title } = req.query
+  let musicToSend = topMusicData
+
+  if (artist) {
+    musicToSend = musicToSend
+      .filter((music) => music.artistName.toLowerCase().includes(artist.toLowerCase()))
+  }
+
+  if (title) {
+    musicToSend = musicToSend
+      .filter((music) => music.trackName.toString().toLowerCase().includes(title.toLowerCase()))
+  }
+  res.json({ length: musicToSend.length, data: musicToSend })
 })
 
-app.get('/list', (req, res) => {
-  res.json(netflixData)
+app.get('/music/:id', (req, res) => {
+  // Use the id to find the music we want info from 
+  // same id from music.id as from the id param in endpoint
+  const { id } = req.params
+  // eslint-disable-next-line no-shadow
+  const music = topMusicData.find((item) => item.id === +id)
+  if (music) {
+    res.json(music)
+  } else {
+    res.status(404).send(`Song with id ${id} does not excist`)
+  }
 })
-// 
-
-// return move id
-// app.get('/type/:type', (req, res) => {
-//   const { type } = req.params
-//   const videoType = netflixData.filter((item) => item.type === type)
-//   res.json(videoType)
-// })
 
 // Start the server
 app.listen(port, () => {
