2f94d68d9a9363b1460334f567d6df911fd7dfeb
Elaine Bergstrom <Elainebergstrom26@gmail.com>
2021-05-08 18:02:45 +0200

    working out routes/endpoints

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ import cors from 'cors'
 // 
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
+import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
+
+const port = process.env.PORT || 8000
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
@@ -23,8 +23,12 @@ app.use(cors())
 app.use(bodyParser.json())
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+app.get('/books', (request, response) => {
+  response.json(booksData)
+})
+
+app.get('/books/:id', (request, response) => {
+  console.log(request.params.id)
 })
 
 // Start the server
