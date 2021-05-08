2924b1b024bdf5857988f8f540536db5ffe432c6
Irina <jekabsone.irina@gmail.com>
2021-05-08 16:26:27 +0200

    project set up, endpoints created, readme done

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express, { request } from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
+import listEndpoints from 'express-list-endpoints'
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -15,33 +15,72 @@ import netflixData from './data/netflix-titles.json'
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 8080
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
-// Start defining your routes here
-//endpoint to get all movies
-app.get('/movies', (req, res) => {
-  const { title } = req.query
-  if (title) {
-    const filteredMovies = netflixData.filter(movie => movie.title.includes(title))
-    res.json(filteredMovies) 
-}
-  res.json(netflixData)
+app.get('/', (req, res) => {
+  res.send(listEndpoints(app))
 })
 
-//endpoint to get one specific movie
-app.get('/movies/:id', (req, res) => {
+
+// Start defining your routes here
+//endpoint to output movies by title, release year, cast and country
+app.get('/movies', (req, res) => {
+  const { title, year, cast, country} = req.query
+  let moviesToSend = netflixData
+
+  if (title) {
+    moviesToSend = moviesToSend
+      .filter(movie => movie.title.toString().toLowerCase().includes(title.toLowerCase()))
+  } 
+
+  if (year) {
+    moviesToSend = moviesToSend
+      .filter(movie => movie.release_year === +year)
+  }
+
+  if (cast) {
+    moviesToSend = moviesToSend
+      .filter(movie => movie.cast.toString().toLowerCase().includes(cast.toLowerCase()))
+  }
+
+  if (country) {
+    moviesToSend = moviesToSend
+      .filter(movie => movie.country.toString().toLowerCase() === country)
+  }
+
+  res.json(moviesToSend)
+})
+
+//endpoint to output possible types of movies
+app.get('/movies/types', (req, res) => {
+  const typesDuplicated = netflixData.map(item => item.type)
+
+  const uniqueTypes = []
+  typesDuplicated.forEach(item => {
+    if (!uniqueTypes.includes(item)) {
+      uniqueTypes.push(item)
+    }
+  })
+  res.json(uniqueTypes)
+})
+
+//endpoint to get one specific movie by id
+app.get('/movies/id/:id', (req, res) => {
   const { id } = req.params
-  console.log(id)
   const movie = netflixData.find(movie => movie.show_id === +id)
   if (!movie) {
     res.status(404).send(`No movie with id number ${id}`)
   }
   res.json(movie)
+})
+
+app.get('/movies/dummy', (req, res) => {
+  res.send('')
 })
 
 // Start the server
