9cd0c7a343ed0885e75572aa66d8b189131cdb17
Caroline Byström <frivridnadagar@gmail.com>
2021-05-08 11:49:42 +0200

    with genresUnique

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express, { query } from 'express'
 import cors from 'cors'
 import listEndpoints from 'express-list-endpoints'
 
-// import booksData from './data/books.json'
 import netflixData from './data/netflix-titles.json'
 
 const port = process.env.PORT || 8080
@@ -11,32 +10,48 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// SAVE THIS AND SEND SOMETHING LATER THAT WILL SHOW IN THE BROWSER
 app.get('/', (req, res) => {
-  //res.send('Hello and welcome to this API - documentation to come here as well as on GitHub // or just program a frontend please ?! What I have time for. Here can be a link')
   res.send(listEndpoints(app))
 }) 
 
-// it's logging in the Console but no output in Postman nor in browser on port ! 
-// Is the problem an if statement that will not be true, always false ? but in that case it should return netflixData always. ÄR det ngt ovan om blockerar den?
-app.get('/shows', (req, res) => {
-  const { director }  = req.query
-  
-  
+app.get('shows', (req, res) => {
+  res.json({ data: netflixData })
+})
+
+app.get('/shows/search', (req, res) => {
+  const { director, title, genres, southkorea }  = req.query
+  let queriedShows = netflixData
+
   if (director) {
-    const queriedDirector = netflixData.filter(show => show.director.toLowerCase().includes(director.toLowerCase()))
-    if (queriedDirector.length === 0 ) { 
-      res.status(404).json({ error: 'Director not found' })
-    } else {
-      res.status(200).json({ data: queriedDirector })
-    }
+    queriedShows = queriedShows.filter(show => show.director.toLowerCase().includes(director.toLowerCase()))
+  }
+
+  if (title) {
+     queriedShows = queriedShows.filter((show) => show.title.toString().toLowerCase().includes(title.toLowerCase()))
+  }
+
+  if (genres) {
+    const genresDuplicated = queriedShows.map(show => show.listed_in)
+    let genresUnique = []
+      genresDuplicated.forEach(show => {
+        if (!genresUnique.includes(show)) {
+          genresUnique.push(show)
+        }
+      })
+  }
+
+  if (southkorea) {
+    queriedShows = queriedShows.filter((show) => show.country.toLowerCase() === "South Korea".toLowerCase()) //not sure one can do this on an actual string?
+  }
+
+  if (queriedShows.length === 0 ) { 
+    res.status(404).json({ error: 'Not found' })
   } else {
-    res.status(400).json({ error: 'Please write the name of a director' }) // 400 
+    res.status(200).json({ length: queriedShows.length, data: queriedShows, genresUnique})
   }
 })
 
-// endpoint to get all tv-shows + query on director name + TODO: add status code in first res. if name of director is not found
-// TODO: do something so that you don't have to spell everything super correct even if user types in lower case eg. 
+// endpoint to get one show based on id param
 app.get('/shows/:id', (req, res) => {
   const { id }  = req.params
   const movieId = netflixData.find(movie => movie.show_id === +id)
@@ -45,22 +60,6 @@ app.get('/shows/:id', (req, res) => {
     res.status(200).json({ data: movieId });
   } else {
     res.status(404).json({ error: 'This movie can not be found' });
-  }
-})
-
-// Rename things ? - NOW it is a bit messy with this kind of param & query http://localhost:3001/countries/South Korea?southKorea=South Korea
-// South Korean shows and films (remove films and filter only on tv-shows to continue this logic) **** this is thought for frontEnd logic appearance
-app.get('/countries/:countries', (req, res) => {
-  const countries = req.params.country // SHOLD PROBABLY REMOVE THE :countries here? What is best practice? 
-
-  const showSouthKorea = req.query.southKorea
-
-  let tvShowsFromSouthKorea = netflixData.filter((show) => show.country === "South Korea") // can I add toLowerCase here as well? 
-  
-  if (showSouthKorea) {
-    res.json({ data: tvShowsFromSouthKorea })
-  } else {
-    res.json({ data: countries }) // ? this is a questionmark
   }
 })
 
