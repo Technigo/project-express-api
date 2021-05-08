11f252bca6c36d39cc8251b04c595a104dbb58b3
Nazanin Adampira <Nazi.pira88@gmail.com>
2021-05-08 21:08:42 +0200

    get and post endpoint

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import cors from 'cors'
 // 
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
+import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
@@ -15,7 +15,7 @@ import cors from 'cors'
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3000
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
@@ -24,7 +24,32 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  return res.send('Hello world')
+})
+
+// Books
+app.get('/api/book', (req, res) => {
+  const { query } = req
+
+
+  
+  return res.json(booksData).status(200)
+})
+
+app.get('/api/book/:bookId', (req, res) => {
+  const { bookId } = req.params
+  const books = booksData.filter((item) => item.bookID.toString() === bookId)
+  if (books.length === 1) {
+    return res.send(books[0]).status(200)
+  } else {
+    return res.status(404).send({ error: `No book found for bookId: ${bookId}` })
+  }
+})
+
+app.post('/api/book', (req, res) => {
+  const newBook = req.body
+  booksData.push(newBook)
+  return res.json(newBook).status(201)
 })
 
 // Start the server
@@ -32,3 +57,4 @@ app.listen(port, () => {
   // eslint-disable-next-line
   console.log(`Server running on http://localhost:${port}`)
 })
+ 
