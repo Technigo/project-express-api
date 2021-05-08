f7076ce8d9b5bd19fbec478ce8e458523d60c5bd
M4L5 <malin.gu2017@gmail.com>
2021-05-08 13:44:08 +0200

    defining routes

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
-import express from 'express'
+import express, { response } from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import avocadoSalesData from './data/avocado-sales.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -23,9 +12,33 @@ app.use(cors())
 app.use(bodyParser.json())
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+app.get('/avocados', (request, response) => {
+  const { region } = request.query
+  if( region ) {
+    const regionList = avocadoSalesData.filter(avocado => avocado.region.includes(region))
+    response.json(regionList)
+  }
+  response.json(avocadoSalesData)
 })
+
+app.get('/avocados/:id', (request, response) => {
+  const { id } = request.params
+  const avocado = avocadoSalesData.find(avocado => avocado.id === +id)
+  if (avocado) {
+    response.json(avocado)
+    } 
+    response.status(404).send(`The requested resource could not be found`)
+})
+
+
+
+// app.put('/avocados/:totalBagsSold', (request, response) => {
+//   const { totalBagsSoldList } = request.params
+//   const totalBags = avocadoSalesData.find(totalBags = totalBags.totalBagsSold === +totalBagsSoldList)
+//   response.json(totalBags)
+// }) 
+
+
 
 // Start the server
 app.listen(port, () => {
