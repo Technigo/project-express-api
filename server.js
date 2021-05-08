9e0db654405ebc1141114d8215831ea3260b2e34
Sari-Anne Peltola <sarianne.peltola@gmail.com>
2021-05-09 01:06:05 +0200

    basic endpoints

--- server.js
+++ server.js
@@ -1,34 +1,64 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import listEndpoints from 'express-list-endpoints';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
+import topMusicData from './data/top-music.json';
+
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+// // import booksData from './data/books.json'
+// // import netflixData from './data/netflix-titles.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(express.json());
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send(listEndpoints(app));
+});
+
+//endpoint to get all the 50 popular Spotify tracks and filtering by artistName, genre, trackName
+app.get('/tracks', (req, res) => {
+  const { artistName, genre, trackName } = req.query;
+  let tracksToSend = topMusicData;
+
+  if (artistName) {
+    tracksToSend = tracksToSend.filter((track) =>
+      track.artistName.toLowerCase().includes(artistName.toLowerCase()))
+  }
+
+  if (genre) {
+    tracksToSend = tracksToSend.filter((track) =>
+      track.genre.toLowerCase().includes(genre.toLowerCase()))
+  }
+
+  if (trackName) {
+    tracksToSend = tracksToSend.filter((track) => 
+      track.trackName.toLowerCase().includes(trackName.toLowerCase()))
+  }
+
+  res.json({ length: tracksToSend.length, data: tracksToSend });
+});
+
+//end point to get one specific track by id
+app.get('/tracks/:id', (req,res) => {
+  const { id } = req.params;
+  const queriedId = topMusicData.find(track => track.id === +id)
+
+  if (queriedId) {
+    res.json({data: queriedId})
+  } else {
+    res.status(404).json({error: 'Not found'})
+  }
 })
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
