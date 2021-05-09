12f44c29fcc851c85d970b064638516e0ab8de5a
liaback <felicia.liaback@gmail.com>
2021-05-09 11:11:22 +0200

    import booksData, create endpoints

--- server.js
+++ server.js
@@ -1,34 +1,35 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express, { request, response } from "express";
+// import bodyParser from 'body-parser'
+// import cors from 'cors'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import booksData from "./data/books.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
-//
+
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+// const port = process.env.PORT || 8080
+const port = 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+// app.use(cors())
+// app.use(bodyParser.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+// endpoint to get all books
+app.get("/books", (req, res) => {
+  res.json(booksData);
+});
+
+// endpoint to get one book
+app.get("/books/:id", (req, res) => {
+  const { id } = req.params;
+  const book = booksData.find(book => book.bookID === +id);
+  res.json(book);
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server is running on http://localhost:${port}`);
+});
