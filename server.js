f2efb0b62d59ad6574593afeb4130d8eae9653e1
Nazanin Adampira <Nazi.pira88@gmail.com>
2021-05-09 15:02:06 +0200

    query endpoint for books

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
+/* eslint-disable camelcase */
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
 import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+
 const port = process.env.PORT || 3000
 const app = express()
 
@@ -22,20 +12,17 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
 app.get('/', (req, res) => {
   return res.send('Hello world')
 })
 
-// Books
+// All books or query by average rating
 app.get('/api/book', (req, res) => {
-  const { query } = req
-
-
-  
-  return res.json(booksData).status(200)
+  const { average_rating } = req.query
+  const books = booksData.filter((book) => book.average_rating >= average_rating)
+  return res.json(books).status(200)
 })
-
+// Book by ID
 app.get('/api/book/:bookId', (req, res) => {
   const { bookId } = req.params
   const books = booksData.filter((item) => item.bookID.toString() === bookId)
@@ -46,6 +33,7 @@ app.get('/api/book/:bookId', (req, res) => {
   }
 })
 
+// Add a new book
 app.post('/api/book', (req, res) => {
   const newBook = req.body
   booksData.push(newBook)
