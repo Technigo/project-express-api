ccf5e7e2826cb78daafd9ba399cdbe9d2ca019c3
Lars Söderman <lars.soderman@gmail.com>
2021-05-09 14:57:13 +0200

    tweaks and readme

--- server.js
+++ server.js
@@ -25,55 +25,41 @@ app.get('/titles', (req, res) => {
 app.get('/titles/id/:id', (req, res) => {
   const { id } = req.params
   const response = titles.find((title) => title.show_id === +id)
-  if (response) {
-    res.json(response)
-  } else {
-    res.send(`Sorry, no title with id ${id}`)
-  }
+  res.status(200).json({ data: response })
 })
 // get the movies
 app.get('/titles/movies', (req, res) => {
-  const movies = titles.filter((title) => title.type === 'Movie')
-  res.json(movies)
+  const response = titles.filter((title) => title.type === 'Movie')
+  res.status(200).json({ data: response })
 })
 
 // get the TV shows
 app.get('/titles/tv-shows', (req, res) => {
-  const tvShows = titles.filter((title) => title.type === 'TV Show')
-  res.json(tvShows)
+  const response = titles.filter((title) => title.type === 'TV Show')
+  res.status(200).json({ data: response })
 })
 
 // get titles for specific year
 app.get('/titles/year/:year', (req, res) => {
   const { year } = req.params
-  const titlesByYear = titles.filter((title) => title.release_year === +year)
-  res.json(titlesByYear)
+  const response = titles.filter((title) => title.release_year === +year)
+  if (response.length > 0) {
+    res.status(200).json({ data: response })
+  } else {
+    res.status(200).send('No titles match this year')
+  }
 })
 
 // search with any search term
 app.get('/titles/search', (req, res) => {
   const { searchTerm } = req.query
-  // console.log('searchTerm ' + searchTerm)
-  const searchResults = titles.filter(
+  const response = titles.filter(
     (item) => item.description.toLowerCase().includes(searchTerm.toLowerCase()) 
       || item.title.toString().toLowerCase().includes(searchTerm.toLowerCase()) 
       || item.cast.toLowerCase().includes(searchTerm.toLowerCase())
   )
-  res.json(searchResults)
+  res.json(response)
 })
-
-// MUSIC
-// List all genres
-// app.get('/music/genres', (req, res) => {
-//   let genres = songs.map((song) => song.genre)
-//   let genresUnique = []
-//   genres.forEach((item) => {
-//     if (!genresUnique.includes(item)) {
-//       genresUnique.push(item)
-//     }
-//   })
-//   res.json(genresUnique)
-// })
 
 // Start the server
 app.listen(port, () => {
