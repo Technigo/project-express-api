e532f33ccf505132d9445ccfed38d0ee892b2060
Elaine Bergstrom <Elainebergstrom26@gmail.com>
2021-05-09 23:27:18 +0200

    New data set created and implemented, ramen-ratings

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
-import booksData from './data/books.json'
-
+// import booksData from './data/books.json'
+import ramenData from './data/ramen-ratings.json'
 
 const port = process.env.PORT || 8000
 const app = express()
@@ -13,24 +13,29 @@ app.use(cors())
 app.use(bodyParser.json())
 
 // Start defining your routes here
-app.get('/books', (request, response) => {
-  const { author } = request.query
-  if (author) {
-    const filteredBooks = booksData.filter( book => book.authors.includes(author))
-    response.json(filteredBooks)
+app.get('/ramen', (request, response) => {
+  const { brand } = request.query
+  const { variety } = request.query
+  if (brand) {
+    const ramenBrandName = ramenData.filter((ramen) => ramen.brand_name.includes(brand))
+    response.json(ramenBrandName)
+  } if (variety) {
+    const ramenVariety = ramenData.filter((ramen) => ramen.ramen_variety.includes(variety))
+    response.json(ramenVariety)
   }
 
-  response.json(booksData)
+  response.json(ramenData)
 })
 
-app.get('/books/:id', (request, response) => {
+app.get('/ramen/:id', (request, response) => {
   const { id } = request.params
-  const book = booksData.find( book => book.bookID === +id)
-  if (!book) {
-    response.status(404).send(`Sorry no book could be found with id number ${id}`)
+  const ramen = ramenData.find((ramen) => ramen.review_ID === +id)
+  if (!ramen) {
+    response.status(404).send(`Sorry no ramen review number ${id} was found.`)
   }
-  response.json(book)
+  response.json(ramen)
 })
+
 
 // Start the server
 app.listen(port, () => {
