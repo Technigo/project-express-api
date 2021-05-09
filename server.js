f3f86e04d55528a933cfd83c35bd74d7039ab8c5
liaback <felicia.liaback@gmail.com>
2021-05-09 12:57:04 +0200

    add edpoint using query and filter by author

--- server.js
+++ server.js
@@ -15,21 +15,30 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 // app.use(cors())
 // app.use(bodyParser.json())
-// 
+//
 
-// endpoint to get all books
+// endpoint to get books by author
 app.get("/books", (req, res) => {
-  res.json(booksData);
+  const { author } = req.query;
+  if (author) {
+    const filteredBooks = booksData.filter(
+      (book) => book.authors.includes(author),
+      res.jsonfilteredBooks
+    );
+  } else {
+    res.json(booksData);
+  }
 });
 
-// endpoint to get one book
+// endpoint to get one book (to get specific items)
 app.get("/books/:id", (req, res) => {
   const { id } = req.params;
-  const book = booksData.find(book => book.bookID === +id);
-  if(!book) {
-    res.send(`No book with id ${id} was found`)
+  const book = booksData.find((book) => book.bookID === +id);
+  if (!book) {
+    res.status(404).send(`No book with id ${id} was found`);
+  } else {
+    res.json(book);
   }
-  res.json(book);
 });
 
 // Start the server
