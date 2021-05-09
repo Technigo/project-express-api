51c31298e0be8681b0e6b7785ca0a31a7eb7ed66
[Darcy Winterhill] <[darcy.winterhill@gmail.com]>
2021-05-09 13:12:03 +0200

    README and all endpoints

--- README.md
+++ README.md
@@ -1,12 +1,50 @@
+Project built during Technigo Bootcamp
+
 # Express API Project
 
-Replace this readme with your own information about your project. 
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+The project was to build an API with some RESTful endpoints using Express.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+In my project I used the netflix-titles.json.
+
+Endpoints:
+
+'/titles'
+Displays all items in the json, both movies and tv shows.
+
+Possible to use the following queries:
+
+?released - release year, YYYY
+?name - movie/tv show title, no need to write entire title to show hits
+?country - production country/ies, no need to write entire country to show hits
+?category - category/genre, no need to write entire category to show hits
+?rating - classifications/maturity ratings
+
+---
+
+'/titles/:id'
+Id param to return a single movie/tv show
+
+---
+
+'/movies'
+Displays all movies
+
+---
+
+'/tvshows'
+Displays all tv shows
+
+Possible to use query:
+
+?seasons - number of seasons
+
+---
+
+'/movies/50_latest'
+Displays the 50 latest released movies based on release year
+
 
 ## View it live
 
--- server.js
+++ server.js
@@ -13,71 +13,57 @@ app.get('/', (req, res) => {
   res.send('Hi! This is the root-endpoint for my Netflix API.')
 })
 
-// endpoint for all titles
+// endpoint for all titles + querys
 app.get('/titles', (req, res) => {
   const { released, name, country, category, rating } = req.query
   let titles = netflixTitles
 
   if (released) {
-    titles = titles.filter(title => title.release_year === +released)
+    titles = titles
+      .filter(title => title.release_year === +released)
   }
 
   if (name) {
-    titles = titles.filter(title => title.title.toString().toLowerCase().replace(/\s/g, '').includes(name.toLowerCase().replace(/\s/g, '')))
+    titles = titles
+      .filter(title => title.title.toString().toLowerCase().replace(/\s/g, '').includes(name.toLowerCase().replace(/\s/g, '')))
   }
 
   if (country) {
-    titles = titles.filter(title => title.country.toLowerCase().replace(/\s/g, '').includes(country.toLowerCase().replace(/\s/g, '')))
+    titles = titles
+      .filter(title => title.country.toLowerCase().replace(/\s/g, '').includes(country.toLowerCase().replace(/\s/g, '')))
   }
 
   if (category) {
-    titles = titles.filter(title => title.listed_in.toLowerCase().replace(/\s/g, '').includes(category.toLowerCase().replace(/\s/g, '')))
+    titles = titles
+      .filter(title => title.listed_in.toLowerCase().replace(/\s/g, '').includes(category.toLowerCase().replace(/\s/g, '')))
   }
 
   if (rating) {
-    titles = titles.filter(title => title.rating.toLowerCase().replace(/-/g, '').includes(rating.toLowerCase().replace(/-/g, '')))
+    titles = titles
+      .filter(title => title.rating.toLowerCase().replace(/-/g, '') === rating.toLowerCase().replace(/-/g, ''))
   }
 
-  res.json({ data: titles })
+  if (titles.length) {
+    res.json({ length: titles.length, data: titles })
+  } else {
+    res.status(404).send({ message: `No match!` })
+  }
+
 })
 
+//param to get a single title
+app.get('/titles/:id', (req, res) => {
+  const { id } = req.params
+  const titleId = netflixTitles.find((title) => title.show_id === +id)
 
-//Ann-sofis lösning funkar INTE
-/* app.get('/titles', (req, res) => {
-  const { released, name, country, category } = req.query
-  
-
-  if (!released && !name && !country && !category) {
-    res.status(404).send({ error: 'error error' })
-    return 
-  }
-
-  let titles = netflixTitles
-
-  if (released) {
-    titles = titles.filter(title => title.release_year === +released)
-  }
-
-  if (name) {
-    titles = titles.filter(title => title.title.toString().toLowerCase().replace(/\s/g, '').includes(name.toLowerCase().replace(/\s/g, '')))
-  }
-
-  if (country) {
-    titles = titles.filter(title => title.country.toLowerCase().replace(/\s/g, '').includes(country.toLowerCase().replace(/\s/g, '')))
-  }
-
-  if (category) {
-    titles = titles.filter(title => title.listed_in.toLowerCase().replace(/\s/g, '').includes(category.toLowerCase().replace(/\s/g, '')))
-  }
-
-  if (netflixTitles.length) {
-  res.json({ data: titles })
+  if (!titleId) {
+    res.status(404).send({ error: `No title with id ${id} found.` })
   } else {
-    res.status(404).send({ error: 'error error error' })
+  res.json({ data: titleId })
   }
-}) */
 
-//OK
+})
+
 //endpoint for movies
 app.get('/movies', (req, res) => {
   const movies = netflixTitles.filter((title) => title.type.toLowerCase() === 'movie')
@@ -86,7 +72,6 @@ app.get('/movies', (req, res) => {
 
 })
 
-//OK
 //endpoint for tv shows + query seasons
 app.get('/tvshows', (req, res) => {
   const { seasons } = req.query
@@ -94,29 +79,23 @@ app.get('/tvshows', (req, res) => {
 
   if (seasons) {
     tvShows = tvShows.filter((title) => title.duration.charAt(0) === seasons)
-  } else {
-    res.json({ data: tvShows })
   }
 
   if (tvShows.length) {
-    res.json({ data: tvShows })
+    res.json({ length: tvShows.length, data: tvShows })
   } else {
-    res.status(404).send({ error: `No tv show with ${seasons} seasons found` })
+    res.status(404).send({ error: `No tv show with ${seasons} seasons found.` })
   }
  
 })
 
-//OK
-//param to get a single title
-app.get('/titles/:id', (req, res) => {
-  const { id } = req.params
-  const titleId = netflixTitles.find((title) => title.show_id === +id)
+//endpoint for 50 latest movies
+app.get('/movies/50_latest', (req, res) => {
+  let movies = netflixTitles.filter((title) => title.type.toLowerCase() === 'movie')
 
-  if (!titleId) {
-    res.status(404).send({ error: `No title with id ${id} found` })
-  } else {
-  res.json({ data: titleId })
-  }
+  movies = movies.sort((a, b) => b.release_year - a.release_year).slice(0, 50)
+
+  res.json({ length: movies.length, data: movies })
 
 })
 
