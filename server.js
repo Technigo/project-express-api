1445d832be325dfacf7b350a62eab5cec654e3d9
Marielle Ohlsson <ohlssonmarielle@gmail.com>
2021-05-09 21:20:27 +0200

    started with readme

--- server.js
+++ server.js
@@ -4,19 +4,7 @@ import cors from 'cors'
 
 import booksData from './data/books.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+// Defines the port the app will run on. Defaults to 8080
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -24,16 +12,11 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here. Endpoint to get all netflix titles
-app.get('/books', (request, response) => {
-  const { author } = request.query
-  if (author) {
-    const booksList = booksData.filter(book => book.authors.includes(author))
-    response.json(booksList)
-  }
-  res.json(booksData)
+app.get('/', (req, res) => {
+  res.send(listEndpoints(app))
 })
 
+//Endpoint to get all books
 app.get('/books', (request, response) => {
   response.json(booksData)
 })
@@ -48,8 +31,16 @@ app.get('/books/:bookid', (request, response) => {
   response.json(book)
 })
 
+//Endpoint to get a specifik author/authors
+app.get('/books/:author', (request, response) => {
+  const { author } = request.query
+  if (author) {
+    const authorsList = booksData.filter(book => book.authors.includes(author))
+    response.json(authorsList)
+  }
+  res.json(booksData)
+})
+
 // Start the server
 app.listen(port, () => {
-  // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
 })
