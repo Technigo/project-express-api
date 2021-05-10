e6dc1ab63ccd059e2fffcba25cf988ba488b9323
mmolliss <malin.vannesjo@gmail.com>
2021-05-10 14:30:11 +0200

    Cleaned up the code

--- server.js
+++ server.js
@@ -2,27 +2,12 @@ import express, { request, response } from 'express'
 import cors from 'cors'
 import booksData from './data/books.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(express.json())
 
-// Start defining your routes here
 app.get('/', (request, response) => {
   response.send('Hello from the other side')
 })
@@ -43,11 +28,8 @@ app.get('/books/:id', (request, response) => {
     response.status(404).send(`The book with id ${id} doesn't exist in this website`)
   }
   response.json(book)
-  // request.params.id
 })
 
-// Start the server
 app.listen(port, () => {
-  // eslint-disable-next-line
   console.log(`WOOP 🚀 Server running on http://localhost:${port}`)
 })
