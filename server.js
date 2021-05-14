9a6465d60779cf51b7417776ebd0d0658d29b3c7
liaback <felicia.liaback@gmail.com>
2021-05-14 10:54:59 +0200

    change from books to music json

--- server.js
+++ server.js
@@ -2,39 +2,40 @@ import express, { request, response } from "express";
 import cors from 'cors'
 
 import booksData from "./data/books.json";
+import musicData from "./data/top-music.json";
 
-const port = process.env.port || 8080;
+const port = process.env.port || 8090;
 const app = express();
 
 app.use(cors())
 app.use(express.json())
 
-// endpoint to get books by author
-app.get("/books", (req, res) => {
-  const { author } = req.query;
-  if (author) {
-    const filteredBooks = booksData.filter(
-      (book) => book.authors.includes(author),
-      res.json(filteredBooks)
-    );
-  } else {
-    res.json(booksData);
-  }
-});
 
-// endpoint to get one book (to get specific items)
-app.get("/books/:id", (req, res) => {
-  const { id } = req.params;
-  const book = booksData.find((book) => book.bookID === +id);
-  if (!book) {
-    res.status(404).send(`No book with id ${id} was found`);
-  } else {
-    res.json(book);
+// endpoint to get music by artist
+app.get('/music', (req,res) => {
+  const { artist } = req.query;
+  if(artist) {
+    const filterArtist = musicData.filter(
+      (artist) => music.artist.includes(artist),
+      res.json(filterArtist)
+    )
   }
-});
+  res.json(musicData)
+})
+
+// endpoint to get one track
+app.get('/music/:id', (req,res) => {
+  const { id } = req.params;
+  const music = musicData.find((item) => item.id === Number(id));
+  if(!music) {
+    res.status(404).send(`No track with id ${id}was found`) 
+  } else {
+    res.json(music);
+  }
+})
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on:${port}`);
 });
