58d651c1f89e906862ead8a0eb36a3dc896a7771
tuttibalutti <emmajbartl@gmail.com>
2021-05-26 23:51:06 +0200

    red level req

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Express API Project
 
-Replace this readme with your own information about your project. 
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+The assignment was to create an RESTful API using Express.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+To solve the problem I created the following API endpoints:
+- GET /top-music that returns an array of 50 tracks.
+- GET /top-music/:id that returns a single track requested by its id.
+- GET /genre that returns an array of unique genres in the dataset.
+- GET /tracks that is able to return an array of tracks filtered by requested artist or track.
+
+If I hade more time I would have tried to build a frontend that could display the data to the user. 
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+Backend deployed on Heroku:
--- server.js
+++ server.js
@@ -1,29 +1,18 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-//import booksData from './data/books.json'
 import listEndpoints from 'express-list-endpoints'
-// import netflixData from './data/netflix-titles.json'
 import topMusicData from './data/top-music.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+// The port the app will run on 
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
+// Middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(express.json())
 
-// Start defining your routes here
+// Routes here
 app.get('/', (req, res) => {
   res.send(listEndpoints(app))
 })
@@ -39,7 +28,7 @@ app.get('/top-music/:id', (req, res) => {
   if (trackId) {
     res.json({ data: trackId })   
   } else {
-    res.status(404).json({ error: `Sorry, we didn't find id: ${id}` })
+    res.status(404).json({ error: `Sorry, we didn't find a track with id ${id}` })
   }
 
   res.json(trackId)
@@ -54,11 +43,10 @@ app.get('/genre', (req, res) => {
   res.json({ data: uniqueGenres })
 })
 
+
 app.get('/tracks', (req, res) => {
   const { artistName } = req.query
   const { trackName } = req.query
-
-  console.log(trackName)
   
   let filteredTracks = topMusicData
 
@@ -79,10 +67,6 @@ app.get('/tracks', (req, res) => {
   res.json({ data: filteredTracks })
 })
 
-app.get('/tracks/popular', (req, res) => {
-
-})
-
 app.get('/tracks/recommended', (req, res) => {
   res.json('Wanna listen to recommended tracks? Coming soon!')
 })
@@ -90,44 +74,6 @@ app.get('/tracks/recommended', (req, res) => {
 app.get('/tracks/trending', (req, res) => {
   res.json("Wanna know the latest music trends? Hold on, we're working on it!" )
 })
-
-/*app.get('/books', (req, res) => {
-  res.json(booksData)
-})
-
-app.get('/books/:isbn', (req, res) => {
-  const { isbn } = req.params
-  const findIsbn = booksData.find((book) => book.isbn === +isbn)
-
-  if (findIsbn) {
-    res.json({ data: findIsbn })   
-  } else {
-    res.status(404).json({ error: 'Not found' })
-  }
-   
-})
-
-app.get('/authors', (req, res) => {
-  const authorsName = booksData.filter((book) => book.authors)
-  res.json(authorsName)
-})
-
-app.get('/ratings', (req, res) => {
-  const booksTopRated = booksData.filter((book) => book.average_rating >= 4.5)
-  booksTopRated.sort(function (a, b) {
-    return b.average_rating - a.average_rating
-  })
-  res.json(booksTopRated)
-})
-
-app.get('/pages', (req, res) => {
-  const booksUnderHundredPages = booksData.filter((book) => book.num_pages <= 100)
-  booksUnderHundredPages.sort(function (a, b) {
-    return a.num_pages - b.num_pages
-  })
-  res.json(booksUnderHundredPages)
-})*/
-
 
 // Start the server
 app.listen(port, () => {
