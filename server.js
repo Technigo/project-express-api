662bf36909e913504f0a5f5de36aea7799fd172d
MT-dotse <madelene.trang333@gmail.com>
2021-12-06 19:56:03 +0200

    created two endpoints, one to find a specific show/movie and one filtering all the movies

--- server.js
+++ server.js
@@ -1,33 +1,54 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
+import netflixData from './data/netflix-titles.json';
 
+//import booksData from './data/books.json';
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
-// 
+//
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
+
 // import topMusicData from './data/top-music.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
+//----END POINTS----
 // Start defining your routes here
+//endpoints to return a collection of results
 app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+	res.json(netflixData);
+});
+
+// endpoint to return a single result
+app.get('/shows/:show_id', (req, res) => {
+	const { show_id } = req.params;
+	const uniqueShowID = netflixData.find((item) => item.show_id === +show_id);
+
+	if (!uniqueShowID) {
+		res.status(404).send('No shows/movies found with that ID');
+	} else {
+		res.json(uniqueShowID);
+	}
+});
+
+app.get('/type/:type', (req, res) => {
+	const type = req.params.type; //get data from the variable
+	let onlyMovies = netflixData.filter((item) => item.type === 'Movie');
+	res.json(onlyMovies);
+});
 
 // Start the server
 app.listen(port, () => {
-  // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+	// eslint-disable-next-line
+	console.log(`Server running on http://localhost:${port}`);
+});
