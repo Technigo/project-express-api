466b9a7aa7921d354f5a76e703a28b51eb34bf1e
Gustav Frid <gustav.frid@gmail.com>
2021-12-06 11:12:48 +0100

    initial setup

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ import cors from 'cors'
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
-// 
+//
 // import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
+import avocadoSalesData from './data/avocado-sales.json'
 // import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
@@ -24,6 +24,10 @@ app.use(express.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/users', (req, res) => {
+  res.json(avocadoSalesData)
 })
 
 // Start the server
