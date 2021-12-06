7da3aa0a42d680113d9955ec522551a3e5a96f87
Никитина Юля <nik.iuliiasw@gmail.com>
2021-12-06 16:15:22 +0100

    added more endpoints

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from 'express'
 import cors from 'cors'
 
 import booksData from './data/books.json'
+//import listEndpoints from 'express-list-endpoints'
+
+const allRoutes = require('express-list-endpoints');
 
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
@@ -15,15 +18,27 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// // Start defining your routes here
-// app.get('/', (req, res) => {
-//   res.send('Hello from Julia')
-// })
+// Start defining your routes here
+app.get('/', (req, res) => {
+  res.send(allRoutes(app))
+})
 
-// // Endpoint for all the books
-// app.get('/books', (req, res) => {
-//   res.json(booksData)
-// })
+// Endpoint for all the books
+app.get('/books', (req, res) => {
+  res.json(booksData)
+})
+
+// Endpoint with the list of popular books and a query for top n books (?top=3 or ?top=35)
+app.get('/books/popular', (req, res) => {
+  const topNumber = req.query.top
+  const sortedTopToBottom = booksData.sort((a, b) => b.average_rating - a.average_rating)
+  if (topNumber) {
+    const topSelected = sortedTopToBottom.splice(0, topNumber)
+    res.json(topSelected)
+  } else {
+    res.json(sortedTopToBottom)
+  }
+})
 
 // Endpoint to search one book by its id
 app.get('/books/:id', (req, res) => {
@@ -31,9 +46,11 @@ app.get('/books/:id', (req, res) => {
   const selectedBook = booksData.find((item) => item.bookID === +bookId)
   if (!selectedBook) {
     res.status(404).send(`Sorry, there is no book with id number ${bookId}`)
+  } else {
+    res.json(selectedBook)
   }
-  res.json(selectedBook)
 })
+
 
 // Start the server
 app.listen(port, () => {
