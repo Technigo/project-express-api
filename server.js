f199c062277762976a75345a96e98a323412daca
Linnea <linnea.iseb@gmail.com>
2021-12-07 13:57:47 +0100

    Blue requirements

--- server.js
+++ server.js
@@ -1,33 +1,43 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+// importing the JSON from the books.json
+import booksData from './data/books.json';
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+// Defines the port the app will run on. Defaults to 8080, but can be
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+  res.send('Hello world');
+});
 
-// Start the server
+// gets all books
+app.get('/books', (req, res) => {
+  res.json(booksData);
+});
+
+// returns an array of books from a specified author
+app.get('/author/:authors', (req, res) => {
+  const author = req.params.authors;
+  const showAuthor = booksData.filter((item) => item.authors === author);
+  res.json(showAuthor);
+});
+
+// returns one book with the specified ISBN-number
+app.get('/isbn/:isbn', (req, res) => {
+  const isbn = req.params.isbn;
+  const myIsbn = booksData.filter((item) => item.isbn === +isbn);
+  res.json(myIsbn);
+});
+
+// Start the server on 8080 local server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
