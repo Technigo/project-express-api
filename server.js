120ca49a26a60b768426fb3ba928205af80e7735
Lisa Blomé <81302626+annaester@users.noreply.github.com>
2021-12-07 21:05:58 +0100

    started with two endpoints

--- server.js
+++ server.js
@@ -1,33 +1,56 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
-// 
+//
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
+import booksData from "./data/books.json";
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/books", (req, res) => {
+  res.send(booksData);
+});
+
+//get a specific book based on id, using param
+app.get("/books/:bookID", (req, res) => {
+  const { bookID } = req.params;
+
+  const bookDetail = booksData.find((book) => book.bookID === +bookID);
+
+  if (!bookDetail) {
+    res.status(404).send("no book found");
+  } else {
+    res.json(bookDetail);
+  }
+  //where you can get the title, author, rating, language
+});
+
+app.get("/books/authors", (req, res) => {
+  // const { authors } = req.params;
+
+  const allAuthors = booksData.map((author) => console.log(author));
+  res.send(allAuthors);
+
+  //sort it alphabetically?
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
