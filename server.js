24a3cf16ffe2a4901c48f9d54f4fce0321198210
Elin Elmvik Diczfalusy <elinelmvikdiczfalusy@Elins-iMac.local>
2021-12-07 15:37:56 +0100

    added first end points

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express"
+import cors from "cors"
+import netflixTitles from "./data/netflix-titles.json"
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
-// 
+//
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
 // import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
+
+console.log(netflixTitles.length)
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -22,8 +25,37 @@ app.use(cors())
 app.use(express.json())
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+app.get("/", (req, res) => {
+  res.send("Hello world")
+})
+
+app.get("/", (req, res) => {
+  res.json(netflixTitles)
+})
+
+app.get("/titles", (req, res) => {
+  res.json(netflixTitles)
+})
+
+app.get("/year/:year", (req, res) => {
+  const year = req.params.year
+  const showType = req.query.type
+  console.log("showType", showType)
+
+  let titlesReleaseYear = netflixTitles.filter(
+    item => item.release_year === +year
+  )
+
+  if (showType === "Movie") {
+    console.log("hello")
+    titlesReleaseYear = titlesReleaseYear.filter(item => item.type === "Movie")
+  } else {
+    titlesReleaseYear = titlesReleaseYear.filter(
+      item => item.type === "TV Show"
+    )
+  }
+
+  res.json(titlesReleaseYear)
 })
 
 // Start the server
