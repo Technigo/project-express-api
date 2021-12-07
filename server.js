9745ca2204c13b55130106670749bf9817382fb6
Anna Dimitrakopoulos <dimitrakopoulos.anna@gmail.com>
2021-12-07 11:47:49 +0100

    added filtering for collection and single result endpoint

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import booksData from "./data/books.json";
+import goldenGlobesData from "./data/golden-globes.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
@@ -15,28 +15,84 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send(booksData);
+  res.send(goldenGlobesData);
 });
 
-app.get("/books", (req, res) => {
-  const { author, title } = req.query;
-  let filteredBooks = booksData;
+app.get("/winners", (req, res) => {
+  let filteredMovies = goldenGlobesData.filter((item) => item.win);
+  res.json(filteredMovies);
+});
 
-  if (author) {
-    filteredBooks.filter((item) =>
-      item.authors.toLocaleLowerCase().includes(author.toLocaleLowerCase())
-    );
-  }
-  if (title) {
-    filteredBooks.filter((item) =>
-      item.title.toLocaleLowerCase().includes(title.toLocaleLowerCase())
-    );
-  }
+app.get("/movies", (req, res) => {
+  let filteredMovies = goldenGlobesData.filter((item) =>
+    item.category.includes("Motion Picture")
+  );
+  res.json(filteredMovies);
+});
 
-  if (filteredBooks.length === 0) {
-    res.status(404).json("Sorry we couldn't find the book you're looking for!");
+app.get("/series", (req, res) => {
+  let filteredMovies = goldenGlobesData.filter((item) =>
+    item.category.includes("Series")
+  );
+  res.json(filteredMovies);
+});
+
+app.get("/animated", (req, res) => {
+  let filteredMovies = goldenGlobesData.filter((item) =>
+    item.category.includes("Animated")
+  );
+  res.json(filteredMovies);
+});
+
+app.get("/foreign", (req, res) => {
+  let filteredMovies = goldenGlobesData.filter((item) =>
+    item.category.includes("Foreign Language")
+  );
+  res.json(filteredMovies);
+});
+
+app.get("/winners/movies", (req, res) => {
+  let filteredMovies = goldenGlobesData.filter(
+    (item) => item.category.includes("Motion Picture") && item.win
+  );
+  res.json(filteredMovies);
+});
+
+app.get("/winners/series", (req, res) => {
+  let filteredMovies = goldenGlobesData.filter(
+    (item) => item.category.includes("Series") && item.win
+  );
+  res.json(filteredMovies);
+});
+
+app.get("/winners/animated", (req, res) => {
+  let filteredMovies = goldenGlobesData.filter(
+    (item) => item.category.includes("Animated") && item.win
+  );
+  res.json(filteredMovies);
+});
+
+app.get("/winners/foreign", (req, res) => {
+  let filteredMovies = goldenGlobesData.filter(
+    (item) => item.category.includes("Foreign Language") && item.win
+  );
+  res.json(filteredMovies);
+});
+
+app.get("/nominee/:nominee", (req, res) => {
+  const { nominee } = req.params;
+  const showNominee = goldenGlobesData.find(
+    (item) =>
+      item.nominee.toString().toLocaleLowerCase() ===
+      nominee.toString().toLocaleLowerCase()
+  );
+
+  if (!showNominee) {
+    res
+      .status(404)
+      .send("Sorry, we couldn't find the nominee you're looking for!");
   } else {
-    res.json(filteredBooks);
+    res.json(showNominee);
   }
 });
 
