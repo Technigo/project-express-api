f1b71c17394cc000697f300cb7edb194d5e48102
Lovisa Carling <lovisa.carling@gmail.com>
2021-12-07 20:02:48 +0100

    added first route

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ import cors from 'cors'
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
-// 
+//
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
 // import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import topMusicData from './data/top-music.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
@@ -23,7 +23,19 @@ app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.json(topMusicData)
+})
+
+app.get('/music/:id', (req, res) => {
+  const { id } = req.params
+
+  const musicId = topMusicData.find((music) => music.id === +id)
+
+  if (!musicId) {
+    res.status(404).send('No music found...')
+  } else {
+    res.json(musicId)
+  }
 })
 
 // Start the server
