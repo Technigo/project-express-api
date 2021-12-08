29ebff10d1b293b708ea2d5bbdda30f9e704c887
Lisa Blomé <81302626+annaester@users.noreply.github.com>
2021-12-08 12:36:45 +0100

    updated endpoints and used query

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ import cors from "cors";
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
 import booksData from "./data/books.json";
+
+// console.log(booksData.length); -- 450st!
+
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
@@ -23,11 +26,42 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/books", (req, res) => {
-  res.send(booksData);
+  const { author } = req.query;
+
+  // const booksDataFirstTwenty = booksData.slice(0, 20);
+
+  let authorToFind = booksData;
+
+  if (author) {
+    authorToFind = authorToFind.filter(
+      (item) => item.authors.toLowerCase().indexOf(author.toLowerCase()) !== -1
+    );
+  }
+
+  res.send({
+    response: authorToFind,
+    success: true,
+  });
+
+  // res.send({
+  //   response: booksData,
+  //   success: true,
+  // });
+
+  // books?firstQuery=helloOne&secondQuery=helloTwo
+
+  // res.send(booksDataFirstTwenty);
+
+  // console.log(booksDataFirstTwenty.length);
+
+  //borde kunna göra något i stil med
+  // chunk = 20
+  //i = 0, i < booksData;
+  //return if (chunk === 1-20) else if (21-40)
 });
 
 //get a specific book based on id, using param
-app.get("/books/:bookID", (req, res) => {
+app.get("/books/bookid/:bookID", (req, res) => {
   const { bookID } = req.params;
 
   const bookDetail = booksData.find((book) => book.bookID === +bookID);
@@ -40,13 +74,29 @@ app.get("/books/:bookID", (req, res) => {
   //where you can get the title, author, rating, language
 });
 
-app.get("/books/authors", (req, res) => {
+app.get("/books/topbooks", (req, res) => {
+  const { topBooks } = req.params;
+
+  const topRatedBooks = booksData.filter((book) => book.average_rating >= +4);
+  res.json(topRatedBooks);
+  // console.log(topBooks.length);
+});
+
+app.get("/books/authors/:authors", (req, res) => {
   // const { authors } = req.params;
 
-  const allAuthors = booksData.map((author) => console.log(author));
-  res.send(allAuthors);
+  const allAuthors = booksData.map((author) => author.authors).sort();
+  res.json(allAuthors);
+
+  // const oneAuthor = booksData.find((author) => booksData.authors === author);
+  // res.json(oneAuthor);
 
   //sort it alphabetically?
+});
+
+app.post("/postbook", (req, res) => {
+  const { body } = req;
+  res.send(body);
 });
 
 // Start the server
