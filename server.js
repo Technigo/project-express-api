e789352a350724ce253aa753f648f3151b76ccb0
MT-dotse <madelene.trang333@gmail.com>
2021-12-08 23:19:07 +0200

    added six query params, res.status msg and comments

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import netflixData from './data/netflix-titles.json';
-
-//import booksData from './data/books.json';
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-//
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-
-// import topMusicData from './data/top-music.json'
+import listEndpoints from 'express-list-endpoints';
 
 // Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
@@ -22,29 +14,107 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-//----END POINTS----
-// Start defining your routes here
-//endpoints to return a collection of results
+//----ENDPOINTS----
+
+//root endpoint
 app.get('/', (req, res) => {
-	res.json(netflixData);
+	res.send('Hello World');
 });
 
-// endpoint to return a single result
-app.get('/shows/:show_id', (req, res) => {
-	const { show_id } = req.params;
-	const uniqueShowID = netflixData.find((item) => item.show_id === +show_id);
+//showing all the possible enpoints in the app
+app.get('/endpoints', (req, res) => {
+	res.send(listEndpoints(app));
+});
+
+//----COLLECTION OF RESULT------
+// endpoint to show all the Netflix titles
+// query params to filter and get collections of results
+app.get('/shows', (req, res) => {
+	const { type, country, release, cast, genre, director } = req.query;
+
+	let netflixShowsToSend = netflixData;
+
+	if (type) {
+		netflixShowsToSend = netflixShowsToSend.filter(
+			(item) => item.type.toLowerCase().indexOf(type.toLowerCase()) !== -1
+		);
+	}
+
+	if (country) {
+		netflixShowsToSend = netflixShowsToSend.filter(
+			(item) => item.country.toLowerCase().indexOf(country.toLowerCase()) !== -1
+		);
+	}
+
+	if (release) {
+		netflixShowsToSend = netflixShowsToSend.filter(
+			(item) => item.release_year === +release
+		);
+	}
+
+	if (cast) {
+		netflixShowsToSend = netflixShowsToSend.filter(
+			(item) => item.cast.toLowerCase().indexOf(cast.toLowerCase()) !== -1
+		);
+	}
+
+	if (genre) {
+		netflixShowsToSend = netflixShowsToSend.filter(
+			(item) => item.listed_in.toLowerCase().indexOf(genre.toLowerCase()) !== -1
+		);
+	}
+
+	if (director) {
+		netflixShowsToSend = netflixShowsToSend.filter(
+			(item) =>
+				item.director.toLowerCase().indexOf(director.toLowerCase()) !== -1
+		);
+	}
+
+	res.json({
+		response: netflixShowsToSend,
+		success: true,
+	});
+});
+
+//----SINGLE RESULT------
+// endpoint to return a single ID
+app.get('/id/:id', (req, res) => {
+	const { id } = req.params;
+	const uniqueShowID = netflixData.find((item) => item.show_id === +id);
 
 	if (!uniqueShowID) {
-		res.status(404).send('No shows/movies found with that ID');
+		res.status(404).json({
+			response: 'No shows/movies found with that ID',
+			success: false,
+		});
 	} else {
-		res.json(uniqueShowID);
+		res.status(200).json({
+			response: uniqueShowID,
+			success: true,
+		});
 	}
 });
 
-app.get('/type/:type', (req, res) => {
-	const type = req.params.type; //get data from the variable
-	let onlyMovies = netflixData.filter((item) => item.type === 'Movie');
-	res.json(onlyMovies);
+//endpoint to find a single title
+app.get('/title/:title', (req, res) => {
+	const { title } = req.params; //get data from the variable
+	const titleName = netflixData.find(
+		(item) => item.title.toLowerCase() === title
+	);
+
+	if (!titleName) {
+		res.status(404).json({
+			response: 'No title found with that name',
+			success: false,
+		});
+	} else {
+		res.status(200).json({
+			response: titleName,
+			success: true,
+		});
+	}
+	res.json(titleName);
 });
 
 // Start the server
