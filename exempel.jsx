badf5a7b221262aec8ff020dc450caee13268e0c
HedvigM <hedvig@mejstedt.se>
2021-12-08 15:53:37 +0100

    used params method and query method

---
+++ exempel.jsx
@@ -0,0 +1,7 @@
+
+const object = { subobject: "detta är subobject", another: "detta är another" };
+
+const subobject = "another";
+
+console.log(object.subobject);
+console.log(object[subobject]);
--- server.js
+++ server.js
@@ -1,33 +1,101 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
+import listEndpoints from 'express-list-endpoints';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import netflixData from './data/netflix-titles.json';
+import topMusicData from './data/top-music.json';
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+/* Defines the port the app will run on. Defaults to 8080, but can be overridden when starting the server. */
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-// Start defining your routes here
+// Takes two arguments. Path and callback function. req, res => request, response. req is handeling what the frontend is sending to the backend and the res is what the backend is sending to the frontend
+
+// lists all the endpoints
+app.get('/endpoints', (req, res) => {
+  res.send(listEndpoints(app));
+});
+
+// Testing the new way of things.
 app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+  res.send('Hello Hedvig ✨ 🥑 🌲');
+});
+
+app.get('/users', (req, res) => {
+  res.json([
+    { id: 1, name: 'Jorid', age: 0 },
+    { id: 2, name: 'Hedvig', age: 30 },
+    { id: 3, name: 'Jobjörn', age: 33 },
+    { id: 4, name: 'Moa', age: 5 },
+  ]);
+});
+
+// ---------- Using the Netflix data ----------//
+
+// '/movies' route will show the whole array with all data from Netflix.
+app.get('/movies', (req, res) => {
+  res.json(netflixData);
+});
+
+// Using the params method to sort by release year.
+app.get('/year/:year', (req, res) => {
+  const year = req.params.year;
+
+  // I use filter because there is a lot of movies that were released in each year.
+  const releasedYear = netflixData.filter(
+    (item) => item.release_year === +year
+  );
+  res.json(releasedYear);
+});
+
+// Using the params method to filter out one movie by title. If the movie are not found, then trigger a 404(418, i'm a teapot).
+app.get('/title/:title', (req, res) => {
+  const { title } = req.params;
+
+  const titleID = netflixData.find((movie) => movie.title === title);
+
+  if (!titleID) {
+    res.status(418).send('No movie found with that id');
+  } else {
+    res.status(200).json({
+      response: titleID,
+      success: true,
+    });
+  }
+});
+// ---------- Using the Music data ----------//
+// Using the query method
+// to sort, write '/music?genre=pop&artist=ed' in the postman or browser.
+app.get('/music', (req, res) => {
+  const { artist, genre } = req.query;
+
+  let musicToSend = topMusicData;
+
+  if (artist) {
+    musicToSend = musicToSend.filter(
+      (item) =>
+        item.artistName.toLowerCase().indexOf(artist.toLowerCase()) !== -1
+    );
+  }
+
+  if (genre) {
+    musicToSend = musicToSend.filter(
+      (item) => item.genre.toLowerCase().indexOf(genre.toLowerCase()) !== -1
+    );
+  }
+
+  res.json({
+    response: musicToSend,
+    success: true,
+  });
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
