0f51bbdc93b3ad8d767d42096b1322df3f3f2464
katiewu1 <83988618+katiewu1@users.noreply.github.com>
2021-12-08 16:41:16 +0100

    remove unnecessary json files. change recipes endpoint and add an authors endpoint

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
 import express from 'express'
 import cors from 'cors'
-
 import recipes from './data/recipes.json'
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-//
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
-//
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
@@ -40,46 +29,88 @@ app.use(express.json())
 // }
 
 // Start defining your routes here
+
+// get all recipes with '/recipes'
+// get all recipes from one author with '/recipes?author={req.query}'
 app.get('/recipes', (req, res) => {
-  const showAllRecipes = recipes
   const showAuthorRecipes = req.query.author
+
+  let showAllRecipes = recipes
+
+  // can use this for searching recipes by the author name
+  if (showAuthorRecipes) {
+    showAllRecipes = showAllRecipes.filter(
+      (recipe) =>
+        recipe.Author.toLowerCase().indexOf(showAuthorRecipes.toLowerCase()) !==
+        -1
+    )
+    if (showAllRecipes.length < 1) {
+      res.send(`no recipes found by author ${showAuthorRecipes}`)
+    }
+  }
+
+  res.json({
+    response: showAllRecipes,
+    success: true,
+  })
 
   // console.log(typeof showAllRecipes) // object
   // console.log(typeof showAuthorRecipes) // string
 
-  // console.log(showAllRecipes.length) // -> 1617
-  // console.log(showAuthorRecipes.length) // -> Paul Hollywood: 14 Mary Cadogan: 12
-
-  if (showAuthorRecipes === undefined) {
-    res.json(showAllRecipes)
-  } else {
-    const authorRecipes = showAllRecipes.filter(
-      (recipe) => recipe.Author === showAuthorRecipes
-    )
-    if (authorRecipes.length < 1) {
-      res.status(404).send('Author not found')
-      return
-    }
-    res.json(authorRecipes)
-  }
+  // use this if we already know the author names,
+  // we can retrieve all the author names with endpoint '/authors'
+  // if (showAuthorRecipes === undefined) {
+  //   res.json(showAllRecipes)
+  // } else {
+  //   const authorRecipes = showAllRecipes.filter(
+  //     (recipe) => recipe.Author === showAuthorRecipes
+  //   )
+  //   if (authorRecipes.length < 1) {
+  //     res.status(404).send('Author not found')
+  //     return
+  //   }
+  //   res.json(authorRecipes)
+  // }
 })
 
+// get data of one specific recipe
 app.get('/recipes/:name', (req, res) => {
   const { name } = req.params
-  // remove white spaces with replace(/\s/g, '')
+  // remove white space with an underscore; replace(/\s/g, '_')
   const recipeDetails = recipes.find(
-    (recipe) => recipe.Name.replace(/\s/g, '_').toLowerCase() === name
+    (recipe) =>
+      recipe.Name.replace(/\s/g, '_').toLowerCase() ===
+      name.replace(/\s/g, '_').toLowerCase()
   )
 
   if (!recipeDetails) {
     res.status(404).send('recipe not found')
+    // res.status(404).json({
+    //   response: 'recipe not found',
+    //   success: false,
+    // })
   } else {
     res.json(recipeDetails)
+    // res.status(200).json({
+    //   response: recipeDetails,
+    //   success: true,
+    // })
   }
 })
 
-// list of all authors
-app.get('/authors', (req, res) => {})
+// list of all unique authors
+app.get('/authors', (req, res) => {
+  // store all the authors from every recipe in a variable call allAuthors
+  const allAuthors = recipes.map((recipe) => recipe.Author)
+  // remove all duplicate elements from the array, allAuthors
+  const uniqueAuthors = [...new Set(allAuthors)]
+
+  // authors = authors.filter(
+  //   (author, index, self) => index === self.findIndex((a) => a === author)
+  // )
+
+  res.json(uniqueAuthors)
+})
 
 // Start the server
 app.listen(port, () => {
