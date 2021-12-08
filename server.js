f2538d8ff9a09b44317d845227d7e81451aa5037
Idautterstrom <ida.utterstrom@hotmail.com>
2021-12-08 11:30:17 +0100

    added routes

--- server.js
+++ server.js
@@ -1,33 +1,42 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+import goldenGlobeData from "./data/golden-globes.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/nominations", (req, res) => {
+  res.json(goldenGlobeData);
+});
+
+app.get("/year/:year", (req, res) => {
+  const year = req.params.year;
+  const showWin = req.query.won;
+  let nominationsFromYear = goldenGlobeData.filter(
+    (item) => item.year_award === +year
+  );
+
+  if (showWin) {
+    nominationsFromYear = nominationsFromYear.filter((item) => item.win);
+  }
+  res.json(nominationsFromYear);
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
