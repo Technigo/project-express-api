f7dedee0b9ef2a9dd7ac1a66881a7a2462ddb868
madeleinesvensson <56533906+madeleinesvensson@users.noreply.github.com>
2021-12-08 13:07:33 +0100

    more endpoints added

--- server.js
+++ server.js
@@ -1,33 +1,66 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+// import listEndpoints from "express-list-endpoints";
+import swiftData from "./data/swift.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+// Startingpoint
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+// List of all the songs
+app.get("/songs", (req, res) => {
+  res.json(swiftData);
+});
+
+// specific song based on index
+app.get("/songs/index/:index", (req, res) => {
+  const { index } = req.params;
+  const songIndex = swiftData.find((song) => song.index === +index);
+  if (!songIndex) {
+    res.status(404).send("No song found");
+  } else {
+    res.json(songIndex);
+  }
+});
+
+// find a song by title
+app.get("/songs/title/:title", (req, res) => {
+  const { title } = req.params;
+  const songName = swiftData.find((song) => song.name === title);
+  if (!songName) {
+    res.status(404).send("No song by that name found, try again");
+  } else {
+    res.json(songName);
+  }
+});
+
+// find songs from a specific album
+app.get("/songs/album", (req, res) => {
+  const { album } = req.query;
+  let filteredAlbums = swiftData;
+
+  if (album) {
+    filteredAlbums = filteredAlbums.filter(
+      (item) => item.album.toLowerCase().indexOf(album.toLowerCase()) !== -1
+    );
+    res.json(filteredAlbums);
+  }
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
