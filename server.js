7a2d461ec7186ede4344de80d682b2f43ce5ce16
Gustav Frid <gustav.frid@gmail.com>
2021-12-08 10:58:07 +0100

    added post request/response

--- server.js
+++ server.js
@@ -83,9 +83,9 @@ app.get('/lang/:lang', (req, res) => {
   }
 })
 
-app.get('/top', (req, res) => {
-  const filteredData = data.sort((a, b) => b.average_rating - a.average_rating).slice(0, 10)
-  res.json(filteredData)
+app.post('/post', (req, res) => {
+  const { body } = req
+  res.send(body)
 })
 
 // Start the server
