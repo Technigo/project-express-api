75df81af5b1054415408485de69982183427cc0f
Maria Thomasson <81348201+MariaThomasson@users.noreply.github.com>
2021-12-09 11:30:52 +0100

    Cleaned up the code

--- server.js
+++ server.js
@@ -16,9 +16,39 @@ app.get('/', (req, res) => {
   res.send(listEndpoints(app))
 })
 
-// Endpoint that show all books:
+// route provides all endpoints
+app.get('/endpoints', (req, res) => {
+  res.json({
+    response: listEndpoints(app),
+    success: true
+  })
+})
+
+// Endpoint for all books:
+// search query title and author:
 app.get('/books', (req, res) => {
-  res.json(booksData)
+  const { title, author } = req.query
+  
+  let books = booksData.sort((a, b) => a.bookID - b.bookID)
+
+  if (title) {
+    // checks if book title is a string (because some are not!)
+    books = books.filter((book) => typeof book.title === "string")
+    // checks for title
+    books = books.filter((book) => book.title.toLowerCase().includes(title.toLowerCase()))
+  }
+
+  if (author) {
+    // checks for author
+    books = books.filter((book) => book.authors.toLowerCase().includes(author.toLowerCase()))
+  }
+
+  if (books.length === 0) {
+    // displays 404 error message if author or title search has no results
+    res.status(404).json('Not Found')
+  } else {
+    res.json(booksData)
+  }
 })
 
 // Endpoint that shows all authors in alphabetic order:
@@ -27,22 +57,27 @@ app.get('/books/authors/:authors/', (req, res) => {
   res.json(listAuthors)
 })
 
-// Endpoint that shows books with specific rating:
-app.get('/books/rating/:rating', (req, res) => {
-  const { rating } = req.params.rating
-  const avarageRating = booksData.filter((item) => item.avarage_rating === +rating)
-  res.json(avarageRating)
-})
-
-// Endpoint that shows book with specific id (for ex: /books/id/45)
+// Endpoint that shows book with specific id (for ex: /books/id/4)
 app.get('/books/id/:id', (req, res) => {
-  const { id } = req.params.id
+  const { id } = req.params
   const getBook = booksData.find((book) => book.bookID === +id)
 
   if (!getBook) {
     res.status(404).json('Not found')
   } else {
     res.json({ data: getBook })
+  }
+})
+
+// Endpoint that shows book with specific ISBN number:
+app.get('/books/isbn/:isbn', (req, res) => {
+  const { isbn } = req.params
+  const filteredIsbn = booksData.find((item) => item.isbn === +isbn)
+
+  if (!filteredIsbn) {
+    res.status(404).send('Invalid ISBN, please try again!')
+  } else {
+    res.json({ data: filteredIsbn })
   }
 })
 
