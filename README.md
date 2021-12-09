73f2e51cdf18bfda920446c12bff3bf93023e1e6
Rosanna Dahlberg <wernerrosanna86@gmail.com>
2021-12-09 13:03:33 +0100

    Added new error message and some comments

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # Express API Project
 
-Replace this readme with your own information about your project. 
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+This project was about building an API using Express and was my very first introduction to backend.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+First I decided what API the use - I used a given one, book API.
+
+I decided to make 3 routes for this project: users, books and books3 and on top of that 2 route parameters book:id and ratings. On the rating route I decided to add some filtering, is the rating larger than or equal to the value put in by the user.
+
+Tech & Tools used:
+- RESTful API
+- Postman
+- JavaScript
+- Node.js
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
--- server.js
+++ server.js
@@ -14,16 +14,16 @@ app.use(cors())
 app.use(express.json())
 
 const users = [
-  { id: 1, name: 'Alice', age: 33 },
-  { id: 2, name: 'Bob', age: 23 },
-  { id: 3, name: 'Chris', age: 3 },
-  { id: 4, name: 'Daniela', age: 67 },
+  { id: 1, name: 'Rosanna', age: 35 },
+  { id: 2, name: 'Anton', age: 36 },
+  { id: 3, name: 'Freyja', age: 4 },
+  { id: 4, name: 'Albin', age: 2 },
 ]
 
 // This is our first endpoint //if remove this we get "Cannot GET response in browser" it's built in by express. The server is running but there aren't any information.
 app.get('/', (req, res) => { //This app.get method takes two arguments: path and maybe users or movies. //The second argument is a call back function that communicates with the front end. 
-  //The request req) is handling what the frontend sends to the backend. The response (res) is what the backend send to the front end.
-  res.send('Hello from Rosanna!')
+  //The request (req) is what the frontend sends to the backend. The response (res) is what the backend send to the frontend.
+  res.send('Hello from Rosanna to you!')
 })
 
 // get a list of users
@@ -31,13 +31,13 @@ app.get('/users', (req, res) => {
   res.json(users)
 })
 
-// get a list of the companies with fundings (from json file)
+// get a list of the books (from json file)
 app.get('/books', (req, res) => {
   res.json(booksData)
 })
 
 // get a specific book based on its id, using param
-app.get('/books/:bookid', (req, res) => {
+app.get('/books/:bookid', (req, res) => { //this endpoint is about finding the book with the specific Id
   const { bookid } = req.params
 
   const book = booksData.find(book => book.bookID == bookid)
@@ -56,18 +56,18 @@ app.get('/books3/:rating', (req, res) => {
 
   const books = booksData.filter(book => book.average_rating >= rating)
 
-  if (!books) {
-    res.status(404).send('No book found with that id')
+  if (books.length === 0) {
+    res.status(404).send('No books with that rating was found')
   } else {
     res.json(books)
   }
 })
 
 
-//Remember to use terminal and Postman A LOT!!!
-//nodemon makes the server listen to changes like a liveserver
-
 // Start the server 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port} ROSANNA TEST`)
 })
+
+//Remember to use terminal and Postman A LOT!!!
+//nodemon makes the server listen to changes like a liveserver
