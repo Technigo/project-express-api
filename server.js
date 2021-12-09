8eb869ad355a018e39ea21f2d664587ee35f9da2
Emelie <Emelie@Jophees-MacBook-Pro.local>
2021-12-09 16:27:40 +0100

    changed route path nameings to be RESTful and tested to add a route path for title search

--- server.js
+++ server.js
@@ -24,30 +24,30 @@ app.use(express.json())
 // Start defining your routes here
 
 app.get("/", (req, res) => {
-  res.send("This is the root-endpoint, like the homepage url")
+  res.send("This is the root-endpoint, sort of like the homepage")
 })
 
-app.get("/netflixtitles", (req, res) => {
+app.get("/netflix-titles", (req, res) => {
   res.json(netflixData)
 })
 
-app.get("/year/:year", (req, res) => {
+app.get("/netflix-titles/year/:year", (req, res) => {
   const year = req.params.year
   let releaseYear = netflixData.filter((item) => item.release_year === +year)
   res.json(releaseYear)
 })
 
-app.get("/tvshows", (req, res) => {
+app.get("/netflix-titles/tvshows", (req, res) => {
   const tvshows = netflixData.filter((item) => item.type === "TV Show")
   res.json(tvshows)
 })
 
-app.get("/movies", (req, res) => {
+app.get("/netflix-titles/movies", (req, res) => {
   const movies = netflixData.filter((item) => item.type === "Movie")
   res.json(movies)
 })
 
-app.get("/netflixtitles/:show_id", (req, res) => {
+app.get("/netflix-titles/:show_id", (req, res) => {
   const { show_id } = req.params
 
   const showId = netflixData.find((show) => show.show_id === +show_id)
@@ -56,6 +56,21 @@ app.get("/netflixtitles/:show_id", (req, res) => {
     res.status(404).send(" we couldn't find a show with that name")
   } else {
     res.json(showId)
+  }
+})
+
+app.get("netflix-titles/title/:title", (req, res) => {
+  const { title } = req.params
+
+  const titleID = netflixData.find((movie) => movie.title === title)
+
+  if (!titleID) {
+    res.status(418).send("No movie found with that id")
+  } else {
+    res.status(200).json({
+      response: titleID,
+      success: true,
+    })
   }
 })
 
