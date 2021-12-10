3fec211ccfbab649ebf9708352e86d756ec89485
Jenny Quach <quach.jen@gmail.com>
2021-12-11 00:50:54 +0100

    added two endpoints

--- server.js
+++ server.js
@@ -1,29 +1,37 @@
 import express from 'express'
+import bodyParser from 'body-parser/'
 import cors from 'cors'
+import booksData from './data/books.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
+// Middlewares added to enable cors and json body parsing
 app.use(cors())
 app.use(express.json())
 
-// Start defining your routes here
+// Defining routes
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Book Reviews')
+})
+
+//get a list of books from json file
+app.get('/books', (req, res) => {
+  res.json(booksData)
+})
+
+//get a specific book based on id
+app.get('/books/id/:id', (req, res) => {
+  const { id } = req.params
+
+  const bookId = booksData.find(book => book.bookID === +id)
+
+  if (!bookId) {
+    res.status(404).json('No book found with that id')
+  } else {
+    res.json(bookId)
+  }
 })
 
 // Start the server
