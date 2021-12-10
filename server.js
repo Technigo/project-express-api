28a2e85c187cd614f459ec797552b8e6bd064d6b
Birgit Nehrwein <nehrwein@posteo.de>
2021-12-10 13:06:17 +0100

    added pagination and requests.rest

--- server.js
+++ server.js
@@ -40,6 +40,9 @@ app.get('/countries', (req, res) => {
 
 // route with all shows (both movies and other) from the provided country
 app.get('/countries/:country', (req, res) => {
+  // req is an object with properties such as query and params, 
+  // both query and params are empty objects from the beginning 
+  // every query parameter consists of key and value
   const { country } = req.params
   const contentByCountry = netflixData.filter((item => item.country.toLowerCase() === country))
 
@@ -56,16 +59,16 @@ app.get('/countries/:country', (req, res) => {
   } 
 })
 
-// route provides all movies and has the possibility to query for director, year and actor
+// route provides all movies and has the possibility to query for director, year and actor. You can also do pagination by setting page & limit as query parameters
 app.get('/movies', (req, res) => {
-  const { director, year, actor } = req.query
+  const { director, year, actor, page, limit } = req.query
 
   const movies = netflixData.filter(item => item.type === "Movie")
 
   let filteredMovies = movies
 
   if (director) {
-    filteredMovies = filteredMovies.filter((item) => item.director.toLowerCase().includes(director.toLowerCase()) === true)
+    filteredMovies = filteredMovies.filter((item) => item.director.toLowerCase().includes(director.toLowerCase()))
   }
 
   if (year) {
@@ -73,8 +76,21 @@ app.get('/movies', (req, res) => {
   }
 
   if (actor) {
-    filteredMovies = filteredMovies.filter(item => item.cast.toLowerCase().includes(actor.toLowerCase()) === true)
+    filteredMovies = filteredMovies.filter(item => item.cast.toLowerCase().includes(actor.toLowerCase()))
   }
+
+  // PAGINATION
+  // limit = no. of items to show per page 
+  if (page && limit) {
+    // first index-no of item from filteredMovies we want to display
+    const startIndex = (page - 1) * limit
+    // last index-no of item from filteredMovies we want to display
+    const endIndex = page * limit 
+    // slice gives us what is in between startIndex and endIndex
+    const paginationArray = filteredMovies.slice(startIndex, endIndex)
+   
+    res.json(paginationArray)
+  }  
 
   res.json({
     response: filteredMovies,
@@ -106,7 +122,7 @@ app.get('/movies/title/:title', (req, res) => {
   const { title } = req.params
   const movies = netflixData.filter(item => item.type === "Movie")
 
-  const movie = movies.filter(item => item.title.toLowerCase().includes(title.toLowerCase()) === true)
+  const movie = movies.filter(item => item.title.toLowerCase().includes(title.toLowerCase()))
 
   if (movie.length === 0) {
     res.status(404).json({
