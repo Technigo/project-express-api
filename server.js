c81337cbaef782192671841070fab8f7825cc4b4
Kaliine <80845418+Kaliine@users.noreply.github.com>
2021-12-10 13:40:20 +0100

    added a list for all endpoints

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 import express from 'express'
 import cors from 'cors'
-
+import listEndpoints from 'express-list-endpoints'
 import booksData from './data/books.json' 
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -23,10 +14,15 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// Start defining your routes here. 
+// First endpoint 
 // The app.get method takes two arguments - the path and a call back function, which can be used by the frontend.
 app.get('/', (req, res) => {
   res.send('This is an API with book rating data')
+})
+
+// A list of all endpoints
+app.get('/endpoints', (req, res) => {
+  res.send(listEndpoints(app))
 })
 
 // Endpoint for all books, with all data (from json file)
