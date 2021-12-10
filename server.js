34c55635c9ebae7e78befbae2e5b4529af77bc35
Maria Petersson <mariapetersson@Marias-MacBook.lan>
2021-12-10 15:09:30 +0100

    add endpoints

--- server.js
+++ server.js
@@ -10,16 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const users = [
-  { id: 1, name: 'Alice', age: 99 },
-  { id: 2, name: 'Balice', age: 29 },
-  { id: 3, name: 'Calice', age: 39 },
-  { id: 4, name: 'Dalice', age: 49 },
-];
-
-// Start defining your routes here
+// Routes start here
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.send('Add /endpoints to see all possible endpoints');
 });
 
 // endpoints sends back list of possible endpoints of app
@@ -27,26 +20,28 @@ app.get('/endpoints', (req, res) => {
   res.send(listEndpoints(app));
 });
 
-// and here endpoints start
-app.get('/users', (req, res) => {
-  res.json(users);
-});
-
-// Query arams starts here 👇🏻👇🏼👇🏽
+// Query params starts here 👇🏻👇🏼👇🏽
 // The endpoint can be named anything, but the data need to have the same name as the imported data.
 app.get('/songs', (req, res) => {
-  const { title, artist } = req.query;
+  const { musicGenre, title, artist } = req.query;
 
   let topMusicDataToSort = topMusicData;
 
   // indexOf... includes search results that include part of the words. Hel = both hello and helsinki.
+  if (musicGenre) {
+    topMusicDataToSort = topMusicDataToSort.filter(
+      (item) =>
+        item.genre.toLowerCase().indexOf(musicGenre.toLowerCase()) !== -1
+    );
+  }
+  // if again! we can enter one, both or none of the if statements.
+  // filtering the same results one more time
   if (title) {
     topMusicDataToSort = topMusicDataToSort.filter(
       (item) => item.trackName.toLowerCase().indexOf(title.toLowerCase()) !== -1
     );
   }
-  // if again! we can enter one, both or none of the if statements.
-  // filtering the same results one more time
+
   if (artist) {
     topMusicDataToSort = topMusicDataToSort.filter(
       (item) =>
@@ -62,17 +57,33 @@ app.get('/songs', (req, res) => {
 
 // Endpoints using path params start here 👇🏻👇🏼👇🏽
 
+app.get('/songs/id/:id', (req, res) => {
+  const { id } = req.params;
+
+  const songId = topMusicData.find((song) => song.id === +id);
+
+  if (!songId) {
+    res.status(404).send('No song with that ID was found.');
+  } else {
+    res.status(200).json({
+      response: songId,
+      success: true,
+    });
+  }
+});
+
 app.get('/songs/bpm/:bpm', (req, res) => {
   const { bpm } = req.params;
 
   const songBPM = topMusicData.find((song) => song.bpm === +bpm);
 
   if (!songBPM) {
-    // handle if there is no data
-    res.status(404).send('No song with that bpm was found.');
+    res.status(404).send('No song with that exact bpm was found.');
   } else {
-    // show the data
-    res.json(songBPM);
+    res.status(200).json({
+      response: songBPM,
+      success: true,
+    });
   }
 });
 
@@ -85,7 +96,7 @@ app.get('/songs/artist/:artist', (req, res) => {
 
   if (!artistByName) {
     res.status(404).json({
-      response: 'no artist found by that name',
+      response: 'No artist by that name was found',
       success: false,
     });
   } else {
