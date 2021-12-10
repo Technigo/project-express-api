341de38c94e4006e20d2c5ceff746ad3cb92e44a
JohannaMJ <johanna.mannestal@gmail.com>
2021-12-10 11:14:04 +0100

    cleaning up code

--- server.js
+++ server.js
@@ -2,23 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import listEndpoints from 'express-list-endpoints';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-//
-// import goldenGlobesData from './data/golden-globes.json';
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json';
 import netflixData from './data/netflix-titles.json';
-// import topMusicData from './data/top-music.json';
 
-// Defines the port the app will run on. Defaults to 8080, but can be
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
@@ -28,12 +16,13 @@ app.use(express.json());
 // });
 
 app.get('/', (req, res) => {
-  res.send('Welcome to netFlix data endpoints');
-});
-
-app.get('/endpoints', (req, res) => {
+  // res.send('Welcome to netFlix data endpoints');
   res.json(listEndpoints(app));
 });
+
+// app.get('/endpoints', (req, res) => {
+
+// });
 
 //endpoints by using one or more queries
 app.get('/shows', (req, res) => {
