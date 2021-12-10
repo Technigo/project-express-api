9105a66dbabf06671fd423b916d641434b03ee7a
Lisa Blomé <81302626+annaester@users.noreply.github.com>
2021-12-10 10:52:52 +0100

    added new search - title

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import cors from "cors";
 //
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
+
 import booksData from "./data/books.json";
 
 // console.log(booksData.length); -- 450st!
@@ -25,8 +26,16 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
+app.get("/", (req, res) => {
+  res.send("Please use an endpoint to fetch requests!");
+});
+
+//main fetch for all books
 app.get("/books", (req, res) => {
-  const { author } = req.query;
+  const { author, title } = req.query;
+
+  // Started with pagination but didn't get the response I wanted.
+  // Wanted the fetch to be on /books and not ?page=n&limit=n
 
   // const page = parseInt(req.query.page);
   // const limit = parseInt(req.query.limit);
@@ -54,10 +63,17 @@ app.get("/books", (req, res) => {
   // res.send(results);
 
   let authorToFind = booksData;
+  let titleToFind = booksData;
 
   if (author) {
     authorToFind = authorToFind.filter(
       (item) => item.authors.toLowerCase().indexOf(author.toLowerCase()) !== -1
+    );
+  }
+
+  if (title) {
+    titleToFind = titleToFind.filter(
+      (item) => item.title.toLowerCase().indexOf(title.toLowerCase()) !== -1
     );
   }
 
@@ -67,13 +83,14 @@ app.get("/books", (req, res) => {
   });
 
   res.send({
+    response: titleToFind,
+    success: true,
+  });
+
+  res.send({
     response: booksData,
     success: true,
   });
-});
-
-app.get("/", (req, res) => {
-  res.send("Please use an endpoint to fetch requests!");
 });
 
 //get a specific book based on id, using param
@@ -87,22 +104,22 @@ app.get("/books/bookid/:bookID", (req, res) => {
   } else {
     res.json(bookDetail);
   }
-  //where you can get the title, author, rating, language
+  //where you can get the title, author, rating, language for each book in your frontend
 });
 
+//all the books with top rating (at least 4/5)
 app.get("/books/topbooks", (req, res) => {
-  const { topBooks } = req.params;
-
   const topRatedBooks = booksData.filter((book) => book.average_rating >= +4);
   res.json(topRatedBooks);
-  // console.log(topBooks.length);
 });
 
+//search the list for a specific author ang get all her/his books
 app.get("/books/authors/:authors", (req, res) => {
   const allAuthors = booksData.map((item) => item.authors).sort();
   res.send(allAuthors);
 });
 
+//just at test to post in postman
 app.post("/postbook", (req, res) => {
   const { body } = req;
   res.send(body);
