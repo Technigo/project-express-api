d1ebebeefcc6800cf96d94327135a9515ea5eaa9
NinaAlejandra <Niinaa___@hotmail.com>
2021-12-13 02:28:08 +0100

    first draft, blue level

--- server.js
+++ server.js
@@ -1,33 +1,67 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+import listEndpoints from "express-list-endpoints";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+// Dataset from Technigo
+import topMusicData from "./data/top-music.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+// Middleware which enables cprs and json body parsing
+app.use(cors());
+app.use(express.json());
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+// This shows up to tell you which endpoints we have
+app.get("/", (req, res) => {
+  res.send("Add /endpoints to see all endpoints");
+});
+
+app.get("/endpoints", (req, res) => {
+  res.send(listEndpoints(app));
+});
+
+// First endpoint for all tracks
+app.get("/tracks", (req, res) => {
+  res.json(topMusicData);
+});
+
+// second endpoint for id
+app.get("/tracks/:id", (req, res) => {
+  const { id } = req.params;
+
+  const trackId = topMusicData.find((tracks) => tracks.id === +id);
+
+  if (!trackId) {
+    res.status(404).send("No track found with that id");
+  } else {
+    res.json(trackId);
+  }
+});
+
+// Third endpoint for name of artist
+app.get("/tracks/artist/:artist", (req, res) => {
+  const { artistName } = req.params;
+
+  const artistByName = topMusicData.find(
+    (item) => item.artistName === artistName
+  );
+
+  if (!artistByName) {
+    res.status(404).json({
+      response: "No artist found with that name",
+      success: false,
+    });
+  } else {
+    res.status(200).json({
+      response: artistByName,
+      success: true,
+    });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
-  // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  // eslint-disable-next-line no-console
+  console.log(`Server running on http://localhost:${port}`);
+});
