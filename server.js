d9498cdae21c572a673e310b4b3aff5efb4f902b
loulunds <lousanne.lundstrom@gmail.com>
2021-12-17 12:45:47 +0100

    api

--- server.js
+++ server.js
@@ -1,33 +1,84 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import listEndpoints from 'express-list-endpoints';
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+import dragQueens from './data/drag-queens.json';
+
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello world');
+// });
+
+// app.get('/', (req, res) => {
+//   res.send(listEndpoints(app))
+// });
+
+app.get('/endpoints', (req, res) => {
+  res.send(listEndpoints(app));
+});
+
+app.get('/queens', (req, res) => {
+  res.json(dragQueens);
+});
+
+app.get('/queens/id/:id', (req, res) => {
+  const { id } = req.params;
+  const queenId = dragQueens.find((queen) => queen.id === +id);
+  if (!queenId) {
+    res.status(404).send("Sorry huney can't find your queen, try again");
+  } else {
+    res.json(queenId);
+  }
+});
+
+app.get('/queens/name/:name', (req, res) => {
+  const { name } = req.params;
+  const queenName = dragQueens.find(
+    (queen) => queen.name.toLowerCase().indexOf(name.toLowerCase()) !== -1
+  );
+
+  if (!queenName) {
+    res.status(404).send("Sorry hunney can't find your queen, try again");
+  } else {
+    res.json(queenName);
+  }
+});
+
+app.get('/queens/winners', (req, res) => {
+  res.json(dragQueens.filter((queen) => queen.winner === true));
+});
+
+app.get('/queens/congeniality', (req, res) => {
+  res.json(dragQueens.filter((queen) => queen.missCongeniality === true));
+});
+
+app.get('/queens/seasons/:seasons', (req, res) => {
+  const { seasons } = req.params;
+  const queensPerSeason = dragQueens.filter(
+    (queen) => queen.seasons[0].seasonNumber === seasons
+  );
+
+  if (!queensPerSeason) {
+    res.status(404).send("Sorry hunney, we don't have that season yet");
+  } else {
+    res.json(queensPerSeason);
+  }
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
