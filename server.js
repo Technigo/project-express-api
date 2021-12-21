df451fde142206e36dd86504a22eb3946880bd15
JessicaNordahl <jessica.nordahl@hotmail.com>
2021-12-21 11:41:22 +0100

    Creates endpoints with single and colection(array) results

--- server.js
+++ server.js
@@ -1,29 +1,76 @@
 import express from 'express'
 import cors from 'cors'
+import listEndpoints from 'express-list-endpoints'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import goldenGlobesData from './data/golden-globes.json'
+
+const data = goldenGlobesData
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+// overridden when starting the server. For example: PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
+app.use(cors())// Add middlewares to enable cors and json body parsing
 app.use(express.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+app.get('/', (req, res) => {// This is an endpiont, and it takes two arguments. First a path and then a call back function
+  res.send('Hello from backend-me!')
+})
+
+app.get('/endpoints', (req, res) => {//this endpoint is going to tell us all possible endpoint we have
+  res.send(listEndpoints(app))
+})
+
+app.get('/nominations', (req, res) => {  //here I put a function to pass two arguments: request and use response to add stuff to it
+  res.json(data)
+})
+
+app.get('/nominations/nominee/:nominee', (req, res) => {
+  const { nominee } = req.params
+  const nomineeId = goldenGlobesData.find(item => item.nominee.toLowerCase() === nominee.toLowerCase())
+
+  if(!nomineeId) {
+    console.log('No nominee')
+    res.status(404).json({
+      response: 'No nominee found with that name.',
+      success: false
+    })
+  }  else { 
+    res.json(nomineeId)
+    }  
+})
+
+app.get('/nominations/category/:category', (req, res) => {
+  const { category } = req.params
+  const categoryId = goldenGlobesData.find(item => item.category.toLowerCase() === category.toLowerCase())
+  
+  if(!categoryId) {
+    console.log('nothing found')
+    res.status(404).json({
+      response: 'No category found with that name.',
+      success: false    
+    })
+  }  else { 
+    res.json(categoryId)
+    }
+})
+
+app.get('/year/:year', (req, res) => {
+  const year = req.params.year
+  const showWin = req.query.win
+  let nominationsFromYear = data.filter((item) => item.year_award === +year)
+  
+  if (!showWin) {
+    res.status(404).json({
+      response: 'No award found in given year',
+    success: false
+    })
+  } else { 
+    nominationsFromYear =  nominationsFromYear.filter((item) => item.win)
+    }
+  
+  res.json(nominationsFromYear)
 })
 
 // Start the server
