f8af3b70aba1825a124fcebe0fdf63f25995db29
Rephili <72921631+Rephili@users.noreply.github.com>
2022-02-16 14:58:47 +0100

    removed comments

--- server.js
+++ server.js
@@ -1,119 +1,106 @@
-import express from "express";
-import cors from "cors";
-import listEndpoints from "express-list-endpoints";
+import express from "express"
+import cors from "cors"
+import listEndpoints from "express-list-endpoints"
 
-import avocadoSalesData from "./data/avocado-sales.json";
+import avocadoSalesData from "./data/avocado-sales.json"
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-//
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
-// Defines the port the app will run on. Defaults to 8080, but can be
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+const port = process.env.PORT || 8080
+const app = express()
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 // Main route
 app.get("/", (req, res) => {
-  res.send("Go to /endpoints to see all the routes");
-});
+  res.send("Go to /endpoints to see all the routes")
+})
 
 // Get the list of endpoints
 app.get("/endpoints", (req, res) => {
-  res.send(listEndpoints(app));
-});
+  res.send(listEndpoints(app))
+})
 
 // Get the list of avocado sales
 app.get("/avocadosales", (req, res) => {
-  res.json(avocadoSalesData);
-});
+  res.json(avocadoSalesData)
+})
 
 // Sort by lowest average price
 app.get("/avocadosales/lowestaverageprice", (req, res) => {
-const ascendingAveragePrice = avocadoSalesData.sort((a, b) => {
-return a.averagePrice - b.averagePrice;
-});
+  const ascendingAveragePrice = avocadoSalesData.sort((a, b) => {
+    return a.averagePrice - b.averagePrice
+  })
 
-res.json(ascendingAveragePrice);
-});
+  res.json(ascendingAveragePrice)
+})
 
 // Sort by highest average price
 app.get("/avocadosales/highestaverageprice", (req, res) => {
   const descendingAveragePrice = avocadoSalesData.sort((a, b) => {
-  return b.averagePrice - a.averagePrice;
-  });
-  
-  res.json(descendingAveragePrice);
-  });
+    return b.averagePrice - a.averagePrice
+  })
+
+  res.json(descendingAveragePrice)
+})
 
 // Get avocado sales based on region, using path param
 app.get("/avocadosales/region/:region", (req, res) => {
-  const { region } = req.params;
+  const { region } = req.params
 
   const saleByRegion = avocadoSalesData.filter(
     (sale) => sale.region.toLowerCase().indexOf(region.toLowerCase()) !== -1
-  );
+  )
 
   if (saleByRegion.length > 0) {
     res.status(200).json({
       response: saleByRegion,
       success: true,
-    });
+    })
   } else {
     res.status(404).json({
       response: "No sale found for that region",
       success: false,
-    });
+    })
   }
-});
+})
 
 // Get avocado sales based on date, using path param
 app.get("/avocadosales/date/:date", (req, res) => {
-  const { date } = req.params;
+  const { date } = req.params
 
   const saleByDate = avocadoSalesData.filter(
     (sale) => sale.date.indexOf(date) !== -1
-  );
+  )
 
   if (saleByDate.length > 0) {
     res.status(200).json({
       response: saleByDate,
       success: true,
-    });
+    })
   } else {
     res.status(404).json({
       response: "No sale found for that date",
       success: false,
-    });
+    })
   }
-});
+})
 
 // Get a specific avocado sale based on id, using path param
 app.get("/avocadosales/id/:id", (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
 
-  const saleId = avocadoSalesData.find((sale) => sale.id === +id);
+  const saleId = avocadoSalesData.find((sale) => sale.id === +id)
 
   if (!saleId) {
-    res.status(404).send("No sale found with that id");
+    res.status(404).send("No sale found with that id")
   } else {
-    res.json(saleId);
+    res.json(saleId)
   }
-});
+})
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Server running on http://localhost:${port}`)
+})
