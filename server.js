ab9afbb704e6a07691daf2f2c3fdadb7d6ae5d9f
idanaslund <idanaslund@users.noreply.github.com>
2022-05-02 16:12:39 +0200

    added routes

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ import cors from "cors";
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
+import netflixDatas from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
-import members from './data/technigo-members.json'
+// import members from './data/technigo-members.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -22,29 +22,59 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("This is my API");
 });
 
-// Getting the entire data structure
-app.get('/members', (req, res) => {
-// We are using json 99% of the times. json sends an array of objects or an object, so that the frontend can unpack it.
-res.status(200).json(members)
-
-// send is more universal, can send all different type of data.
-// res.send()
+app.get("/netflixStreams", (req, res) => {
+  res.status(200).json(netflixDatas)
 })
 
-// Getting one specific object sorted by the name in the params.
-app.get('/members/:name', (req, res) => {
-const memberByName = members.find(
-(member) => member.name === req.params.name
-)
+app.get('/netflixStreams/:type', (req, res) => {
+  const netflixTypes = netflixDatas.filter(
+  (netflixData) => netflixData.type === req.params.type
+  )
+  res.status(200).json(netflixTypes)
+  })
 
-res.status(200).json(memberByName)
+app.get('/year/:release_year', (req, res) => {
+  const releaseYear = netflixDatas.filter(
+    (netflixData) => netflixData.release_year === +req.params.release_year
+  )
+  res.status(200).json(releaseYear)
 })
+
+app.get('/name/:title', (req, res) => {
+  const netflixTitle = netflixDatas.find(
+  (netflixData) => netflixData.title === req.params.title
+  )
+  res.status(200).json(netflixTitle)
+  })
 
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+
+
+////////////////////////////////////////////////////////////
+
+// // Getting the entire data structure
+// app.get('/members', (req, res) => {
+
+  // //  We are using json 99% of the times. json sends an array of objects or an object, so that the frontend can unpack it.
+  // res.status(200).json(members)
+  
+  // send is more universal, can send all different type of data.
+  // res.send()
+  // })
+  
+  // // Getting one specific object sorted by the name in the params.
+  // app.get('/members/:name', (req, res) => {
+  // const memberByName = members.find(
+  // (member) => member.name === req.params.name
+  // )
+  
+  // res.status(200).json(memberByName)
+  // })
