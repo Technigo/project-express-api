b435a59f9ee4a4906dd3e57d468be7392d098b7f
SofiePellegrini <sofie.pellegrini@outlook.com>
2022-05-03 21:42:43 +0200

    first endpoint

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
+import netflixData from './data/netflix-titles.json';
 // import topMusicData from "./data/top-music.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
@@ -20,8 +20,20 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+app.get('/', (req, res) => {
+  res.send('Hello Technigo!');
+});
+
+app.get('/movies', (req, res) => {
+  res.status(200).json(netflixData);
+});
+
+app.get('/movies/:country', (req, res) => {
+  const whatCountry = netflixData.find(
+    (movie) => movie.country == req.params.country
+  );
+
+  res.status(200).json(whatCountry);
 });
 
 // Start the server
