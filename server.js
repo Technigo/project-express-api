ce599640e54f317286e5984d8e27d74bf1e0e52c
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2022-05-03 09:15:28 +0200

    Return a collection of astronauts

--- server.js
+++ server.js
@@ -1,30 +1,41 @@
 import express from "express";
 import cors from "cors";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import nasaAstronauts from "./data/nasa-astronauts.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Astronauts!");
 });
 
-// Start the server
+app.get("/astronauts", (req, res) => {
+  res.status(200).json({
+    success: true,
+    astronauts: nasaAstronauts
+  })
+});
+
+app.get("/astronauts/:id", (req, res) => {
+  const { id } = req.params;
+  const specificAstronaut = nasaAstronauts.find((item) => item.id === +id);
+  if (!specificAstronaut) {
+    res.status(404).json({
+      success: false, 
+      status_code: 404, 
+      status_message: "Astronaut not found" 
+    })
+  }
+  res.status(200).json({ 
+    success: true,
+    astronaut: specificAstronaut 
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
