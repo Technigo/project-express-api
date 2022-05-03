8fbfed7caffee10d0e0b9f8c61720feb455e20d0
sofiaannajonsson <84452023+sofiaannajonsson@users.noreply.github.com>
2022-05-03 14:54:48 +0200

    added single item endpoint

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
+import songs from "./data/top-music.json";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+//import goldenGlobesData from "./data/golden-globes.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -22,6 +22,17 @@ app.use(express.json());
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.get("/songs", (req, res) => {
+  res.status(200).json(songs)
+})
+
+app.get("/songs/:id", (req, res) => {
+  const songById = songs.find(
+    (song) => song.id == req.params.id
+  );
+  res.status(200).json(songById);
 });
 
 // Start the server
