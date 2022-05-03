a2768b261fcfeb9d7dc780053bed0b043fa8e965
joannaringqvist <joanna.ringqvist@gmail.com>
2022-05-03 14:54:31 +0200

    Added some data and did some testing

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ import cors from "cors";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
+// import members from './data/technigo-members.json';
+ import animals from './data/zoo-animals.json';
+
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -23,6 +26,73 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+app.get("/members", (req, res) => {
+  //console.log(members);
+
+  res.status(200).json(members);
+  //res.json(members)
+});
+
+//Entire data set
+app.get('/nominations', (req, res) => {
+  res.json(goldenGlobesData);
+});
+
+//nominations/year/2010
+//+ to make year into a number
+//http://localhost:8080/nominations/year/2010?win=true
+app.get('/nominations/year/:year', (req, res) => {
+  const year = req.params.year;
+  const showWon = req.query.win;
+  console.log({ showWon });
+  let nominationsFromYear = goldenGlobesData.filter((item) => item.year_award === +year);
+
+  if (showWon) {
+    nominationsFromYear = nominationsFromYear.filter((item) => item.win);
+  }
+
+  res.json(nominationsFromYear);
+});
+
+//Only the ones which has won
+app.get('/nominations/won', (req, res) => {
+  //const won = req.params.win;
+  const allWon = goldenGlobesData.filter((item) => item.win === true );
+  res.json(allWon);
+})
+
+app.get("/members/:name", (req, res) => {
+  //console.log(req.params);
+  const memberByName = members.find(member => member.name === req.params.name);
+  res.status(200).json(memberByName);
+});
+
+//All animals
+app.get("/animals/", (req, res) => {
+  console.log(animals);
+  res.status(200).json(animals);
+});
+
+//Mammal, Bird, Fish...
+//animals.animal_type
+app.get("/animals/:animal_type", (req, res) => {
+  console.log(req.params);
+  const animalByType = animals.filter((animal) => animal.animal_type === req.params.animal_type);
+  res.status(200).json(animalByType);
+});
+
+//Id 
+//animals/id/186
+app.get("/animals/id/:id", (req, res) => {
+  const animalById = animals.find((animal) => animal.id === +req.params.id);
+  //console.log(animalById);
+  res.status(200).json(animalById);
+});
+
+
+
+
 
 // Start the server
 app.listen(port, () => {
