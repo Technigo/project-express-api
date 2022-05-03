410bfdca3adc63ec421aee75141d7eff6e10293d
Arianna Allahyar <anallahyar@gmail.com>
2022-05-03 10:44:38 +0200

    Dataset imported

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 import express from "express";
 import cors from "cors";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import data from "./data/seattle-restaurants.json";
+
+// list of unique areas, 65 values (one of which is '')
+const areas = new Set(data.map((item) => item.Area).sort());
+
+// restaurants with a rating of 4 stars or higher
+const highRanked = data.filter((item) => item.Star >= 4);
+
+// Capitol Hill restaurants
+const capitolHillRestaurants = data.filter((item) => item.Area === "Capitol Hill");
+
+// Example of a specific restaurant
+const canlis = data.filter((item) => item.Name === "Canlis");
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
+
 const port = process.env.PORT || 8080;
 const app = express();
 
