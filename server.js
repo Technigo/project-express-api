38ac7e127337fb175184e4c5dd9a71ff534a7a59
SofiePellegrini <sofie.pellegrini@outlook.com>
2022-05-04 14:40:18 +0200

    three endpoints

--- server.js
+++ server.js
@@ -25,23 +25,48 @@ app.get('/', (req, res) => {
 });
 
 app.get('/movies', (req, res) => {
-  res.status(200).json(netflixData);
+  res.status(200).json({
+    data: netflixData,
+    success: true,
+  });
 });
 
-app.get('/movies/:country', (req, res) => {
-  const whatCountry = netflixData.find(
+app.get('/movies/country/:country', (req, res) => {
+  /*const { country } = req.params;*/
+  const whatCountry = netflixData.filter(
     (movie) => movie.country.toLowerCase() === req.params.country
   );
 
-  res.status(200).json(whatCountry);
+  res.status(200).json({
+    data: whatCountry,
+    success: true,
+  });
 });
 
-app.get('/:title', (req, res) => {
+app.get('/movies/title/:title', (req, res) => {
   const whatTitle = netflixData.find(
     (movie) => movie.title.toLowerCase() === req.params.title
   );
 
-  res.status(200).json(whatTitle);
+  if (!whatTitle) {
+    res.status(404).json('Sorry! Not found.');
+  } else {
+    res.status(200).json({
+      data: whatTitle,
+      success: true,
+    });
+  }
+});
+
+app.get('/movies/releaseyear/:year', (req, res) => {
+  const whatYear = netflixData.filter(
+    (movie) => movie.release_year.toString() === req.params.year
+  );
+
+  res.status(200).json({
+    data: whatYear,
+    success: true,
+  });
 });
 
 // Start the server
