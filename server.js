3ed576f6c0242522460823c8f0d60b306849d812
Emma Lindell <emma.lindell4@gmail.com>
2022-05-04 21:39:13 +0200

    Deploying to Heroku

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ import cors from "cors";
 
 import books from "./data/books.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
+// Defines the port the app will run on. Defaults to 8080
 // PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
@@ -13,7 +12,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// defining my routes here, starts here
+
+// ROUTES
+
+// defining my routes, starts here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!")
 })
@@ -23,7 +25,7 @@ app.get('/books', (req, res) => {
 res.status(200).json(books)
 })
 
-// title to get all titles
+// title to get the title
 app.get('/books/:title', (req, res) => {
     const booksTitle = books.find(
       (books) => books.title === req.params.title)
@@ -31,19 +33,29 @@ app.get('/books/:title', (req, res) => {
     res.status(200).json(booksTitle)
 })
 
-// to get all id:s
-
-app.get('/books/title/:id', (req, res) => {
-const bookID = books.find(
-  (books) => books.id === req.params.bookID)
-
-res.status(200).json(bookID)
+// http://localhost:8080/books/top-rated
+// res will be: top rated books sorted from top ranked -> least top ranked
+// using slice() to show top 10 out of all rated books
+app.get('/books/top-rated', (req, res) => {
+  const bookRating = books.sort(
+    (a, b) => b.average_rating - a.average_rating
+  )
+  res.json(bookRating.slice(0, 50))
 })
 
-// app.get('/authors', (req, res) => {
+// /books/book/1
+// :bookID = param to trigger a param example console(req params)
+// will look for a specific param, in this case a specific ID from books
+app.get('/books/book/:bookID', (req, res) => {
+const { bookID } = req.params
+const { book } = books.find((b) => b.bookID === +bookID)
 
+if (!book) {
+  res.status(404).send(`Error, there is no book with book-ID ${bookID}`)
+} 
+res.json(book)
+})
 
-// })
 
 // Start the server
 app.listen(port, () => {
