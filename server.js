c2e0060b4c08da161e7ecf7b8a98b012eb9fc563
StepbyStepp <91485842+SteppbySteph@users.noreply.github.com>
2022-05-04 15:08:20 +0200

    creating and editing routes and code

--- server.js
+++ server.js
@@ -1,28 +1,106 @@
 import express from "express";
 import cors from "cors";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import goldenGlobesData from "./data/golden-globes.json";
+import lionMembers from "./data/team-lions.json";
+import booksData from "./data/books.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+//endpoint 1
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Hello there! This page is for educational purpose only");
 });
+
+//endpoint 2 finding array of elements from 
+app.get("/members", (req, res) => {
+
+  res.status(200).json({
+    data: lionMembers,
+    success: true,
+  });
+});
+
+//endpoint 3 finding single lionMember by name
+app.get("/members/name/:name", (req, res)=> {
+  const { name } = req.params;
+  const memberByName = lionMembers.find(
+    (member) => member.name === name); 
+  
+  if (!memberByName) {
+    res.status(404).json({
+      data: `${name} not found in the database.`,
+      success: false, 
+    });
+  } else {
+    res.status(200).json({
+      data: memberByName,
+      success: true,
+    });
+  }
+});
+
+//endpoint 4 finding array of elements 
+app.get("/nominations", (req, res) => {
+
+  res.status(200).json({
+    data: goldenGlobesData,
+    success: true,
+  });
+});
+
+//endpoint 5 filtering nominations by year.
+app.get("/nominations/year/:year", (req, res)=> {
+
+  const year = req.params.year
+  const showWon = req.query.won
+  let nominationsByYear = goldenGlobesData.filter((item) => item.year_award === +year);
+
+  if (showWon) {
+    nominationsByYear = nominationsByYear.filter((item) => item.win)
+  }
+  
+  res.status(200).json({
+    data: nominationsByYear,
+    success: true,
+  });
+})
+
+//endpoint 6 finding array of books
+app.get("/books", (req, res) => {
+
+  res.status(200).json({
+    data: booksData,
+    success: true,
+  });
+})
+
+//endpoint 7 , searching title with filter(), include() and 'toLocaleLowerCase()' to maximize search results. 
+app.get("/books/title/:title", (req, res) =>{
+
+  const { title } = req.params;
+  const bookTitle = booksData.filter(
+    (item) => item.title.toLocaleLowerCase()
+    .includes(title.toLocaleLowerCase()));
+
+  //First if statement gives us the amount of books filtered out from the entire booksData. If there is/are object(s) returned then "status(200)". 
+  if (bookTitle.length !== 0) {
+    res.status(200).json({
+      data: bookTitle,
+      success: true,});
+    //console.log(bookTitle.length)
+  } else {
+    res.status(404).json({
+      data: "Title not found. Please check spelling.",
+      success: false,
+    });
+  }
+})
 
 // Start the server
 app.listen(port, () => {
