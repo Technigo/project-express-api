df51d005b5a6e7c20c8980ee9c1650164bce6daf
Isabell Wästfelt <isabell@iMac-som-tillhor-Isabell.local>
2022-05-04 09:54:17 +0200

    did blue level

--- server.js
+++ server.js
@@ -1,30 +1,70 @@
-import express from "express";
-import cors from "cors";
+import express from 'express'
+import cors from 'cors'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import netflixData from './data/netflix-titles.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+const port = process.env.PORT || 8080
+const app = express()
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
+
+const divideTitles = (titlesArray, pageNumber) => {
+  titlesArray = titlesArray.slice(pageNumber * 10, (pageNumber + 1) * 10)
+  return titlesArray
+}
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+app.get('/', (req, res) => {
+  res.send(listEndpoints(app))
+})
+
+app.get('/titles', (req, res) => {
+  const { year, type, director, cast, country, page = 0 } = req.query
+  let queriedTitles = netflixData
+
+  if (year) {
+    queriedTitles = queriedTitles.filter(
+      (title) => title.release_year === +year
+    )
+  }
+  if (type) {
+    queriedTitles = queriedTitles.filter(
+      (title) => title.type.toLowerCase() === type.toLowerCase()
+    )
+  }
+  if (director) {
+    queriedTitles = queriedTitles.filter((title) =>
+      title.director.toLowerCase().includes(director.toLowerCase())
+    )
+  }
+  if (cast) {
+    queriedTitles = queriedTitles.filter((title) =>
+      title.cast.toLowerCase().includes(cast.toLowerCase())
+    )
+  }
+  if (country) {
+    queriedTitles = queriedTitles.filter((title) =>
+      title.country.toLowerCase().includes(country.toLowerCase())
+    )
+  }
+
+  queriedTitles
+    ? res.status(200).json({ data: divideTitles(queriedTitles, +page) })
+    : res.status(404).json({ error: 'Not found' })
+})
+
+app.get('/titles/:id', (req, res) => {
+  const { id } = req.params
+
+  const queriedTitle = netflixData.find((title) => title.show_id === +id)
+  queriedTitle
+    ? res.status(200).json({ data: queriedTitle })
+    : res.status(404).json({ error: 'Not found' })
+})
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Server running on http://localhost:${port}`)
+})
