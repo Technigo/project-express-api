021d63e37e58dd963f7382bea2bc5c2643f56054
Sherin Susan Thomas <sherinsusanthomas95@gmail.com>
2022-05-04 11:58:48 +0200

    initial steps done

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ import cors from "cors";
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
+//import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import topMusicData from "./data/top-music.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -23,7 +23,48 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
-
+app.get("/songs", (req, res) => {
+  res.status(200).json({
+    data: topMusicData,
+    success: true,
+  });
+});
+app.get("/songs/trackName/:trackName", (req, res) => {
+  const trackname = topMusicData.find(
+    (data) =>
+      data.trackName.toLocaleLowerCase() ===
+      req.params.trackName.toLocaleLowerCase()
+  );
+  if (!trackname) {
+    res.status(404).json({
+      data: "not found",
+      success: false,
+    });
+  } else {
+    res.status(200).json({
+      data: trackname,
+      success: true,
+    });
+  }
+});
+app.get("/songs/artistName/:artistName", (req, res) => {
+  const artistName = topMusicData.filter(
+    (data) =>
+      data.artistName.toLocaleLowerCase() ===
+      req.params.artistName.toLocaleLowerCase()
+  );
+  if (!artistName) {
+    res.status(404).json({
+      data: "not found",
+      success: false,
+    });
+  } else {
+    res.status(200).json({
+      data: artistName,
+      success: true,
+    });
+  }
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
