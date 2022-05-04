cf59734bde6c2105666f9fae16a9d3685a9a1b65
bostrombundock <sbostrom@ymail.com>
2022-05-04 14:45:08 +0200

    added a file

--- server.js
+++ server.js
@@ -1,59 +1,46 @@
-import express from "express";
-import cors from "cors";
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import data from './data.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+
+
+import goldenGlobesData from "./data/golden-globes.json";
+import res from 'express/lib/response';
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
-const app = express();
+const app = express()
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(bodyParser.json());
 
-// Start defining your routes here
-app.get("/members/name/:name", (req, res) => {
-  const { name } = req.params;
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
 
-  const memberByName = members.find((member) => member.name === name);
+app.get('/nominations', (req, res) => {
+  res.json(data)
+})
 
-  if (!memberByName) {
-    res.status(404).json({
-      data: "Not found",
-      sucess: false,
-    });
-  } else {
-    res.status(200).json({
-      data: memberByName,
-      sucess: true,
-    });
+app.get('/year/:year', (req, res) => {
+  const year = req.params.year
+  const showWon = req.query.won
+  let nominationsFromYear = data.filter((item) => item.year_award === +year)
+  
 
-  }
-});
+if (showWon) {
+  nominationsFromYear = nominationsFromYear.filter((item) => item.win)
+}
 
-app.get("/members/role/:role", (req, res) => {
-  const { role } = req.params;
-
-  const membersByRole = members.filter(
-    (member) => member.role.toLowerCase() === role.toLowerCase()
-  );
-
-  res.status(200).json({
-    data: membersByRole,
-    success: true,
-  });
-});
+res.json(nominationsFromYear)
+})
 
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
-});
+})
