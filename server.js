179959262577927fb5392e646a5685c806ceb7ec
Emma Lindell <emma.lindell4@gmail.com>
2022-05-04 18:55:06 +0200

    Routes

--- server.js
+++ server.js
@@ -1,42 +1,51 @@
 import express from "express";
 import cors from "cors";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-import members from './data/technigo-members.json'
+import books from "./data/books.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+const port = process.env.PORT || 8080
+const app = express()
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-// Start defining your routes here
+// defining my routes here, starts here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
-
-app.get('/members', (req, res) => {
-res.status(200).json(members);
+  res.send("Hello Technigo!")
 })
 
-app.get('/members/:name', (req, res) => {
-    const memberByName = members.find(
-      (member) => member.name === req.params.name);
-
-    res.status(200).json(memberByName)
+// books to get all the books
+app.get('/books', (req, res) => {
+res.status(200).json(books)
 })
+
+// title to get all titles
+app.get('/books/:title', (req, res) => {
+    const booksTitle = books.find(
+      (books) => books.title === req.params.title)
+
+    res.status(200).json(booksTitle)
+})
+
+// to get all id:s
+
+app.get('/books/title/:id', (req, res) => {
+const bookID = books.find(
+  (books) => books.id === req.params.bookID)
+
+res.status(200).json(bookID)
+})
+
+// app.get('/authors', (req, res) => {
+
+
+// })
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Server running on http://localhost:${port}`)
+})
