9d90e40519a92d0a05ede940fbaa72d26d0d1ae7
idanaslund <idanaslund@users.noreply.github.com>
2022-05-04 14:10:17 +0200

    added 404

--- server.js
+++ server.js
@@ -25,30 +25,54 @@ app.get("/", (req, res) => {
   res.send("This is my API");
 });
 
+// Simple get request, sending back the complete list
 app.get("/netflixStreams", (req, res) => {
-  res.status(200).json(netflixDatas)
+  res.status(200).json({
+    data: netflixDatas,
+    success: true})
 })
 
+// Find one specific movie/show by name
+app.get('/netflixStreams/name/:title', (req, res) => {
+  const { title } = req.params
+
+  const netflixTitle = netflixDatas.find(
+  (netflixData) => netflixData.title.toLowerCase() === title.toLowerCase()
+  )
+
+  if(!netflixTitle){
+    res.status(404).json({
+      data: "Not found",
+      success: false })
+  } else {
+    res.status(200).json({
+      data: netflixTitle,
+      success: true })
+  }
+  })
+
+// Filter movies/shows by release year
+  app.get('/netflixStreams/year/:release_year', (req, res) => {
+    const { release_year } = req.params
+
+    const releaseYear = netflixDatas.filter(
+      (netflixData) => netflixData.release_year === +release_year
+    )
+
+  res.status(200).json({
+      data: releaseYear,
+      success: true })
+    
+  })
+
+// Filter by dynamic type, i.e. Movie or TV Show... Maybe change it.
 app.get('/netflixStreams/:type', (req, res) => {
   const netflixTypes = netflixDatas.filter(
-  (netflixData) => netflixData.type === req.params.type
+  (netflixData) => netflixData.type.toLowerCase() === req.params.type.toLowerCase()
   )
-  res.status(200).json(netflixTypes)
+    res.status(200).json(netflixTypes) 
   })
 
-app.get('/year/:release_year', (req, res) => {
-  const releaseYear = netflixDatas.filter(
-    (netflixData) => netflixData.release_year === +req.params.release_year
-  )
-  res.status(200).json(releaseYear)
-})
-
-app.get('/name/:title', (req, res) => {
-  const netflixTitle = netflixDatas.find(
-  (netflixData) => netflixData.title === req.params.title
-  )
-  res.status(200).json(netflixTitle)
-  })
 
 
 // Start the server
@@ -77,4 +101,29 @@ app.listen(port, () => {
   // )
   
   // res.status(200).json(memberByName)
+  // })
+
+  
+  // QUERY PARAMETERS
+
+  // app.get("/netflixStreams", (req, res) => {
+  //   const { type, release_year } = req.query
+
+  //   let allStreams = netflixDatas
+
+  //   if(type){
+  //     allStreams = allStreams.filter(
+  //     (stream) => stream.type.toLowerCase() === type.toLowerCase()
+  //     )
+  //   }
+
+  //   if(release_year) {
+  //     allStreams = allStreams.filter(
+  //       (stream) => stream.release_year === release_year
+  //       )
+  //   }
+
+  //   res.status(200).json({
+  //     data: allStreams,
+  //     success: true })
   // })
