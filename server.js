913ff0f24f028ffe6c8c2b00c9259eb3320fc5f6
Ida Halling <idahalling@Idas-MacBook-Pro.local>
2022-05-05 14:45:50 +0200

    2 end points done

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
+//import members from "./data/technigo-members.json";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
+
+import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
@@ -21,7 +23,33 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Hello MovieJunky sa sa sa!");
+});
+
+app.get("/shows", (req, res) => {
+  res.status(200).json({ data: netflixData, sucess: true });
+});
+
+app.get("/shows/title/:title", (req, res) => {
+  const { title } = req.params;
+
+  const showByTitle = netflixData.find((item) => item.title === title);
+
+  if (showByTitle) {
+    res.status(200).json({ data: showByTitle, sucess: true });
+  } else {
+    res.status(404).json({ data: "not found", sucess: false });
+  }
+});
+
+app.get("/shows/country/:country", (req, res) => {
+  const { country } = req.params;
+
+  const showByCountry = netflixData.filter(
+    (item) => item.country.toLocaleLowerCase() === country.toLocaleLowerCase()
+  );
+
+  res.status(200).json({ data: showByCountry, sucees: true });
 });
 
 // Start the server
