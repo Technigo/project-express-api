a426c0212091853d960b4f137159b83afd2ecd64
jiiin <jiiin.cho.dev@gmail.com>
2022-05-05 07:07:29 +0200

    Assignment completed

--- server.js
+++ server.js
@@ -1,27 +1,68 @@
 import express from "express";
 import cors from "cors";
+import booksData from "./data/books.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Welcome to My Book Store📚");
+});
+
+app.get("/books", (req, res) => {
+  const { limit, title } = req.query;
+
+  let data = booksData;
+  let success = true;
+  let message = "";
+
+  if (title) {
+    data = booksData.filter(
+      (book) => book.title.toLowerCase().search(title.toLowerCase()) !== -1
+    );
+  }
+
+  if (limit) {
+    const numLimit = Number(limit);
+    if (numLimit < 0 || !Number.isInteger(numLimit)) {
+      success = false;
+      message = "Invalid limit value";
+    } else {
+      data = data.slice(0, numLimit);
+    }
+  }
+
+  if (!data.length) {
+    success = false;
+    message = "No result found";
+  }
+
+  res.status(200).json({ data, success, message });
+});
+
+app.get("/books/:bookId", (req, res) => {
+  const id = Number(req.params.bookId);
+
+  let data = {};
+  let success = true;
+  let message = "";
+
+  if (id < 0 || !Number.isInteger(id)) {
+    res.status(404).json({ data, success: false, message: "Invalid bookId" });
+  }
+
+  data = booksData.find((book) => book.bookID === id);
+
+  if (!data) {
+    data = {};
+    success = false;
+    message = "No result found";
+  }
+
+  res.status(200).json({ data, success, message });
 });
 
 // Start the server
