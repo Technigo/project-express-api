d6773ab9cd37cbd6e02b7f43b0bc29b7a828d98e
Arianna Allahyar <anallahyar@gmail.com>
2022-05-05 19:21:24 +0200

    Pagination moved to function for reuse

--- server.js
+++ server.js
@@ -7,7 +7,48 @@ const formatText = (result) => {
   return result
     .toString()
     .toLowerCase()
-    .replace(/[\W_]+/g, " ");
+    .replace(/[\W_]+/g, "");
+};
+
+const orderData = (data, sorting) => {
+  switch (sorting) {
+    case "cheapest":
+      data = data.sort((a, b) => formatText(a.Price.length) - formatText(b.Price.length));
+    case "priciest":
+      data = data.sort((a, b) => formatText(b.Price.length) - formatText(a.Price.length));
+    case "popular":
+      data = data.sort((a, b) => Number(b.Stars_count) - Number(a.Stars_count));
+    case "unpopular":
+      data = data.sort((a, b) => Number(a.Stars_count) - Number(b.Stars_count));
+    default:
+      data = data;
+  }
+  return data;
+};
+
+const paginateResults = (data, req, res) => {
+  const page = req.query.page ? req.query.page : 1;
+  const size = req.query.pageSize ? req.query.pageSize : 10;
+  const start = (page - 1) * size;
+  const end = start + size;
+  const totalPages = Math.ceil(data.length / size);
+
+  if (page < 1 || (page > totalPages && data.length > 0)) {
+    res.status(422).json({
+      success: false,
+      statusMessage: `Invalid page number. Start index: 1. End index: ${totalPages}`,
+    });
+  }
+
+  return {
+    success: true,
+    total: data.length,
+    pages: totalPages,
+    startIndex: 1,
+    currentPage: page,
+    pageSize: size,
+    results: data.slice(start, end),
+  };
 };
 
 // Defines the port the app will run on
@@ -24,20 +65,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/restaurants", (req, res) => {
-  const page = req.query.page ? req.query.page : 1;
-  const size = req.query.pageSize ? req.query.pageSize : 10;
-  const start = (page - 1) * size;
-  const end = start + size;
-  const restaurants = data.slice(start, end);
-
-  res.json({
-    total: data.length,
-    pages: Math.ceil(data.length / size),
-    currentPage: page,
-    pageSize: size,
-    results: restaurants,
-    success: true,
-  });
+  res.json(paginateResults(data, req, res));
 });
 
 /// This works but look into it.. maybe make "orderedBy" query param instead?
@@ -45,8 +73,8 @@ app.get("/restaurants/popular", (req, res) => {
   const popular = data.sort((a, b) => Number(b.Stars_count) - Number(a.Stars_count)).splice(0, 10);
 
   res.status(200).json({
-    results: popular,
     success: true,
+    results: popular,
   });
 });
 
@@ -54,24 +82,23 @@ app.get("/restaurants/:name", (req, res) => {
   const { name } = req.params;
   const namedRestaurant = data.find((item) => formatText(item.Name) === formatText(name));
 
-  if (namedRestaurant) {
-    res.status(200).json({
-      response: namedRestaurant,
-      success: true,
-    });
-  } else {
-    res.status(404).json({
-      results: "Not found",
-      success: true,
-    });
-  }
+  namedRestaurant
+    ? res.status(200).json({
+        success: true,
+        response: namedRestaurant,
+      })
+    : res.status(404).json({
+        success: true,
+        results: "Not found",
+      });
 });
 
 app.get("/neighborhoods", (req, res) => {
   const neighborhoods = [...new Set(data.map((item) => item.Area))].sort();
   res.status(200).json({
-    results: neighborhoods,
     success: true,
+    total: neighborhoods.length,
+    results: neighborhoods,
   });
 });
 
@@ -80,12 +107,7 @@ app.get("/neighborhoods/:neighborhood", (req, res) => {
   const neighborhoodResults = data.filter(
     (item) => formatText(item.Area) === formatText(neighborhood)
   );
-
-  res.status(200).json({
-    total: neighborhoodResults.length,
-    results: neighborhoodResults,
-    success: true,
-  });
+  res.status(200).json(paginateResults(neighborhoodResults, req, res));
 });
 
 // make this a query
