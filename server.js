e5918f99ec3dddc762843f968f5da49a1fc3b638
Camilla Hallberg <camillahallberg@Camillas-MacBook-Pro.local>
2022-05-05 14:34:12 +0200

    added route to search for a specific title

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import listEndpoints from "express-list-endpoints";
 
-// Importing the JSON data 
+// Importing the Netflix JSON data 
 import netflixData from "./data/netflix-titles.json";
 
 // Defines the port the app will run on.
@@ -15,7 +15,7 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Welcome to the Netflix API! Enter /endpoints to see which endpoints there are. 🍿 🎬 🥤");
+  res.send("Welcome to the Netflix API! Enter /endpoints to see what endpoints there are. 🍿 🎬 🥤");
 });
 
 app.get("/endpoints", (req, res) => {
@@ -24,13 +24,49 @@ app.get("/endpoints", (req, res) => {
 
 // Route to get all the Netflix data
 app.get("/directory", (req, res) => {
-  res.json(netflixData)
+  res.status(200).json({
+    data: netflixData,
+    success: true,
+  });
 });
 
-// Route to get the titles from a specific year
-app.get("/year/:year", (req, res) => {
+// Route for user to type a title and either show it (200 = successful) or not (404 = not found).
+app.get("/directory/title/:title", (req, res) => {
+  const netflixTitle = netflixData.find((data) => data.title.toLocaleLowerCase() === req.params.title.toLocaleLowerCase());
+
+  if (!netflixTitle) {
+    res.status(404).json({
+      data: "I'm sorry! I don't have that title. Try something else. Beep-boop. 🤖",
+      success: false,
+    });
+  } else {
+    res.status(200).json({
+      data: netflixTitle,
+      success: true,
+    });
+  };
+});
+
+// Route for user to type an actor/actress name and see what movies/tv shows they are in. (Code not correct yet)
+app.get("/directory/cast/:cast", (req, res) => {
+  const castMember = netflixData.find((data) => data.cast.toLocaleLowerCase() === req.params.cast.toLocaleLowerCase());
+  if (!castMember) {
+    res.status(404).json({
+      data: "I couldn't find any cast member with that name. 😭 Please try another name!",
+      success: false,
+    });
+  } else {
+    res.status(200).json({
+      data: castMember,
+      success: true,
+    });
+  };
+});
+
+// Route to get the titles from a specific year.
+app.get("/directory/year/:year", (req, res) => {
   const year = req.params.year
-  const titlesFromYear = netflixData.filter((item) => item.release_year === +year)
+  const titlesFromYear = netflixData.filter((data) => data.release_year === +year)
   res.json(titlesFromYear)
 });
 
