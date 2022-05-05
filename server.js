aeb2008e63a722eecfa20140ebc7083a5002689e
Kyparn <93533136+Kyparn@users.noreply.github.com>
2022-05-05 10:08:28 +0200

    first push

--- server.js
+++ server.js
@@ -1,30 +1,40 @@
-import express from "express";
-import cors from "cors";
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import netflixDatas from './data/netflix-titles.json'
+console.log(netflixDatas)
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+const port = process.env.PORT || 8080
+const app = express()
+
+//parseInt or number or + will converrt string to numbers
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
-
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.json())
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+app.get('/', (req, res) => {
+  res.json('Movie time!')
+})
+
+app.get('/netflix', (req, res) => {
+  res.json(netflixDatas)
+})
+app.get('/name/:title', (req, res) => {
+  const showTitle = netflixDatas.filter(
+    (netflixData) => netflixData.type === req.params.type,
+  )
+  console.log(showTitle)
+  res.status(200).json(showTitle)
+})
+app.get('/')
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Server running on http://localhost:${port}`)
+})
