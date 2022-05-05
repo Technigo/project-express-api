e7a87f91d41ef37fca14f7c836ede2c9bcdf3536
Arianna Allahyar <anallahyar@gmail.com>
2022-05-05 22:33:34 +0200

    Refactored pagination

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-
 import data from "./data/seattle-restaurants.json";
 
 const formatText = (result) => {
@@ -8,6 +7,25 @@ const formatText = (result) => {
     .toString()
     .toLowerCase()
     .replace(/[\W_]+/g, "");
+};
+
+const paginateResults = (data, req) => {
+  data = data.sort((a, b) => Number(b.Stars_count) - Number(a.Stars_count));
+  const page = req.query.page ? req.query.page : 1;
+  const size = req.query.pageSize ? req.query.pageSize : 10;
+  const start = (page - 1) * size;
+  const end = start + size;
+
+  return {
+    success: true,
+    total: data.length,
+    pages: Math.ceil(data.length / size),
+    startIndex: 1,
+    currentPage: page,
+    pageSize: size,
+    orderedBy: "popularity",
+    results: data.slice(start, end),
+  };
 };
 
 const orderData = (data, sorting) => {
@@ -26,56 +44,18 @@ const orderData = (data, sorting) => {
   return data;
 };
 
-const paginateResults = (data, req, res) => {
-  const page = req.query.page ? req.query.page : 1;
-  const size = req.query.pageSize ? req.query.pageSize : 10;
-  const start = (page - 1) * size;
-  const end = start + size;
-  const totalPages = Math.ceil(data.length / size);
-
-  if (page < 1 || (page > totalPages && data.length > 0)) {
-    res.status(422).json({
-      success: false,
-      statusMessage: `Invalid page number. Start index: 1. End index: ${totalPages}`,
-    });
-  }
-
-  return {
-    success: true,
-    total: data.length,
-    pages: totalPages,
-    startIndex: 1,
-    currentPage: page,
-    pageSize: size,
-    results: data.slice(start, end),
-  };
-};
-
-// Defines the port the app will run on
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080; //defines the port the app will run on
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors());
+app.use(cors()); //middlewares to enable cors and json body parsing
 app.use(express.json());
 
-// Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Welcome to this API!");
+  res.send("Seattle restaurants");
 });
 
 app.get("/restaurants", (req, res) => {
-  res.json(paginateResults(data, req, res));
-});
-
-/// This works but look into it.. maybe make "orderedBy" query param instead?
-app.get("/restaurants/popular", (req, res) => {
-  const popular = data.sort((a, b) => Number(b.Stars_count) - Number(a.Stars_count)).splice(0, 10);
-
-  res.status(200).json({
-    success: true,
-    results: popular,
-  });
+  res.json(paginateResults(data, req));
 });
 
 app.get("/restaurants/:name", (req, res) => {
@@ -85,13 +65,16 @@ app.get("/restaurants/:name", (req, res) => {
   namedRestaurant
     ? res.status(200).json({
         success: true,
-        response: namedRestaurant,
+        results: namedRestaurant,
       })
     : res.status(404).json({
-        success: true,
+        success: false,
         results: "Not found",
       });
 });
+
+const neighborhoods = new Set(data.map((item) => item.Area));
+console.log(neighborhoods);
 
 app.get("/neighborhoods", (req, res) => {
   const neighborhoods = [...new Set(data.map((item) => item.Area))].sort();
@@ -107,14 +90,9 @@ app.get("/neighborhoods/:neighborhood", (req, res) => {
   const neighborhoodResults = data.filter(
     (item) => formatText(item.Area) === formatText(neighborhood)
   );
-  res.status(200).json(paginateResults(neighborhoodResults, req, res));
+  res.status(200).json(paginateResults(neighborhoodResults, req));
 });
 
-// make this a query
-// restaurants with a rating of 4 stars or higher
-const highRanked = data.filter((item) => item.Star >= 4);
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
