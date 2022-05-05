9749502c6290ce507e90b97b1d53dff771610827
vanhaj <vanessa.hajek@hotmail.com>
2022-05-05 11:29:51 +0200

    deploy testing

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -8,6 +8,7 @@ import cors from "cors";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
+import drivers from './data/drivers-csv.json';
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -20,8 +21,28 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+app.get('/', (req, res) => {
+  res.send('Hello Technigo!');
+});
+
+// all the drivers
+// http://localhost:8080/drivers
+app.get('/drivers', (req, res) => {
+  res.status(200).json(drivers);
+  res.send(drivers);
+  // console.log(drivers);
+});
+
+// specific driver
+// ex: http://localhost:8080/drivers/1
+app.get('/drivers/:driverId', (req, res) => {
+  // console.log(req.params);
+
+  const driverById = drivers.find(
+    (driver) => driver.driverId === +req.params.driverId
+  );
+
+  res.status(200).json(driverById);
 });
 
 // Start the server
