700107488a5c148aad724b24a334e88dc20cf55e
HWallberg <91497486+HWallberg@users.noreply.github.com>
2022-05-05 11:41:26 +0200

    added routes using params and got everything to work

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 import express from "express";
 import cors from "cors";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import netflixData from "./data/netflix-titles.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -19,10 +10,40 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Netflix & chill!");
 });
+
+app.get("/stream", (req, res) => {
+  res.status(200).json(netflixData)
+});
+
+app.get("/stream/year/:year", (req, res) => {
+  const { year } = req.params;
+  const streamByYear = netflixData.filter((stream => stream.release_year === +year))
+  streamByYear
+    ? res.status(200).json({ data: streamByYear })
+    : res.status(400).json({ error: "Not found" })
+})
+
+app.get("/stream/title/:title", (req, res) => {
+  const { title } = req.params;
+  const streamByTitle = netflixData.find(stream => stream.title.toLowerCase() === title.toLowerCase())
+  streamByTitle
+    ? res.status(200).json({ data: streamByTitle })
+    : res.status(400).json({ error: "Not found" })
+})
+
+app.get("/stream/type/:type", (req, res) => {
+  const { type } = req.params;
+  const streamByTitle = netflixData.filter(stream => stream.type.toLowerCase() === type.toLowerCase())
+  streamByTitle
+    ? res.status(200).json({ data: streamByTitle })
+    : res.status(400).json({ error: "Not found" })
+})
+
 
 // Start the server
 app.listen(port, () => {
