edd2cefbd16c794aa95ea9bb0bab50f4836581aa
Amanda Tange <amandatange@hotmail.com>
2022-05-06 16:00:12 +0200

    commented out unused code that I still want to keep for future reference

--- models/pagination.js
+++ models/pagination.js
@@ -1,17 +1,19 @@
-const pagination = (data, pageNumber) => {
-    const pageSize = 20
-    const startIndex = (pageNumber-1) * pageSize
-    const endIndex = startIndex + pageSize
-    const itemsOnPage = data.slice(startIndex, endIndex)
+// not used but saved for future reference
 
-    const returnObject = {
-        page_size: pageSize,
-        page: pageNumber,
-        num_of_pages: Math.ceil(data.length / pagesize),
-        items_on_page: booksOnPage.length,
-        results: itemsOnPage
-    }
-    return returnObject
-}
+// const pagination = (data, pageNumber) => {
+//     const pageSize = 20
+//     const startIndex = (pageNumber-1) * pageSize
+//     const endIndex = startIndex + pageSize
+//     const itemsOnPage = data.slice(startIndex, endIndex)
 
-export default pagination;
+//     const returnObject = {
+//         page_size: pageSize,
+//         page: pageNumber,
+//         num_of_pages: Math.ceil(data.length / pagesize),
+//         items_on_page: booksOnPage.length,
+//         results: itemsOnPage
+//     }
+//     return returnObject
+// }
+
+// export default pagination;
--- models/sorting.js
+++ models/sorting.js
@@ -1,13 +1,15 @@
-const sorting = ( arr, key, direction = asc ) => {
-	if(direction === asc) {
-		return arr.sort((a, b) => (a[key] > b[key]) ? 1 : ((b[key] > a[key]) ? -1 : 0))
-	} else {
-		return arr.sort((a, b) => (a[key] < b[key]) ? 1 : ((b[key] < a[key]) ? -1 : 0))
-	}
-}
+// not used but saved for future reference
+
+// const sorting = ( arr, key, direction = asc ) => {
+// 	if(direction === asc) {
+// 		return arr.sort((a, b) => (a[key] > b[key]) ? 1 : ((b[key] > a[key]) ? -1 : 0))
+// 	} else {
+// 		return arr.sort((a, b) => (a[key] < b[key]) ? 1 : ((b[key] < a[key]) ? -1 : 0))
+// 	}
+// }
 
 // const sortedByAuthorAsc = sorting(books, author)
 // const sortedByTitleDsc = sorting(books, author, desc)
 // const sortedByPagesAsc = sorting(books, author, asc)
 
-export default sorting;
+// export default sorting;
