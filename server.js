fed2d28e58205115965a3ff1f7f72631915f40d5
Amanda Tange <amandatange@hotmail.com>
2022-05-06 13:08:12 +0200

    fixed filter function for queries

--- server.js
+++ server.js
@@ -1,60 +1,51 @@
 import express from "express";
 import cors from "cors";
 
-import sorting from "./models/sorting";
-import pagination from "./models/pagination";
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
 import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
-// console.log(booksData);
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.json(
+    {
+      "Open API": "Books",
+      "Routes": [
+        {
+          "/books": "Get all books",
+          "/books/:id": "Get book whose id matches",
+          "/ratings/:rating": "Get books whose rating starts with the number provided"
+        }
+      ],
+      "Queries": [
+        {
+          "/books?author=string": "Get all books whose author contains the string provided",
+          "/books?title=string": "Get all books whose title contains the string provided",
+        }
+      ]
+    }
+  );
 });
-
-// app.get("/books", (req, res) => {
-  // const { name, id } = req.query;
-
-  // const allBooks = booksData;
-
-//   res.status(200).json({
-//     data: booksData,
-//     success: true
-//   });
-// });
-
 
 app.get('/books', (req, res) => {
 	const { author, title } = req.query;
 	let filteredBooks = booksData;
-  console.log(author, title)
 	
 	if(author) {
-		filteredBooks.filter((item) => 
-			item.authors.toLocaleLowerCase()
-			.includes(author.toLocaleLowerCase()))
-	}
+    console.log(filteredBooks.length);
+		filteredBooks = filteredBooks.filter((item) => 
+      item.authors.toLowerCase()
+      .includes(author.toLowerCase()));
+	};
+
 	if(title) {
-		filteredBooks.filter((item) => 
-			item.title.toLocaleLowerCase()
-			.includes(title.toLocaleLowerCase()))
-	}
+		filteredBooks = filteredBooks.filter((item) => 
+			item.title.toLowerCase()
+			.includes(title.toLowerCase()))
+	};
 	
 	if (filteredBooks.length === 0) {
 		res.status(404).json({
@@ -72,16 +63,23 @@ app.get('/books', (req, res) => {
 app.get("/books/:id/", (req, res) => {
   const bookByID = booksData.find(
     (book) => book.bookID === parseInt(req.params.id));
-  // console.log(req.params);
 
-    
-  res.status(200).json(bookByID);
+  if (!bookByID) {
+    res.status(404).json({
+      data: "Not found",
+      success: false
+    })
+  } else {
+    res.status(200).json({
+      data: bookByID,
+      success: true
+    })
+  }
 });
 
 app.get("/ratings/:rating", (req, res) => {
   const rating = req.params.rating;
-  // let averageRatings = booksData.filter((book) => book.average_rating.toString().slice(0, 1) === rating);
-  let averageRatings = booksData.filter((book) => parseInt(book.average_rating) === parseInt(rating));
+  const averageRatings = booksData.filter((book) => parseInt(book.average_rating) === parseInt(rating));
 
   if (!averageRatings) {
     res.status(404).json({
@@ -94,10 +92,8 @@ app.get("/ratings/:rating", (req, res) => {
       success: true
     });
   }
-  
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
