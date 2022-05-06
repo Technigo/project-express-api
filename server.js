a8fd899d10281026189319648c04bf29777e9d9c
Dopest0727 <74298827+Dopest0727@users.noreply.github.com>
2022-05-06 11:41:41 +0200

    1

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 import express from "express";
 import cors from "cors";
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import booksData from "./data/books.json";
+import listEndpoints from 'express-list-endpoints'
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -19,12 +13,47 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+// First endpoint 
+// The app.get method takes two arguments - the path and a call back function, which can be used by the frontend.
+app.get('/', (req, res) => {
+  res.send('This is an API with data about books.')
+})
+
+// ALL ENDPOINTS
+app.get('/endpoints', (req, res) => {
+  res.send(listEndpoints(app))
+})
+
+// DATA OF ALL BOOKS
+app.get('/books', (req, res) => {
+  res.json(booksData)
+})
+
+// LOOK FOR SPECIFIC BOOK
+app.get('/books/:id', (req, res) => {
+  const { id } = req.params
+  const book = booksData.find(item => item.bookID === +id)
+  if (!book) {
+    res.status(404).send('No book found with this ID')
+  } else {
+    res.json(book)
+  }
+})
+
+// ALL AUTHORS ALPAHABETICAL
+app.get('/books/authors/:authors/', (req, res) => {
+  const allAuthors = booksData.map(item => item.authors).sort()
+  res.json(allAuthors)
+})
+
+// ALL BOOKS ALPAHABETICAL
+app.get('/books/title/:title/', (req, res) => {
+  const allTitles = booksData.map(item => item.title).sort()
+  res.json(allTitles)
+})
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  // eslint-disable-next-line
+  console.log(`Server running on http://localhost:${port}`)
+})
