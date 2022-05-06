a9f88a2b4e7611b99973beadad69eff2aee35cb6
Emma Springs <emmasprings@gmail.com>
2022-05-06 17:23:50 +0200

    added own json file

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ import cors from "cors";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
+// import members from "./data/technigo-members.json";
+import prints from './data/print-collection.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -21,8 +23,69 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Find a print");
 });
+
+app.get("/prints", (req, res) => {
+  const { title, year, media, price, size, url } = req.query;
+  
+  const allPrints = prints;
+
+  if (title) {
+    allPrints = allPrints.filter(print => print.title.toLowerCase() === title.toLowerCase())
+  }
+
+  // if (media) {
+  //   allPrints = allPrints.filter(print => print.media.toLowerCase() === media.toLowerCase())
+  // }
+
+  res.status(200).json({
+    data: prints,
+    success: true,
+  });
+});
+
+app.get("/prints/year/:year", (req, res) => {
+  const { year } = req.params;
+
+  const printByYear = prints.filter(print => print.year === year);
+  console.log('print made year:', printByYear)
+
+  if (!printByYear) {
+    res.status(404).json({
+      data: "Not found",
+      success: false,
+    });
+  } else {
+    res.status(200).json({
+      data: printByYear,
+      success: true,
+    });
+  }
+});
+
+app.get('/prints/title/:title', (req, res) => {
+  const { title } = req.params;
+
+  const printByTitle = prints.find((prints) => prints.title.toLowerCase() === title.toLowerCase())
+  // console.log('Prints by price:', printByPrice)
+
+  res.status(200).json({
+    data: printByTitle,
+    success: true,
+  })
+})
+
+app.get('/prints/media/:media', (req, res) => {
+  const { media } = req.params;
+
+  const printByMedia = prints.filter(print => print.media.toLowerCase() === media.toLowerCase())
+
+  res.status(200).json({
+    data: printByMedia,
+    success: true,
+  })
+})
 
 // Start the server
 app.listen(port, () => {
