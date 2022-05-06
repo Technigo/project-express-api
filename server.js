df18179aaca6c8385387a6cfc7d7f78154508344
sofiaannajonsson <84452023+sofiaannajonsson@users.noreply.github.com>
2022-05-06 13:30:12 +0200

    removed unused data and brushed up code

--- server.js
+++ server.js
@@ -2,35 +2,84 @@ import express from "express";
 import cors from "cors";
 import songs from "./data/top-music.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import netflixData from "./data/netflix-titles.json";
-//import goldenGlobesData from "./data/golden-globes.json";
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+// Mddlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+// Defining rotes
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  const Main = {
+    About:
+      "An API with 50 popular Spotify tracks and some data about the music type",
+    Routes: [
+      {
+        "/api/songs": "Get all songs",
+        "/api/genres/{genre}": "Get all songs in a specific genre",
+        "/api/artists/{artist}": "Get songs from a specific artist",
+        "/api/titles/{title}": "Get a song by its title name",
+        "/api/ids/{number}": "Get a songs by its ID",
+      },
+    ],
+  };
+  res.send(Main);
 });
 
-app.get("/songs", (req, res) => {
+app.get("/api/songs", (req, res) => {
   res.status(200).json(songs)
 })
 
-app.get("/songs/id/:id", (req, res) => {
+app.get("/api/genres/:genre", (req, res) => {
+  const { genre } = req.params
+  
+  const songsByGenre = songs.filter(
+    (songs) => songs.genre.toLowerCase() === genre.toLowerCase()
+  );
+  
+  res.status(200).json({
+    data: songsByGenre,
+    success: true,
+  });
+});
+
+app.get("/api/artists/:artist", (req, res) => {
+  const { artist } = req.params
+  
+  const songsByArtist = songs.filter(
+    (songs) => songs.artistName.toLowerCase() === artist.toLowerCase()
+  );
+  
+  res.status(200).json({
+    data: songsByArtist,
+    success: true,
+  });
+});
+
+app.get("/api/titles/:title", (req, res) => {
+  const { title } = req.params
+  const songByTitle = songs.find(
+    (song) => song.trackName.toLowerCase() == title.toLowerCase()
+  );
+
+  if (!songByTitle) {
+    res.status(400).json({
+      data: "Not found",
+      success: false,
+    });
+  } else {
+    res.status(200).json({
+      data: songByTitle,
+      sucess: true,
+    });
+  }
+});
+
+app.get("/api/ids/:id", (req, res) => {
+  const { id } = req.params;
   const songById = songs.find(
-    (song) => song.id == req.params.id
+    (song) => song.id == id
   );
 
   if (!songById) {
@@ -44,19 +93,6 @@ app.get("/songs/id/:id", (req, res) => {
       sucess: true,
     });
   }
-});
-
-app.get("/songs/artist/:artistName", (req, res) => {
-  const { artistName } = req.params
-  
-  const songsByArtist = songs.filter(
-    (songs) => songs.artistName.toLowerCase() === artistName.toLowerCase()
-  );
-  
-  res.status(200).json({
-    data: songsByArtist,
-    success: true,
-  });
 });
 
 // Start the server
