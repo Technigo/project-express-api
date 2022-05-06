2f4718d367a6b5a2890aebf7a8b48bfce75586f1
mathildakarlsson <86979661+mathildakarlsson@users.noreply.github.com>
2022-05-07 00:34:49 +0200

    basic structure with two endpoints built

--- server.js
+++ server.js
@@ -1,30 +1,52 @@
 import express from "express";
 import cors from "cors";
+import goldenGlobesData from "./data/golden-globes.json";
+// import { json } from "express/lib/response";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
+
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+
+
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+const data = goldenGlobesData
+
+
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
-// Start the server
+app.get('/nominations', (req, res) => {
+  res.status(200).json(data);
+});
+
+
+app.get('/')
+
+
+app.get('/year/:year', (req, res) => {
+  const year = req.params.year
+  // const showWon = req.query.won
+  let nominationsFromYear = goldenGlobesData.find(
+    (item) => item.year_award === +year
+  );
+  
+  // if (showWon) {
+  //   nominationsFromYear = nominationsFromYear.filter((item) => item.win)
+  // }
+
+  res.status(200).json(nominationsFromYear);
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
