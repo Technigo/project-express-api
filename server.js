6eb5b427218dcb535e3924a36b922e98fb34c911
anki palviainen <anki.palviainen@gmail.com>
2022-05-06 08:39:04 +0300

    delete comments

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
@@ -10,13 +8,10 @@ import cors from "cors";
 // import topMusicData from "./data/top-music.json";
 //import members from "./data/technigo-members.json"
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
@@ -70,23 +65,6 @@ app.get('/tvshows/:title', (req, res) => {
   }  
 })
 
-// // Start defining your routes here
-// app.get("/", (req, res) => {
-//   res.send("Hello Technigo!");
-// });
-
-// app.get('/members', (req, res) => {
-//   //res.json(members)
-//   res.status(200).json(members)
-// })
-
-// app.get("/members/:name", (req, res) => {
-//   const memberByName = members.find(member => member.name === req.params.name)
-
-//   res.status(200).json(memberByName)
-// })
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
