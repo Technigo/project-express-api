039e40284dcbc08a2422c430a9c7f1d25bd20b78
Jessica Sandler <jessica.sandler@hotmail.com>
2022-05-06 17:57:38 +0200

    Add football-players and routes

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 import express from "express";
 import cors from "cors";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import players from "./data/football-players.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -21,8 +15,101 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+
+  const documentation = {
+    "Welcome": "An API about Sweden's football team",
+    "Routes": [
+      {
+        "/players": "List all players",
+        "/players/goals/descending": "List players sorted by number of goals in descending order",
+        "/players/games/descending": "List players sorted by number of games in descending order",
+        "/players/position/:position": "List all players in the specified position",
+        "/players/random-player": "Show info about a random player",
+        "/players/name/:name": "Show info about a specific player",
+      }
+    ]
+  }
+
+  res.send(documentation);
 });
+
+// List all players
+app.get('/players', (req, res) => {
+  res.status(200).json({
+    numberOfPlayers: players.length,
+    players: players,
+  });
+})
+
+app.get('/players/goals/descending', ( req, res ) => {
+
+  players.sort((a, b) => b.goals - a.goals)
+
+  res.status(200).json(players)
+
+})
+
+app.get('/players/games/descending', ( req, res ) => {
+
+  players.sort((a, b) => b.games - a.games)
+
+  res.status(200).json(players)
+
+})
+
+// List all players in a specific position
+app.get('/players/position/:position', (req, res) => {
+
+  const { position } = req.params
+
+  const playersByPosition = players.filter(
+    (player) => player.position === position
+  )
+
+  if (playersByPosition.length > 0) {
+    res.status(200).json({
+      data: playersByPosition,
+      success: true,
+    })
+  } else {
+    res.status(404).json({
+      data: "Not found",
+      success: true,
+    })
+  }
+  
+})
+
+
+// Kanske göra om detta till en query i stället?
+app.get('/players/random-player', (req, res) => {
+
+  const randomPlayer = players[Math.floor(Math.random()*players.length)]
+
+  res.status(200).json(randomPlayer)
+})
+
+// Get data about a specific player
+app.get('/players/name/:name', ( req, res ) => {
+  const { name } = req.params;
+
+  const playerByName = players.find(
+    (player) => player.name === name
+  );
+
+  if (playerByName) {
+    res.status(200).json({
+      data: playerByName,
+      success: true,
+    })
+  } else {
+    res.status(404).json({
+      data: "Not found",
+      success: false,
+    })
+  }
+
+})
 
 // Start the server
 app.listen(port, () => {
