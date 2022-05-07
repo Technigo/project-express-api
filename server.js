31f01e158a981ca453959b95932d4ce550565a3a
rijad90 <rijad.i@hotmail.com>
2022-05-07 21:34:30 +0200

    added filter and find

--- server.js
+++ server.js
@@ -1,49 +1,63 @@
 import express from "express"
 import cors from "cors"
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
 import netflixData from "./data/netflix-titles.json"
 import topMusicData from "./data/top-music.json"
 import members from "./data/technigo-members.json"
+import res from "express/lib/response"
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(express.json())
 
-// Start defining your routes here (endpoint)
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!")
-})
+/* app.get("/", (req, res) => {
+  res.send(listEndpoints(app));
+}); */
 
 // request = information that comes to the backend when we send a request
 // response = what information in sent to the frontend when we use logic
 
+app.get("/", (req, res) => {
+  res.send("Hello Technigo!")
+})
+
+// first endpoint, GET request with URL /members
 app.get("/members", (req, res) => {
-  res.status(200).json(members)
+  res.status(200).json({
+    data: members,
+    success: true,
+  })
 })
 
-app.get("/members/:name", (req, res) => {
-  const memberByName = members.find(
-    (member) => member.name === req.params.name)
-      res.status(200).json(memberByName)
+// second endpoint, added path parameter for more dynamic and to enable reuse just like reusable function
+// find = finds the first element that meets the req (this is used with id which is UNIQ compared to a name like below example)
+// filter = finds all elements that meet the req
+
+app.get("/members/name/:name", (req, res) => {
+  const { name } = req.params
+
+  const memberByName = members.find((member) => member.name === name)
+
+  if (!memberByName) {
+    res.status(404).json({
+      data: "Not Found",
+      success: false
+    })
+  } else {
+    res.status(200).json({
+      data: memberByName,
+      success: true
+    })
+  }
 })
 
-app.get("/netflixData", (req, res) => {
-  res.json(netflixData)
-})
+app.get("/members/role/:role", (req, res)=> {
+  const { role } = req.params
 
-app.get("/topMusicData", (req, res) => {
-  res.json(topMusicData)
+  const membersByRole = members.filter(member => member.role === role)
+  console.log('members by role:', membersByRole)
 })
 
 // Start the server
