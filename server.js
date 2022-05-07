6c4c722087b63ea0425056bc6164d8fe5847155a
rijad90 <rijad.i@hotmail.com>
2022-05-07 23:15:28 +0200

    done with the project

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 import express from "express"
 import cors from "cors"
 
-import netflixData from "./data/netflix-titles.json"
-import topMusicData from "./data/top-music.json"
 import members from "./data/technigo-members.json"
-import res from "express/lib/response"
 
 const port = process.env.PORT || 8080
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-
-/* app.get("/", (req, res) => {
-  res.send(listEndpoints(app));
-}); */
 
 // request = information that comes to the backend when we send a request
 // response = what information in sent to the frontend when we use logic
