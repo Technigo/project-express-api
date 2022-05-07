26e8f368bd0f351728c83b66805b35cabe31f7c7
rijad90 <rijad.i@hotmail.com>
2022-05-07 13:29:39 +0200

    added endpoints and then built another endpoint path/param more complicated

--- server.js
+++ server.js
@@ -1,30 +1,52 @@
-import express from "express";
-import cors from "cors";
+import express from "express"
+import cors from "cors"
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import netflixData from "./data/netflix-titles.json"
+import topMusicData from "./data/top-music.json"
+import members from "./data/technigo-members.json"
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+const port = process.env.PORT || 8080
+const app = express()
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-// Start defining your routes here
+// Start defining your routes here (endpoint)
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+  res.send("Hello Technigo!")
+})
+
+// request = information that comes to the backend when we send a request
+// response = what information in sent to the frontend when we use logic
+
+app.get("/members", (req, res) => {
+  res.status(200).json(members)
+})
+
+app.get("/members/:name", (req, res) => {
+  const memberByName = members.find(
+    (member) => member.name === req.params.name)
+      res.status(200).json(memberByName)
+})
+
+app.get("/netflixData", (req, res) => {
+  res.json(netflixData)
+})
+
+app.get("/topMusicData", (req, res) => {
+  res.json(topMusicData)
+})
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Server running on http://localhost:${port}`)
+})
