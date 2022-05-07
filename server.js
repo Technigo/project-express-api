0ad0a05a3baf0660133b2c0e85fcfebb6292a1ba
toal13 <tomoyo.tomoyo.m@gmail.com>
2022-05-07 15:20:21 +0200

    change some file

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 import express from "express";
 import cors from "cors";
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
 import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
