02b9d9950288010d90466846d05bcd6b0f7afc80
Laura Sjolander <laura.sjolander@gmail.com>
2022-05-08 22:24:21 +0200

    api fetch Book by Title and status codes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
+import booksData from "./data/books.json";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
+
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
@@ -15,16 +16,49 @@ import cors from "cors";
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+// Add middlewares to enable cors and json body parsing- dont change them
 app.use(cors());
 app.use(express.json());
+
+
 
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
+//the get is just the last part of https://localhost8080/booksData for example:
+app.get('/booksData', (req, res) => {
+  res.status(200).json({
+    data: booksData,
+    sucess: true,
+  });
+});
+
+
+app.get('/booksData/:title', (req, res) => {
+  const { title} = req.params;
+
+  const bookByTitle = booksData.filter((booksData) => booksData.title === title);
+  console.log("Book by title:", bookByTitle);
+});
+
+  //or if(!bookByTitle)
+  //if(bookByTitle===undefined){
+    //res.status(404).json({
+      //data:"Not found",
+      //sucess: false,
+  //  });
+  //} else{
+  //res.status(200).json({
+   // data: bookByTitle,
+   // sucess: true,
+  //});
+//};
+  
+//});
+
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`this is shown in terminal:Server running on http://localhost:${port}`);
 });
