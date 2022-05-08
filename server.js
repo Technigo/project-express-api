2c89bb8de3532427271c95db4abfdece573dbcc2
vanhaj <vanessa.hajek@hotmail.com>
2022-05-08 15:35:15 +0200

    cleaned up the code

--- server.js
+++ server.js
@@ -2,34 +2,21 @@ import express from 'express';
 import cors from 'cors';
 import listEndpoints from 'express-list-endpoints';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
 import drivers from './data/drivers-csv.json';
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+// Routes
 app.get('/', (req, res) => {
   res.send(listEndpoints(app));
 });
 
-// all the drivers
-// http://localhost:8080/drivers
+// https://vanhaj-express-api.herokuapp.com/
 app.get('/drivers', (req, res) => {
-  // lägg till fler här om du skapar fler endpoints
   const { driverId } = req.query;
 
   let allDrivers = drivers;
@@ -42,12 +29,9 @@ app.get('/drivers', (req, res) => {
     data: allDrivers,
     sucess: true,
   });
-
-  // console.log(drivers);
 });
 
-// specific driver
-// ex: http://localhost:8080/drivers/1
+// specific driver:
 // https://vanhaj-express-api.herokuapp.com/drivers?driver=1
 app.get('/drivers/driver/:driverId', (req, res) => {
   const { driverId } = req.params;
