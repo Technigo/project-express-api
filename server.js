28ab94dfc535685d163ab280bd6fb429f65a4f82
Kristiina Kolu <kristiinakolu@Maxs-Air.lan>
2022-05-14 23:28:22 +0300

    added endpoints

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 import express from "express";
 import cors from "cors";
+import listEndpoints from "express-list-endpoints";
+import booksData from "./data/books.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -20,9 +15,69 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  app.get("/", (req, res) => {
+    res.json({
+      message: 'This is API for book data endpoints',
+      data: listEndpoints(app)
+    })
 });
+
+//this checks all the endpoints:
+app.get('/endpoints', (req, res) => {
+  res.send(listEndpoints(app))
+})
+
+
+//get data by book's name and author using query parameters
+app.get("/books", (req, res) => {
+  const {bookName, bookAuthor} = req.query
+  let booksToSend = booksData
+
+  if(bookName) {
+    booksToSend = booksData.filter((item) => item.title.toLowerCase().indexOf(bookName.toLowerCase()) !== -1)
+  }
+
+  if(bookAuthor) {
+    booksToSend = booksToSend.filter((item) => item.authors.toLowerCase().indexOf(bookAuthor.toLowerCase()) !== -1)
+  }
+
+  res.json({
+    response: booksToSend,
+    success:true
+  })
+  
+});
+
+//get book data by id
+app.get('/books/id/:bookID', (req, res) => {
+  const { bookID } = req.params;
+  console.log(req)
+  const bookById = booksData.find((item) => item.bookID === +bookID);
+
+  if (!bookById) {
+    res.status(404).send('No book found!')
+  };
+  
+  res.json(bookById);
+})
+
+//get bbok data by name
+app.get('/books/name/:bookName', (req, res) => {
+  const { bookName } = req.params
+  const bookByName = booksData.find((item) => item.title.toLowerCase() === bookName.toLowerCase())
+
+  if(!bookName) {
+    res.status(404).json({
+      response: 'No book found with that name',
+      success: false
+    }) 
+  } else {
+    res.status(200).json({
+      response: bookByName,
+      success: true
+    }) 
+  }
+})
 
 // Start the server
 app.listen(port, () => {
