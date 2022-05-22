775f7e53f5d807a72336a48d6de78e63b19ae489
Dorothea-B <barre.dorothea@gmail.com>
2022-05-22 20:43:29 +0200

    recipe json file and multiple endpoints

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
+import bodyParser from 'body-parser'
 import cors from "cors";
+import listEndpoints from "express-list-endpoints"
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -8,6 +10,8 @@ import cors from "cors";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
+
+import recipes from "./data/recipes.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -20,11 +24,116 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+// app.get("/", (req, res) => {
+//   res.send("Hello Technigo!");
+// });
+
+app.get('/', (req, res) => {
+  // res.send(`Welcome to Dorothea's recipe API!`)
+  res.send(listEndpoints(app))
+})
+
+//Basic main endpoint
+
+// app.get('/recipes', (req, res) => {
+//   res.json(recipes)
+// })
+
+//Enpoint for individual object
+app.get('/recipes/:index', (req, res) => {
+  const {index} = req.params
+
+  const recipeId = recipes.find(recipe => recipe.id === +index)
+
+  if (!recipeId) {
+    console.log("No recipe found");
+    res.status(404).send('No recipe found')
+  } else {
+    res.json(recipeId)
+  }
 });
+
+
+app.get('/recipes/id/:index', (req, res) => {
+  const {index} = req.params
+
+  const recipeId = recipes.find(recipe => recipe.id === +index)
+
+  if (!recipeId) {
+    console.log("No recipe found with that ID");
+    res.status(404).send('No recipe found with that ID')
+  } else {
+    res.json(recipeId)
+  }
+});
+
+
+app.get('/vegetarian', (req, res) => {
+
+    const filteredRecipes = recipes.filter( recipe => recipe.vegetarian === true )
+    res.json(filteredRecipes)
+
+})
+
+app.get('/recipes', (req, res) => {
+
+
+
+const { name, type } = req.query
+
+let recipesToSend = recipes
+
+if (name) {
+  recipesToSend = recipesToSend.filter(
+    (item) => item.name.toLowerCase().indexOf(name.toLowerCase()) !== -1
+  )
+}
+if (type) {
+  recipesToSend = recipesToSend.filter(
+    (item) => item.type.toLowerCase() === type.toLowerCase()
+  )
+}
+
+
+res.json({recipesToSend})
+
+})
+
+
+// app.get('/recipes/type/:type', (req, res) => {
+//   const {typeinput} = req.params
+
+//   const recipesByType = recipes.filter((item) => item.type === typeinput)
+
+//   res.json(recipesByType)
+
+// })
+
+
+// app.get('/recipes/recipe/:name', (req, res) => {
+  
+// const {name} = req.params
+// const recipeByName = recipes.find(item => item.id === name)
+
+// if (!recipeByName) {
+// res.status(404).json({
+//  response: 'No recipe with that name',
+//  success: false
+// }) 
+// } else {
+//   res.status(200).json({
+//     response: recipeByName,
+//     success: true
+//   })
+// }
+// });
+
+
+
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+
