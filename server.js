7afeac8701444b3fe8ffb8def911893e17af4fe0
Petra Eriksson <eriksson.petra@gmail.com>
2022-11-14 15:27:18 +0100

    changed the res.send message to see if it updates on google cloud

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Hello TTechnigo!");
 });
 
 // Start the server
