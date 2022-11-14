567e1e47d7b66904c0168b9b67cbd9216c91431a
Kristin <kristin@iMac.local>
2022-11-14 15:17:16 +0100

    Video lession with Jennie

--- server.js
+++ server.js
@@ -1,30 +1,50 @@
-import express from "express";
-import cors from "cors";
+import express from "express"
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+const app = express()
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+app.get('/', (req, res) => res.send("Hello World"))
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.get("/users", (req, res) => {
+  res.json([
+    { name:"Alice"},
+    { name: "Bob"},
+    { name: "Chris"}
+  ])
+})
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+app.listen(3000,() => console.log('App listening on port'))
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+
+
+
+
+// import express from "express";
+// import cors from "cors";
+
+// // If you're using one of our datasets, uncomment the appropriate import below
+// // to get started!
+// // import avocadoSalesData from "./data/avocado-sales.json";
+// // import booksData from "./data/books.json";
+// // import goldenGlobesData from "./data/golden-globes.json";
+// // import netflixData from "./data/netflix-titles.json";
+// // import topMusicData from "./data/top-music.json";
+
+// // Defines the port the app will run on. Defaults to 8080, but can be overridden
+// // when starting the server. Example command to overwrite PORT env variable value:
+// // PORT=9000 npm start
+// const port = process.env.PORT || 8080;
+// const app = express();
+
+// // Add middlewares to enable cors and json body parsing
+// app.use(cors());
+// app.use(express.json());
+
+// // Start defining your routes here
+// app.get("/", (req, res) => {
+//   res.send("Hello Technigo!");
+// });
+
+// // Start the server
+// app.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}`);
+// });
