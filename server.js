8fddb667fd9af99b4616d17b6e6780bb8c0d5c71
Sneezan <ceciliafraanlund@gmail.com>
2022-11-15 16:09:04 +0100

    started building API, added three routes

--- server.js
+++ server.js
@@ -1,30 +1,49 @@
 import express from "express";
 import cors from "cors";
+import bookData from "./data/books.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  const navigation = {
+    API_description: "These are the routes for this book API",
+    Endpoints: [
+      {
+        "/bookData": "Display all books",
+        "/bookData/average_rating/": "Average rating of books",
+        "/bookData/num_pages/": "Sorts the books based on number of pages",
+      },
+    ],
+  };
+  res.send(navigation);
 });
 
-// Start the server
+// get all the books and their data
+app.get('/bookData', (req, res) => {
+  res.status(200).json({
+    data: bookData,
+    success: true,
+  })
+  })
+
+// sorts the api to display  book rating from highest to lowest
+app.get("/bookData/average_rating/", (req, res) => {
+  const { average_rating } = req.params;
+  const bookRating = bookData.sort((a, b) => b.average_rating - a.average_rating)
+    res.json(bookRating.slice(0, [-1])) 
+})
+
+// sorts the books in order from most to fewest pages
+app.get("/bookData/num_pages/", (req, res) => {
+  const { num_pages } = req.params;
+  const bookPages = bookData.sort((a,b)=>b.num_pages-a.num_pages)
+  res.json(bookPages[0]) 
+})
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
