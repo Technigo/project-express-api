82fceede20a899a9c14b3ac87325070515690ddc
Tina Bruce <tinabruce42@hotmail.com>
2022-11-15 17:33:00 +0100

    Added Initial Routes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
+import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
@@ -19,10 +19,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+// Route that returns all data on all books
+app.get("/books", (req, res) => {
+  // usually written: res.send({responseMessage: "Hello Technigo"})
+  let allBooks = booksData;
+
+  // Filter for top-rated books
+  const showTop = req.query.top;
+  if (showTop) {
+    allBooks = allBooks.filter((book) => book.average_rating > '4.5')
+  } 
+  res.json({ booksData: allBooks }) 
 });
+
+
+// Route for books by a specific author
+app.get("/books/:author", (req, res) => {
+  const author = req.params.author
+  const byAuthor = booksData.filter((book) => book.authors.toLowerCase() === author.toLowerCase())
+  res.json({ byAuthor }) 
+})
 
 // Start the server
 app.listen(port, () => {
