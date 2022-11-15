aa9b4e716887c2d9777ccfe4630066e65f225fab
Julia Ostedt <julia.ostedt@gmail.com>
2022-11-15 12:05:43 +0100

    added Tuesday lecture

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import technigoMembers from "./data/technigo-members.json"
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -21,8 +22,26 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  // console.log("req", req);
+  // console.log("res", res);
+  // res.send({responseMessage: "Hello Technigo!"});
+  res.json({responseMessage: "Hello Technigo!"});
 });
+//  HTMLElement.addEventListener('nameOfTheListener', () => {
+//  })
+app.get("/members", (req, res) => {
+  res.status(200).json({technigoMembers: technigoMembers});
+});
+app.get("/members/:id", (req, res) => {
+  const singleMember = technigoMembers.find((member) => {
+    return member.id === Number(req.params.id);
+    // return member.id === +req.params.id;
+    // return member.id.toString() === req.params.id;
+    // return member.id === req.params.id;
+  });
+  res.status(200).json(singleMember);
+});
+
 
 // Start the server
 app.listen(port, () => {
