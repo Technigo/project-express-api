377be78f40a80e431db5657ace9f7757026b2d24
Julia Ostedt <julia.ostedt@gmail.com>
2022-11-16 11:59:17 +0100

    displaying avocadosales

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-import express from "express";
+import express, { query } from "express";
 import cors from "cors";
-import technigoMembers from "./data/technigo-members.json"
+import avocadoSalesData from "./data/avocado-sales.json";
+
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
 
+// import booksData from "./data/books.json";
+// import topMusicData from "./data/top-music.json";
+// import netflixData from "./data/netflix-titles.json";
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -21,25 +21,39 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
+
 app.get("/", (req, res) => {
-  // console.log("req", req);
-  // console.log("res", res);
-  // res.send({responseMessage: "Hello Technigo!"});
-  res.json({responseMessage: "Hello Technigo!"});
+   res.send("Hello world!!");
 });
-//  HTMLElement.addEventListener('nameOfTheListener', () => {
-//  })
-app.get("/members", (req, res) => {
-  res.status(200).json({technigoMembers: technigoMembers});
-});
-app.get("/members/:id", (req, res) => {
-  const singleMember = technigoMembers.find((member) => {
-    return member.id === Number(req.params.id);
-    // return member.id === +req.params.id;
-    // return member.id.toString() === req.params.id;
-    // return member.id === req.params.id;
+
+app.get("/avocadosales", (req, res) => {
+  res.status(200).json({
+    data: avocadoSalesData,
+    success: true,
   });
-  res.status(200).json(singleMember);
+});
+
+app.get("/regions", (req, res) => {
+  const regionsList = avocadoSalesData.map((data) => data.region);
+  const regions = [...new Set(regionsList)];
+
+  if (regions) {
+    res.status(200).json({ data: regions, success: true });
+  } else {
+    res.status(200).json({ data: [], success: false });
+  }
+});
+
+app.get("/avocadosales/:region", (req, res) => {
+  const region = req.params.region;
+  const avocadoRegion = avocadoSalesData.filter(
+    (item) => item.region.toLowerCase() === region.toLowerCase()
+  );
+  if (avocadoRegion) {
+    res.status(200).json({ data: avocadoRegion, success: true });
+  } else {
+    res.status(200).json({ data: [], success: false });
+  }
 });
 
 
