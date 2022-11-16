a2cf940eaf28cf64e84c202c3ca009a14d44a030
Maria Westling <maria_westling@hotmail.com>
2022-11-16 13:00:52 +0100

    json added

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 import express from "express";
 import cors from "cors";
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import eurovisionWinners from "./data/eurovision-winners.json"
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -21,8 +14,28 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.json("Hello Europe!");
 });
+
+app.get("/winners", (req, res) => {
+  res.status(200).json({ eurovisionWinners: eurovisionWinners })
+})
+
+app.get("/winners/:year", (req, res) => {
+  const singleWinner = eurovisionWinners.find((w) => {
+    return w.year === +req.params.year
+  })
+  console.log(singleWinner)
+  res.status(200).json({ singleWinner })
+})
+
+app.get("/:country", (req, res) => {
+  const winningCountry = eurovisionWinners.filter((c) => {
+    return c.country === req.params.country
+  })
+  console.log(winningCountry)
+  res.status(200).json({ winningCountry })
+})
 
 // Start the server
 app.listen(port, () => {
