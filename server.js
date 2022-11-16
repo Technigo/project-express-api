f078a027ec16461b4e323a7527d471b654cf579c
Josefine Engdahl <josefineengdahl@MacBook-Air-som-tillhor-Josefine.local>
2022-11-16 16:03:56 +0100

    added 2 routes

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 import express from "express";
 import cors from "cors";
+import goldenGlobesData from "./data/golden-globes.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
+// console.log(data.length)
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -19,10 +13,50 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+
+// Start route on default port
+// app.get("/", (req, res) => {
+  //Remove ResponsMessage and curly brackets to just get the message as usual in the browser, not in an API
+  // res.send("Hello Technigo!");
+//   res.json({ResponsMessage:"Hello Technigo!"}); //expects a JSON object, not just a string in a browser
+// });
+
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.sendFile(__dirname + "/index.html");
 });
+
+//the style-sheet is linked in its own route, and sent as the response using sendFile
+app.get("/style.css", (req, res) => {
+  res.sendFile(__dirname + "/style.css");
+});
+
+
+// ROUTE 1 - collection of results (array of elements)
+app.get("/nominations", (req, res) => {
+  res.status(200).json(goldenGlobesData); 
+});
+
+//ROUTE 2 - - collection of results (array of elements) using filter
+app.get("/year/:year", (req, res) => {
+  const year = +req.params.year;
+  const yearAward = goldenGlobesData.filter((item)=>item.year_award === year)
+
+  if (!year) {
+  res.status(404).send("no season found with that id");
+  } else 
+  res.json(yearAward)
+  })
+ 
+  //ROUTE 3 - a single result (single element) using find
+
+  app.get("/nominations/:film", (req, res) => {
+      const singleFilm = goldenGlobesData.find((movie) => {
+        return movie.film.toLowerCase() === req.params.film.toLowerCase();
+      }); 
+       res.status(200).json(singleFilm);
+    });
+    
+// Route 4 Two filters
 
 // Start the server
 app.listen(port, () => {
