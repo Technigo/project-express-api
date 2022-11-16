3c5dc0cc702da224f541bd835cc7e2c8ccf5c6a7
Fonnt <dballesterfont@gmail.com>
2022-11-16 16:10:18 +0100

    [db] added endpoints: collection /movies. Year, title, type, director

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
+/* import techingomembers from "./data/technigo-members.json" */
+import netflixData from "./data/netflix-titles.json";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
+
 // import topMusicData from "./data/top-music.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
@@ -20,9 +22,59 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
+
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.status(200).json({ resMessage: "Netflix titles" });
 });
+
+app.get("/movies", (req, res) => {
+  res.status(200).json({ netflixData: netflixData });
+});
+
+app.get("/movies/year/:release_year", (req, res) => {
+  const allMoviesByYear = netflixData.filter((movie) => {
+    return movie.release_year === +req.params.release_year;
+  });
+  res.status(200).json({ allMoviesByYear })
+});
+
+app.get("/movies/title/:title", (req, res) => {
+  const singleMovieByTitle = netflixData.find((movie) => {
+    return movie.title === req.params.title;
+  });
+  res.status(200).json({ singleMovieByTitle })
+});
+
+app.get("/movies/type/:type", (req, res) => {
+  const singleMovieByType = netflixData.filter((movie) => {
+    return movie.type === req.params.type;
+  })
+  res.status(200).json({ singleMovieByType })
+})
+
+app.get("/movies/director/:director", (req, res) => {
+  const moviesByDirector = netflixData.filter((movie) => {
+    return movie.director === req.params.director;
+  })
+  res.status(200).json({ moviesByDirector })
+})
+
+// app.get("/", (req, res) => {
+//   res.status(200).json({ responseMessage: "Hello Technigo!" });
+// });
+
+// app.get("/members", (req, res) => {
+//   res.status(200).json({ techingomembers: techingomembers })
+// })
+// app.get("/members/:id", (req, res) => {
+//   const singleMember = techingomembers.find((member) => {
+//     return member.id === +req.params.id;
+//     // return member.id === Number(req.params.id)
+//     // return member.id.toString() === req.params.id;
+//     // return member.id == request.params.id;
+//   })
+//   res.status(200).json(singleMember);
+// })
 
 // Start the server
 app.listen(port, () => {
