315c3f0eb4c7d4f259d8c4815811b25e5de75d18
KajaWilbik <wilbik.kaja@gmail.com>
2022-11-17 15:56:46 +0100

    everything done

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-import express from "express";
+import express, { request } from "express";
 import cors from "cors";
+import booksData from './data/books.json'
+// import average_rating from '.data/books.json'
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -21,9 +23,55 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  // res.send({responseMessage: "Hello Technigo!"}); // we are send when we would like to sent translate json object
+  res.json ({responseMessage: "Hello Technigo!"}) // it is sending as a json, which makes it more secure if there is some error
 });
 
+app.get("/library", (req, res) => {
+ const { title, isbn, authors } = req.query;
+   let books = booksData;
+
+  if (title) {
+    books = books.filter(singleBook => singleBook.title.toLowerCase() === title.toLowerCase()); // to be sure that API will read all possible way of writing it
+                                                                                                // it is important to use toLowerCase function
+  }
+
+  if (isbn){
+    books = books.filter(singleBook => singleBook.isbn == isbn);
+  }
+
+  if (authors) {
+    books = books.filter(singleBook => singleBook.authors.toLowerCase() === authors.toLowerCase())
+  }
+
+
+
+  res.status(200).json ({
+    success: true,
+    message: "OK",
+    body: {
+      booksData: books
+    }
+   }) // it is sending as a json, which makes it more secure if there is some error
+
+  
+
+});
+
+app.get("/library/:id", (req, res) => {
+  const singleBook = booksData.find((item) => {
+    return(item.bookID === + req.params.id)
+  });
+  console.log(singleBook)
+  res.json ({booksData  : singleBook}) // it is sending as a json, which makes it more secure if there is some error
+});
+
+app.get("/library", (req, res) => {
+  res.json (singleBook) // it is sending as a json, which makes it more secure if there is some error
+});
+
+
+// res.satus() allowed us to choose the response code message for examole 200, 404 etc.
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
