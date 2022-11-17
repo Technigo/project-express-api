8839c7dcae8b735f7b33d9d15d6e4251c06c98f0
Tina Bruce <tinabruce42@hotmail.com>
2022-11-17 16:18:48 +0100

    Added response codes

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 # Project Express API
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+The aim of this project is to create an API with RESTful endpoints returning either an array of data or a single item.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+Node.js and framework Express is used to create the API and run the server 
+
+ENDPOINTS CREATED:
+
+/books - returns all the books with all their data, has two filters:
+
+  /books?author=Firstname Lastname  - returns all books by specific author
+  /books?top=true - returns books with a rating higher than 4.3 
+
+/books/:id - returns a single book according to a specific id entered 
+
+With more time, I plan to add pagination to limit the number of results shown for a request, as well as implement a frontend part for this project to help isualize how the API could be used.
 
 ## View it live
+Google Cloud Platform is used to deploy the API:
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+https://project-express-api-eyln4nn5aa-lz.a.run.app/books
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import booksData from "./data/books.json";
+// install for viewing all routes?? https://www.npmjs.com/package/express-list-endpoints
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -12,43 +13,59 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Route welcoming user
+app.get('/', (req, res) => {
+  res.send("Welcome to ReadIt! - See this API live at: https://xxx-app.netlify.app/")
+});
+
 // Route that returns all data on all books
 app.get('/books', (req, res) => {
 
   // function for pagination:
-  const pagination = (data, pageNumber) => {
-    const pageSize = 45
-    const startIndex = (pageNumber-1) * pageSize
-    const endIndex = startIndex + pageSize
-    const itemsOnPage = data.slice(startIndex, endIndex)
+//   const pagination = (data, pageNumber) => {
+//     const pageSize = 45
+//     const startIndex = (pageNumber-1) * pageSize
+//     const endIndex = startIndex + pageSize
+//     const itemsOnPage = data.slice(startIndex, endIndex)
 
-    const returnObject = {
-        page_size: pageSize,
-        page: pageNumber,
-        num_of_pages: Math.ceil(data.length / pageSize),  //was pagesize
-        items_on_page: booksOnPage.length,
-        results: itemsOnPage
-    }
-    return returnObject
-}
-  const author = req.query.author
-  const top = req.query.top
+//     const returnObject = {
+//         page_size: pageSize,
+//         page: pageNumber,
+//         num_of_pages: Math.ceil(data.length / pageSize),  //was pagesize
+//         items_on_page: booksOnPage.length,
+//         results: itemsOnPage
+//     }
+//     return returnObject
+// }
+  //FILTERS:
+  const { author, top } = req.query
   let allBooks = booksData;
   
-  // Filter for specific author: /books?author=douglas%20adams
+  // ... for specific author: e.g. /books?author=douglas%20adams
   if (author) {
     allBooks = allBooks.filter((book) => 
       book.authors.toLocaleLowerCase()
       .includes(author.toLocaleLowerCase()))
   }
-  // Filter for top-rated books: /books?top=true
+  // ... for top-rated books: e.g. /books?top=true
   if (top) {
     allBooks = allBooks.filter((book) => 
       book.average_rating > '4.3')
   } 
   if (allBooks.length === 0) {
-		res.status(404).json("Sorry we couldn't find the book you seek")
-	} else res.status(200).json(allBooks) 
+		res.status(404).json({
+      success: false,
+      message: "Sorry we couldn't find any match for your search",
+      body: {}
+    }
+      )
+	} else res.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        booksData: allBooks
+      }
+  }) 
 });
 
 
@@ -59,8 +76,18 @@ app.get("/books/:id", (req, res) => {
     return book.bookID === +id})
 
   if (!singleBook) {
-    res.status(404).json("Sorry we couldn't find a book with that id")
-	} else res.status(200).json(singleBook) 
+    res.status(404).json({
+      success: false,
+      message: "Sorry we couldn't find a book with that id",
+      body: {} 
+    })
+	} else res.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        book: singleBook 
+      }
+  }) 
 })
 
 // Start the server
