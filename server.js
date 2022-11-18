3e896aeb13788d6e0edd57f508aafdc0373dd98a
Annika Gadman <annika.gadman@gmail.com>
2022-11-18 15:54:00 -0600

    tried to sort on average price

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import technigoMembers from "./data/technigo-members.json";
+//import technigoMembers from "./data/technigo-members.json";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -22,69 +22,23 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  // console.log("req", req);
-  // console.log("res", res);
-  // res.send({responseMessage: "Hello Technigo!"});
-  res.json({responseMessage: "Hello Technigo!"});
+  res.json({responseMessage: "🥑 Check out US avocado sales! 🥑"});
 });
 
+// -----------  THIS WEEK'S PROJECT - AVOCADO SALES -------------------
 
-// CODE ALONG LIVE SESSIONS
-app.get("/members", (req, response) => {
-  const { name, role } = req.query;
-  let members = technigoMembers;
-
-  if (role) {
-    // members = technigoMembers.filter(singleTechnigoMember => {return singleTechnigoMember.role === role});
-    members = members.filter(singleTechnigoMember => singleTechnigoMember.role.toLowerCase() === role.toLowerCase());
-  }
-
-  if (name) {
-    members = members.filter(singleTechnigoMember => {return singleTechnigoMember.name.toLowerCase() === name.toLowerCase()});
-    //members = technigoMembers.filter(singleTechnigoMember => singleTechnigoMember.role.toLowerCase() === role.toLowerCase());
-  }
-
-  response.status(200).json({
-    success: true,
-    message: "OK",
-    body: {
-      technigoMembers: members
-    }
-  });
-});
-
-app.get("/members/:id", (request, response) => {
-    const singleMember = technigoMembers.find((member) => {
-    return member.id === Number(request.params.id);
-    // return member.id === +request.params.id;
-    // return member.id.toString() === request.params.id;
-    // return member.id == request.params.id;
-  });
-  if(singleMember) {
-      response.status(200).json({
-      success: true,
-      message: "OK",
-      body: {
-        technigoMembers: singleMember
-      }
-    });
-  } else {
-      response.status(404).json({
-      success: false,
-      message: "Not found",
-      body: {}
-  });
-}
-  console.log(singleMember);
-  
-});
-
-
-// THIS WEEK'S PROJECT - AVOCADO
+// get the total list of data
 app.get("/avocado-sales", (req, res) => {
   res.status(200).json({avocadoSalesData});
 });
 
+// get the total list of data in reversed order
+app.get("/avocado-sales/reversed", (req, res) => {
+  const avocadoSalesDataReversed = avocadoSalesData.reverse()
+  res.status(200).json({avocadoSalesDataReversed });
+});
+
+// search by id
 app.get("/avocado-sales/:id", (req, res) => {
   const singleAvocado = avocadoSalesData.find((avocado) => {
   return avocado.id === Number(req.params.id);
@@ -107,17 +61,31 @@ if(singleAvocado) {
 console.log(singleAvocado);
 });
 
+// search by region
 app.get("/avocado-sales/region/:region", (req, res) => {
   const region = req.params.region
-  const filteredOnRegionName = avocadoSalesData.filter((item) => item.region === region)
+  const filteredOnRegionName = avocadoSalesData.filter((item) => item.region.toLowerCase() === region.toLowerCase());
   res.status(200).json({filteredOnRegionName});
 });
 
+// get a list of avocado sales under or equal to the route parameter
+app.get('/avocado-sales/averagePrice/:averagePrice', (req, res) => {
+  const { averagePrice } = req.params
+  const avocados = avocadoSalesData.filter(avocado => avocado.averagePrice <= averagePrice)
+
+  if (avocados.length === 0) {
+    res.status(404).send('No avocado sales found to this price....🥑')
+  } else {
+    res.json(avocados)
+  }
+})
+
+// get a list of the lowest to highest average price - Doesn't work! 
 app.get("/avocado-sales/averagePrice", (req, res) => {
-  const{ averagePrice } = req.params;
-  const avocadoAveragePrice = avocadoSalesData.sort((a, b) => b.averagePrice - a.averagePrice)
-  res.json(avocadoAveragePrice.slice(0, [-1]))
+  const avocadoAveragePrice = avocadoSalesData.sort((a, b) => Number(b.averagePrice) - Number(a.averagePrice))
+  res.status(200).json(avocadoAveragePrice.slice(1, [-1]))
 });
+
 
 
 // Start the server
