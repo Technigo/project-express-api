a1cf9751e566e1733c9be28f03a78a35a52281f3
Cecilia Råden <you@example.com>
2022-11-18 11:24:05 +0100

    cleaned code

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express, { response } from "express";
 import cors from "cors";
-//import technigoMembers from "./data/technigo-members.json";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -16,32 +15,28 @@ import booksData from "./data/books.json";
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+// Middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+// My routes
 app.get("/", (req, res) => {
-//console.log("req", req);
-//console.log("req", res);
- // res.send({responseMessage: "Hello Technigo!" }) // Already processing something as a json property, a raw body. String before = will be sending a string
-  res.json({responseMessage: "Welcome to my page where you can find information on 500 books. Please add /*** in the address field to read about a book" }) // Expects an json object and will be sent accordingly. More secure. 
-});  //KLAR
+  res.json({responseMessage: "Welcome to my page where you can find information on 500 books. Please add /books/ and then a number in between 1-500, or /languages/ and for example 'fre' to see books in french." }) // Expects an json object and will be sent accordingly. More secure. 
+});  
 
 app.get("/books/:bookID", (req, res) => {
   const singleBookId = booksData.find((singleId) => {
     return singleId.bookID === +req.params.bookID;
   })
   res.status(200).json({singleBookId});
-}); //KLAR
+}); 
 
 app.get("/languages/:language_code", (req, res) => {
   const showLanguage = booksData.filter((test) => {
     return test.language_code === req.params.language_code;
   })
   res.status(200).json({showLanguage});
-}); //KLAR 
-
+}); 
 
 // Start the server
 app.listen(port, () => {
