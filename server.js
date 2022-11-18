54991395a066fd7c6f592a8ce47fbc30e70b90b8
jessikalind <jessika.lind@me.com>
2022-11-18 10:20:43 +0100

    queries added

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { response } from "express"
 import cors from "cors"
 import booksData from "./data/books.json"
 
@@ -12,14 +12,28 @@ const app = express()
 app.use(cors());
 app.use(express.json())
 
-// ROUTE 1: Main page
+// ROUTE 1: Main "page"
 app.get("/", (req, res) => {
-  res.json({responseMessage: "Welcome to Jessika's book-API 📚. The following endpoints are available: /books; /books/:bookID"})
+  res.json({responseMessage: "Welcome to Jessika's book-API 📚. The following endpoints are available: /books (with the query parameters language_code and authors) and /books/:bookID"})
 })
 
-// ROUTE 2: All books
+// ROUTE 2: All books with query paramenters (language code and authors)
 app.get("/books", (req, res) => {
-  res.status(200).json({booksData: booksData})
+  const { authors, language_code } = req.query
+  let books = booksData
+  if (language_code) {
+    books = books.filter(code => code.language_code.toLowerCase() === language_code.toLocaleLowerCase())
+  }
+  if (authors) {
+    books = books.filter(name => { return name.authors.toLowerCase() === authors.toLocaleLowerCase()})
+  }
+  res.status(200).json({
+    success: true, 
+    message: "OK",
+    body: {
+      booksData: books
+    }
+    })
 })
 
 // ROUTE 3: Single books displayed by ID
@@ -27,11 +41,24 @@ app.get("/books/:bookID", (req, res) => {
   const singleBook = booksData.find((book) => {
     return book.bookID ===  +req.params.bookID
   })
-  console.log(singleBook)
-  res.status(200).json({singleBook})
+  if (singleBook) {
+    res.status(200).json({
+      success: true, 
+      message: "OK",
+      body: {
+        books: singleBook
+      }
+  })
+} else {
+  res.status(404).json({
+    sucess: false,
+    message: "Not found",
+    body: {}
+  })
+}
 })
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
-});
+})
