9038cd49d7f1bc14dbcc8a10a4ef33d8ad332cef
Iracuzzi <arnau.monforte@gmail.com>
2022-11-18 17:00:15 +0100

    API info and routes

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import cors from "cors";
+import netflixData from "./data/netflix-titles.json";
+
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
@@ -21,8 +22,58 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send('Welcome to Netflix data!');
 });
+
+app.get("/movies", (req, res) => {
+  const moviesList = netflixData.filter((movie) => movie.type === "Movie")
+
+  if (!moviesList){
+    res.status(404).json({
+      message: 'Sorry, we no have movies for now. Try another day!',
+      error: 404}) 
+  } 
+
+  res.status(200).json(moviesList) 
+})
+
+app.get("/shows", (req, res) => {
+  const showsList = netflixData.filter((show) => show.type === "TV Show")
+
+  if (!showsList){
+    res.status(404).json({
+      message: 'Sorry, we no have any show for now. Try another day!',
+      error: 404}) 
+  } 
+
+  res.status(200).json(showsList) 
+})
+
+app.get("/titles/:id", (req, res) => {
+  const { id } = req.params
+  const showInfoId = netflixData.filter((show) => show.show_id === +id)
+  if (showInfoId && showInfoId?.length > 0){
+    res.status(200).json(showInfoId)
+  } else {
+    res.status(404).send({
+    message: "showId not found, try another number",
+    error: 404})
+  }
+});
+
+app.get("/titles/name/:title", (req, res) => {
+  const { title } = req.params
+  const showInfoTitle = netflixData.filter((show) => show.title.toLowerCase().includes(title.toLowerCase()))
+  if (!showInfoTitle) {
+    res.status(404).json({
+      message: 'Sorry, no show/film with that name. Try another',
+      error: 404}) 
+  }
+
+  res.status(200).json(showInfoTitle) 
+});
+
+
 
 // Start the server
 app.listen(port, () => {
