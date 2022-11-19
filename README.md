6b75e04952c02c04b26000c91aa7736ccfb406e0
MariaWellander <maria.wellander@gmail.com>
2022-11-19 19:10:06 +0100

    final commit: added comments, google cloud URL and README.

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # Project Express API
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+This is Maria Wellander's first backend project.
+I've created an RESTful avocado sales API in Node.js with the Express framework.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it.
-How did you plan?
-What technologies did you use?
-If you had more time, what would be next?
+To solve the problem I made a good plan. The fundamentals for me was to not overcomplicate the solution by adding features that wasn't needed in the project description - that I could add in the end if I had some spare time and extra coding energy. Focus was to make something that worked and that I could understand. I didn't want to lack time, be stressed and don't understand what I'd built. Therefore, I chose a JSON consisting of sales data. Loving avocados, having a background in business and adoring numbers, I knew that data could give me the safe and secure start that is so much appreciated.
+
+Firstly, I planned on getting a good understanding of the project goals and achieve a decent result by; reading and watching all the material and taking part in all the activities for the week. During the building of the project, if I got stuck I wanted to really take the time to understand by re-read and re-watch the material. Secondly, I made a Google Jamboard with a Kanban board to structure up my project and my plans. Finally, I planned on code along to the lectures to have a base and then to modify the code during off-lecture-time and to add the features that I needed.
+
+For this backend project I used; Node.js, Express, Postman, nodemon, Google Cloud, REST API, JSON, Google Jamboard & Kanban board.
+
+If I had more time I would've enjoyed building a frontend to it.
 
 ## View it live
 
+https://project-express-api-xccmrh5kgq-lz.a.run.app/
--- server.js
+++ server.js
@@ -1,67 +1,36 @@
 import express from "express";
 import cors from "cors";
 import avocadoSales from "./data/avocado-sales.json";
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
-// The cors helps us to have both frontend and backend
-// to be on the local host, without having errors.
+// The cors() helps us to have both frontend and backend to be on the local host, without having errors.
+// The express.json() helps us to read the bodies from the request as a json ( => no need for ex JSON.parse() or JSON.stringify() ).
 app.use(cors());
-//Allows us to read the bodies from the request as a json
 app.use(express.json());
 
-// Start defining your routes here
-// This is the first Get request
-//The path is the slash. Is the base path in this case.
-// req = request, res = result
+// --- START OF ROUTES ---
+
+// Route 1: gets the base path with the "welcome" message.
 app.get("/", (req, res) => {
-  //console.log("req", req);
-  //console.log("res", res);
-  //res.send({responseMessage: "Hello Technigo!"});
   res.json({responseMessage: "Hello Avocado Sales Lover!"});
 });
-//HTMLElement.addEventListener('nameOfTheListener', () => {
 
-//});
-// http://localhost:8080/members?role=Code coach
-// in the browser, the blank space is represented by %20
-// syntax for the query parameters:
-// ?paramname=paramvalue&anotherparamname=anotherparamvalue
-// apply the query parameter name by using the questionmark: ?
-// query parameters = after the ?
-// apply the query parameter value by using the equal sign: =
-// parameters value = after the =
+// Route 2: gets the sales endpoint. Shows an array of all the avocado sales elements from the json.
 app.get("/sales", (req, response) => {
   const { date, averagePrice } = req.query;
   let sales = avocadoSales;
 
+  // The toString() method converts the requested number into a string.
   if (averagePrice) {
-    // http://localhost:8080/members?role=code coach
-    // members = technigoMembers.filter(singleTechnigoMember => { return singleTechnigoMember.role === role})
     sales = sales.filter(singleAvocadoSale => singleAvocadoSale.averagePrice.toString() === averagePrice);
   }
   if (date) {
-    // several parameter names => use the ampersant: &
-    // http://localhost:8080/members?role=Code coach&name=matilda
-    // sales = sales.filter(singleAvocadoSale => { return singleAvocadoSale.date.toLowerCase() === date.toLowerCase()});
     sales = sales.filter(singleAvocadoSale => { return singleAvocadoSale.date.toString() === date});
 
   }
 
-    // important that the 3rd property is the same in your responses
-    // have the same structure. 3rd on our case: body
   response.status(200).json({
     success: true,
     message: "OK",
@@ -71,13 +40,10 @@ app.get("/sales", (req, response) => {
   });
 });
 
+// Route 3: gets the sales id endpoint. Shows a single avocado sales element from the json.
 app.get("/sales/:id", (request, response) => {
   const singleSale = avocadoSales.find((sale) => {
-    // need the + to not be a string, make it a number.
-    //return member.id === +req.params.id;
-    // The 2 underneath is the same;
-    // return member.id.toString() === +req.params.id;
-    // return member.id == +req.params.id;
+    // The Number() method converts the requested string to a number.
     return sale.id === Number(request.params.id);
 
   })
@@ -99,10 +65,9 @@ app.get("/sales/:id", (request, response) => {
   console.log(singleSale);
 });
 
-// Start the server, write: npm run dev
+// --- END OF ROUTES ---
+
+// To start the server.
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-// Kill the server when done testing, to save battery:
-// = ctrl + c
-// To start the terminal in Windows = ctrl + ö
