5a1bb6ff18fb64cfd5bc6985ac900eb78bd09fd3
Elin <elin.segelov@gmail.com>
2022-11-20 22:00:55 +0100

    Add generic query and random title logic. Update readme.

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # Project Express API
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+During Thechnigo Bootcamp week 13 our assignment is to build a restful API using Express. The API had to have at least two endpoints, one showing a collection of data and one showing only one element.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+Using a json-file with data from Netflix, I first created a few endpoints using both queries and params, specific for title, release year, and country. Then I re-worked the code so I could use the same code for any query. If I had more time, I'd implement the possiblity to filter data with several keys, using the generic query, and also make some documentation for the API.
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+The API can be accessed via Google Cloud at:
+https://project-express-api-nerogguddq-lz.a.run.app/
--- server.js
+++ server.js
@@ -1,16 +1,6 @@
 import express from "express";
 import cors from "cors";
-import avocado from './data/avocado-sales.json'
-import books from './data/books.json'
-import movies from './data/netflix-titles.json'
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import netflixTitles from './data/netflix-titles.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -22,30 +12,97 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send(movies);
 });
 
+// at /movies filtering matches the query with the corresponding key of the data 
 app.get("/movies", (req, res) => {
-  const releaseYear = req.query.release_year;
-  console.log(releaseYear);
-  let allMovies = movies.filter((movie) => movie.type === "Movie");
-  if(releaseYear) {
-    allMovies = allMovies.filter((movie) => movie.release_year === +releaseYear )
+  let { key, value } = req.query;
+  key = key.toLowerCase();
+  value = value.toLowerCase()
+
+// if query contain a key but no value or vice versa the api sends bad request.
+  if((!key && value) || (key && !value)) {
+    res.status(400).json({
+      success: true,
+      message: "OK",
+      body: "Bad request"
+    });
   }
-  res.json(allMovies)
+
+  let allMovies = netflixTitles.filter((movie) => movie.type === "Movie");
+
+  if(key && value) {
+    // if query key is show_id or release_year, the value of the query compared
+    // to the value of the corresponding key in the data.
+    if(key === 'show_id' || key === 'release_year') {
+      allMovies = allMovies.filter((movie) => {
+        if(movie[key].toString() === value) return true;
+        else return false;
+      })
+      // If query key is any other than show_id or release_year, the api will return 
+      // all items including the query value.
+    } else {
+      allMovies = allMovies.filter((movie) => {
+        if(movie[key].toString().toLowerCase().includes(value)) return true;
+        else return false;
+      })
+    }
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "OK",
+    body: {
+      result: allMovies
+    }
+  });
 })
 
 app.get("/tvShows", (req, res) => {
-  res.send(movies.filter((movie) => movie.type === "TV Show"))
+  let { key, value } = req.query;
+  key = key.toLowerCase();
+  value = value.toLowerCase()
+
+  if((!key && value) || (key && !value)) {
+    res.status(400).json({
+      success: true,
+      message: "OK",
+      body: "Bad request"
+    });
+  }
+  let allShows = (netflixTitles.filter((show) => show.type === "TV Show"))
+
+  if(key && value) {
+    if(key === 'show_id' || key === 'release_year') {
+      allShows = allShows.filter((show) => {
+        if(show[key].toString() === value) return true;
+        else return false;
+      })
+    } else {
+      allShows = allShows.filter((show) => {
+        if(show[key].toString().toLowerCase().includes(value)) return true;
+        else return false;
+      })
+    }
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "OK",
+    body: {
+      result: allShows
+    }
+  });
+
 })
 
-app.get('/country/:country', (req, res) => {
-  const selectedCountry = req.params.country;
-  selectedCountry.toLowerCase();
-  const moviesFromCountry = movies.filter((movie) => movie.country.toLowerCase() === selectedCountry);
-  res.send(moviesFromCountry);
+//generate random title
+app.get('/random', (req, res) => {
+  let selectedTitle = netflixTitles[Math.floor(Math.random()*(netflixTitles.length))]  
 })
 
 // Start the server
