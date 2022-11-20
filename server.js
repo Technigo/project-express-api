8eeb4893acc940c9ca5973d5755eb6ce32e02c87
fannycardell <fannycardell@gmail.com>
2022-11-20 19:47:34 +0100

    tested all with Postman which worked

--- server.js
+++ server.js
@@ -11,51 +11,71 @@ import booksData from "./data/books.json";
 // import topMusicData from "./data/top-music.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
+// when starting the server. Example command to overwrite PORT enviremental variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
-const listEndpoints = require('express-list-endpoints');
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+// Start defining your routes here, the thing that you want to get from the database. To get to the route
+// in the browser you write "/" and then the name of the route 
 app.get("/", (req, res) => {
-  res.json(listEndpoints(app));
-  res.json("Let us see if this message will show and if it is possible to search for book titles and authors")
+  res.json({responsMessage: "Let us see if this message will show and if it is possible to search for book titles and authors"});
 });
 
 app.get("/books", (req, res) => {
   const { authors, title } = req.query;
   let books = booksData;
-
-  if (author) {
+  //new endpoint of the route
+  if (authors) {
     books = books.filter(name => name.authors.toLowerCase() === 
     authors.toLowerCase());
   }
-
+// another endpoint
   if (title) {
     books = books.filter(singleBook => { return singleBook.title.toLowerCase() === 
       title.toLowerCase()});
   }
-  respons.status(200).json({
+  res.status(200).json({
     success: true,
     message: "OK",
     body: {
-      booksData: filteredBooks
+      booksData: books
     }
   });
 });
 
+ /* if(authors) {
+		books.filter((item) => 
+			item.authors.toLocaleLowerCase()
+			.includes(authors.toLocaleLowerCase()))
+	}
+	if(title) {
+		books.filter((item) => 
+			item.title.toLocaleLowerCase()
+			.includes(title.toLocaleLowerCase()))
+	}
+	
+	if(books.length === 0) {
+		res.status(404)
+			.json("Sorry we couldn't find book you seek")
+	} else {
+		res.json(books)
+	}
+}); */
+
+// This is the path param
 app.get("/books/:bookID", (req, res) => {
   const singleBook = booksData.find((item) => {
-    return (item.bookID === +req.params.id);
+    return item.bookID === +req.params.bookID;
   });
+  console.log(singleBook);
 
   if(singleBook) {
-    response.status(200).json({
+    res.status(200).json({
       success: true,
       message: "OK",
       body: {
@@ -63,13 +83,12 @@ app.get("/books/:bookID", (req, res) => {
       }
     });
   } else {
-    response.status(404).json({
+    res.status(404).json({
       success: false,
       message: "Not Found",
       body: {}
     });
   }
-  console.log(singleBook);
 });
 
 
