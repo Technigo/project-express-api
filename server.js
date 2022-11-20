037af54741f79775d34c9a0b23caca3f5fe486f5
Mine Mine Mine <MasterNina@NBs-MacBook-Pro.local>
2022-11-20 21:46:44 +0100

    finished API Endpoints

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 import express from "express";
 import cors from "cors";
 // import technigoMembers from "./data/technigo.json";
-// import data from "./data/netflix-titles.json";
-
 import tedTalkData from "./data/ted-talks.json";
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started! 
-// NB3: This in the import nr 2
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
 
-// import topMusicData from "./data/top-music.json";
 
 // Defines the port the app will run on.
 const port = process.env.PORT || 8080;
@@ -22,60 +13,91 @@ app.use(cors());
 app.use(express.json());
 
 // Start ROUTE on defalt port 8080.
-// --------skapa mer detaljerat meddelande med alla endpoints-----------
+
 app.get("/", (req, res) => {
-   res.json({responseMassage: "Hello Everybody, welcome!"});
-  //  const navigation = {
-  //   guide: "These are the routes for this book API!",
-  //   Endpoints: [
-  //     {
-  //       "/bookData": "Display all books",
-  //       "/bookData/authors/:authors": "Search for a author",
-  //       "/bookData/title/:title": "Search for a title", 
-  //       "/bookData/average_rating/": "Average rating of books - high to low",
-  //       "/bookData/num_pages/": "The book with most number of pages",
-  //     },
-  //   ],
-  // };
-  // res.send(navigation);
+   res.send({
+    responseMassage: "Hello Everybody and welcome!",
+    guide: "These are the routes for this TED Talk API!",
+    Endpoints:"",
+    Routes:[{
+        "/allTedTalks": "All TED Talks",
+        "/speakers": "All TED Talk speakers, and how many",
+        "/speakers/:speaker/talks": "TED Talks from a Requested speaker, and how many", 
+        "/events": "All TED Talk events",
+        "/tedTalk/:id": "search for a TED Talk with specific Id",
+        "/tedTalks/mostViewedTedTalk": "How many views has the most popular talk?",
+        "/randomTedTalk": "Press enter and get a random TED Talk"
+      }],
+    Querys: [{
+      "/tedTalks": "Filter by title, speaker and/or event, amoung a selected amount (500) of talks ex. /tedTalks?speaker=al gore&event=ted2006",
+    }]
+  });
 });
 
-// ROUTE 1 - collection of results (array of elements)
+// Route 1 - collection of results (array of elements)
 app.get("/allTedTalks", (req, res) => {
   res.status(200).json(tedTalkData); 
 });
 
-// ROUTE 2 - collection of results (array of elements) using filter
-// ex /tedTalks?speaker=hans rosling
-app.get("/tedTalks", (req, res) => {
-  const { title, speaker, event } = req.query;
- 
-  let tedTalks = tedTalkData.slice(0, 8)
+// Route 2 all speakers list
 
-  if (title) {
-  tedTalks = tedTalks.filter((tedTalk) => {
-    return tedTalks.title.toLowerCase() === title.toLowerCase()});
+app.get("/speakers", (req, res) => {
+  const allSpeakers = tedTalkData.map((talk) => talk.speaker);
+  // New set to remove all dublicates
+  const allSpeakersSet = Array.from(new Set(allSpeakers));
+  if (allSpeakersSet){
+    res.status(200).json({
+      success: true,
+      message: "All TED Talk spekers",
+      'Total amount of categories': allSpeakersSet.length,
+      body: {
+          tedTalkData: allSpeakersSet
+      }
+    })
   }
-  if (speaker) {
-    tedTalks = tedTalks.filter((tedTalk) => {
-      return tedTalks.speaker.toLowerCase() === speaker.toLowerCase()});
-  }
-  if (event) {
-    tedTalks = tedTalks.filter((tedTalk) => {
-      return tedTalks.event.toLowerCase() === event.toLowerCase()});
-  }
+});
 
-  res.status(200).json({
-    success: true,
-    massage: "OK",
-    body: {
-      tedTalkData: tedTalks
-    }
-  });
-  // console.log(tedTalks);
-}); 
+// Route 3 talks by requested speakers list
+// ex /speaker/hans rosling/talks
 
-// Route 3 -  id, return a single result
+app.get("/speakers/:speaker/talks", (req, res) => {
+   const talksBySpeaker = tedTalkData.filter((talkSpeaker) => {
+    return talkSpeaker.speaker.toLowerCase() === req.params.speaker.toLowerCase();
+  })
+  if (talksBySpeaker) {
+    res.status(200).json({
+      success: true,
+      message: "All TED Talks by requested speker",
+      'Amount of talks': talksBySpeaker.length,
+      body: {
+        tedTalkData: talksBySpeaker
+      }
+    })
+  }
+});
+
+// Route 4 All TED Talk Events list
+// ex /events
+
+app.get("/events", (req, res) => {
+  const allEvents = tedTalkData.map((talkEvent) => talkEvent.event);
+  // New set to remove all dublicates
+  const allEventsSet = Array.from(new Set(allEvents));
+
+  if (allEventsSet) {
+    res.status(200).json({
+      success: true,
+      message: "All TED Talk Events",
+      'Total amount of events': allEventsSet.length,
+      body: {
+        tedTalkData: allEventsSet,
+      }
+    });
+  }
+});
+
+// Route 5 -  id, return a single result
+// ex /tedTalk/7
 
 app.get("/tedTalk/:id", (req, res) => {
   const singleTedTalk = tedTalkData.find((talk) => {
@@ -94,106 +116,85 @@ app.get("/tedTalk/:id", (req, res) => {
   } else {
     res.status(404).json({
       success: false,
-      message: "Not Found",
+      message: "Sorry, no TED Talk with that specific number, try again",
       body: {}
     })
   }
    
 });
 
-// ROUTE 4 Two filters - see won movies in a specific year
-// ----------skapa specifik filtrering----------
-// app.get("/year/:year", (req, res) => {
-//   const year = req.params.year
-//   const showWin = req.query.win
-//   let nominationsFromYear = goldenGlobesData.filter ((item) => item.year_award === +year)
-
-// if (showWin) {
-//   nominationsFromYear = nominationsFromYear.filter ((item) => item.win)
-// }
-//   res.json(nominationsFromYear)
-// })
-
-// // --------Netflix ----------------------------
-
-// app.get("/allMovies", (req, res) => {
-//   const { title, director, duration } = req.query;
+// Route 6 - collection of results (array of elements) using filter
+// ex /tedTalks?speaker=al gore&event=Ted2006
+app.get("/tedTalks", (req, res) => {
+  const { title, speaker, event } = req.query;
  
-//   let allMovies = data.slice(0, 5)
+  let tedTalks = tedTalkData.slice(0, 500)
 
-//   if (title) {
-//   allMovies = allMovies.filter((movies) => {
-//     return movies.title.toLowerCase() === title.toLowerCase()});
-//   }
-//   if (director) {
-//     allMovies = allMovies.filter((movies) => {
-//       return movies.director.toLowerCase() === director.toLowerCase()});
-//   }
-//   if (duration) {
-//     allMovies = allMovies.filter((movies) => {
-//       return movies.duration.toLowerCase() === duration.toLowerCase()});
-//   }
+  if (title) {
+  tedTalks = tedTalks.filter((tedTalk) => {
+    return tedTalk.title.toLowerCase() === title.toLowerCase()});
+  }
+  if (speaker) {
+    tedTalks = tedTalks.filter((tedTalk) => {
+      return tedTalk.speaker.toLowerCase() === speaker.toLowerCase()});
+  }
+  if (event) {
+    tedTalks = tedTalks.filter((tedTalk) => {
+      return tedTalk.event.toLowerCase() === event.toLowerCase()});
+  }
 
-//   res.status(200).json({
-//     success: true,
-//     massage: "OK",
-//     body: {
-//       data: allMovies
-//     }
-//   });
-//   // console.log(allMovies);
-// });   
+  res.status(200).json({
+    success: true,
+    massage: "OK",
+    body: {
+      tedTalkData: tedTalks
+    }
+  });
+  // console.log(tedTalks);
+}); 
 
-// // Route 2 -  id, return a single result
+// Route 7 How many views does the most popular TED Talk have?
+app.get("/tedTalks/mostViewedTedTalk", (req, res) => {
+  const mostViews = tedTalkData.reduce((a, b) => (a.views > b.views) ? a : b) .views
+  
+  if(mostViews) {
+        res.status(200).json({
+          success: true,
+          message: "Copy number, an go to /tedTalks/mostViewed",
+          body: {
+            tedTalkData: mostViews
+          }
+        })
+  }        
+  // console.log(mostViews) 
+ });
 
-// app.get("/movie/:id", (req, res) => {
-//   const singleMovie = data.find((movie) => {
-//     return movie.show_id === +req.params.id;
-//   }); 
-//   // res.status(200).json(singleMovie);
-    
-//   if(singleMovie) {
-//     res.status(200).json({
-//       success: true,
-//       message: "OK",
-//       body: {
-//         movie: singleMovie
-//       }
-//     });
-//   } else {
-//     res.status(404).json({
-//       success: false,
-//       message: "Not Found",
-//       body: {}
-//     })
-//   }
-   
-// });
+// Route 8 random TED Talk
 
-// -------------------------------------
-
-// ----skapa en random val av tedtalk, -------------------
-// // random movie title
-// app.get("/random-movie", (req,res) => {
-//   const randomMovie = data[Math.floor(Math.random()* data.length)]
-//   if(!randomMovie) {
-//     res.status(404).json({
-//       data: "Opps, we couldnt find any random movie right now. Please try again.",
-//       success: false, 
-//     })
-//   } else {
-//     res.status(200).json({
-//       data: randomMovie,
-//       success: true,
-//     })
-//   }
-// })
+app.get("/randomTedTalk", (req,res) => {
+  const randomTedTalk = tedTalkData[Math.floor(Math.random()* tedTalkData.length)]
+  
+  if(randomTedTalk) {
+    res.status(200).json({
+      success: true,
+      message: "Enjoy your random selected TED Talk",
+      body: {
+        tedTalkData: randomTedTalk
+      }
+    })
+  } else {
+    res.status(404).json({
+      success: false, 
+      message: "Enjoy your TED Talk",
+      body: {}
+  })
+}
+});
 
 
 
-// NBStart the server, port = (const port = process.env.PORT || 8080;) längst upp.
+// Start the server, port = (const port = process.env.PORT || 8080;) 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
 
-// ------ändra "duration" till hur många minuter tedtalket är
