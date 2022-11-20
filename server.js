ef7bc2067089557bf6efec0931bc6b4772faabd2
Archkrull <102250838+Archkrull@users.noreply.github.com>
2022-11-20 12:51:08 +0100

    cleaned up code

--- server.js
+++ server.js
@@ -2,14 +2,6 @@ import express from "express";
 import cors from "cors";
 import books from "./data/books.json"
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -20,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+// Defines the routes
 app.get("/", (req, response) => {
 const bookGuide = {
   Routes: [
@@ -56,7 +48,7 @@ app.get("/books/:bookID", (request, response) => {
   
 });
 
-// Gets book by Author (need to do lower case)
+// Gets book by Author
 app.get("/authors/:authors", (request, response) => {
   const authors = books.find((author) => {
    return author.authors.toLowerCase() === request.params.authors.toLocaleLowerCase();
@@ -72,7 +64,7 @@ app.get("/authors/:authors", (request, response) => {
 // Get book by language
 app.get("/languages/:language_code", (request, response) => {
   const languages = books.filter((language) => {
-   return language.language_code === request.params.language_code;
+   return language.language_code.toLowerCase() === request.params.language_code.toLowerCase();
   })
   if(languages.length !== 0) { 
     response.json(languages)
