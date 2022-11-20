3e1051b8b0d3ca76ec75981dc09b9d52fc8eed76
LinneaAjger <linnea.ajger@gmail.com>
2022-11-20 20:16:40 +0100

    found bugs in the sorting variable, so i deleted it. Cleaned the code up and ready to send in the project

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 import express, { query } from "express";
 import cors from "cors";
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
 import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -23,6 +17,14 @@ app.get("/", (req, res) => {
   res.json({booksData});
 });
 
+// An alternative to get the array in pages 
+// e.g. http://localhost:8080/allbooks?page=1&limit=100
+app.get("/", paginatedResults(booksData), (req, res) => {
+  res.json(res.paginatedResults);
+});
+
+//filter books by typing author or title as a query parameter
+// e.g. http://localhost:8080/books?author=Bill%20Bryson
 app.get('/books', (req, res) => {
 	const { author, title } = req.query
 	let filteredBooks = booksData
@@ -49,6 +51,8 @@ app.get('/books', (req, res) => {
 })
 
 // filter books by id
+// e.g. http://localhost:8080/books/23
+
 app.get("/books/:id", (req, res) => {
   const singleBook = booksData.filter((item) => item.bookID == req.params.id)
   if (singleBook.length === 0) {
@@ -58,35 +62,17 @@ app.get("/books/:id", (req, res) => {
   }
 });
 
-//filter books by specific author
-app.get("/authors/:author", (req, res) => {
-  const singleAuthor = booksData.filter((item) => item.authors === req.params.author)
-  if (singleAuthor.length === 0) {
-    res.status(404).json("Sorry, this author does not exist. Try again with another name.");
-  } else {
-    res.json(singleAuthor);
-  }
-});
-
-
-//sort by ratings in ascending order
-app.get("/ratings", paginatedResults(booksData), (req, res) => {
-  booksData.sort((a, b) => a.average_rating - b.average_rating).reverse();
-  res.json(res.paginatedResults);
-});
-
-
 function paginatedResults (model) {
   return (req, res, next) => {
-    const page = +req.query.page
+    const page = +req.query.page 
     const limit = +req.query.limit
   
     const startIndex = (page - 1) * limit
-    const endIndex = 100
+    const endIndex = page * limit
     
     const results = {}
   
-    if (endIndex < 100) {
+    if (endIndex < model.length) {
       results.next = {
         page: page + 1,
         limit: limit
@@ -105,35 +91,6 @@ function paginatedResults (model) {
     next()
     }
 }
-
-
-
-
-// app.get("/members", (req, res) => {
-//   res.status(200).json({technigoMembers : technigoMembers});
-// });
-
-// app.get("/members/:id", (req, res) => {
-//   const singleId = req.params.id
-//   const singleMember = technigoMembers.find((member) => member.id == singleId)
-//   res.status(200).json(singleMember);
-// });
-
-// app.get("/nominations", (req, res) => {
-//   res.json(goldenGlobesData)
-// })
-
-// app.get("/year/:year", (req, res) => {
-//   const year = req.params.year
-//   const showWin = req.query.win
-//   let nominationsFromYear = goldenGlobesData.filter((item) => item.year_award === +year)
-
-//   if (showWin) {
-//     nominationsFromYear = nominationsFromYear.filter((item) => item.win)
-
-//   }
-//   res.json(nominationsFromYear)
-// })
 
 // Start the server
 app.listen(port, () => {
