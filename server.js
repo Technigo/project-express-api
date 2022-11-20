a1508d0be1c5bbd64d0b8bcb4427d08acbd6910a
lindanorberg <linda@norberg.tv>
2022-11-20 22:51:37 +0100

    add highest price and :id routes

--- server.js
+++ server.js
@@ -12,26 +12,32 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+// Showing on first page
 app.get("/", (req, res) => {
   // console.log("res", req);
   res.json({responseMessage: "Avocados!"});
 });
 
-// This returns the whole dataset
+// This route returns the whole dataset
 app.get("/avocadoSales", (req, res) => {
   res.json(avocadoSalesData)
 });
 
-// this returns an array with all sales objects for a specific date
+// This route returns an array with all sales objects for a specific date
 app.get("/avocadoSales/:date", (req, res) => {
+
+  // gets the date from the url
   const date = req.params.date;
+
+  // filters the data and only showing sales for right date
   const dateSale = avocadoSalesData.filter((item) => item.date === date)
 
-  if (dateSale.length = 0) {
+  // if the length of the array is 0, there is no data for that date and 
+  // an error message shows
+  if (dateSale.length === 0) {
     res.status(404).json({
       success: false, 
-      message: "No sales data for that date.", 
+      message: "Sorry, no sales data for that date.", 
       body: {}
     });
   } else {
@@ -46,58 +52,41 @@ app.get("/avocadoSales/:date", (req, res) => {
 })
 
 // this returns the highest average price 
+app.get("/highestPrice", (req, res) => {
+  let price = []
+  avocadoSalesData.map((p) => { return price.push(p.averagePrice)})
+  price.sort((a, b) => b - a)
 
-// app.get("/avocadoSales/highestPrice", (req, res) => {
-//   const price 
-// });
+  res.status(200).json({
+    success: true, 
+    message: "OK", 
+    body: {
+      "Highest average price": price[0]
+    }
+  });
+})
 
-// app.get("/avocadoSales", (req, res) => {
-//   const { name, role } = req.query;
-//   let members = technigoMembers;
+// returns a single result 
+app.get("/avocados/:id", (req, res) => {
+  const singleAvocadoSale = avocadoSalesData.find((i) => i.id === Number(req.params.id)
+  )
 
-//   // you could skip {} and return if the sentence below is on one line
-//   if (role) {
-//     members = members.filter(sinlgeTechnigoMember => {return sinlgeTechnigoMember.role.toLowerCase() === role.toLowerCase()});
-//   };
-
-//   if (name) {
-//     members = members.filter(singleTechnigoMember => {return  singleTechnigoMember.name.toLowerCase()})
-//   }
-
-//   res.status(200).json({
-//     success: true, 
-//     message: "OK", 
-//     body: {
-//       technigoMembers: members
-//     }
-//   });
-// });
-
-// app.get("/members/:id", (req, res) => {
-//   const singleMember = technigoMembers.find((item) => {
-//     all of these works the same
-//     return item.id === Number(req.params.id);
-//     return item.id === +request.params.id;
-//     return item.id.toString === request.params.id;
-//     return item.id == request.params.id;
-//   });
-//   if (singleMember) {
-//   res.status(200).json({
-//     success: true, 
-//     message: "OK", 
-//     body: {
-//       members: singleMember
-//     }
-//   });
-//   } else {
-//     res.status(404).json({
-//       success: false, 
-//       message: "Not found", 
-//       body: {}
-//     });
-//   }
-
-// });
+  if (singleAvocadoSale) {
+    res.status(200).json({
+      success: true,
+      message: "OK", 
+      body: {
+        AvocadoSale: singleAvocadoSale
+      }
+    })
+  } else {
+    res.status(404).json({
+      success: false, 
+      message: "No id found", 
+      body: {}
+    });
+  }
+})
 
 // Start the server
 app.listen(port, () => {
