5a2e7af1058bdccdef5501cb9aeb0ec82afcfede
Petra Eriksson <eriksson.petra@gmail.com>
2022-11-20 22:49:29 +0100

    changed the route for year to movies/year/:year and cleaned the code

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import netflixData from "./data/netflix-titles.json"; //import the json file with data
+console.log('netflixData', netflixData)
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -12,87 +13,126 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (request, respons) => {
-  respons.status(200).json({
+app.get("/", (request, response) => {
+  response.status(200).json({
     Hello: "Here you can see all my routes!",
     Routes: [
       { "/movies": "all movie data" },
       { "/titles": "a list of all titles" },
-      { "/movies/:show_id": "E.g. if you add 81193313 as the id, it will show information about a specific movie, for instance id 81193313 will give us info about the movie Chocolate" },
-      { "/movies/:year": "a list of movie data from a specific release year" },
+      { "/movies/:id": "E.g. if you add 81193313 as the id, it will show information about a specific movie, for instance id 81193313 will give us info about the movie Chocolate" },
+      { "/movies/year/:year": "a list of movie data from a specific release year" },
     ],
   });
 });
 //get all data
-app.get("/movies", (request, respons) => {
-  const { rating, type } = request.query;
-  let filteredMovies = netflixData;
-  if (rating) {
-    // members = technigoMembers.filter(singleTechnigoMember => { return singleTechnigoMember.role === role});
-    filteredMovies = filteredMovies.filter(singleMovie => singleMovie.rating.toLowerCase() === rating.toLowerCase());
+app.get("/movies", (request, response) => {
+  console.log('Movies...')
+  if (netflixData) {
+    response.status(200).json({ 
+      data: netflixData, 
+      success: true,
+      message: "OK",
+      debug: 'Movies' });
+  } else {
+    response.status(200).json({ 
+      data: [], 
+      success: false,
+      message: "Not Found"});
   }
-  if (type) {
-    filteredMovies = filteredMovies.filter(singleMovie => { return singleMovie.type.toLowerCase() === type.toLowerCase()});
-  }
-  respons.status(200).json({
-    success: true,
-    message: "OK",
-    body: {
-      netflixData: filteredMovies
-    }
-  });
-console.log('filteredMovies',filteredMovies)
 });
+
 //get all titles
 
 app.get("/titles", (request, respons) => {
   const titleList = netflixData.map((movie) => movie.title);
 
   if (titleList) {
-    respons.status(200).json({ data: titleList, success: true });
+    respons.status(200).json({ 
+      data: titleList, 
+      success: true,
+      message: "Ok"});
   } else {
-    respons.status(200).json({ data: [], success: false });
+    respons.status(200).json({ 
+      data: [], 
+      success: false,
+      message: "Not Found"});
   }
 });
-//get all movie data from a specific year
-app.get("/movies/:year", (request, respons) => {
-  let year = request.params.year;
-  let releaseYearMovieData = netflixData.filter(
-    (movie) => +movie.release_year() === +movie.year()
-  );
 
-  if (releaseYearMovieData) {
-    respons.status(200).json({ data: releaseYearMovieData, success: true });
-  } else {
-    respons.status(200).json({ data: [], success: false });
-  }
-});
 //get singel specific movie
-app.get("/movies/:show_id", (request, response) => {
+app.get("/movies/:id", (request, response) => {
+  console.log('Looking up id')
   const singleMovie = netflixData.find((movie) => {
-    return movie.show_id === Number(request.params.show_id);
+  return movie.show_id === +request.params.id;
+   // return movie.show_id === +id;
     // return member.id === +request.params.id;
     // return member.id.toString() === request.params.id;
     // return member.id == request.params.id;
   });
+  console.log('singleMovie', singleMovie)
   if(singleMovie) {
     response.status(200).json({
       success: true,
       message: "OK",
-      body: {
-        movie: singleMovie
+      data: {
+      netflixData: singleMovie,
+      debug: 'Movies ID'
       }
     });
   } else {
     response.status(404).json({
       success: false,
       message: "Not Found",
-      body: {}
+      data: {},
+      debug: 'Movies ID false' 
     });
   }
-  console.log('singleMovie', singleMovie);
 });
 
+
+
+//get all movie data from a specific year
+app.get("/movies/year/:year", (request, response) => {
+  console.log('Looking up year')
+  let year = request.params.year;
+  let releaseYearMovieData = netflixData.filter((movie) => movie.release_year === +year
+  );
+
+  if (releaseYearMovieData) {
+    response.status(200).json({ 
+      data: releaseYearMovieData, 
+      success: true,
+      debug: 'Movies year' });
+  } else {
+    response.status(200).json({ 
+      data: [], 
+      success: false,
+      debug: 'Movies year false'  });
+  }
+});
+
+
+
+
+//filter rating
+// app.get("/movies/", (request, respons) => {
+//   const { rating, type } = request.query;
+//   let filteredMovies = netflixData;
+//   if (rating) {
+//     // members = technigoMembers.filter(singleTechnigoMember => { return singleTechnigoMember.role === role});
+//     filteredMovies = filteredMovies.filter(singleMovie => singleMovie.rating.toLowerCase() === rating.toLowerCase());
+//   }
+//   if (type) {
+//     filteredMovies = filteredMovies.filter(singleMovie => singleMovie.type.toLowerCase() === type.toLowerCase());
+//   }
+//   respons.status(200).json({
+//     success: true,
+//     message: "OK",
+//     body: {
+//       netflixData: filteredMovies
+//     }
+//   });
+// });
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
