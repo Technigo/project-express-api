6211a8335af73171a97caacd4d26e7d4eb073977
Petra Eriksson <eriksson.petra@gmail.com>
2022-11-22 13:27:18 +0100

    cleaned the code

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (request, response) => {
-  response.status(200).json({
+response.status(200).json({
     Hello: "Here you can see all my routes!",
     Routes: [
       { "/movies": "all movie data" },
