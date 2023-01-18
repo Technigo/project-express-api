d35179ac5be957a8b9ef6a9240af0e565dcb3543
jnbjensen <mail@jnbjensen.com>
2023-01-18 15:44:40 +0100

    Readme updated with link to API

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 A simple project where the aim was to build an API server. I learned:
 
 How to install and set up an Express server / How to deploy Node projects / How to build an API in Node using Express / How to create routes in Express
+
+## Link
+
+https://project-express-api-47f2s3sfea-lz.a.run.app/
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-import express, { request } from "express";
+import express from "express";
 import cors from "cors";
-import booksData from "./data/books.json"
+// import technigoMembers from "./data/technigo-books.json";
 
+import booksData from "./data/books.json";
+
+// Defines the port the app will run on. Defaults to 8080, but can be overridden
+// when starting the server. Example command to overwrite PORT env variable value:
+// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -11,36 +16,116 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.json({responseMessage: "Welcome to my API! Navigate to /books to view the entire array, or enter any of the following queries to find a specific book: bookID, title, authors, average_rating, isbn, isbn13, language_code, num_pages, ratings_count, text_reviews_count"});
+  res.send({
+    "Books API": "Look for books by ID, ISBN number or title",
+    Routes: [
+      {
+        "/books": "Get all books",
+        "/books/isbn/:isbn": "Get a book by its ISBN number",
+        "/books/id/:BooksID": "Get a book by its ID number in the database",
+        "/books?authors=:author": "Filter a book by its author/authors",
+        "/books?title=:title": "Filter a book by its title",
+        "/books?language_code=:language_code":
+          "Filter a book by its language code",
+      },
+    ],
+  });
 });
 
-// This get request below includes query parameters, allowing you to filter by the role or name in the array.
+// Books endpoint
 app.get("/books", (req, res) => {
-  const { title, authors } = req.query;
+  const { title, authors, language_code } = req.query;
   let books = booksData;
-  if (title) {
-    books = books.filter(singleBook => singleBook.title.toLowerCase() === role.toLowerCase());
-  }
   if (authors) {
-    books = books.filter(singleBook => singleBook.name.toLowerCase() === role.toLowerCase());
+    books = books.filter((singleAuthor) => {
+      return singleAuthor.authors.toLowerCase() === authors.toLowerCase();
+    });
   }
-
-
+  if (title) {
+    books = books.filter((singleBook) => {
+      return singleBook.title.toLowerCase() === title.toLowerCase();
+    });
+  }
+  if (language_code) {
+    books = books.filter((language) => {
+      return (
+        language.language_code.toLowerCase() === language_code.toLowerCase()
+      );
+    });
+  }
   res.status(200).json({
     success: true,
     message: "OK",
     body: {
-      booksData: books
-    }
-    
+      booksData: books,
+    },
   });
 });
 
-app.get("/books/id/:bookID", (req, res) => {
-  const singleBook = booksData.find((book) => {
-    return book.bookID === Number(+req.params.bookID);
+// Books by ISBN endpoint using path parameter
+app.get("/books/isbn/:isbn", (request, response) => {
+  const bookByISBN = booksData.find((book) => {
+    return book.isbn === Number(request.params.isbn);
   });
-  res.status(200).json({singleBook});
+  if (bookByISBN) {
+    response.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        booksData: bookByISBN,
+      },
+    });
+  } else {
+    response.status(404).json({
+      success: false,
+      message: "ISBN not Found",
+      body: {},
+    });
+  }
+});
+
+// Books/bookID endpoint
+app.get("/books/id/:bookID", (request, response) => {
+  const singleBook = booksData.find((book) => {
+    return book.bookID === Number(request.params.bookID);
+  });
+  if (singleBook) {
+    response.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        book: singleBook,
+      },
+    });
+  } else {
+    response.status(404).json({
+      success: false,
+      message: "ID not Found",
+      body: {},
+    });
+  }
+});
+
+// Books/average rating endpoint
+app.get("/books/rating/:average_rating", (request, response) => {
+  const ratingAverage = booksData.find((book) => {
+    return book.average_rating === Number(request.params.average_rating);
+  });
+  if (ratingAverage) {
+    response.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        book: ratingAverage,
+      },
+    });
+  } else {
+    response.status(404).json({
+      success: false,
+      message: "ID not Found",
+      body: {},
+    });
+  }
 });
 
 // Start the server
