c8f58b4ede1d5391bc4427fe5b7021070dd1988f
Ed <edvard.ekstrom@gmail.com>
2023-04-30 20:25:42 +0200

    init and two simple endpoints

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
+import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
@@ -21,8 +21,20 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Hello!");
 });
+
+app.get("/books", (req, res) => {
+  res.json(booksData);
+})
+
+app.get("/books/:bookId", (req, res) => {
+  const bookId = req.params.bookId;
+
+  let bookFromId = booksData.filter((item) => item.bookID === +bookId);
+
+  res.json(bookFromId);
+})
 
 // Start the server
 app.listen(port, () => {
