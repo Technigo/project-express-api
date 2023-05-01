54efd87ef88dbcbd79151376f89a424f1fb19145
Frid Matilda <matilda.x.frid@scania.com>
2023-05-02 00:10:32 +0200

    test

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Hello Technigo! This is Matilda speaking");
 });
 
 // Start the server
