934984e8e318e3d49da1df38cdcdd6a6c50fb750
Frid Matilda <matilda.x.frid@scania.com>
2023-05-02 17:52:50 +0200

    data added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import booksData from "./data/books.json";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -20,8 +21,54 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo! This is Matilda speaking");
+app.get("/", (request, response) => {
+  response.send("Hello Technigo! This is Matilda speaking");
+});
+
+// Will return a full list of the dataset
+// If it does not find any books, it returns error message and empty body
+app.get("/books", (request, response) => {
+  let books = booksData
+  if (books) {
+    response.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        books: books
+      }
+    });
+} else {
+  response.status(500).json({
+    success: false,
+    message: "Something went wrong",
+    body: {}
+  });
+}
+});
+
+// Will return a single book based on the id
+// If the book does nit exist, it will return an error code & empty body
+app.get("/book/:id", (request, response) => {
+  const { id } = request.params;
+  const booksId = booksData.find((item) => {return item.bookID === Number(id)})
+  console.log('id:', id)
+  console.log('booksId:', booksId)
+
+  if (booksId) {
+      response.status(200).json({
+        success: true,
+        message: "OK",
+        body: {
+          book: booksId
+        }
+      });
+    } else {
+      response.status(404).json({
+        success: false,
+        message: "Book not found",
+        body: {}
+      });
+}
 });
 
 // Start the server
