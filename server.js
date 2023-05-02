a1b2d360bef656dd2e7e90b6a45719369b2e8166
tess.aquilon <tess.aquilon@gmail.com>
2023-05-02 14:15:45 +0200

    route not working

--- server.js
+++ server.js
@@ -17,8 +17,18 @@ app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
+
+// In this callback function, I am displaying all the data from the JSON
 app.get("/games", (req, res) => {
   res.json(videoGames)
+})
+
+// Ratings endpoint with a rating variable
+app.get("/ratings/:rating", (req, res) => {
+  const rating = req.params.rating
+  console.log({ rating })
+  const ratedAs = videoGames.filter((item) => item.Rating === +rating)
+  res.json(ratedAs)
 })
 
 // Start the server
