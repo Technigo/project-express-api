bd9af350328b22934ca539d703afa4bfd0bab48e
Nora Terbocs <nora.terbocs@gmail.com>
2023-05-02 08:03:11 +0200

    Created basic structure

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import data from './data/golden-globes.json'
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -16,15 +17,29 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
+app.use(cors()); // a technology that allwos the apis to say where requests can come from to add a little bit of extra security
 app.use(express.json());
 
-// Start defining your routes here
+// Start defining your routes here, this is the end point
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
-// Start the server
+app.get('/nominations', (req,res)=>{
+  res.json(data)
+})
+
+app.get('/year/:year', (req,res)=>{
+   const year=req.params.year
+   const showWon = req.query.won
+  let nominationsFromYear = data.filter((item)=>item.year_award === +year)
+  if(showWon){
+    nominationsFromYear = nominationsFromYear.filter((item)=> item.win)
+  }
+  res.json(nominationsFromYear)
+})
+
+// Start the server that is on line 15
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
