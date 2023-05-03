f4318cea0778f73de55287b5afa5895eb2098025
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-05-03 10:45:48 +0200

    Cleaned up comments

--- server.js
+++ server.js
@@ -2,15 +2,11 @@ import express from "express";
 import cors from "cors";
 import workoutsData from "./data/workouts.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-
 const port = process.env.PORT || 8080;
 const app = express();
 const listEndpoints = require('express-list-endpoints');
 
-// Add middlewares to enable cors and json body parsing
+// Middleware to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
