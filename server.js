0e29138b336c11b7c7d5609cc40fd9368a9e7f75
khleoth <leothunell@gmail.com>
2023-05-03 10:58:08 +0200

    done with basic requirements

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 import express from "express";
 import cors from "cors";
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+// import technigoMembers from "./data/technigo-members.json";
+import booksData from "./data/books.json";
+import netflixData from "./data/netflix-titles.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
+const listEndpoints = require('express-list-endpoints');
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
@@ -21,10 +17,120 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  // res.send("Hello Technigo!");
+  res.json(listEndpoints(app));
+
 });
 
+// get all technigo members
+app.get("/books", (request, response) => {
+  const { title } = request.query;
+  let books = booksData;
+  if (title) {
+    books = booksData.filter((singleBook) => {
+      return singleBook.title.toLowerCase() === title.toLowerCase();
+    });
+  } 
+
+  if (books) {
+    response.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        booksData: books
+      }
+    });
+  } else {
+    response.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      body: {}
+    });
+  }
+});
+// DRY - don't repeat yourself
+// get all technigo members
+app.get("/books/:id", (request, response) => {
+  const { id } = request.params;
+  console.log("id: ", id);
+  const singleBook = booksData.find((books) => {
+    // return member._id == id;
+    return books.bookID === Number(id);
+    // return member._id.toString() === id;
+    // return member._id === +id; 
+  });
+  if (singleBook) {
+    response.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        books: singleBook
+      }
+    });
+  } else {
+    response.status(404).json({
+      success: false,
+      message: "item not found",
+      body: {}
+    });
+  }
+});
+
+app.get("/netflix", (request, response) => {
+  const { title } = request.query;
+  let item = netflixData;
+  if (title) {
+    item = netflixData.filter((singleItem) => {
+      return singleItem.title.toLowerCase() === title.toLowerCase();
+    });
+  } 
+
+  if (item) {
+    response.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        netflixData: item
+      }
+    });
+  } else {
+    response.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      body: {}
+    });
+  }
+});
+// DRY - don't repeat yourself
+// get all technigo members
+app.get("/netflix/:id", (request, response) => {
+  const { id } = request.params;
+  console.log("id: ", id);
+  const singleItem = netflixData.find((item) => {
+    // return member._id == id;
+    return item.show_id === Number(id);
+    // return member._id.toString() === id;
+    // return member._id === +id; 
+  });
+  if (singleItem) {
+    response.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        item: singleItem
+      }
+    });
+  } else {
+    response.status(404).json({
+      success: false,
+      message: "Item not found",
+      body: {}
+    });
+  }
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+/// endpoint/:pathParam1/:pathParam2?queryParamName=queryParamValue&queryParam5Name=queryParam5Value&queryParam2Name=queryParam2Value
