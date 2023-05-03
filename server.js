658ed36f7295bd142491822a6290cc4942e17f14
AjmalZ <ajmal.zahir@hotmail.com>
2023-05-03 15:09:45 +0200

    added routs and endpoints

--- server.js
+++ server.js
@@ -1,30 +1,119 @@
+// /////////////////// IMPORTS //////////////////////////////// //
+// The first two lines import the express and cors packages,
+// which are used to create the server and enable Cross-Origin Resource Sharing (CORS).
+// The third line imports a JSON file containing data about avocado sales.
 import express from "express";
 import cors from "cors";
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+import topMusicData from "./data/top-music.json";
+// ///////////////// APP //////////////////////////////// //
+// The first line sets the server port number to either the PORT environment variable or 8080 if PORT is not defined.
 const port = process.env.PORT || 8080;
 const app = express();
-
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
-
-// Start defining your routes here
+// This code defines a basic route at the root URL ("/") that responds with the text "Hello Technigo!"
+// when accessed with an HTTP GET request.
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send({
+    Hello: "Welcome to this top-music API!",
+    Routes: [
+      { "/": "Startpage / Api Info" },
+      { "/music": "all tracks-data" },
+      { "/music/:id": "singel track" },
+      { "/music/danceability/:danceability": "sort the tracks after the danceability score you want to see" },
+      { "/music/popularityover/:popularity": "sort the tracks after the minimum popularity score you want to see" }
+    ]
+  });
 });
-
+// get all music
+app.get("/music", (req, response) => {
+  const music = topMusicData;
+  if (music) {
+    response.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        topMusicData: music
+      }
+    });
+  } else {
+    response.status(404).json({
+      success: false,
+      message: "Page not found",
+      body: {}
+    });
+  }
+});
+// get single tracks
+app.get("/music/:id", (req, response) => {
+  const { id } = req.params;
+  const singleTrack = topMusicData.find((track) => {
+    return track.id === Number(id);
+  })
+  if ((singleTrack.length !== 0)) {
+    response.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        track: singleTrack
+      }
+    });
+  } else {
+    response.status(404).json({
+      success: false,
+      message: "Id not found",
+      body: {}
+    });
+  }
+});
+// sorted by danceability rating
+app.get("/music/danceability/:danceability", (req, response) => {
+  const { danceability } = req.params;
+  const singleTrack = topMusicData.filter((track) => {
+    return track.danceability === Number(danceability);
+  })
+  if (singleTrack.length !== 0) {
+    response.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        track: singleTrack
+      }
+    });
+  } else {
+    response.status(404).json({
+      success: false,
+      message: "Danceability score not found",
+      body: {}
+    });
+  }
+});
+// sorted by popularite over 90 rating
+app.get("/music/popularity/:popularity", (req, response) => {
+  const { popularity } = req.params;
+  const singleTrack = topMusicData.filter((track) => {
+    return track.popularity >= Number(popularity);
+  })
+  if (singleTrack.length !== 0) {
+    response.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        track: singleTrack
+      }
+    });
+  } else {
+    response.status(404).json({
+      success: false,
+      message: "Popularity score not found",
+      body: {}
+    });
+  }
+});
 // Start the server
+// Finally, this code starts the server and listens for incoming requests on the specified port.
+// When the server is running, it logs a message to the console.
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
