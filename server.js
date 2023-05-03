035046ade22e522199b6b9770a21fb68b5e6084a
Vio Szabo <vio@vioszabo.se>
2023-05-03 15:24:19 +0200

    commit before deploy

--- server.js
+++ server.js
@@ -2,14 +2,6 @@ import express from "express";
 import cors from "cors";
 import avocadoSalesData from "./data/avocado-sales.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -20,31 +12,83 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+// The endpoints are:
+// /: "Hello avocados!" message
+// /avocadosales - all the data (example: http://localhost:8080/avocadosales)
+// /regions - list of unique regions from the sales data (example: http://localhost:8080/regions)
+// /regions/ - list of sales for specific regions (example: http://localhost:8080/regions?region=Albany)
+// /months/:month  - avocado sales data for a specific month (example: http://localhost:8080/months/10)
+//  avocados/:id - avocado sales data for a specific id (example: http://localhost:8080/avocados/5)
+
 app.get("/", (req, res) => {
   res.send("Hello avocados!");
 });
 
-//avocados by region
-//avocados by month
 //total avocados
 app.get("/avocadosales", (req, res) => {
   res.json(avocadoSalesData)
 })
 
+//avocados by region
 app.get("/regions", (req, res) => {
 const regions = req.query.region
-const regionList = avocadoSalesData.map((sale) => sale.region)
-  res.json(regionList)
-})
+const regionList = avocadoSalesData.map((sales) => sales.region)
+if (regions) {
+  const filteredRegions = avocadoSalesData.filter((sales) => sales.region === regions);
+  res.status(200).json({
+    success: true,
+    message: "OK",
+    body: {
+      regions: filteredRegions
+    }
+  });
+} else {
+  res.status(200).json({
+    success: true,
+    message: "OK",
+    body: {
+      regions: regionList,
+    }
+  });
+}
+});
 
-app.get("/month", (req, res) => {
-  const date = req.params.date
-  const month = avocadoSalesData.includes((date) => date.date)
-  res.json (month)
-})
+//avocados by month
+app.get("/months/:month", (req, res) => {
+  const month = req.params.month;
+  const filteredAvocados = avocadoSalesData.filter((sales) => {
+    // includes the month in the middle of the list
+    return sales.date.includes('-' + month + '-');
+  })
+  res.json(filteredAvocados)
+  })
+
+
+//Avocado by id
+app.get("/avocados/:id", (req, res) => {
+  const id = req.params.id
+  const singleAvocado = avocadoSalesData.find((avocado) => {
+    return avocado.id === Number(id);
+  })
+  if (singleAvocado) {
+    res.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        avocado: singleAvocado
+      }
+    });
+  } else {
+    res.status(404).json({
+      success: false,
+      message: "Avocado not found",
+      body: {}
+    });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
