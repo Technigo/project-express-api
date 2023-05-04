1fac5feb64a30b383ba0b04d4835de4cabf0221d
Yu Miao <yu_miao@yahoo.com>
2023-05-04 12:14:50 +0200

    creat three rounte

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Project Express API
 
-Replace this readme with your own information about your project.
+bulid the first API by useing Node.js and Express
 
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+The API has 3 routes:
+
+ROUTE 1: "/music"       to view all top music.
+ROUTE 2: "/music/:id"   to view top music from an input ID.
+ROUTE 3: "/music/artist/:artist"    to view top music by a specific artist when input artist name.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+
--- server.js
+++ server.js
@@ -1,28 +1,103 @@
-import express from "express";
+import express, { request, response } from "express";
 import cors from "cors";
+import musicData from "./data/top-music.json"
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+
+
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
+const listEndpoints = require('express-list-endpoints');
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+app.get("/", (request, response) => {
+  // response.send("Welcome to Top music!!")
+  response.json(listEndpoints(app))
+
+})
+
+// Route1 : creat a new route with collection of all music data
+app.get("/music", (request, response) => {
+  const music = musicData;
+  if (music) {
+    response.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        musicData: music
+      }
+    })
+  } else {
+    response.status(500).json({
+      success: false,
+      message: "music not found",
+      body: {}
+    })
+  }
+
+})
+
+//Route2 : get one item music from music data by input music ID.
+app.get("/music/:id", (request, response) => {
+  const { id } = request.params
+  const singleMusic = musicData.find((music) => {
+    // return music.id == id;
+    // return music.id === Number(id);
+    return music.id.toString() === id;
+
+  });
+  if (singleMusic) {
+    response.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        music: singleMusic
+      }
+    });
+  } else {
+    response.status(404).json({
+      success: false,
+      message: "music not found",
+      body: {}
+    })
+  }
+})
+
+//Route3 : creat a new endpoint with specific artist when input artistname
+app.get("/music/artist/:artist", (request, response) => {
+  const { artist } = request.params;
+  let filteredMusic = musicData;
+
+  if (artist) {
+    filteredMusic  = musicData.filter((singleMusic) => {
+      return singleMusic.artistName.toLowerCase() === artist.toLowerCase()
+    });
+  } 
+  if (filteredMusic) {
+    response.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        musicData: filteredMusic 
+      }
+    })
+  } else {
+    response.status(500).json({
+      success: false,
+      message: "music not found",
+      body: {}
+    })
+  }
+})
+
+
 
 // Start the server
 app.listen(port, () => {
