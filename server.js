878eea2013a3824b1af1ee1d0007580df7c4a993
Ylva Karlsson <121539816+YlvaKarlsson@users.noreply.github.com>
2023-05-04 15:53:24 +0200

    Added endpoints

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 import express from "express";
 import cors from "cors";
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import booksData from "./data/books.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -20,9 +13,33 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+app.get("/books", (req, res) => {
+  res.send(booksData);
+})
+// added error-handlers for eventual Bad Request and 404-page
+// because the isbn is a number and the parameter is a string - I needed to add the toString-function after the books isbn-number 
+// One endpoint to return a single result (one book), based on the isbn-number requested in the url
+app.get("/books/:isbn", (req, res) => {
+  if (typeof req.params.isbn !== "number") {
+    res.status(400).send("ISBN must be a valid number");
+  }
+  let hits = booksData.filter(book => book.isbn.toString() === req.params.isbn)
+  if (hits.length === 0) {res.status(404).send("Sorry, no book with that ISBN-nr exists =(")}
+  res.send(hits)
+})
+// One endpoint to return a single result (one book), based on the isbn13-number requested in the url
+app.get("/books/isbn13/:isbn", (req, res) => {
+  if (typeof req.params.isbn !== "number") {
+    res.status(400).send("ISBN must be a valid number");
+  }
+  let hits = booksData.filter(book => book.isbn13.toString() === req.params.isbn)
+  if (hits.length === 0) {res.status(404).send("Sorry, no book with that ISBN-nr exists =(")}
+  res.send(hits)
+})
+
+app.post("/books", (req, res) => {
+  res.status(201).send();
+})
 
 // Start the server
 app.listen(port, () => {
