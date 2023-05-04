c894212fe2d455f7bbb986a16fcc7a8474af5211
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-05-04 17:25:05 +0200

    Add res.status

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import cors from 'cors';
 import netflixData from './data/netflix-titles.json';
 // import topMusicData from "./data/top-music.json";
 
-console.log(netflixData.length)
+// console.log(netflixData.length)
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -33,34 +33,83 @@ app.get('/', (req, res) => {
 // The code defines an endpoint to return a collection of results
 // Route takes query parameters and returns a collection of filtered data from the netflixData array.
 // http://localhost:8080/shows?type=Movie&country=United%20States
-// http://localhost:8080/shows?type=Movie&director=M. Night Shyamalan
+// http://localhost:8080/shows?type=Movie&director=M.%20Night%20Shyamalan
 app.get('/shows', (req, res) => {
   const query = req.query;
   let filteredData = netflixData;
   Object.keys(query).forEach(key => {
     filteredData = filteredData.filter(item => item[key] === query[key]);
   });
-  res.json(filteredData);
+
+  res.status(200).json({
+    success: true,
+    message: 'OK',
+    body: {
+      netflixData: filteredData
+    }
+  });
 });
 
 // Defines a route that takes a parameter and returns a filtered collection of data.
 // The code defines an endpoint to return a single result
 // http://localhost:8080/year/2020 (or other years up to 2020)
-app.get('/year/:year', (req,res) => {
-  const year = req.params.year
-  const releaseFromYear = netflixData.filter((item) => item.release_year === +year)
-  const formattedReleaseFromYear = JSON.stringify(releaseFromYear, null, 2)
-  res.type('json').send(formattedReleaseFromYear)
-})
+app.get('/year/:year', (req, res) => {
+  const year = req.params.year;
+  const releaseFromYear = netflixData.filter((item) => item.release_year === +year);
+
+  res.status(200).json({
+    success: true,
+    message: 'OK',
+    body: {
+      netflixData: releaseFromYear
+    }
+  });
+
+});
+
 
 // Defines a route that takes a parameter and returns a filtered collection of data.
-// http://localhost:8080/type/Movie or http://localhost:8080/type/TV Show
+// http://localhost:8080/type/Movie or http://localhost:8080/type/TV%20Show
 app.get('/type/:type', (req,res) => {
   const type = req.params.type
-  const releaseFromType = netflixData.filter((item) => item.type === type)
-  const formattedReleaseFromType = JSON.stringify(releaseFromType, null, 2)
-  res.type('json').send(formattedReleaseFromType)
-})
+  const releaseFromType = netflixData.filter((item) => item.type === type) 
+
+    res.status(200).json({
+    success: true,
+    message: 'OK',
+    body: {
+      netflixData: releaseFromType
+    }
+  });
+});
+
+// Filters Netflix data by title and/or director
+// title: 
+// http://localhost:8080/netflix-data?title=dead%20to%20me
+
+// director: 
+// http://localhost:8080/netflix-data?director=M.%20Night%20Shyamalan
+app.get('/netflix-data', (req, res) => {
+  const { title, director } = req.query;
+  let data = netflixData;
+
+  if (title) {
+    data = data.filter(item => item.title.toLowerCase() === title.toLowerCase());
+  }
+
+  if (director) {
+    data = data.filter(item => item.director.toLowerCase() === director.toLowerCase());
+  }
+
+  res.status(200).json({
+    success: true,
+    message: 'OK',
+    body: {
+      netflixData: data
+    }
+  });
+});
+
 
 // Start the server
 app.listen(port, () => {
