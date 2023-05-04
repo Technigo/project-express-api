b2f28e39f5df47e21032e31e04ce9ad2b11f657b
AntoniaGranit <antonia.granit@gmail.com>
2023-05-04 22:13:06 +0300

    add first route

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 import express from "express";
 import cors from "cors";
 import technigoMembers from "./data/technigo-members.json"
+import ITsalaryData from "./data/IT-salary.json"
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+// The following defines the port the app will run on. Defaults to 8080,
+// but can be overridden when starting the server (if needed). Example command to
+// overwrite PORT env variable value: PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 const listEndpoints = require('express-list-endpoints');
@@ -21,69 +15,51 @@ const listEndpoints = require('express-list-endpoints');
 app.use(cors());
 app.use(express.json());
 
+
 // Start defining your routes here ("req" = request, "res" = response)
 app.get("/", (req, res) => {
-  // res.send("Hello Technigo!");
   res.json(listEndpoints(app));
 });
 
 // Get all Technigo members:
-app.get("/members", (req, res) => {
-  const name = req.query.name;
-  const role = req.query.role;
-  let members = technigoMembers;
-  // const { name, role } = request.query;
+app.get("/developers", (req, res) => {
+  let salary = req.query.salary;
+  let developers = ITsalaryData;
 
-  if (role) {
-    members = technigoMembers.filter((singleMember) => {
-      return singleMember.role.toLowerCase() === role.toLowerCase();
+  if (salary) {
+    developers = ITsalaryData.filter((singleDeveloper) => {
+      return singleDeveloper.yearly_salary === parseInt(salary);
     });
   } 
-
-  if (members) {
-    res.status(200).json({
-      success: true,
-      message: "OK",
-      body: {
-        technigoMembers: members
-      }
-    });
-  } else {
-    res.status(500).json({
-      success: false,
-      message: "Something went wrong",
-      body: {}
-    });
-  }
+  res.send(developers);
 });
 
-// :,(
 // Get one specific technigo member:
-app.get("/members/:id", (request, response) => {
-  const { id } = request.params;
-  console.log("id: ", id);
-  const singleMember = technigoMembers.find((member) => {
-    // return member._id == id;
-    return member._id === Number(id);
-    // return member._id.toString() === id;
-    // return member._id === +id; 
-  });
-  if (singleMember) {
-    response.status(200).json({
-      success: true,
-      message: "OK",
-      body: {
-        member: singleMember
-      }
-    });
-  } else {
-    response.status(404).json({
-      success: false,
-      message: "Member not found",
-      body: {}
-    });
-  }
-});
+// app.get("/members/:id", (request, response) => {
+//   const { id } = request.params;
+//   console.log("id: ", id);
+//   const singleMember = technigoMembers.find((member) => {
+//     // return member._id == id;
+//     return member._id === Number(id);
+//     // return member._id.toString() === id;
+//     // return member._id === +id; 
+//   });
+//   if (singleMember) {
+//     response.status(200).json({
+//       success: true,
+//       message: "OK",
+//       body: {
+//         member: singleMember
+//       }
+//     });
+//   } else {
+//     response.status(404).json({
+//       success: false,
+//       message: "Member not found",
+//       body: {}
+//     });
+//   }
+// });
 
 // Start the server
 app.listen(port, () => {
