462409cd1c265cb95d64e88a4924ffa0df31082e
Jonas Jakobson <jonas.jakobson@gmail.com>
2023-05-04 14:53:18 +0200

    added endpoint for pages, rating and authors

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
 import express from "express";
 import cors from "cors";
-import technigomembers from './data/technigomembers.json';
-import bodyParser from "body-parser";
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
 import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+
+// App running on port 8080
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -26,43 +17,89 @@ app.get("/", (req, res) => {
 });
 
 // get the whole booklist
-app.get("/booklist", (req, res) =>
+app.get("/books", (req, res) =>
   res.json(booksData)
 );
 
-//Get single book by Id
-app.get("/booklist/:id", (req, res) => {
+//Sorting function for ascending order
+app.get("/books/authors", (req, res) => {
+  const sorting = (arr, key, direction = 'asc') => {
+    if (direction === 'asc') {
+      return arr.sort((a, b) => (a[key] > b[key]) ? 1 : (b[key] > a[key] ? -1 : 0))
+    } else {
+      return arr.sort((a, b) => (a[key] < b[key]) ? 1 : ((b[key] < a[key]) ? -1 : 0))
+    }
+  };
+
+  const sortedByAuthorAsc = sorting(booksData, 'authors');
+  //Make if-statement to sort by title etc.
+  // const sortedByTitle = sorting(booksData, 'title');
+  res.json(sortedByAuthorAsc);
+});
+
+// Books sorted by rating and top N rated numbers by query (eg. localhost:8080/rating?topN=5 gives top 5)
+app.get("/books/ratings", (req, res) => {
+  const topN = req.query.topN;
+
+  let booksByRating = booksData.sort((a, b) => b.average_rating - a.average_rating);
+  
+  if (topN) {
+    booksByRating = booksByRating.slice(0, topN)
+  };
+
+  res.json(booksByRating);
+});
+
+// Get books by min/max page number by query (Eg. /books/pages?minPages=100&maxPages=500)
+app.get('/books/pages', (req, res) => {
+  const { min, max } = req.query;
+  let countedBooks = booksData;
+
+  if (min && max) {
+    countedBooks = countedBooks.filter((book) => {
+      return book.num_pages >= Number(min) && book.num_pages <= Number(max)
+    }) 
+  } else if (min) {
+    countedBooks = countedBooks.filter((book) => {
+      return book.num_pages >= Number(min)
+    }) 
+  } else if (max) {
+    countedBooks = countedBooks.filter((book) => {
+      return book.num_pages <= Number(max)
+    }) 
+  };
+  //Sorts the filtered list in ascending order
+  const sortedCount = countedBooks.sort((a, b) => a.num_pages - b.num_pages);
+  res.json(sortedCount);
+});
+
+//Get single book by Id, title or name
+app.get("/books/:id", (req, res) => {
   const singleBook = booksData.find((book) => {
     const { id } = req.params;
     return book.bookID === Number(req.params.id);
   });
 
   if (singleBook) {
-    res.status(200).json(singleBook)
+    res.status(200).json({
+      sucess: true,
+      message: 'OK',
+      body: {
+        book: singleBook
+      }
+      });
   } else {
     res.status(500).json({
-      error: 'book not found'
-    })
+      sucess: false,
+      message: 'Book not found',
+      body: {}
+    });
   };
-})
-
-// Books sorted by rating and top N rated numbers by query (eg. localhost:8080/rating?topN=5 gives top 5)
-app.get("/rating", (req, res) => {
-  const topN = req.query.topN;
-
-  let booksByRating = booksData.sort((a, b) => b.average_rating - a.average_rating);
-  
-  if (topN) {
-    booksByRating = booksByRating.slice(0, topN);
-  }
-
-  res.json(booksByRating);
 });
-
 
 /* More todos:
 Add more filters, like: Return books that has a ratings count over 100
-Post, put, patch...
+// Add a return body for the response status(200) and (500)
 
 */
 
@@ -70,3 +107,6 @@ Post, put, patch...
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+
+/// Api for fetching booklist from pretend server
