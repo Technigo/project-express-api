36fb8c35a65d53706dd52f52c18e7b6ccc160db3
Ylva Karlsson <121539816+YlvaKarlsson@users.noreply.github.com>
2023-05-05 11:57:51 +0200

    Added endpoints and query parameters + updated readme

--- README.md
+++ README.md
@@ -1,13 +1,28 @@
 # Project Express API
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+This weeks project was to build an API to meet at least the criterias listed here:
+✓ At least two routes 
+✓ A minimum of one endpoint to return a collection of results (array of elements).
+✓ A minimum of one endpoint to return a single result (single element).
+✓ Your API should be [RESTful]
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+I focused mainly on the API and has not built a frontend to handle the API yet. 
+I had a meeting with my codementor and got guidance along the way on how to set up the API from scratch. It felt fun and I liked this week even though backend feels tricky now this first week. 
+I would try to build a frontend for this API if I had more time :D
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+Routes
+https://express-api-w13-dn5qt3iqpa-ew.a.run.app/books
+https://express-api-w13-dn5qt3iqpa-ew.a.run.app/books/isbn
+https://express-api-w13-dn5qt3iqpa-ew.a.run.app/books/isbn13
+
+Endpoint to a single book:
+https://express-api-w13-dn5qt3iqpa-ew.a.run.app/books/isbn/439785960
+(you can change the number to get another book returned from the API)
+
+URL to query parameters to filter on author or title:
+https://express-api-w13-dn5qt3iqpa-ew.a.run.app/books/authors?author=AuthorName&title=Title
+(you can change the author and title in URL above to filter)
--- server.js
+++ server.js
@@ -11,34 +11,36 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 // Start defining your routes here
 app.get("/books", (req, res) => {
   res.send(booksData);
 })
-// added error-handlers for eventual Bad Request and 404-page
+// added error-handlers for 404-responses
 // because the isbn is a number and the parameter is a string - I needed to add the toString-function after the books isbn-number 
 // One endpoint to return a single result (one book), based on the isbn-number requested in the url
-app.get("/books/:isbn", (req, res) => {
-  if (typeof req.params.isbn !== "number") {
-    res.status(400).send("ISBN must be a valid number");
-  }
+app.get("/books/isbn/:isbn", (req, res) => {
   let hits = booksData.filter(book => book.isbn.toString() === req.params.isbn)
   if (hits.length === 0) {res.status(404).send("Sorry, no book with that ISBN-nr exists =(")}
-  res.send(hits)
+  res.send(hits[0])
 })
+// query parameter - filters on author or title using query parameters
+app.get("/books/authors", (req, res) => {
+  let author = req.query.author
+  let title = req.query.title
+  let hits = booksData
+  if (author !== undefined) {hits = hits.filter(book => book.authors === author)}
+  if (title !== undefined) {hits = hits.filter(book => book.title === title)}
+  console.log(hits)
+  if (hits.length === 0) {res.status(404).send("Sorry, we couldn't find the book you're looking for =(")}
+  res.send(hits)
+  })
 // One endpoint to return a single result (one book), based on the isbn13-number requested in the url
 app.get("/books/isbn13/:isbn", (req, res) => {
-  if (typeof req.params.isbn !== "number") {
-    res.status(400).send("ISBN must be a valid number");
-  }
   let hits = booksData.filter(book => book.isbn13.toString() === req.params.isbn)
   if (hits.length === 0) {res.status(404).send("Sorry, no book with that ISBN-nr exists =(")}
-  res.send(hits)
-})
-
-app.post("/books", (req, res) => {
-  res.status(201).send();
+  res.send(hits[0])
 })
 
 // Start the server
