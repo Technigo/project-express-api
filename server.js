f3be92511298229ed58c09714b252bc989adfef0
Fiona Klacar <fiona.klacar@gmail.com>
2023-05-05 12:23:05 +0200

    re-wrote endpoints with a mutable copy of netflixData

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-import express, { response } from "express";
+// import express, { response } from "express";
 import cors from "cors";
+import { response } from "express";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -10,36 +11,119 @@ import netflixData from "./data/netflix-titles.json";
 // console.log(netflixData.length)
 // import topMusicData from "./data/top-music.json";
 
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  swaggerJsdoc = require("swagger-jsdoc"),
+  swaggerUi = require("swagger-ui-express");
+
+  const app = express();
+
+
+  // const options = {
+  //   definition: {
+  //     openapi: "3.0.0",
+  //     info: {
+  //       title: "LogRocket Express API with Swagger",
+  //       version: "0.1.0",
+  //       description:
+  //         "This is a simple CRUD API application made with Express and documented with Swagger",
+  //       license: {
+  //         name: "MIT",
+  //         url: "https://spdx.org/licenses/MIT.html",
+  //       },
+  //       contact: {
+  //         name: "LogRocket",
+  //         url: "https://logrocket.com",
+  //         email: "info@email.com",
+  //       },
+  //     },
+  //     servers: [
+  //       {
+  //         url: "http://localhost:8080",
+  //       },
+  //     ],
+  //   },
+  //   apis: ["./routes/*.js"],
+  // };
+  
+  // const specs = swaggerJsdoc(options);
+  // app.use(
+  //   "/api-docs",
+  //   swaggerUi.serve,
+  //   swaggerUi.setup(specs)
+  // );
+
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
-const app = express();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+const listEndpoints = require("express-list-endpoints")
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("This is an api for netflix data");
-  res.json(netflixData.endpoints.api)
-
+  // res.send("This is an api for netflix data");
+  res.json(listEndpoints(app))
 });
 
-// all releases by title, to return an array = http://localhost:8080/titles
-app.get('/titles', (req, res) => {
-  res.json(netflixData)
-})
 
-// finding one title by id, to return a single result
+// all releases by title, to return an array = https://fiona-klacar-project-express-api.onrender.com/titles
+app.get('/titles', (req, res) => {
+  const type = req.query.type;
+  const country = req.query.country;
+
+let titles = netflixData
+
+// filter all titles by type of title, to return an array = https://fiona-klacar-project-express-api.onrender.com/titles?type=movie
+if (type) {
+  const typeWithoutSpaces = type.replace(/\s+/g, '');
+  titles = titles.filter(singleNetlixDataPoint =>
+    singleNetlixDataPoint.type.replace(/\s+/g, '').toLowerCase() === typeWithoutSpaces.toLowerCase()
+  );
+}
+
+// filter all titles by type of country, to return an array = https://fiona-klacar-project-express-api.onrender.com/titles?country=france
+
+if (country) {
+  const countryWithoutSpaces = country.replace(/\s+/g, '');
+  titles = titles.filter(singleNetflixDataPoint =>
+    singleNetflixDataPoint.country.replace(/\s+/g, '').toLowerCase() === countryWithoutSpaces.toLowerCase()
+  );
+}
+
+if (titles) {
+  res.status(200).json({
+    success: true,
+    message: "OK",
+    body: {
+      netflixData: titles
+    }
+  });
+}
+
+else {
+res.status(500).json({
+  success: false,
+  message: "Something has gone wrong",
+  body: {}
+})
+};
+});
+
+// filter by both type of title and country: http://localhost:8080/titles?type=tvshow&country=france
+
+// finding one title by id, to return a single result = https://fiona-klacar-project-express-api.onrender.com/titles/81213894
 app.get('/titles/:show_id', (req, res) => {
   const { show_id } = req.params; // extracting from object, need {}
   console.log({ show_id });
   
-  const singleTitle = netflixData.find((item) => {
+ const singleTitle = netflixData.find((item) => {
     return item.show_id === Number(show_id);
   })
+
 if (singleTitle) {
   res.status(200).json ({
     success: true,
@@ -59,38 +143,62 @@ res.json(singleTitle)
 })
 
 
-app.get('/type/:type', (req, res) => {
-  const type = req.params.type.toLowerCase();
-  const releaseYear = req.query.year;
-  const country = req.query.country;
-  console.log({ type })
-  console.log(releaseYear)
-  console.log({ country} )
-
-  // filter by type of title, to return an array = https://localhost:8080/type/movie
-  let typeOfTitle = netflixData.filter((item) => item.type.toLowerCase() === type)
-  
-  // filter by type of title and year, to return an array = http://localhost:8080/type/movie?year=2019
-  if (releaseYear) {
-    typeOfTitle = typeOfTitle.filter((item) => item.release_year === Number(releaseYear))
-  }
-
-  // filter by type of title and country, to return an array = http://localhost:8080/type/tv%20show?country=france
-  if (country) {
-    typeOfTitle = typeOfTitle.filter((item) => item.country.toLowerCase() === country )
-  }
-  
-  res.json(typeOfTitle)
-})
-
-// Tried to create a dummy endpoint
-// app.post('title', (req, res) => {
-//   res.json({ message: 'title created successfully' });
-// })
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
 
-// https://project-express-api-1z7o.onrender.com/
+// ORIGINALLY DID THIS
+// all releases by title, to return an array = https://fiona-klacar-project-express-api.onrender.com/titles
+// app.get('/titles', (req, res) => {
+//   res.json(netflixData)
+// })
+
+// finding one title by id, to return a single result
+// app.get('/titles/:show_id', (req, res) => {
+//   const { show_id } = req.params; // extracting from object, need {}
+//   console.log({ show_id });
+  
+//   const singleTitle = netflixData.find((item) => {
+//     return item.show_id === Number(show_id);
+//   })
+// if (singleTitle) {
+//   res.status(200).json ({
+//     success: true,
+//     message: 'OK',
+//     body: {
+//       title: singleTitle
+//     }
+//   });
+// } else {
+//   res.status(404).json({
+//     success: false,
+//     message: 'Title not found',
+//     body: {}
+//   })
+// }
+// res.json(singleTitle) 
+// })
+
+
+// app.get('/type/:type', (req, res) => {
+//   const type = req.params.type.toLowerCase();
+//   const releaseYear = req.query.year;
+//   const country = req.query.country;
+//   console.log({ type })
+//   console.log(releaseYear)
+//   console.log({ country} )
+
+  // filter by type of title, to return an array = https://fiona-klacar-project-express-api.onrender.com/type/movie
+  // let typeOfTitle = netflixData.filter((item) => item.type.toLowerCase() === type)
+  
+  // filter by type of title and year, to return an array = https://fiona-klacar-project-express-api.onrender.com/type/movie?year=2019
+  // if (releaseYear) {
+  //   typeOfTitle = typeOfTitle.filter((item) => item.release_year === Number(releaseYear))
+  // }
+
+  // filter by type of title and country, to return an array = https://fiona-klacar-project-express-api.onrender.com/type/tv%20show?country=france
+  // if (country) {
+  //   typeOfTitle = typeOfTitle.filter((item) => item.country.toLowerCase() === country )
+  // }
+
+// https://fiona-klacar-project-express-api.onrender.com/
