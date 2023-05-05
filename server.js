b2b283de25909be0e1a2eddb5f9f5263bdd5ed57
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2023-05-05 22:56:30 +0200

    kick off with the project, imported own json and started defining routes

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 import express from "express";
+import bodyParser from 'body-parser';
 import cors from "cors";
+import gender from "./data/gender-inequality.json";
+
+console.log(gender.length)
+// We have 195 countires in the data package
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -9,6 +14,10 @@ import cors from "cors";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
 
+// I imported data from Kaggle. I converted CSV to JSON and copied my json file to the folder data
+// I am using data set about gender inequality
+//  https://www.kaggle.com/datasets/gianinamariapetrascu/gender-inequality-index
+
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -17,12 +26,23 @@ const app = express();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
+app.use(bodyParser.json())
 app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Hello Technigo lol!");
 });
+
+app.get('/inequalitydata', (req, res) => {
+  res.json(gender)
+})
+
+app.get('/Rank:Rank', (req, res) => {
+  const Rank = req.params.Rank
+  const inequalitydataRank = data.filter((item) => item.Rank === +Rank)
+  res.json(inequalitydataRank)
+})
 
 // Start the server
 app.listen(port, () => {
