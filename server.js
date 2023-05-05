0b7b0f3ecb6def9b3135c91044ddc2d639986e87
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-05-05 12:58:18 +0200

    cleaned up code

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
-// Access locally our server and recieve req cross origin (cors)
 app.use(cors());
 app.use(express.json());
 
@@ -18,8 +17,8 @@ app.get("/", (req, res) => {
       {
         "/books": "Display all books",
         "/books/reversed": "Reversed order of all books",
-        "/books/:id": "Search by numbers through all listed books",
-        "/books/title/:title": "Search for a title",
+        "/books/:id": "Search by id numbers (1-1461).",
+        "/books/title/:title": "Search for a title that includes a word in title you want to see",
       },
     ],
   };
@@ -52,73 +51,63 @@ app.get("/books", (req, res) => {
 //Reverse order of books
   app.get("/books/reversed", (req, res) => {
     const booksDataReversed = booksData.reverse()
+    res.status(200).json({booksDataReversed})
+  })
 
-    if (booksDataReversed) {
-    res.status(200).json({
-      success: true,
-      message: "OK",
-      body: {
-      content: "Reversed order of books",
-      booksDataReversed
-    }
-  });
-  } else {
-   res.status(404).json({
-    success: false, 
-    message: "Something went wrong",
-    body: {}
-  });
-}
-});
 
 // if the user searches for title 
+
 app.get("/books/title/:title", (req, res) => {
   const title = req.params.title;
 
-  let byTitle = booksData;
+   let byTitle = booksData;
 
   if (title) {
     byTitle = byTitle
-.filter((data) => data.title.toLowerCase().includes(title.toLowerCase()));
-  };
+ .filter((data) => data.title.toLowerCase().includes(title.toLowerCase()));
+   };
   if (byTitle.length === 0) {
     res.status(200).json({
       message: "There's no books here by that name",
-      success: true,
-      body: {}
-    });
-  } else {
+       success: true,
+       body: {}
+     });
+   } else {
     res.status(200).json({
       data: byTitle,
-      success: true,
-    });
+       success: true,
+      body: {
+        booksData
+       }
+     });
   };
-  });
+   });
 
 //Returning single book based on ID number if it exists otherwise 404-not found.
-  app.get("/books/:id", (req, res) => {
-    const { id } = req.params;
-  const booksID = booksData.find((item) => {
-    return item.bookID === Number(id)}
-  );
+
+app.get("/books/:id", (req, res) => {
+  const { id } = req.params;
+ const booksID = booksData.find((item) => {
+   return item.bookID === Number(id)}
+ );
  
-    if (booksID) {
-      res.status(200).json({
-        success: true,
-        message: "ok",
-        body: {
-          content: `Book with ID ${id}`,
-          book: booksID
-        }
-      });
-    } else {
-      res.status(404).json({
-        success: false,
-        message: "Sorry, book not found",
+  if (booksID) {
+    res.status(200).json({
+       success: true,
+       message: "ok",
+       body: {
+        content: `Book with ID ${id}`,
+         book: booksID
+       }
+     });
+   } else {
+     res.status(404).json({
+       success: false,
+       message: "Sorry, book not found",
         body: {}
-      });
-    } 
-  })
+     });
+  } 
+ })
 
 // Start the server/application. It needs to listen to a port.
 app.listen(port, () => {
