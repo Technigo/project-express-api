cb08ef58e5003dda2c8826203dfbeebf2e04a077
Andrea <andrea@Andreas-Air.lan>
2023-05-06 22:58:11 +0200

    installed express-list-endpoints

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ import fullMoonData from "./data/csvjson.json";
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
-
+const listEndpoints = require('express-list-endpoints')
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
-//app.use(bodyParser.json())
+
 
 // Start defining your routes here
 
