de8c05611acde689efeef405453d8c7defe426de
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2023-05-06 20:25:09 +0200

    added two more routes, one returns one object and another an array with the chosen value

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-import express from "express";
+import express, { response } from "express";
 import bodyParser from 'body-parser';
 import cors from "cors";
-import gender from "./data/gender-inequality.json";
+import genderData from "./data/gender-inequality.json";
+import listEndpoints from "express-list-endpoints";
 
-console.log(gender.length)
+console.log(genderData.length)
 // We have 195 countires in the data package
 
 // If you're using one of our datasets, uncomment the appropriate import below
@@ -23,6 +24,7 @@ console.log(gender.length)
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
+// const listEndpoints = require('express-list-endpoints');
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
@@ -30,19 +32,67 @@ app.use(bodyParser.json())
 app.use(express.json());
 
 // Start defining your routes here
+
 app.get("/", (req, res) => {
-  res.send("Hello Technigo lol!");
+//res.send("This will give you information about gender inequality in all countires!");
+res.json(listEndpoints(app));
 });
 
-app.get('/inequalitydata', (req, res) => {
-  res.json(gender)
+// get all the countries and the data 
+app.get('/data', (req, response) => {
+  // res.send(genderData);
+  const data = genderData;
+  if (data) {
+    response.status(200).json({
+      success: true, 
+      message: "OK",
+      body: {
+        genderData: data
+      }
+    });
+  } else {
+    response.status(500).json({
+      success: false, 
+      message: "Something went wrong",
+      body: {}
+    });
+  }
+});
+
+// get all the data based on the Rank (not all numbers available)
+app.get('/data/:rank', (req, response) => {
+  const singleData = genderData.find((data) => {
+    const { rank } = req.params; 
+    return data.Rank === Number(rank);
+    // return data.Rank.toString() === id;
+    // return data.Rank === +rank 
+    // these are the three options available, the last one is the least professional
+  });
+  if (singleData) {
+    response.status(200).json({
+      success: true, 
+      message: "OK",
+      body: {
+        genderData: singleData
+      }
+    });
+  } else {
+    response.status(404).json({
+      success: false, 
+      message: "Country not found",
+      body: {}
+    });
+  }
+});
+
+// get the country based on the human development 
+app.get('/development/:development', (req, res) => {
+  const development = req.params.development
+  const countriesBasedonLevel = genderData.filter((item) => 
+    item.Human_development ===  development)
+    res.json(countriesBasedonLevel)
 })
 
-app.get('/Rank:Rank', (req, res) => {
-  const Rank = req.params.Rank
-  const inequalitydataRank = data.filter((item) => item.Rank === +Rank)
-  res.json(inequalitydataRank)
-})
 
 // Start the server
 app.listen(port, () => {
