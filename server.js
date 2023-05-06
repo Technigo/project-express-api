7783f03ca08da73717c000ccb66ae019d811a5f0
Sammy Olsson <sammy.olsson@gmail.com>
2023-05-06 22:05:45 +0200

    Backend done for now

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 import express from "express";
 import cors from "cors";
-import data from "./data/best-selling-video-games.json"
 
-
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+// IMPORTING THE NETFLIX JSON DATA
+import netflixData from "./data/netflix-titles.json";
+import listEndpoints from "express-list-endpoints"
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -24,30 +17,73 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send({
+    Message: "Here you can browse movies and tv-shows from Netflix. Below you find the endpoints you can use 🍿 🎬 🥤",
+    Routes: [
+      {
+        "/directory": "= all movies and tv-shows",
+        "/directory/title/:title": "= a specific movie or tv-show, write the title of the movie/tv-show instead of :title",
+        "/directory/year/:year": "= get the titles from a specific year",
+        "/random": "= need inspiration for a new movie or tv-show to watch? Will give you a random title"
+
+      }
+    ],
+  })
+});
+// LIST ENDPOINTS FOR THE ROUTES 
+app.get("/endpoints", (req, res) => {
+  res.send(listEndpoints(app))
 });
 
-app.get('/games', (req, res) => {
-  res.json(data)
+// ALL MOVIES
+app.get("/directory", (req,res) => {
+  res.status(200).json(netflixData)
 });
 
-app.get('/games/title', (req, res) => {
-  res.json(data)
+// SPECIFIC MOVIE
+app.get("/directory/title/:title", (req, res) => {
+  const { title } = req.params;
+  const directoryTitle = netflixData.find(
+    (item) => item.title.toLowerCase() === title.toLowerCase()
+  );
+
+  if (!directoryTitle) {
+    res.status(404).json({
+      data: "ERROR: Sorry, can not find any movie or tv-show with that title 😭",
+      success: false,
+    });
+  } else {
+    res.status(200).json({
+      data: directoryTitle,
+      success: true,
+    });
+  }
 });
 
-// app.get('/year/:year', (req, res) => {
-//   const year = req.params.year
-//   const showWon = req.query.won
-//   let nominationsFromYear = data.filter((item) => item.year_award === +year)
+// YEAR
+app.get("/directory/year/:year", (req, res) => {
+  const year = req.params.year
+  const titlesFromYear = netflixData.filter((data) => data.release_year === +year)
+  res.json(titlesFromYear)
+});
 
-//   if (showWon) {
-//     nominationsFromYear = nominationsFromYear.filter((item) => item.win)
-//   }
+// RANDOM
+app.get("/random", (req,res) => {
+  const randomMovie = netflixData[Math.floor(Math.random()* netflixData.length)]
+  if(!randomMovie) {
+    res.status(404).json({
+      data: "ERROR: Sorry, can not find any random movie or tv-show 😭",
+      success: false, 
+    });
+  } else {
+    res.status(200).json({
+      data: randomMovie,
+      success: true,
+    });
+  }
+});
 
-//   res.json(nominationsFromYear)
-// })
-
-// Start the server
+// START THE SERVER
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
