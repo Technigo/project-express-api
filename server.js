37f4aeeaf014c7a2a99fccdfb0195f335988ea5e
Amanda <amanda.sl747@gmail.com>
2023-05-08 09:43:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
-app.get("/quotes", (req, res) => {
+app.get("/", (req, res) => {
   res.json(gotQuotesData);
 });
 
