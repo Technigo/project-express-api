4c40c8847c1daa9dded9fe6783a15edc7da67fe4
cpz-manifold <carol.pinzon.ardila@gmail.com>
2023-05-10 14:49:36 +0200

    project

--- server.js
+++ server.js
@@ -1,30 +1,115 @@
 import express from "express";
 import cors from "cors";
+import booksData from "./data/books.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
+const listEndpoints = require("express-list-endpoints");
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+app.get("/", (request, response) => {
+  response.send("Hello! please type");
+  response.json(listEndpoints(app));
 });
 
-// Start the server
+// get all books with optional average_rating filter
+// http://localhost:8080/books?minRating=4.5
+
+app.get("/books", (request, response) => {
+  const minRating = parseFloat(request.query.minRating) || 0;
+  const filteredBooks = booksData.filter((b) => b.average_rating >= minRating);
+
+  response.status(200).json({
+    success: true,
+    message: "OK",
+    body: {
+      books: filteredBooks
+    }
+  });
+});
+
+
+
+// books by author
+app.get("/books/author/:author", (request, response) => {
+  const searchResults = booksData.filter((b) => b.authors.toLowerCase().includes(request.params.author.toLowerCase()));
+
+  if (searchResults.length > 0) {
+    response.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        searchResults
+      }
+    });
+  } else {
+    response.status(404).json({
+      success: false,
+      message: "No books found matching the given author",
+      body: {}
+    });
+  }
+});
+
+
+// books by id
+
+app.get("/books/:id", (request, response) => {
+  const book = booksData.find((b) => b.bookID === Number(request.params.id));
+
+  if (book) {
+    response.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        book
+      }
+    });
+  } else {
+    response.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      description: "Use route /books for available books",
+      body: {}
+    });
+  }
+});
+
+
+// get the top 10 suggested books
+app.get("/suggest-books", (request, response) => {
+  if (booksData.length > 0) {
+    const sortedBooks = booksData.sort((a, b) => b.average_rating - a.average_rating);
+    const topTenBooks = sortedBooks.slice(0, 10);
+
+    response.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        topTenBooks
+      }
+    });
+  } else {
+    response.status(404).json({
+      success: false,
+      message: "No books available",
+      body: {}
+    });
+  }
+});
+
+// Dummy endpoint to search books by genre
+app.get("/books/genre/:genre", (request, response) => {
+  response.status(501).json({
+    success: false,
+    message: "Not Implemented",
+    description: "This endpoint is not yet implemented",
+    body: {}
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
