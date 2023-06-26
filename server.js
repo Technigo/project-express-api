28a3f5cbc43e855667b517e199ed44ec5c8f2fb0
Emma Engvall <emma.engvall@ica.se>
2023-06-26 21:52:18 +0200

    created routes for booksData

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 import express from "express";
 import cors from "cors";
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import booksData from "./data/books.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
+const listEndpoints = require('express-list-endpoints');
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
@@ -21,7 +15,50 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(listEndpoints(app));
+});
+
+// Get books by author
+app.get("/books/authors/:author", (req, res) => {
+  const author = req.params.author;
+  let booksFromAuthor = booksData.filter((items) => items.authors === author);
+  
+  if (booksFromAuthor) {
+    res.status(200).json({
+      success: true,
+      message: "Found books from author",
+      body: {
+        books: booksFromAuthor
+      }
+    })
+  } else {
+    res.status(404).json({
+      success: false,
+      message: "Author/books could not be found",
+      body: {}
+    })
+  }
+});
+
+// Get one book
+app.get("/books/:bookId", (req, res) => {
+  const bookId = req.params.bookId;
+  let bookFromId = booksData.filter((item) => item.bookID === +bookId);
+
+  if (bookFromId) {
+    res.status(200).json({
+      success: true,
+      message: "Found book",
+      body: {
+        book: bookFromId[0] },
+    })
+  } else {
+    res.status(404).json({
+      success: false,
+      message: "Book not found.",
+      body: {}
+    })
+  }
 });
 
 // Start the server
