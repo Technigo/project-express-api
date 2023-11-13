6e94ca63255ec091931a813fd4739c2adc9a1e08
Isa Robertini <robertiniisa@gmail.com>
2023-11-13 14:42:36 +0100

    tryout

--- server.js
+++ server.js
@@ -1,30 +1,116 @@
+// server.js
+
 import express from "express";
 import cors from "cors";
+import data from "./data/golden-globes.json";
+import routePaths from "./routeInfo";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+// Root endpoint
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(routePaths);
+});
+
+// Endpoint for manual route information
+app.get("/routeInfo", (req, res) => {
+  res.json(routePaths);
+});
+
+// Nominee endpoint
+app.get("/nominee/:nominee", (req, res) => {
+  const nominee = req.params.nominee;
+  const nominationsByNominee = data.filter((item) => item.nominee === nominee);
+
+  res.json(nominationsByNominee);
+});
+
+// movie endpoint
+app.get("/movie/:movie", (req, res) => {
+  const movie = req.params.movie;
+  const nominationsBymovie = data.filter((item) => item.film === movie);
+
+  res.json(nominationsBymovie);
+});
+
+// release year endpoint
+app.get("/releaseYear/:releaseYear", (req, res) => {
+  const releaseYear = req.params.releaseYear;
+  const nominationsByreleaseYear = data.filter((item) => item.year_film === +releaseYear);
+
+  res.json(nominationsByreleaseYear);
+});
+
+// Award Year endpoint
+app.get("/awardYear/:awardYear", (req, res) => {
+  const year = req.params.awardYear;
+  const showWon = req.query.won;
+  const showLost = req.query.lost;
+  let nominationsFromYear = data.filter((item) => item.year_award === +year);
+
+  if (showWon) {
+    nominationsFromYear = nominationsFromYear.filter((item) => item.win);
+  } else if (showLost) {
+    nominationsFromYear = nominationsFromYear.filter((item) => !item.win);
+  }
+
+  res.json(nominationsFromYear);
+});
+
+
+// Nominations endpoint with filters
+app.get("/nominations", (req, res) => {
+  const { year, category, winner } = req.query;
+  let filteredNominations = [...data];
+
+  if (year) {
+    filteredNominations = filteredNominations.filter((item) => item.year_award === +year);
+  }
+
+  if (category) {
+    filteredNominations = filteredNominations.filter((item) => item.category === category);
+  }
+
+  if (winner !== undefined) {
+    const isWinner = winner.toLowerCase() === 'true';
+    filteredNominations = filteredNominations.filter((item) => item.win === isWinner);
+  }
+
+  res.json(filteredNominations);
+});
+
+
+
+// Single nomination by ID endpoint, not found
+app.get("/nomination/:id", (req, res) => {
+  const nominationId = req.params.id;
+  const nomination = data.find((item) => item.id === +nominationId);
+
+  if (!nomination) {
+    return res.status(404).json({ error: "Nomination not found" });
+  }
+
+  res.json(nomination);
 });
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
+});
+
+// Dummy endpoints
+app.post("/nominations", (req, res) => {
+  res.status(501).json({ error: "I'm too dummy to be implemented yet" });
+});
+
+app.put("/nomination/:id", (req, res) => {
+  res.status(501).json({ error: "I'm too dummy to be implemented yet" });
+});
+
+app.delete("/nomination/:id", (req, res) => {
+  res.status(501).json({ error: "I'm too dummy to be implemented yet)" });
 });
