e2212ad75109244632041a5ded94f2bdbf7fe2ee
jennylarsen <jennymarikalarsen@gmail.com>
2023-11-14 11:17:52 +0100

    added a few endpoints and addad the default list of endpoints

--- server.js
+++ server.js
@@ -1,30 +1,77 @@
 import express from "express";
 import cors from "cors";
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import netflixData from "./data/netflix-titles.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
-const app = express();
+//const app = express();
 
-// Add middlewares to enable cors and json body parsing
+// Import a module for listing all endpoints in the app
+const listEndpoints = require("express-list-endpoints")
+const app = require('express')();
+
+// Add middlewares to enable cors and json body parsing (cors= API kan berätta var requests kmr ifrån, express läser requesten)
 app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
+
+// Default route to list all available endpoints in the app
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(listEndpoints(app));
 });
 
-// Start the server
+// Endpoint for all the data from netflixData
+app.get('/titles', (req, res) => {
+  res.json(netflixData)
+  console.log(`Showing netliixdata`)
+})
+
+// Endpoint to fetch a specific title by its show_id
+app.get('/titles/id/:id', (req, res) => {
+  const id = req.params.id;
+  const movieId = netflixData.find((netflixTitle) => netflixTitle.show_id === +id);
+
+    // Respond with the details of the specific title
+  res.json(movieId);
+});
+
+// Endpoint to fetch titles in a specific category
+app.get('/titles/categories/:category', (req, res) => {
+  const requestedCategory = req.params.category.toLowerCase();
+  const moviesInCategory = netflixData.filter(movie => movie.listed_in.toLowerCase().includes(requestedCategory));
+  const movieTitles = moviesInCategory.map(movie => movie.title);
+
+    // Respond with the titles in the specified category
+  res.json({ moviesInCategory: movieTitles });
+});
+
+// Endpoint to fetch titles of a specific type (movie or TV show)
+app.get('/titles/types/:type', (req, res) => {
+  const requestedType = req.params.type.toLowerCase();
+  const movieOrTvshow = netflixData.filter(typeOfMedia => typeOfMedia.type.toLowerCase().includes(requestedType));
+  const mediaTitlesType = movieOrTvshow.map(typeOfMedia => typeOfMedia.title);
+
+    // Respond with the titles of the specified type
+  res.json({ movieOrTvshow: mediaTitlesType });
+})
+
+// Endpoint to fetch titles from a specific country
+app.get('/titles/countries/:country', (req, res) => {
+  const requestedCountry = req.params.country.toLowerCase();
+  const mediaFromCountry = netflixData.filter(mediaCountry => mediaCountry.country.toLowerCase().includes(requestedCountry));
+  const mediaTitlesCountry = mediaFromCountry.map(mediaCountry => mediaCountry.title);
+
+    // Respond with the titles from the specified country
+  res.json({ mediaFromCountry: mediaTitlesCountry })
+})
+
+
+// Start the server and log a message when it's running
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+
