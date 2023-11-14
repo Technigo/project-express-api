73d0f8ebb86c036e37098a4d0162c4b76d2fd6fd
Klaudia Wróblewska <kwroblewska0410@gmail.com>
2023-11-14 05:58:17 +0100

    TEST

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
+import avocadoSalesData from './data/avocado-sales.json';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -20,10 +10,20 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+app.get('/', (req, res) => {
+  res.send('Hello Technigo!');
 });
 
+app.get('/sales', (req, res) => {
+  res.json(avocadoSalesData);
+});
+
+app.get('/regions/:region', (req, res) => {
+  const region = req.params.region.toLowerCase();
+  const avocadoRegions = avocadoSalesData.filter((id) => id.region.toLowerCase() === region);
+  console.log('message ', avocadoRegions);
+  res.json(avocadoRegions);
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
