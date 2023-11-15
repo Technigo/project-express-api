d83fc9c2344f7ee93f1c48aa1b9fc99c0f0c0a4e
veronica astrom <v.aastrom@gmail.com>
2023-11-15 17:48:50 +0100

    moved all explanations to a separate file. I'll add short comments to server.js

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
 import express from "express";
 import cors from "cors";
 
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
- import boardGameData from "./data/boardGameData.json";
-
-/* 
-This part of the code is setting up a variable port that will be used to specify the port on which the server will listen. It uses the process.env.PORT environment variable, which is a way to allow configuration from the outside. If process.env.PORT is not defined (i.e., it's falsy, which includes being undefined), it defaults to using port 8080.
-
-If I want to run the server on a different port, I can override the value by setting the PORT environment variable when starting the server like this:
-PORT=9000 npm start
-*/
+import boardGameData from "./data/boardGameData.json";
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -24,23 +12,84 @@ app.use(cors());
 app.use(express.json());
 
 // Define a route for the root path ('/') to display a welcome message.
-/* This route is handling HTTP GET requests to the root path ('/'). When a user accesses the main URL of your application (e.g., http://localhost:8080/), this route will respond with the message "Welcome to the Board Games API!". */
 app.get("/", (req, res) => {
-  res.send("Welcome to the Board Games API!");
+  res.send("Welcome to the Top 100 Board Games API!");
 });
 
 // Define a route to get all board games.
 app.get("/games", (req, res) => {
-  // Respond with the JSON data containing information about all board games.
-  res.json(boardGameData);
+
+
+    const { year, gametype, sortBy, name, page = 1, pageSize = 10, category } = req.query;
+  // Filter by year:
+// /games?year=2017, for example.
+
+  if (year) {
+    const filteredGamesYear = boardGameData.filter((game) => game.Year === +year);
+
+    res.json(filteredGamesYear);
+  }
+
+  // Filter by type of game:
+  // /games?gametype=strategy 
+
+  if (gametype) {
+    const filteredGamesType = boardGameData.filter((game) => game.Type.toLowerCase() === gametype.toLowerCase());
+
+    res.json(filteredGamesType);
+  }
+
+  // Sort by rating:
+    // /games?sortBy=rating, for example.
+
+  if (sortBy === "rating") {
+
+    // The result is a new array ('sortedGames') with games sorted by descending rating.
+    const sortedGames = boardGameData.sort((a, b) => b.Rating - a.Rating);
+
+    res.json(sortedGames);
+  }
+
+
+  // Search by name:
+     // /games?name=Gloomhaven
+
+  if (name) {
+
+    const matchingGames = boardGameData.filter((game) =>
+      game.Name.toLowerCase().includes(name.toLowerCase())
+    );
+
+    return res.json(matchingGames);
+  }
+
+
+  // Implement pagination:
+/*
+  Examples: 
+    /games?page=1&pageSize=all
+    /games?page=1 (defaults to pageSize = 10, the first 10 games)
+    /games (also defaults to pageSize = 10, the first 10 games)
+    /games?page=2&pageSize=5 get the second page with 5 games per page.
+  */
+  const isAllGamesRequested = pageSize === "all" || pageSize > boardGameData.length;
+
+  const start = (page - 1) * pageSize;
+ 
+  const end = isAllGamesRequested ? boardGameData.length : start + parseInt(pageSize);
+
+  const paginatedGames = boardGameData.slice(start, end);
+
+  res.json(paginatedGames);
+
+
 });
 
 // Define a route to get a specific board game based on its rank.
 app.get("/games/:rank", (req, res) => {
-  // Extract the game rank from the URL parameter.
+
   const gameRank = parseInt(req.params.rank);
-  
-  // Find the board game in the data with the matching rank.
+
   const game = boardGameData.find((item) => item.Rank === gameRank);
 
   // If the game is not found, respond with a 404 error.
