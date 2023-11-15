422d233e4e05715495254f66cfa0a3e2c5cd56ca
jforjanice <jjanice.cheng@gmail.com>
2023-11-15 21:05:25 +0100

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-
+import listEndpoints from "express-list-endpoints";
+import netflixData from "./data/netflix-titles.json";
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
@@ -15,13 +16,51 @@ import cors from "cors";
 const port = process.env.PORT || 8080;
 const app = express();
 
+console.log(netflixData.length);
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  const endpoints = listEndpoints(app);
+  res.json({ endpoints });
+});
+
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.get("/allTitles", (req, res) => {
+  res.json(netflixData);
+});
+
+//Filter
+// app.get("/movies", (req, res)) => {
+//   const typeMovie = req.query.type;
+//   let movies = netflixData.filter((item)=>item.type === typeMovie)
+// }
+
+//Filter the movies / tv show from certain year with params, then further filter the data with query
+app.get("/year/:releaseYear", (req, res) => {
+  const years = req.params.releaseYear;
+  const countries = req.query.country;
+
+  let releaseYear = netflixData.filter((item) => item.release_year === +years);
+  // Filter the movies / tv show from certain year & certain country
+  if (countries) {
+    // Filter the data base on both year and country
+    releaseYear = releaseYear.filter((item) => {
+      // Split the item's countries string into an array
+      const itemCountries = item.country
+        .split(",")
+        .map((country) => country.trim());
+      // Check if any of the requested countries is included in the movie/tv show's countries
+      // The URL should look like this /year/:releaseYear?country=YourCountry
+      return itemCountries.includes(countries);
+    });
+  }
+  res.json(releaseYear);
 });
 
 // Start the server
