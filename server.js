ee0de216c1c6c34fb32413bf30824da07f34486f
Susanne Ekenheim <susanne.ekenheim@gmail.com>
2023-11-15 15:10:30 +0100

    Requirements done

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 import express from "express";
 import cors from "cors";
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import booksData from "./data/books.json";
+import listEndpoints from "express-list-endpoints";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -19,9 +13,45 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+// All endpoints
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  const endpoints = listEndpoints(app);
+  res.json(endpoints);
+});
+
+// Get all books
+app.get("/books", (req, res) => {
+  res.json(booksData);
+});
+
+// Get a book by id
+app.get("/books/:id", (req, res) => {
+  const id = req.params.id;
+  // const {id} = req.params; // same as above, saved for future reference
+  const book = booksData.find((item) => item.bookID === +id);
+
+  if (book) {
+    // if book is found
+    res.json(book); // return the book
+  } else {
+    res.status(404).json({ error: "Book not found" }); // else return error message
+  }
+});
+
+// Get all books by author
+app.get("/books/authors/:author", (req, res) => {
+  const author = req.params.author;
+  // const {author} = req.params; // same as above, saved for future reference
+
+  // Filter the array of books by author
+  const booksByAuthor = booksData.filter((item) => item.authors === author);
+
+  if (booksByAuthor.length > 0) {
+    // if the array is not empty
+    res.json(booksByAuthor); // return the array
+  } else {
+    res.status(404).json({ error: "Author not found" }); // else return error message
+  }
 });
 
 // Start the server
