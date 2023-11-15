d5ddd506006ac9885a34b2567f88146ae10572be
jennylarsen <jennymarikalarsen@gmail.com>
2023-11-15 15:00:42 +0100

    added documentation for the endpoints, added error messages and updated readme.

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 # Project Express API
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+In this project, an Express.js server is created to serve information about Netflix titles based on various endpoints. The endpoints are RESTful with error responses. Documentation is also provided for the available endpoints. 
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+To be able to solve this first backend project I watched the learning material, read about how to structure the endpoints and talked with a dear friend (Chat gpt). 
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
--- server.js
+++ server.js
@@ -1,51 +1,158 @@
-import express from "express";
-import cors from "cors";
-import netflixData from "./data/netflix-titles.json";
+import express from 'express';
+import cors from 'cors';
+import netflixData from './data/netflix-titles.json';
+import MarkdownIt from 'markdown-it';
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+// Defines the port the app will run on. Defaults to 8080.
 const port = process.env.PORT || 8080;
-//const app = express();
 
 // Import a module for listing all endpoints in the app
-const listEndpoints = require("express-list-endpoints")
 const app = require('express')();
+const md = new MarkdownIt();
 
-// Add middlewares to enable cors and json body parsing (cors= API kan berätta var requests kmr ifrån, express läser requesten)
+// Add middlewares to enable cors and json body parsing 
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
 
 // Default route to list all available endpoints in the app
-app.get("/", (req, res) => {
-  res.send(listEndpoints(app));
+app.get('/', (req, res) => {
+    // MarkdownIt is a library for converting Markdown to HTML.
+    // We use it here to render our API documentation in a more readable format.
+  const documentation = `
+  ## Endpoints
+  
+  ### 1. Get All Endpoints
+  
+  #### Request
+  
+  - **Method:** GET
+  - **Path:** /
+  
+  #### Response
+  
+  - **Status Code:** 200 OK
+  - **Body:** A JSON object listing all available endpoints in the application.
+  
+  ### 2. Get All Netflix Titles
+  
+  #### Request
+  
+  - **Method:** GET
+  - **Path:** /titles
+  
+  #### Response
+  
+  - **Status Code:** 200 OK
+  - **Body:** A JSON array containing all Netflix titles.
+  
+  ### 3. Get a Specific Title by ID
+  
+  #### Request
+  
+  - **Method:** GET
+  - **Path:** /titles/:id
+  
+  #### Parameters
+  
+  - id (integer): The unique identifier of the title.
+  
+  #### Response
+  
+  - **Status Code:** 200 OK if the title is found, 404 Not Found otherwise.
+  - **Body:** A JSON object representing the details of the specific title.
+  
+  ### 4. Get Titles in a Specific Category
+  
+  #### Request
+  
+  - **Method:** GET
+  - **Path:** /titles/categories/:category
+  
+  #### Parameters
+  
+  - category (string): The category for which titles are requested.
+  
+  #### Response
+  
+  - **Status Code:** 200 OK if the category is found, 404 Not Found otherwise.
+  - **Body:** A JSON object containing an array of titles in the specified category.
+  
+  ### 5. Get Titles of a Specific Type (Movie or TV Show)
+  
+  #### Request
+  
+  - **Method:** GET
+  - **Path:** /titles/types/:type
+  
+  #### Parameters
+  
+  - type (string): The type of media (movie or TV show) for which titles are requested.
+  
+  #### Response
+  
+  - **Status Code:** 200 OK if the media type is found, 404 Not Found otherwise.
+  - **Body:** A JSON object containing an array of titles of the specified type.
+  
+  ### 6. Get Titles from a Specific Country
+  
+  #### Request
+  
+  - **Method:** GET
+  - **Path:** /titles/countries/:country
+  
+  #### Parameters
+  
+  - country (string): The country for which titles are requested.
+  
+  #### Response
+  
+  - **Status Code:** 200 OK if the country is found, 404 Not Found otherwise.
+  - **Body:** A JSON object containing an array of titles from the specified country.
+  
+  ## Error Responses
+  
+  - **Status Code:** 404 Not Found
+  - **Body:** An error message indicating that the requested resource was not found.
+  
+  - **Status Code:** 500 Internal Server Error
+  - **Body:** An error message indicating that there was an internal server error.
+  `;
+  const htmlDocumentation = md.render(documentation);
+  res.send(htmlDocumentation);
 });
 
 // Endpoint for all the data from netflixData
 app.get('/titles', (req, res) => {
-  res.json(netflixData)
-  console.log(`Showing netliixdata`)
+  res.json(netflixData);
+  console.log(`Showing netliixdata`);
 })
 
 // Endpoint to fetch a specific title by its show_id
-app.get('/titles/id/:id', (req, res) => {
+app.get('/titles/:id', (req, res) => {
   const id = req.params.id;
-  const movieId = netflixData.find((netflixTitle) => netflixTitle.show_id === +id);
+  const titleId = netflixData.find((titleId) => titleId.show_id === +id);
 
-    // Respond with the details of the specific title
-  res.json(movieId);
+    // Respond with the details of the specific title otherwise send 404 status
+  if (titleId) {
+    res.json(titleId);
+  } else {
+    res.status(404).send('No title was found, please try again!');
+  }
 });
 
 // Endpoint to fetch titles in a specific category
 app.get('/titles/categories/:category', (req, res) => {
   const requestedCategory = req.params.category.toLowerCase();
-  const moviesInCategory = netflixData.filter(movie => movie.listed_in.toLowerCase().includes(requestedCategory));
-  const movieTitles = moviesInCategory.map(movie => movie.title);
+  const mediaInCategory = netflixData.filter(media => media.listed_in.toLowerCase().includes(requestedCategory));
+  const mediaTitles = mediaInCategory.map(media => media.title);
 
-    // Respond with the titles in the specified category
-  res.json({ moviesInCategory: movieTitles });
+    // Respond with the titles in the specified category, otherwise send 404 status
+    if (mediaTitles.length === 0) {
+      res.status(404).send('Category not found, please try again!');
+    } else {
+      res.json({ mediaInCategory: mediaTitles });
+    }
 });
 
 // Endpoint to fetch titles of a specific type (movie or TV show)
@@ -54,9 +161,13 @@ app.get('/titles/types/:type', (req, res) => {
   const movieOrTvshow = netflixData.filter(typeOfMedia => typeOfMedia.type.toLowerCase().includes(requestedType));
   const mediaTitlesType = movieOrTvshow.map(typeOfMedia => typeOfMedia.title);
 
-    // Respond with the titles of the specified type
+    // Respond with the titles of the specified type, otherwise send 404 status
+  if (mediaTitlesType.length === 0){
+    res.status(404).send('That is not an available media type, try movie or tv show please.');
+  } else {
   res.json({ movieOrTvshow: mediaTitlesType });
-})
+  }
+});
 
 // Endpoint to fetch titles from a specific country
 app.get('/titles/countries/:country', (req, res) => {
@@ -64,9 +175,13 @@ app.get('/titles/countries/:country', (req, res) => {
   const mediaFromCountry = netflixData.filter(mediaCountry => mediaCountry.country.toLowerCase().includes(requestedCountry));
   const mediaTitlesCountry = mediaFromCountry.map(mediaCountry => mediaCountry.title);
 
-    // Respond with the titles from the specified country
-  res.json({ mediaFromCountry: mediaTitlesCountry })
-})
+    // Respond with the titles from the specified country, otherwise send 404 status
+   if (mediaTitlesCountry.length === 0){
+    res.status(404).send('That is not an available country, please try again!')
+   } else {
+    res.json({ mediaFromCountry: mediaTitlesCountry });
+   }
+});
 
 
 // Start the server and log a message when it's running
