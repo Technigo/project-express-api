684de35aa178abc5a870ee205b3639bebf2e36e3
Anna Robertsson <robertsson_anna@hotmail.com>
2023-11-15 17:26:28 +0100

    changed to shark attack data instead of movies

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 import express from "express";
 import cors from "cors";
 import listEndpoints from "express-list-endpoints";
+import globalSharkAttackData from "./data/global-shark-attacks.json"
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
- import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+// import netflixData from "./data/netflix-titles.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -23,36 +21,80 @@ app.get("/", (req, res) => {
   res.json(endpoints);
 });
 
-// Shows endpoint
-app.get("/shows", (req, res) => {
-  res.json(netflixData)
+// Shark attacks endpoint
+app.get("/shark-attacks", (req, res) => {
+  res.json(globalSharkAttackData)
 })
 
-// Show ID endpoint
-app.get("/shows/:id", (req, res) => {
+// Shows endpoint
+// app.get("/shows", (req, res) => {
+//   res.json(netflixData)
+// })
+
+// Shark attacks "id" (original_order in json) endpoint
+app.get("/shark-attacks/:id", (req, res) => {
   const {id} = req.params
-  const show = netflixData.find(show => show.show_id === +id)
-console.log("showID:", id, typeof id)
-if (show) {
-  res.json(show)
-} else {
-  res.status(404).send("No show was found!")
+  const sharkAttack = globalSharkAttackData.find(sharkAttack => sharkAttack.original_order === +id)
+  console.log("SharkAttack:", id, typeof id)
+
+  if (sharkAttack) {
+    res.json(sharkAttack)
+  } else {
+  res.status(404).send("No shark attack was found!")
 }
 })
 
-// Release year endpoint
-app.get("/releaseyear/:year", (req, res) => {
+// Show ID endpoint
+// app.get("/shows/:id", (req, res) => {
+//   const {id} = req.params
+//   const show = netflixData.find(show => show.show_id === +id)
+// console.log("showID:", id, typeof id)
+// if (show) {
+//   res.json(show)
+// } else {
+//   res.status(404).send("No show was found!")
+// }
+// })
+
+// Year of shark attacks endpoint
+app.get("/shark-attacks/year/:year", (req, res) => {
   const year = req.params.year
-  const showsFromReleaseYear = netflixData.filter((item) => item.release_year === +year)
+  const sharkAttacksFromYear = globalSharkAttackData.filter((sharkAttack) => sharkAttack.Year === +year)
+  console.log("SharkAttack:", year, typeof year)
 
-if (showsFromReleaseYear.length === 0) {
-    return res.status(404).json({ error: "No shows found for this release year" });
-  }
+if (sharkAttacksFromYear.length === 0) {
+  return res.status(404).json({ error: "No shark attacks found from this year" })
+}
 
-  res.json(showsFromReleaseYear)
+res.json(sharkAttacksFromYear)
 })
 
+// Release year endpoint
+// app.get("/releaseyear/:year", (req, res) => {
+//   const year = req.params.year
+//   const showsFromReleaseYear = netflixData.filter((item) => item.release_year === +year)
 
+// if (showsFromReleaseYear.length === 0) {
+//     return res.status(404).json({ error: "No shows found for this release year" });
+//   }
+
+//   res.json(showsFromReleaseYear)
+// })
+
+// Shark attacks species endpoint
+app.get("/shark-attacks/species", (req, res) => {
+const speciesSet = new Set()
+
+globalSharkAttackData.forEach((attack) => {
+    if (attack.Species) {
+      speciesSet.add(attack.Species);
+    }
+  });
+
+  const uniqueSpecies = Array.from(speciesSet); // Convert Set to array
+
+  res.json(uniqueSpecies);
+})
 
 // // Media type endpoint
 // app.get("/mediatype", (req, res) => {
