b2442822404ad9be9ff4f6089b456649f2ca8a57
Carolina Luna <kroluna@gmail.com>
2023-11-15 10:55:41 +0100

    created an API using Express, the API has a couple of endpoint, one for all the books and one for a specific book

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
+import listEndpoints from "express-list-endpoints";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
+import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
@@ -19,9 +20,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+// List of all enpoints
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.json(listEndpoints(app));
+});
+
+//All books
+app.get("/books", (req, res) => {
+  res.json(booksData);
+});
+
+//One book
+app.get("/book/:bookID", (req, res) => {
+  const book = booksData.find((b) => b.bookID === parseInt(req.params.bookID));
+  if (!book) return res.status(404).send("Book not found.");
+  res.json(book);
 });
 
 // Start the server
