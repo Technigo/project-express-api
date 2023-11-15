518022554e0ab73bd817906e3b5ef5d6b93f6c1d
Malin <malinlundhe@hotmail.com>
2023-11-15 21:43:40 +0100

    changed server and reinstalled packages, starting with the server.js

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 import express from "express";
 import cors from "cors";
+import expressListEndpoints from "express-list-endpoints";
+import topMusicData from "./data/top-music.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+console.log(topMusicData);
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
@@ -21,7 +17,23 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  const endpoints = expressListEndpoints(app);
+  res.json(endpoints);
+});
+
+app.get("/top-music", (req, res) => {
+  res.json(topMusicData);
+});
+
+app.get("/top-music/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const result = topMusicData.find((item) => item.id === id);
+
+  if (!result) {
+    res.status(404).json({ message: "Not Found" });
+  } else {
+    res.json(result);
+  }
 });
 
 // Start the server
