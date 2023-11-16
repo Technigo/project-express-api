b9fc7cadc4f7bfa7be696787156c24b9a88e730b
Emmy Jansson <emmybengtsson@live.se>
2023-11-16 09:26:59 +0100

    added a find function

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import goldenGlobesData from "./data/golden-globes.json";
+import booksData from "./data/books.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -17,30 +17,61 @@ app.get("/", (req, res) => {
   res.send("Hello Emmy!");
 });
 
-app.get(`/nominations`, (req, res) => {
-  res.json(goldenGlobesData);
+app.get(`/title`, (req, res) => {
+  res.json(booksData);
 });
 
-app.get(`/year/:year`, (req, res) => {
-  const year = req.params.year;
-  const showWon = req.query.won;
-  let nominationsFromYear = goldenGlobesData.filter(
-    (item) => item.year_award === +year
+app.get(`/rating/:average_rating`, (req, res) => {
+  const requestedRating = req.params.average_rating;
+  const showRating = req.query.showRating === "true";
+  let filteredBooks = booksData.filter(
+    (item) => item.average_rating === +requestedRating
   );
 
-  if (showWon) {
-    nominationsFromYear = nominationsFromYear.filter((item) => item.win);
+  if (showRating) {
+    filteredBooks = filteredBooks.filter(
+      (item) => item.average_rating === +requestedRating
+    );
   }
-  res.json(nominationsFromYear);
+
+  res.json(filteredBooks);
 });
 
-// app.get(`/category`, (req, res) => {
-//   const category = req.params.category;
-//   const movieCategory = goldenGlobesData.filter(
-//     (item) => item.category === category
-//   );
-//   res.json(movieCategory);
-// });
+app.get(`/language/:language_code`, (req, res) => {
+  const language = req.query.language_code;
+  const showLanguage = req.query.showLanguage === "true";
+
+  if (!language) {
+    // Handle the case where the language parameter is missing
+    return res.status(400).json({ error: "Language code is missing" });
+  }
+
+  let filteredBooks = booksData.filter(
+    (item) => item.language_code === language
+  );
+
+  if (showLanguage) {
+  }
+
+  res.json(filteredBooks);
+});
+
+app.get(`/author/:authors`, (req, res) => {
+  const findAuthorLastName = req.params.authors;
+
+  // Use the find() function to search for an author with the specified last name
+  const foundAuthor = booksData.find((item) => {
+    const authorNames = item.authors.split(" ");
+    const authorLastName = authorNames[authorNames.length - 1];
+    return authorLastName === findAuthorLastName;
+  });
+
+  if (foundAuthor) {
+    res.json(foundAuthor);
+  } else {
+    res.status(404).json({ error: "Author not found" });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
