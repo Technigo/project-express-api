06a1aad46195929492b297ad256ca750d17e8b77
Minadarabi <minaadabirii@gmail.com>
2023-11-17 13:03:22 -0800

    last commit

--- server.js
+++ server.js
@@ -1,36 +1,30 @@
 import express from "express";
 import cors from "cors";
-import data from "./data/books.json"
+import data from "./data/books.json";
+import listEndpoints from "express-list-endpoints";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+
+
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+
+
 app.get("/", (req, res) => {
-  const limitData = data.slice(0 , 50)
- 
-  res.json(limitData)
+  const endpoints = listEndpoints(app);
+  res.json({endpoints})
+
   
 });
 app.get("/books" , (req,res) => {
-  const bookTitles = data.map((book) =>{
-   return book.title})
-   res.json(bookTitles)
+  const limitData = data.slice(0 , 50)
+ 
+  res.json(limitData)
 } )
 
 app.get('/books/:bookId', (req, res) => {
