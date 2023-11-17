2fb418133114e056ea4dc794c6331d881efa0e79
So Youn Choi <manghatuhyo@gmail.com>
2023-11-17 14:53:11 +0100

    .

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from "express";
+// import bodyParser from 'body-parser'
 import cors from "cors";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
+import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
+
+console.log(goldenGlobesData.length)
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -23,6 +26,23 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+app.get('/nominations', (req, res) => {
+  res.json(goldenGlobesData)
+})
+
+app.get('/year/:year', (req, res) => {
+  const year = req.params.year
+  const showWon = req.query.won
+  let nominationsFromYear = goldenGlobesData.filter((item) => item.year_award === +year)
+  
+
+  if (showWon) {
+    nominationsFromYear = nominationsFromYear.filter((item) => item.win)
+  }
+
+  res.json(nominationsFromYear)
+})
 
 // Start the server
 app.listen(port, () => {
