53907e51101f71d45083846ade37259d6e2d6fcb
Carolina Luna <kroluna@gmail.com>
2023-11-17 09:59:50 +0100

    m

--- server.js
+++ server.js
@@ -2,13 +2,9 @@ import express from "express";
 import cors from "cors";
 import listEndpoints from "express-list-endpoints";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
 import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+
+require("dotenv").config();
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -16,7 +12,7 @@ import booksData from "./data/books.json";
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+// Add middlewares to enable cors
 app.use(cors());
 app.use(express.json());
 
@@ -27,14 +23,39 @@ app.get("/", (req, res) => {
 
 //All books
 app.get("/books", (req, res) => {
-  res.json(booksData);
+  let filteredBooks = booksData;
+
+  //Filter by title
+  if (req.query.title) {
+    filteredBooks = filteredBooks.filter((book) =>
+      book.title.toLowerCase().includes(req.query.title.toLowerCase())
+    );
+  }
+
+  //Filter by author
+  if (req.query.author) {
+    filteredBooks = filteredBooks.filter((book) =>
+      book.authors.toLowerCase().includes(req.query.author.toLowerCase())
+    );
+  }
+  res.json(filteredBooks);
 });
 
-//One book
+//Show only one book with the selected Id number
 app.get("/book/:bookID", (req, res) => {
   const book = booksData.find((b) => b.bookID === parseInt(req.params.bookID));
   if (!book) return res.status(404).send("Book not found.");
   res.json(book);
+});
+
+app.put("/books/:bookID", (req, res) => {
+  // Future implementation here
+  res.status(501).send("Update book feature not implemented yet.");
+});
+
+app.delete("/books/:bookID", (req, res) => {
+  // Future implementation here
+  res.status(501).send("Delete book feature not implemented yet.");
 });
 
 // Start the server
