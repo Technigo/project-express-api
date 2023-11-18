d2e26221302b5981bbcba1f2196a626982dd9a89
mirelcac <141237662+mirelcac@users.noreply.github.com>
2023-11-18 21:47:22 +0100

    more comments

--- server.js
+++ server.js
@@ -15,10 +15,12 @@ app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
+
 //ROUTE PARAMETERS//
 
 // FIND each book by title
 //This had to be before find by ID in order to work
+//URL: /books/TitleOfBook
 app.get('/books/:title', (req, res) => {
   const requestedTitle = req.params.title.replace(/%20/g, ' ');
   const bookWithTitle = booksData.find((book) => book.title === requestedTitle);
@@ -30,6 +32,7 @@ app.get('/books/:title', (req, res) => {
 });
 
 //FIND each book by ID
+//URL: /books/bookID (number of book ID)
 app.get('/books/:bookID', (req, res) => {
   const bookID = req.params.bookID
   const singleBook = booksData.find((book) => book.bookID === +bookID)
@@ -41,51 +44,43 @@ app.get('/books/:bookID', (req, res) => {
   }
 })
 
+
 //QUERY PARAMETERS//
 //Reminder: Query is shown in url after the "?"
 
-/*
 // FILTER books by author
-app.get('/books/author', (req, res) => {
+//URL: /books?author=AuthorName
+app.get('/books', (req, res) => {
   const authorName = req.query.author;
   if (!authorName) {
-    return res.status(400).send('Author parameter is missing');
+    return res.status(400).send('Author not found');
   }
-
-  const encodedAuthorName = encodeURIComponent(authorName);
-
+  const lowercasedAuthorName = authorName.toLowerCase();//To make sure the author is uncluded even if the name should be spelled with different casing
   const booksByAuthor = booksData.filter((book) =>
-    book.authors.toLowerCase().includes(encodedAuthorName.toLowerCase())
+    book.authors.toLowerCase().includes(lowercasedAuthorName)
   );
-
-  if (booksByAuthor.length > 0) {
-    res.json(booksByAuthor);
-  } else {
-    res.status(404).send(`No books found by author ${authorName}`);
-  }
+  res.json(booksByAuthor);
 });
-/
 
-/*
-// Route for getting top N books by average rating
+/*TRIED THE SLICE METHOD, BUT DID NOT MAKE IT WORK :(
+// SLICE for getting top books by average rating
+// URL: books/top?count=5 (can change number)
 app.get('/books/top', (req, res) => {
   const count = parseInt(req.query.count, 10);
-  if (!count || isNaN(count)) {
-    return res.status(400).send('Count parameter is missing or invalid');
-  }
+  console.log('count:', count);
 
+  // Sort books by average rating in descending order and slice to get the top books
   const topBooks = booksData
-    .sort((a, b) => b.average_rating - a.average_rating)
-    .slice(0, count);
-
-  if (topBooks.length > 0) {
-    res.json(topBooks);
-  } else {
-    res.status(404).send(`No books found for the top ${count} ratings`);
-  }
+    .sort((a, b) => b.average_rating - a.average_rating) //Sort books sorted in descending order
+    .slice(0, count); //The slice method is used to get the top books according to the specified count in url
+  console.log('topBooks:', topBooks);
+  // Return 
+  res.json(topBooks);
 });*/
 
+//ROUTE PARAMETER//
 //Get all books
+//URL: /books
 app.get('/books', (req, res) => {
   res.json(booksData)
 })
