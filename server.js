d91a090949826421bf6664363b7b76675b5cc688
mirelcac <141237662+mirelcac@users.noreply.github.com>
2023-11-18 20:21:46 +0100

    three routes

--- server.js
+++ server.js
@@ -15,24 +15,83 @@ app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
-//All books
-app.get('/books', (req, res) => {
-  res.json(booksData)
-})
+//ROUTE PARAMETERS//
 
-//Each book by iD
-app.get('/books/:bookID', (req, res => {
+// FIND each book by title
+//This had to be before find by ID in order to work
+app.get('/books/:title', (req, res) => {
+  const requestedTitle = req.params.title.replace(/%20/g, ' ');
+  const bookWithTitle = booksData.find((book) => book.title === requestedTitle);
+  if (bookWithTitle) {
+    res.json(bookWithTitle);
+  } else {
+    res.status(404).send("No book was found");
+  }
+});
+
+//FIND each book by ID
+app.get('/books/:bookID', (req, res) => {
   const bookID = req.params.bookID
   const singleBook = booksData.find((book) => book.bookID === +bookID)
   console.log('bookID', bookID, typeof bookID)
-  if (book) {
+  if (singleBook) {
     res.json(singleBook)
   } else {
-    res.statusCode(404).send("No book was found")
+    res.status(404).send("No book was found")
   }
-}))
+})
+
+//QUERY PARAMETERS//
+//Reminder: Query is shown in url after the "?"
+
+/*
+// FILTER books by author
+app.get('/books/author', (req, res) => {
+  const authorName = req.query.author;
+  if (!authorName) {
+    return res.status(400).send('Author parameter is missing');
+  }
+
+  const encodedAuthorName = encodeURIComponent(authorName);
+
+  const booksByAuthor = booksData.filter((book) =>
+    book.authors.toLowerCase().includes(encodedAuthorName.toLowerCase())
+  );
+
+  if (booksByAuthor.length > 0) {
+    res.json(booksByAuthor);
+  } else {
+    res.status(404).send(`No books found by author ${authorName}`);
+  }
+});
+/
+
+/*
+// Route for getting top N books by average rating
+app.get('/books/top', (req, res) => {
+  const count = parseInt(req.query.count, 10);
+  if (!count || isNaN(count)) {
+    return res.status(400).send('Count parameter is missing or invalid');
+  }
+
+  const topBooks = booksData
+    .sort((a, b) => b.average_rating - a.average_rating)
+    .slice(0, count);
+
+  if (topBooks.length > 0) {
+    res.json(topBooks);
+  } else {
+    res.status(404).send(`No books found for the top ${count} ratings`);
+  }
+});*/
+
+//Get all books
+app.get('/books', (req, res) => {
+  res.json(booksData)
+})
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
