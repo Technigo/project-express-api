f9de6208c9ebd4d2aba6e680b610bd5b90556525
mirelcac <141237662+mirelcac@users.noreply.github.com>
2023-11-18 18:15:44 +0100

    two routes

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
 import express from "express";
 import cors from "cors";
-
-// If you're using one of our datasets, uncomment the appropriate import below to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
 import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+// Defines the port the app will run on. Defaults to 8080, but can be overridden when starting the server. Example command to overwrite PORT env variable value: PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -22,6 +14,23 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+//All books
+app.get('/books', (req, res) => {
+  res.json(booksData)
+})
+
+//Each book by iD
+app.get('/books/:bookID', (req, res => {
+  const bookID = req.params.bookID
+  const singleBook = booksData.find((book) => book.bookID === +bookID)
+  console.log('bookID', bookID, typeof bookID)
+  if (book) {
+    res.json(singleBook)
+  } else {
+    res.statusCode(404).send("No book was found")
+  }
+}))
 
 // Start the server
 app.listen(port, () => {
