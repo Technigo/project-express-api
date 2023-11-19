a41cf6c6078fe9ce68a804e9fff3976791cfc711
Sakura Tanaka <sito6496t@gmail.com>
2023-11-19 21:40:53 +0100

    changed url path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Card } from "./components/Card";
 import { NobelRes } from "./types/type";
 import { Spinner } from "./components/Spinner";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://my-first-api-91k4.onrender.com/";
 
 function App() {
   const [nobels, setNobels] = useState<Array<NobelRes>>([]);
