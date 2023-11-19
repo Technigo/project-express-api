7c1f8713ae5a7ed77444e52e62542aec09e01914
Anna Robertsson <robertsson_anna@hotmail.com>
2023-11-19 20:53:47 +0100

    sorted out /species and /activities. updated comments and readme

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 # Project Express API
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+This project is our first backend project in which we create an API using Express. The API were also supposed to get RESTful endpoints and return either an array of data or a single item.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+I decided that I wanted to choose a different set of data through Kaggle. Sharks fascinate me so I ended up picking data about shark attacks from around the world. Interesting data to look at, but I at first had a hard time implementing it because there wasn't a clear id. So I therefore started working with the provided netflixData and got that to work. After that I commented it out and tried to do the same with the shark data instead. From what I have seen, the original order seems to somewhat work as an id to pick a single shark attack to display. I later added the endpoints for species and activities but didn't get them to show at all. I used the endpoints '/shark-attacks/species' and 'shark-attacks/activities' but nothing but 404's showed up in the browser which confused me. I then tried to remove '/shark-attacks' and just use '/species' and '/activities' as endpoints, and then it worked just fine.
+
+In '/shark-attacks/:id' you can change ':id' to a number.
+
+In the query param in the 'Year of shark attacks' endpoint, you have the possibility to filter if the shark attacks from the specified year were unprovoked or not. They can be labelled as 'provoked', 'unprovoked' or 'questionable' so there are three alternatives to try out. You can for example try typing in: /shark-attacks/year/2023?type=unprovoked
+
+In /species, please note that the same type of shark can be displayed many times because of how the data is structured. For example you can in the list find: 'white shark', 'white sharks', 'white shark, 3 M' and 'white shark, 3M' etc.
+
+It was interesting to start working a bit with backend. But I thought it was more challenging to troubleshoot when you don't see it in the browser or console in the same way as you're used to. When I got stuck I asked my team mates or chatGPT for help, but also looked some more at the provided material from Technigo.
+
+Link to the dataset Global Shark Attack on Kaggle: https://www.kaggle.com/datasets/mexwell/global-shark-attack/?select=global-shark-attack.csv
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+Please visit: https://shark-attack-express-api.onrender.com
--- server.js
+++ server.js
@@ -19,12 +19,12 @@ app.get("/", (req, res) => {
   res.json(endpoints);
 });
 
-// Shark attacks endpoint
+// Shark attacks endpoint to receive a collection of all shark attacks
 app.get("/shark-attacks", (req, res) => {
   res.json(globalSharkAttackData)
 });
 
-// Shark attacks "id" endpoint, (used the original_order in json).
+// Shark attacks "id" endpoint, (used the original_order in json), to receive a single shark attack
 app.get("/shark-attacks/:id", (req, res) => {
   const {id} = req.params
   const sharkAttack = globalSharkAttackData.find(sharkAttack => sharkAttack.original_order === +id)
@@ -36,15 +36,14 @@ app.get("/shark-attacks/:id", (req, res) => {
 }
 })
 
-// Year of shark attacks endpoint. Including query param for the possibility to filter if a shark attack was unprovoked, for example by typing /shark-attacks/year/2023?type=unprovoked
+// Year of shark attacks endpoint. Also added query param for the possibility to filter if the shark attacks from a certain year was unprovoked
 app.get("/shark-attacks/year/:year", (req, res) => {
   const year = req.params.year
   const typeUnprovoked = req.query.type
   let sharkAttacksFromYear = globalSharkAttackData.filter((sharkAttack) => sharkAttack.Year === +year)
-  console.log("SharkAttack:", year, typeof year)
 
 if (sharkAttacksFromYear.length === 0) {
-  return res.status(404).json({ error: "No shark attacks found from this year" })
+  return res.status(404).json({ error: "Calm waters - No shark attacks found from this year!" })
 }
 
 if (typeUnprovoked) {
@@ -54,46 +53,42 @@ sharkAttacksFromYear = sharkAttacksFromYear.filter((sharkAttack) => sharkAttack.
 res.json(sharkAttacksFromYear)
 })
 
-// Shark attacks species endpoint
-app.get("/shark-attacks/species", (req, res) => {
-   res.json(globalSharkAttackData)
-// const speciesCount = {}
+// Shark attacks species endpoint. Presents in a descending order the shark species that is most common in shark attacks
+app.get("/species", (req, res) => {
+ const speciesCount = {}
 
-// globalSharkAttackData.forEach((attack) => {
-//   if (attack.Species) {
-//     const species = attack.Species
-//     if (speciesCount[species]) {
-//       speciesCount[species]++
-//     } else {
-//       speciesCount[species] = 1
-//     }
-//   }
-// })
+ globalSharkAttackData.forEach((attack) => {
+  const species = attack.Species ? attack.Species.toUpperCase() : null; // Declare species variable first and convert to upper case to ensure that variations in case won't result in separate counts for the same species
+   if (species) {
+     if (speciesCount[species]) {
+       speciesCount[species]++
+     } else {
+       speciesCount[species] = 1
+     }
+   }
+ })
 
-// console.log(speciesCount)
+ // Convert object into an array of key-value pairs
+  const speciesCountArray = Object.entries(speciesCount);
 
-// res.json(speciesCount)
-})
+  // Sort the array based on count in descending order
+  speciesCountArray.sort((a, b) => b[1] - a[1]);
 
-// app.get("/shark-attacks/species", (req, res) => {
-// const speciesSet = new Set()
+  // Convert the sorted array back into an object
+  const sortedSpeciesCount = {};
+  speciesCountArray.forEach(([species, count]) => {
+    sortedSpeciesCount[species] = count;
+  });
 
-// globalSharkAttackData.forEach((attack) => {
-//     if (attack.Species) {
-//       speciesSet.add(attack.Species);
-//     }
-//   });
+  res.json(sortedSpeciesCount);
+});
 
-//   const uniqueSpecies = Array.from(speciesSet); // Convert Set to array
-//  console.log(uniqueSpecies); // Add this line to log the unique species
-//   res.json(uniqueSpecies);
-// })
-
-app.get("/shark-attacks/activities", (req, res) => {
+// Shark attacks activities endpoint. Presents in a descending order what the person who were attacked by the shark was doing at the time of the attack
+app.get("/activities", (req, res) => {
   const activitiesCount = {};
 
   globalSharkAttackData.forEach((attack) => {
-    const activity = attack.Activity;
+    const activity = attack.Activity ? attack.Activity.toUpperCase() : null; // Declare activity variable first and convert to upper case to ensure that variations in case won't result in separate counts for the same activity
     if (activity) {
       if (activitiesCount[activity]) {
         activitiesCount[activity]++;
@@ -103,11 +98,32 @@ app.get("/shark-attacks/activities", (req, res) => {
     }
   });
 
-  console.log(activitiesCount); // Log activities and their occurrences
-  
-  res.json(activitiesCount);
+  // Convert object into an array of key-value pairs
+  const activitiesCountArray = Object.entries(activitiesCount);
+
+  // Sort the array based on count in descending order
+  activitiesCountArray.sort((a, b) => b[1] - a[1]);
+
+  // Convert the sorted array back into an object
+  const sortedActivitiesCount = {};
+  activitiesCountArray.forEach(([activities, count]) => {
+    sortedActivitiesCount[activities] = count;
+  });
+
+  res.json(sortedActivitiesCount);
 });
 
+// ------- Dummy endpoints -------
+// An endpoint focusing specifically on shark attacks related to surfing.
+ app.get("/surfing-incidents", (req, res) => {
+ res.json("Here is a dummy endpoint for surfer incidents")
+ })
+
+// An endpoint that categorizes and provides statistics on fatal and non-fatal shark attacks.
+ app.get("/fatal", (req, res) => {
+ res.json("Here is a dummy endpoint for statistics on fatal and non-fatal shark attacks")
+ })
+// ------- End of dummy endpoints -------
 
 // Start the server
 app.listen(port, () => {
