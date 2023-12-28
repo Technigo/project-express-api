3f495a91eef5662773a6300277600daf5914b49c
elbines <141184135+elbines@users.noreply.github.com>
2023-12-28 19:08:25 +0100

    finished

--- README.md
+++ README.md
@@ -6,24 +6,23 @@ This project is a basic Express.js application that serves as a book information
 
 In working on this project, I faced a practical challenge in organizing the routes correctly. Specifically, both authors and books were being accessed through the same /books route, and I would get errors. Figuring out how to separate these two functionalities within the same path was a bit tricky. I still don't quite understand why we solved it the way we did as I was collaborating with someone else. This experience taught me a lot about organizing a web project in a way that makes sense and is easy to navigate, I do however struggle a bit with understanding how I am meant to use it moving forward in other projects.
 
+Updated: I struggeled with this one, more than with the mongo-api for some reason. I think it meets all the requirements now. I think it works the way it should. 
+
 ## View it live
 
 https://ec-express-api-books.onrender.com 
 
 URL: /
-Description: Displays API documentation.
-Get Top N Books by Average Rating:
-
-URL: /books/top?count=R
-Description: Shows top rated books by average rating. Replace 'R' with a number (default is 5).
-Find Book by Title:
-
-URL: /books/:title
-Description: Retrieves a book by title. Replace ':title' with the book's title. For example, /books/Harry Potter.
-Get All Books or Filter by Author:
+- Displays API documentation.
 
 URL: /books
-Description: Lists all books.
+- Lists all books.
 
 URL: /books?author=AuthorName
-Description: Shows books by a specific author. Replace 'AuthorName' with the author's name.
+- Shows books by a specific author. Replace 'AuthorName' with the author's name.
+
+URL: /books/top?count=R
+- Shows top rated books by average rating. Replace 'R' with a number (default is 5).
+
+URL: /books/:title
+- Retrieves a book that includes the specified substring in its title. Replace ':title' with the part of the book's title you are searching for. For example, /books/Harry Potter will return any book with 'Harry Potter' in the title.
--- server.js
+++ server.js
@@ -10,47 +10,52 @@ app.use(cors());
 app.use(express.json());
 
 // Base URL - Updated to return API documentation
-// Changed to provide API documentation at the root endpoint for better API usability.
-// This utilizes express-list-endpoints to auto-generate and show API endpoints.
-// CHANGED from:
-// app.get("/", (req, res) => {
-//   res.send("Welcome to the Book API!");
-// });
-// to:
 app.get("/", (req, res) => {
   res.json(listEndpoints(app)); // Using express-list-endpoints to show API endpoints
 });
 
+
+// Get all books or filter by author (README.md - view it live)
+app.get('/books', (req, res) => {
+  const authorName = req.query.author;
+  if (authorName) {
+    const lowercasedAuthorName = authorName.toLowerCase();
+    const booksByAuthor = booksData.filter((book) =>
+      book.authors.toLowerCase().includes(lowercasedAuthorName)
+    );
+
+    if (booksByAuthor.length === 0) {
+      res.status(404).send("No books found by that author");
+    } else {
+      res.json(booksByAuthor);
+    }
+  } else {
+    res.json(booksData);
+  }
+});
+
+// Changed to ensure consistent case-insensitive author name filtering.
+
+
+
 // Route for getting top rated books by average rating
-// Improved to handle non-positive and excessively large values for 'count'.
-// CHANGED from: 
-// app.get('/books/top', (req, res) => {
-//   let count = parseInt(req.query.count, 10);
-//   if (isNaN(count)) {
-//     count = 5; // default value if count is not provided or invalid
-//   }
-
-//   const sortedBooks = [...booksData].sort((a, b) => b.average_rating - a.average_rating);
-//   const topBooks = sortedBooks.slice(0, count);
-
-//   if (topBooks.length > 0) {
-//     res.json(topBooks);
-//   } else {
-//     res.status(404).send(`No books found for the top ${count} ratings`);
-//   }
-// });
-// to:
+// Changed to handle non-positive and excessively large values for 'count'.
 app.get('/books/top', (req, res) => {
   let count = parseInt(req.query.count, 10);
   if (isNaN(count) || count <= 0) {
-    count = 5; // Default value if count is not provided or invalid
+    count = 5;
   } else if (count > 100) {
-    count = 100; // Limiting the maximum count to prevent too large datasets
+    count = 100;
   }
 
   const sortedBooks = [...booksData].sort((a, b) => b.average_rating - a.average_rating);
   const topBooks = sortedBooks.slice(0, count);
-  res.json(topBooks);
+
+  if (topBooks.length === 0) {
+    res.status(404).send("No top books found");
+  } else {
+    res.json(topBooks);
+  }
 });
 // If count is less than or equal to zero, it defaults to 5.
 // If count is greater than 100, it's limited to 100.
@@ -59,58 +64,21 @@ app.get('/books/top', (req, res) => {
 
 
 // Find book by title
-// Enhanced to support case-insensitive title search for better user experience.
-// CHANGED from:
-// find book by title
-// app.get('/books/:title', (req, res) => {
-//   const requestedTitle = req.params.title.replace(/%20/g, ' ');
-//   const bookWithTitle = booksData.find((book) => book.title === requestedTitle);
-//   if (bookWithTitle) {
-//     res.json(bookWithTitle);
-//   } else {
-//     res.status(404).send("No book was found");
-//   }
-// });
-//to:
+// Update: Now the search works even if you don't type the whole title exactly as it is in the book. It ignores upper/lower case and finds the book as long as the title you type is part of the actual book title.
 app.get('/books/:title', (req, res) => {
-  const requestedTitle = decodeURIComponent(req.params.title).toLowerCase(); // Decode and convert to lowercase
-  const bookWithTitle = booksData.find((book) => book.title.toLowerCase() === requestedTitle);
+  const requestedTitle = decodeURIComponent(req.params.title).toLowerCase();
+  const bookWithTitle = booksData.find((book) =>
+    book.title.toLowerCase().includes(requestedTitle)
+  );
+
   if (bookWithTitle) {
     res.json(bookWithTitle);
   } else {
-    res.status(404).send("No book was found");
+    res.status(404).send("No book with that title was found");
   }
 });
 
-// Get all books or filter by author
-// Updated to perform consistent case-insensitive author name filtering.
-// CHANGED from:
-// get all books or filter by author
-// app.get('/books', (req, res) => {
-//   const authorName = req.query.author;
-//   if (authorName) {
-//     const booksByAuthor = booksData.filter((book) =>
-//       book.authors.toLowerCase().includes(authorName.toLowerCase())
-//     );
-//     res.json(booksByAuthor);
-//   } else {
-//     res.json(booksData);
-//   }
-// });
-//to:
-app.get('/books', (req, res) => {
-  const authorName = req.query.author;
-  if (authorName) {
-    const lowercasedAuthorName = authorName.toLowerCase(); // Making the search case-insensitive
-    const booksByAuthor = booksData.filter((book) =>
-      book.authors.toLowerCase().includes(lowercasedAuthorName)
-    );
-    res.json(booksByAuthor);
-  } else {
-    res.json(booksData); // Return all books when no author query parameter is provided
-  }
-});
-// Changed to ensure consistent case-insensitive author name filtering
+
 
 
 // Start the server
