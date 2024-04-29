d469feebe1125a1b254fd99cc69c0ce283026d00
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-04-29 18:51:47 +0200

    Polish the error handling

--- server.js
+++ server.js
@@ -9,65 +9,123 @@ import expressListEndpoints from "express-list-endpoints";
 const port = process.env.PORT || 8080;
 const app = express();
 
+// handle not implemented error for requests other than GET
+const handle501Error = action => {
+  const jsonMsg = {
+    success: false,
+    code: 501,
+    message: "Not Implemented",
+    body: {
+      error: `${action}ing a song is not implemented yet. Try GET method instead.`,
+    },
+  };
+  return jsonMsg;
+};
+
+// handle bad requet error for incorrect params
+const handle400Error = () => {
+  const jsonMsg = {
+    success: false,
+    code: 400,
+    message: "Bad Request",
+    body: {
+      error:
+        "Your params or query params are not acceptable. Go to the homepage and check what params are acceptable.",
+    },
+  };
+  return jsonMsg;
+};
+
+// handle not found error when no matching items for the given params
+const handle404Error = errorMsg => {
+  const jsonMsg = {
+    success: false,
+    code: 404,
+    message: "Not Found",
+    body: {
+      error: errorMsg,
+    },
+  };
+  return jsonMsg;
+};
+
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-// Start defining your routes here
+// Define routes
+// Homepage route
 app
   .route("/")
   .all((req, res, next) => {
-    console.log("Add authentication middleware");
+    if (req.method !== "GET") {
+      res.status(501).json(handle501Error(req.method));
+    }
     next();
   })
   .get((req, res) => {
     const endpoints = expressListEndpoints(app);
     res.render("index.ejs", { data: endpoints });
-    // res.json(endpoints);
-  })
-  .post((req, res) => {
-    res.send("Add a song");
-  })
-  .put((req, res) => {
-    res.send("Update a song");
-  })
-  .delete((req, res) => {
-    res.send("Delete a song");
   });
-// app.get("/", (req, res) => {
-//   const endpoints = expressListEndpoints(app);
-//   res.json(endpoints);
-// });
 
 // display all songs
-app.get("/songs", (req, res) => {
-  const genre = req.query.genre;
-  let songs = topMusicData;
-  if (genre) {
-    songs = topMusicData.filter(song => song.genre === genre);
-  }
-  // no songs matching with the genre
-  if (songs.length === 0) {
-    res.status(400).send(`There are no songs matching the ${genre} genre...`);
-  }
-  res.json(songs);
-});
+app
+  .route("/songs")
+  .all((req, res, next) => {
+    if (req.method !== "GET") {
+      res.status(501).json(handle501Error(req.method));
+    }
+    next();
+  })
+  .get((req, res) => {
+    const genre = req.query.genre;
+    if (!genre) {
+      return res.status(501).json(handle400Error());
+    }
+    let songs = topMusicData;
+    if (genre) {
+      songs = topMusicData.filter(song => song.genre === genre);
+    }
+    // no songs matching with the genre
+    if (songs.length === 0) {
+      res
+        .status(400)
+        .send(
+          handle404Error(`There are no songs matching the ${genre} genre...`)
+        );
+    } else {
+      res.json(songs);
+    }
+  });
 
 //display a single song
-app.get("/songs/:songId", (req, res) => {
-  const songId = req.params.songId;
-  const song = topMusicData.find(song => song.id === +songId);
-  if (song) {
-    res.json(song);
-  } else {
-    res
-      .status(400)
-      .send(
-        `There are no songs matching the id: ${songId}... Make sure you are inquering id from 1 - 50.`
-      );
-  }
-});
+app
+  .route("/songs/:songId")
+  .all((req, res, next) => {
+    if (req.method !== "GET") {
+      res.status(501).json(handle501Error(req.method));
+    }
+    next();
+  })
+  .get((req, res) => {
+    const songId = req.params.songId;
+    if (!+songId || +songId % 1 !== 0) {
+      return res.status(400).json(handle400Error());
+    }
+    const song = topMusicData.find(song => song.id === +songId);
+    if (song) {
+      res.json(song);
+    } else {
+      res
+        .status(400)
+        .send(
+          handle404Error(
+            `There are no songs matching the id: ${songId}... Make sure you are inquering id from 1 - 50.`
+          )
+        );
+    }
+  });
 
 // Start the server
 app.listen(port, () => {
