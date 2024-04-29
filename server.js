2f81f2d3f8ebf69b94020f86a8e87be3f3868e42
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-04-29 12:56:05 +0200

    Display all the endpoints using expree list endpoints

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  // res.send("Hello Technigo!");
+  const endpoints = expressListEndpoints(app);
+  res.json(endpoints);
 });
 
 // display all songs
