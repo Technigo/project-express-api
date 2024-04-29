1fe5396af184d8cf090a8adeb4ffc466e1827c89
Tejpex <equis_angel@hotmail.com>
2024-04-29 15:06:01 +0200

    Added data

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
-import express from "express";
-import cors from "cors";
+import express from "express"
+import cors from "cors"
+import zooAnimalsData from "./data/zoo-animals.json"
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -26,5 +16,5 @@ app.get("/", (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Server running on http://localhost:${port}`)
+})
