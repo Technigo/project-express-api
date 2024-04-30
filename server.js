aef637abe44ee6c61ec2a7bb666d19c1825ff34a
Martin Jönsson <martin.t.joensson@gmail.com>
2024-04-30 15:58:54 +0200

    Basic functionality done

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ import cors from "cors";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import avocadoSalesData from "./data/avocado-sales.json";
+import booksData from "./data/books.json";
+import goldenGlobesData from "./data/golden-globes.json";
+import netflixData from "./data/netflix-titles.json";
+import topMusicData from "./data/top-music.json";
+import magicItems from "./data/magic-items.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -22,6 +23,21 @@ app.use(express.json());
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+// Avocado Tree
+app.get("/avocado", (req, res) => {
+  res.send(avocadoSalesData);
+});
+app.get("/avocado/:avocadoId", (req, res) => {
+  const { avocadoId } = req.params;
+  const item = avocadoSalesData.find((findItem) => +avocadoId === findItem.id);
+
+  if (item) {
+    res.send(item);
+  } else {
+    res.status(404).send("No avocado found with that Id!");
+  }
 });
 
 // Start the server
