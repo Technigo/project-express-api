9d1bc128bbfda19ad39dc0f37f59bde18c98b42b
Viktoria Olofsson <viktoriaolofsson@gmail.com>
2024-04-30 20:33:14 +0200

    small fix

--- server.js
+++ server.js
@@ -2,13 +2,7 @@ import express from "express";
 import cors from "cors";
 import expressListEndpoints from "express-list-endpoints";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
 import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -82,11 +76,11 @@ app.post("/books", (req, res) => {
   // Placeholder
 });
 
-app.put("/books:bookId", (req, res) => {
+app.put("/books/:bookId", (req, res) => {
   // Placeholder
 });
 
-app.delete("/books:bookId", (req, res) => {
+app.delete("/books/:bookId", (req, res) => {
   // Placeholder
 });
 
