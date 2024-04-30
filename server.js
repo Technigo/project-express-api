53336ff5c1c1b4c66a8d38677ab6437a1ba89933
Tejpex <equis_angel@hotmail.com>
2024-04-30 14:36:32 +0200

    Changed data and added some routes

--- server.js
+++ server.js
@@ -2,17 +2,34 @@ import express from "express"
 import cors from "cors"
 import zooAnimalsData from "./data/zoo-animals.json"
 
-const port = process.env.PORT || 8080;
-const app = express();
+const port = process.env.PORT || 8080
+const app = express()
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+  res.send("Hello Technigo!")
+})
+
+app.get("/animals", (req, res) => {
+  res.json(zooAnimalsData)
+})
+
+app.get("/animals/:animalId", (req, res) => {
+  const id = req.params.animalId
+  console.log(id)
+  const animal = zooAnimalsData.find(item => item.id === +id)
+  res.json(animal)
+})
+
+app.get("/type/:type", (req, res) => {
+  const type = req.params.type
+  const onlyOneType = zooAnimalsData.filter(animal => animal.type === type)
+  res.json(onlyOneType)
+})
 
 // Start the server
 app.listen(port, () => {
