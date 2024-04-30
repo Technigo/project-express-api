74fd60614c88d79c74f411293b6ae3cfe7b470f4
Viktoria Olofsson <viktoriaolofsson@gmail.com>
2024-04-30 18:09:28 +0200

    initial commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
+import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
@@ -21,7 +21,25 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Here there be books");
+});
+
+// Get all books
+app.get("/books", (req, res) => {
+  res.json(booksData);
+});
+
+// Get info about a particular book
+app.get("/books/:bookId", (req, res) => {
+  const { bookId } = req.params;
+
+  const book = booksData.find((book) => +bookId === book.bookID);
+
+  if (book) {
+    res.json(book);
+  } else {
+    res.status(404).send("Could not find book");
+  }
 });
 
 // Start the server
