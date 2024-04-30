03ffebc38b4d7e0adf1552b19c5de4af8ccef357
Pernilla Sterner <hey@pernillasterner.se>
2024-04-30 18:47:08 +0200

    Basic blueprint for frontend topics (#1)
    * Add link to production
    
    * Ignore note file
    
    * Add dataset with different topics
    
    * Add routes to get all topics and topic names
    
    * Add babel

--- README.md
+++ README.md
@@ -4,10 +4,15 @@ Replace this readme with your own information about your project.
 
 Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
 
+# Setting up project
+
+npm init
+npm install nodemon @babel/core @babel/preset -env @babel/node
+
 ## The problem
 
 Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+[Express API](https://technigo-project-express-api.onrender.com/)
--- server.js
+++ server.js
@@ -1,30 +1,38 @@
 import express from "express";
 import cors from "cors";
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import { topics } from "./data/topics.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+// // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+// Function to extraxt all topic names
+const extractTopicNames = () => topics.map((topic) => topic.name);
+
+// // Start defining your routes here
+// http://localhost:8000/
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
-// Start the server
+// Route to get all topics
+app.get("/topics", (req, res) => {
+  res.json(topics);
+});
+
+// Route to get name of all topics
+app.get("/topics/name", (req, res) => {
+  const topicNames = extractTopicNames();
+  res.json(topicNames);
+});
+
+// // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
