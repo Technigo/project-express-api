816c81d372b0c7f4bae5ba90f59fbec0419cfff0
izzibizz <contact@izabellind.com>
2024-05-01 10:53:53 +0200

    started project, created own json, did the get method, endpoints

--- server.js
+++ server.js
@@ -1,30 +1,78 @@
-import express from "express";
-import cors from "cors";
+import express from 'express'
+import cors from 'cors'
+import expressListEndpoints from 'express-list-endpoints'
+import artistData from './data/artists.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+// Defines the port the app will run on
+const port = process.env.PORT || 8080
+const app = express()
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+app.get('/', (req, res) => {
+  const endpoints = expressListEndpoints(app)
+  res.json(endpoints)
+})
+
+// List of all artists
+app.get('/artists', (req, res) => {
+  res.json(artistData)
+})
+
+//Endpoint unique id
+app.get('/artists/:id', (req, res) => {
+  const id = req.params.id
+  const artistWithId = artistData.filter((artist) => artist.id === +id)
+  if (artistWithId.lenght > 0) {
+    res.json(artistWithId)
+  } else {
+    res.status(404).json({ error: 'Could not find any artist with this ID' })
+  }
+})
+
+// Endpoint gender
+app.get('/gender/:gender', (req, res) => {
+  const gender = req.params.gender
+  const showAlive = req.query.alive === 'true'
+  const showDeceased = req.query.alive === 'false'
+  let artistsOfGender = artistData.filter(
+    (artist) => artist.gender.toLowerCase() === gender
+  )
+
+  if (showAlive) {
+    artistsOfGender = artistsOfGender.filter((artist) => artist.alive === true)
+  }
+  if (showDeceased) {
+    artistsOfGender = artistsOfGender.filter((artist) => artist.alive === false)
+  }
+  if (artistsOfGender.length > 0) {
+    res.json(artistsOfGender)
+  } else {
+    res.status(404).json({ error: 'Could not find artists of this kind' })
+  }
+})
+
+//Endpoint technique
+app.get('/technique/:technique', (req, res) => {
+  const technique = req.params.technique
+  const artistsUsingTechnique = artistData.filter((artist) => {
+    const lowerCase = artist.technique.map((tech) => tech.toLowerCase())
+    return lowerCase.includes(technique)
+  })
+
+  if (artistsUsingTechnique.length > 0) {
+    res.json(artistsUsingTechnique)
+  } else {
+    res
+      .status(404)
+      .json({ error: 'Could not find artists making art of this kind' })
+  }
+})
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Server running on http://localhost:${port}`)
+})
