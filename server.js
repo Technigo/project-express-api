54c788fd644124ebe31a5026daec5c5097d77f27
SofieFerrari <sofiestrahl@gmail.com>
2024-05-01 20:47:49 +0200

    set up project and chose music-api

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
+console.log("hello world")
+
 import express from "express";
 import cors from "cors";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import topMusicData from "./data/top-music.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -23,6 +19,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+app.get("/songs", (req, res) => {
+  res.json(topMusicData)
+})
 
 // Start the server
 app.listen(port, () => {
