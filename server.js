959b2dfdb812d6f01a68e1742240bd322bf1b2a8
Trista Shan <trista8703@hotmail.com>
2024-05-01 18:27:34 +0300

    done

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import avocadoData from "./data/avocado-sales.json"
+import expressListEndpoints from 'express-list-endpoints';
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -20,9 +22,54 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
+
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  const endpoints = expressListEndpoints(app);
+  res.json(endpoints)
+})
+ 
+app.get("/avocados", (req, res) => {
+  res.send(avocadoData);
 });
+
+app.get("/avocados/:avocadoId", (req, res) => {
+  const {avocadoId} = req.params
+
+  const avocado = avocadoData.find (avocado => +avocadoId === avocado.id)
+  
+  if(avocado){
+    res.json(avocado)
+  } else {
+    res.status (404).send ('Avocado is not found')
+  }
+})
+
+app.get("/avocados/:date", (req, res) => {
+  const {date} = req.params
+
+  const avocadoDate = avocadoData.filter (item => date === item.date)
+
+  if(avocadoDate){
+     res.json(avocadoDate)
+  }else {
+    res.status (404).send ('Avocado is not found')
+  }})
+
+app.get("/avocados/region", (req, res) => {
+
+  const regions = { }
+
+  avocadoData.forEach((item) => {
+    
+    const regionName = item.region
+
+    if (!regions[regionName]){
+     regions[regionName] = []
+    } regions[regionName].push(item)
+  })
+  res.json(regions)
+
+})
 
 // Start the server
 app.listen(port, () => {
