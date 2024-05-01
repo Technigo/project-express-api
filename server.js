3f8cb38342fd6a34b7cfb8a89442055dc2e90350
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-05-01 13:16:52 +0200

    Added routes to get all data, single data using id, data based on colour and type.

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 import express from "express";
 import cors from "cors";
+import flowerData from "./data/old-flowers.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+// Defines the port the app will run on.
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -19,9 +10,65 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//Raplece ÅÄÖ with A and O in url
+function replaceSwedishCharacters(text) {
+  return text
+    .toLowerCase()
+    .replace(/å/g, "a")
+    .replace(/ä/g, "a")
+    .replace(/ö/g, "o");
+}
+
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Flowers for an old garden");
+});
+
+//Get all flowers
+app.get("/flowers", (req, res) => {
+  res.json(flowerData);
+});
+
+//Get one flower based on id
+app.get("/flowers/:flowerId", (req, res) => {
+  const { flowerId } = req.params;
+
+  const flower = flowerData.find((flower) => +flowerId === flower.id);
+
+  if (flower) {
+    res.json(flower);
+  } else {
+    res.status(404).send("No flower was found");
+  }
+});
+
+// Get flowers based on color
+flowerData.forEach((flower) => {
+  flower.color.forEach((color) => {
+    app.get(`/flowers/color/${color.toLowerCase()}`, (req, res) => {
+      const flowersWithColor = flowerData.filter((flower) =>
+        flower.color.includes(color)
+      );
+      res.json(flowersWithColor);
+    });
+  });
+});
+
+// Get flowers based on type
+app.get("/flowers/type/:flowerType", (req, res) => {
+  const { flowerType } = req.params;
+  const sanitizedType = replaceSwedishCharacters(flowerType);
+
+  const flowersWithType = flowerData.filter(
+    (flower) =>
+      replaceSwedishCharacters(flower.type.toLowerCase()) === sanitizedType
+  );
+
+  if (flowersWithType.length > 0) {
+    res.json(flowersWithType);
+  } else {
+    res.status(404).send("No flowers of that type were found");
+  }
 });
 
 // Start the server
