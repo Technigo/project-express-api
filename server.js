5832f8baf433f420cafc7d275c96c44aac7d247b
Martin Jönsson <martin.t.joensson@gmail.com>
2024-05-02 16:56:06 +0200

    Avocado API

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import expressListEndpoints from "express-list-endpoints";
 import cors from "cors";
 
 // If you're using one of our datasets, uncomment the appropriate import below
@@ -13,7 +14,7 @@ import magicItems from "./data/magic-items.json";
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9923;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
@@ -22,13 +23,35 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  const endpoints = expressListEndpoints(app);
+  res.json(endpoints);
 });
 
 // Avocado Tree
 app.get("/avocado", (req, res) => {
-  res.send(avocadoSalesData);
+  let filterList = [...avocadoSalesData];
+
+  // Query for a specific region
+  const regionSearch = req.query.region;
+  if (regionSearch) {
+    filterList = filterList.filter((item) =>
+      item.region.toLowerCase().includes(regionSearch.toLowerCase())
+    );
+  }
+
+  // Query to filter out all entries at a price point higher than the query.
+  const priceSearch = req.query.lowestprice;
+  if (priceSearch) {
+    filterList = filterList.filter((item) => item.averagePrice >= +priceSearch);
+  }
+
+  if (filterList.length > 0) {
+    res.json(filterList);
+  } else {
+    res.status(404).send("No region found with that name!");
+  }
 });
+
 app.get("/avocado/:avocadoId", (req, res) => {
   const { avocadoId } = req.params;
   const item = avocadoSalesData.find((findItem) => +avocadoId === findItem.id);
