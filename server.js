70aa2436201d0295bd38138c0e59f120268154b4
schouenkes <m.schouenke@gmail.com>
2024-05-02 14:02:12 +0200

    started to add some routes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
+import booksData from "./data/books.json";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
+
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
@@ -20,8 +21,30 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
-app.get("/", (req, res) => {
+app.route("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.get(`/books`, (req, res) => {
+  res.json(booksData);
+});
+
+app.get(`/author/:author`, (req, res) => {
+  const author = req.params.author.toLowerCase();
+  const booksFromAuthor = booksData.filter((item) => item.authors.toLowerCase().includes(author));
+  res.json(booksFromAuthor);
+});
+
+app.get(`/title/:title`, (req, res) => {
+  const title = req.params.title.toLowerCase();
+  const bookTitle = booksData.filter((item) => item.title.toLowerCase().includes(title));
+  res.json(bookTitle);
+});
+
+app.get(`/rating/:rating`, (req, res) => {
+  const rating = req.params.rating;
+  const bookRating = booksData.filter((item) => item.average_rating >= rating && item.average_rating < rating + 1);
+  res.json(bookRating);
 });
 
 // Start the server
