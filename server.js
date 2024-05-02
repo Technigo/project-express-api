7252954e630f388543dbe7faee1890223a2ee401
Martin Jönsson <martin.t.joensson@gmail.com>
2024-05-02 18:38:29 +0200

    removed unused code

--- server.js
+++ server.js
@@ -5,16 +5,12 @@ import cors from "cors";
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 import avocadoSalesData from "./data/avocado-sales.json";
-import booksData from "./data/books.json";
-import goldenGlobesData from "./data/golden-globes.json";
-import netflixData from "./data/netflix-titles.json";
-import topMusicData from "./data/top-music.json";
 import magicItems from "./data/magic-items.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 9923;
+const port = process.env.PORT || 8080;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
