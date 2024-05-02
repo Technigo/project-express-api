1547dd3ffce8a262859ce1683e72cafce7b87e1f
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-05-02 20:03:28 +0200

    comments

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
-
-
+import expressListEndpoints from "express-list-endpoints";
 import goldenGlobesData from "./data/golden-globes.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
@@ -11,13 +10,12 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
+// express is the framework.
 app.use(cors());
 app.use(express.json());
 
-
-
 // this returns the whole data set
-// app is the express object
+// app is the express object. app.get is end point functions ... a middleware function runs for every request.
 app.get("/nominations", (req, res) => {
   res.json(goldenGlobesData);
 });
@@ -25,17 +23,18 @@ app.get("/nominations", (req, res) => {
 app.get("/nominations/:id", (req, res) => {
   const id = req.params.id;
   const nomination = goldenGlobesData[parseInt(id)];
-  
+
   if (nomination) {
     res.json(nomination);
   } else {
+    // remmber 404 means "not found". if it start with a 2 evertyhing is ok. if it starts with a 4 or 5 something is wrong. 4 means your request is wrong. 5 means the server is wrong.
     res.status(404).send(`No nomination with id ${id} found.`);
   }
 });
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(expressListEndpoints(app));
 });
 // Start the server
 app.listen(port, () => {
