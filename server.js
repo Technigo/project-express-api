e082d7c87e1d922390a4d51deb0cd3f7fc56f244
Teddy <yoo.ji.yg@gmail.com>
2024-05-02 11:58:13 +0200

    params and queries

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
-import expressListEndpoints from "express-list-endpoints"
+import expressListEndpoints from "express-list-endpoints";
 import cors from "cors";
-import goldenGlobesData from "./data/golden-globes.json";
+import booksData from "./data/books.json";
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -10,22 +10,46 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-  
 // Start defining your routes here
 app.get("/", (req, res) => {
-
-const endpoints = expressListEndpoints(app)
+  const endpoints = expressListEndpoints(app);
   res.json(endpoints);
 });
 
-app.get("/nominations", (req, res) => {
-  res.json(goldenGlobesData)
-})
+app.get("/books", (req, res) => {
+  let filterBooks = [...booksData];
 
-// app.get("/endpoints", (req, res) => {
-//   const endpoints = expressListEndpoints(app)
-//   res.json(endpoints)
-// })
+  res.json(filterBooks);
+
+  //query for book title
+  const searchTitle = req.query.title;
+
+  if (searchTitle) {
+    filterBooks = filterBooks.filter((book) =>
+      book.title.includes(searchTitle)
+    );
+  } else {
+  }
+});
+
+app.get("/books/:bookId", (req, res) => {
+  let filterBooks = [...booksData];
+
+  const { bookId } = req.params;
+
+  filterBooks = filterBooks.find((book) => +bookId === book.bookID);
+
+  if (filterBooks) {
+    res.json(filterBooks);
+  } else {
+    res.status(404).send("No book was found");
+  }
+});
+
+app.get("/books/:bookTitle", (req, res) => {
+  let filterBooks = [...booksData];
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
