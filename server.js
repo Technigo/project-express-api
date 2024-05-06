66cf541a3a08777b91ed8433e576340ca4a26c1a
AHPIXI <148851757+AHPIXI@users.noreply.github.com>
2024-05-06 10:54:02 +0200

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import cors from "cors";
 
+
+
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import topMusicData from "./data/top-music.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -20,9 +22,30 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
+// http://localhost:8080
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+// Get all top-music
+// http://localhost:8080/top-music
+app.get("/top-music",(req, res)=>{
+res.json(topMusicData)
+})
+
+// Get one song on id
+app.get("/top-music/:songID", (req, res) =>{
+const { songID } = req.params
+console.log(req.params)
+
+const song = topMusicData.find(song => +songID === song.id)
+
+if (song) {
+res.json(song)
+} else {
+  res.status(404).send ('No song with that ID was found :(')
+}
+})
+
 
 // Start the server
 app.listen(port, () => {
