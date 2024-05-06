9f2c42c60e63ce211c1273d0529bb406cc575f4b
Pernilla Sterner <psterner@hotmail.com>
2024-05-06 11:16:48 +0200

    Change port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { topics } from "./data/topics.json";
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5001;
 const app = express();
 
 // // Add middlewares to enable cors and json body parsing
