cf5c3777a2d8d2b409d2397ef9300ea081e2294a
AHPIXI <148851757+AHPIXI@users.noreply.github.com>
2024-05-07 14:21:26 +0200

    added two queries

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
 import express from "express";
 import cors from "cors";
-
-
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
 import topMusicData from "./data/top-music.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+// Defines the port the app will run on.
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -26,27 +15,42 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
-// Get all top-music
+// Get all top music songs
 // http://localhost:8080/top-music
-app.get("/top-music",(req, res)=>{
-res.json(topMusicData)
-})
+app.get("/top-music", (req, res) => {
+  let filterSongs = [...topMusicData];
 
-// Get one song on id
-app.get("/top-music/:songID", (req, res) =>{
-const { songID } = req.params
-console.log(req.params)
+  //query for a song genre
+  // http://localhost:8080/top-music?genre=POP <for example
+  const genreSearch = req.query.genre;
 
-const song = topMusicData.find(song => +songID === song.id)
+  if (genreSearch) {
+    filterSongs = filterSongs.filter((song) =>
+      song.genre.toLowerCase().includes(genreSearch.toLowerCase())
+    );
+  }
+  if (filterSongs.length > 0) {
+    res.json(filterSongs);
+  } else {
+    res.status(404).send("No song was found based on typed genre");
+  }
+  // res.json(topMusicData)
+});
 
-if (song) {
-res.json(song)
-} else {
-  res.status(404).send ('No song with that ID was found :(')
-}
-})
+// Get one song based on id
+//http://localhost:8080/top-music/9 <for e
+app.get("/top-music/:songID", (req, res) => {
+  const { songID } = req.params;
+  console.log(req.params);
 
+  const song = topMusicData.find((song) => +songID === song.id);
 
+  if (song) {
+    res.json(song);
+  } else {
+    res.status(404).send("No song with that ID was found :(");
+  }
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
