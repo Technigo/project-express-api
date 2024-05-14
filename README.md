ef7fdd2d4fa684c8c10195837bd1e192e85ea8a4
Kathinka Sewell <4355167+kathinka@users.noreply.github.com>
2024-05-14 09:58:39 +0200

    formatted and refactored to separate the routes from the server file. removed unused dependencies

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ this week we chose a json who we would like to use as our basedata for out API
 ## The problem
 
 looked at the data to figure out what could be interesting for the user to fetch.
-after I  made a bunch of routes, I made one query filter who could also be used to filter the data as well.
+after I made a bunch of routes, I made one query filter who could also be used to filter the data as well.
 then i looked into how to document the api, and write that to a file who would be updated if the file changed to make sure the api always is updated.
 
 ## View it live
---
+++ routes/Routes.js
@@ -0,0 +1,273 @@
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import goldenGlobesData from "../data/golden-globes.json";
+const router = express.Router();
+
+// Start with showing the updated documentation for the api
+router.get("/", (req, res) => {
+  const endpoints = listEndpoints(router);
+  const updatedEndpoints = endpoints.map((endpoint) => {
+    if (endpoint.path === "/movies/filter") {
+      return {
+        path: endpoint.path,
+        methods: endpoint.methods,
+        queryParameters: [
+          {
+            name: "category",
+            description:
+              "filter by category, example: /movies/filter?category=Best Motion Picture. Can be chained with other parameters. Example: /movies/filter?category=Best Motion Picture&year=2020",
+          },
+          {
+            name: "year",
+            description:
+              "filter by year, example: /movies/filter?year=2020. Can be chained with other parameters. Example: /movies/filter?year=2020&category=Best Motion Picture",
+          },
+          {
+            name: "win",
+            description:
+              "filter by true or false, example: /movies/filter?win=true.  Can be chained with other parameters. Example: /movies/filter?win=true&category=Best Motion Picture",
+          },
+          {
+            name: "nominee",
+            description:
+              "filter by nomination, example: /movies/filter?nominee=Tom Hanks. Can be chained with other parameters. Example: /movies/filter?nominee=Tom Hanks&category=Best Motion Picture",
+          },
+          {
+            name: "film",
+            description:
+              "filter by the film example: /movies/filter?film=The Crown. Can be chained with other parameters. Example: /movies/filter?film=The Crown&category=Best Motion Picture&year=2020",
+          },
+        ],
+      };
+    }
+    return {
+      path: endpoint.path,
+      methods: endpoint.methods,
+    };
+  });
+  res.json(updatedEndpoints);
+});
+
+//  make filter with query params
+router.get("/movies/filter", (req, res) => {
+  try {
+    let query = {}; // Create an empty object to store the query parameters
+    let filteredMovies = goldenGlobesData; // Create a variable to store the filtered movies
+    const { category, year, win, nominee, film } = req.query;
+
+    if (category) {
+      query.category = category;
+      filteredMovies = filteredMovies.filter((item) =>
+        item.category.toLowerCase().includes(category.toLowerCase())
+      );
+    }
+    if (year) {
+      query.year = year;
+      filteredMovies = filteredMovies.filter(
+        (item) => item.year_award === +year
+      );
+    }
+    if (win) {
+      query.win = win;
+      filteredMovies = filteredMovies.filter((item) => item.win === true);
+    }
+    if (nominee) {
+      query.nominee = nominee;
+      filteredMovies = filteredMovies.filter((item) =>
+        item.nominee.toLowerCase().includes(nominee.toLowerCase())
+      );
+    }
+    if (film) {
+      query.film = film;
+      filteredMovies = filteredMovies.filter((item) =>
+        item.film.toLowerCase().includes(film.toLowerCase())
+      );
+    }
+
+    if (filteredMovies.length === 0) {
+      res.status(404).send(`No movies found`);
+    } else {
+      res.json(filteredMovies);
+    }
+  } catch (error) {
+    res.status(500).send(`Internal Server Error`);
+  }
+});
+
+// get golden globes data
+router.get("/movies", (req, res) => {
+  res.json(goldenGlobesData);
+});
+
+// get a movies who won
+router.get("/movies/:win", (req, res) => {
+  const WinningMovies = goldenGlobesData.filter((item) => item.win === true);
+  if (WinningMovies.length === 0) {
+    res.status(404).send(`No movies found`);
+  }
+  res.json(WinningMovies);
+});
+
+//get movies from a certain year
+
+router.get("/movies/year/:year", (req, res) => {
+  const year = req.params.year;
+  const moviesFromYear = goldenGlobesData.filter(
+    (item) => item.year_award === +year
+  );
+  if (moviesFromYear.length === 0) {
+    res.status(404).send(`No movies found from year ${year}`);
+  } else {
+    res.json(moviesFromYear);
+  }
+});
+
+// get movies from a certain category
+router.get("/movies/category/:category", (req, res) => {
+  const category = req.params.category;
+  const moviesFromCategory = goldenGlobesData.filter((item) =>
+    item.category.toLowerCase().includes(category.toLowerCase())
+  );
+  if (moviesFromCategory.length === 0) {
+    res.status(404).send(`No movies found in this category`);
+  } else {
+    res.json(moviesFromCategory);
+  }
+});
+
+// get the movie from a certain  year and category
+router.get("/movies/year/:year/category/:category", (req, res) => {
+  const category = req.params.category;
+  const year = req.params.year;
+  const moviesFromCategoryAndYear = goldenGlobesData.filter(
+    (item) =>
+      item.category.toLowerCase().includes(category.toLowerCase()) &&
+      item.year_award === +year
+  );
+  if (moviesFromCategoryAndYear.length === 0) {
+    res.status(404).send(`No movies found in this category and year`);
+  }
+  res.json(moviesFromCategoryAndYear);
+});
+
+// return all winning movies from a certain year, if win=true
+router.get("/movies/year/:year/:win", (req, res) => {
+  const year = req.params.year;
+  const WinningMoviesFromYear = goldenGlobesData.filter(
+    (item) => item.win === true && item.year_award === +year
+  );
+  if (WinningMoviesFromYear.length === 0) {
+    res.status(404).send(`No movies found in this category and year`);
+  }
+  res.json(WinningMoviesFromYear);
+});
+
+// return all winning movies from a certain year, category and win=true
+
+router.get("/movies/year/:year/category/:category/:win", (req, res) => {
+  const year = req.params.year;
+  const category = req.params.category;
+  const WinningMoviesFromYearAndCategory = goldenGlobesData.filter(
+    (item) =>
+      item.win === true &&
+      item.year_award === +year &&
+      item.category.toLowerCase().includes(category.toLowerCase())
+  );
+  if (WinningMoviesFromYearAndCategory.length === 0) {
+    res.status(404).send(`No movies found in this category and year`);
+  }
+  res.json(WinningMoviesFromYearAndCategory);
+});
+
+// return all winning movies from a certain category, nominee and win=true
+router.get("/movies/nominee/:nominee/category/:category", (req, res) => {
+  const category = req.params.category;
+  const nominee = req.params.nominee;
+  const ActorInCategory = goldenGlobesData.filter(
+    (item) =>
+      item.category.toLowerCase().includes(category.toLowerCase()) &&
+      item.nominee.toLowerCase().includes(nominee.toLowerCase())
+  );
+  if (ActorInCategory.length === 0) {
+    res.status(404).send(`No movies found for this nominee in this category`);
+  }
+  res.json(ActorInCategory);
+});
+
+// return all winning movies from a certain category, nominee and win=true
+router.get("/movies/nominee/:nominee/category/:category/:win", (req, res) => {
+  const category = req.params.category;
+  const nominee = req.params.nominee;
+  const WinningActorInCategory = goldenGlobesData.filter(
+    (item) =>
+      item.category.toLowerCase().includes(category.toLowerCase()) &&
+      item.nominee.toLowerCase().includes(nominee.toLowerCase()) &&
+      item.win === true
+  );
+  if (WinningActorInCategory.length === 0) {
+    res
+      .status(404)
+      .send(`No winning movies found for this nominee in this category`);
+  }
+  res.json(WinningActorInCategory);
+});
+
+// return all movies from a nominee
+router.get("/movies/nominee/:nominee", (req, res) => {
+  const nominee = req.params.nominee;
+  const moviesFromNominee = goldenGlobesData.filter((item) =>
+    item.nominee.toLowerCase().includes(nominee.toLowerCase())
+  );
+  if (moviesFromNominee.length === 0) {
+    res.status(404).send(`No movies found`);
+  } else {
+    res.json(moviesFromNominee);
+  }
+});
+
+// return all wins for a certain nominee
+router.get("/movies/nominee/:nominee/:win", (req, res) => {
+  const nominee = req.params.nominee;
+  const WinningNominee = goldenGlobesData.filter(
+    (item) =>
+      item.win === true &&
+      item.nominee.toLowerCase().includes(nominee.toLowerCase())
+  );
+  if (WinningNominee.length === 0) {
+    res.status(404).send(`No movies found`);
+  } else {
+    res.json(WinningNominee);
+  }
+});
+
+// return all nominations for a certain film or series
+router.get("/movies/film/:film", (req, res) => {
+  const film = req.params.film;
+  const filmsNomination = goldenGlobesData.filter((item) =>
+    item.film.toLowerCase().includes(film.toLowerCase())
+  );
+  if (filmsNomination.length === 0) {
+    res.status(404).send(`No movies found`);
+  } else {
+    res.json(filmsNomination);
+  }
+});
+
+//return all winning nominations for a certain film or series
+router.get("/movies/film/:film/:win", (req, res) => {
+  const film = req.params.film;
+  const filmsNominationWin = goldenGlobesData.filter(
+    (item) =>
+      item.film.toLowerCase().includes(film.toLowerCase()) && item.win === true
+  );
+  if (filmsNominationWin.length === 0) {
+    res.status(404).send(`No movies found`);
+  } else {
+    res.json(filmsNominationWin);
+  }
+});
+
+// Get the list of endpoints
+const endpoints = listEndpoints(router);
+
+export default router;
