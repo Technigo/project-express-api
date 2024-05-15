781007dc824f9e697bec5ad983bdb350e45bd3e9
Katarina Sjölin <kat.sjolin@gmail.com>
2024-05-15 13:17:35 +0200

    added route /books

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 import express from "express";
 import cors from "cors";
+import booksData from "./data/books.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
-
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -23,6 +17,11 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+// Get all books
+app.get('/books', (req, res) => {
+  res.json(booksData)
+})
 
 // Start the server
 app.listen(port, () => {
