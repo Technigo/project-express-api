0f022be0bdafaa2f2ecdd38d3485fe00cfae53e7
Katarina Sjölin <kat.sjolin@gmail.com>
2024-05-19 20:36:12 +0200

    added express list endpoint

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import booksData from "./data/books.json";
+import expressListEndpoints from "express-list-endpoints"
 
-// import topMusicData from "./data/top-music.json";
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -14,9 +14,10 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+app.get('/', (req, res) => {
+  const endpoints = expressListEndpoints(app)
+  res.json(endpoints)
+})
 
 // Get all books
 // http://localhost:8080/books
