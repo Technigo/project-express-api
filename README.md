5c9267eb89f7e90a63a9d87465b1545dc359ec16
Katarina Sjölin <kat.sjolin@gmail.com>
2024-05-19 20:59:02 +0200

    updated readme file

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Project Express API
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+The assignment was to create an API using Express with at least a couple of RESTful endpoints.
 
 ## The problem
 
@@ -10,4 +8,4 @@ Describe how you approached to problem, and what tools and techniques you used t
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+https://project-express-api-kat.onrender.com/
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-import express from "express";
-import cors from "cors";
-import booksData from "./data/books.json";
+import express from "express"
+import cors from "cors"
+import booksData from "./data/books.json"
 import expressListEndpoints from "express-list-endpoints"
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+const port = process.env.PORT || 8080
+const app = express()
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
@@ -62,5 +62,5 @@ app.get('/books/:bookID', (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Server running on http://localhost:${port}`)
+})
