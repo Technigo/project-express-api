8424dd1dbd565e02b3abca78c412402b185da0bc
Anna <annah80@gmail.com>
2024-12-04 21:01:05 +0100

    added json file for cats, created first route with a category query filter for name

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 import express from "express";
 import cors from "cors";
+import cats from "./data/cats.json"
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9000;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
@@ -20,11 +17,32 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+app.get("/", (request, response) => {
+  response.send("Miao miao!");
 });
+
+// Combined cats route with category query filter for name
+app.get("/cats", (request, response) => {
+  const name = request.query.name;
+  
+  if (name) {
+    const filteredCats = cats.filter(
+      cats => cats.name.toLowerCase() === name.toLowerCase()
+    );
+    response.json(filteredCats);
+  } else {
+    response.json(cats);
+  }
+});
+
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+
+
+
+
+
