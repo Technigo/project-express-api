6d0864007bf4eaa1390b2c0775a831d118faca4e
Anna <annah80@gmail.com>
2024-12-04 22:29:04 +0100

    added filter for fur length and find id for cat and added error handling with 404 responses.

--- server.js
+++ server.js
@@ -21,19 +21,60 @@ app.get("/", (request, response) => {
   response.send("Miao miao!");
 });
 
-// Combined cats route with category query filter for name
-app.get("/cats", (request, response) => {
-  const name = request.query.name;
+// Combined cats route filtering on all cats and cats with category query filter for breed.
+app.get("/cats", (request, response) => {  // http://localhost:9000/cats- route for displaying all cat breeds. 
+  const breed = request.query.breed;
   
-  if (name) {
+  if (breed) {
     const filteredCats = cats.filter(
-      cats => cats.name.toLowerCase() === name.toLowerCase()
+      cat => cat.breed.toLowerCase() === breed.toLowerCase()  // ex. http://localhost:9000/cats?breed=bengal
     );
-    response.json(filteredCats);
+
+    if (filteredCats.length>0) {
+      response.json (filteredCats) //Return matching cats
+    } else {
+      response.status(404).send("No cat breed found with that name ") //if no matches when writing a cat breed that does not exist, this message will show. 
+    }
+    } else {
+      response.json (cats) //Return all cats if no name of breed is provided
+
+    }
+  })
+
+app.get("/cats/personality/:personality", (request, response) => {
+  const personality = request.params.personality
+  
+  const catFilter = cats.filter (cats => cats.personality.toLowerCase() === personality.toLowerCase())
+  if (catFilter.length >0) {  // checks if any cats matched the personality.
+    response.status(200).json(catFilter)
+    } else {
+    response.status(404).send("No personality found with that name ") //if no matches when writing a personality that does not exist, this message will show. 
+    }
+  })
+
+
+app.get ("/cats/fur_length/:fur_length", (request, response) => {
+  const fur_length = request.params.fur_length
+
+  const catFilter = cats.filter (cats => cats.fur_length.toLowerCase() === fur_length.toLowerCase())
+  if (catFilter.length >0){
+    response.status (200).json (catFilter)
   } else {
-    response.json(cats);
+    response.status (404).send ("No fur length found with that type")
   }
-});
+})
+
+  
+  app.get("/cats/:id", (request, response) => {
+  const id = request.params.id
+  
+  const cat = cats.find(cat => cat.id === +id)
+  if (cat) {
+  response.status(200).json(cat)
+  } else {
+  response.status(404).send("No cat found with that ID")
+  }
+  })
 
 
 // Start the server
