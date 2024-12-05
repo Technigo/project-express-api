22b076c886afc623aa9fc040dbafb8302b00a343
Emelie Nyberg <emelie.kedert@gmail.com>
2024-12-05 15:32:33 +0100

    requirements are met

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import listEndpoints from "express-list-endpoints";
+
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -8,11 +10,12 @@ import cors from "cors";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
+import videogames from "./data/nintendoswitch-games.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
@@ -21,7 +24,51 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.json(listEndpoints(app));
+});
+
+// Endpoint that returns all the videogames
+app.get("/videogames", (req, res) => {
+  const category = req.query.category;
+  const year = req.query.year;
+
+  // Filter on category if category is sent
+  // For example http://localhost:3000/videogames?category=racing
+  if (category) {
+    const filterCategory = videogames.filter(game => game.category.toLowerCase() === category);
+    res.json(filterCategory);
+
+    // Endpoint that returns a specific year
+    // For example http://localhost:3000/videogames?year=2020
+  } if (year) {
+    const videogameByYear = videogames.filter(game => game.release_year === +year);
+    res.json(videogameByYear);
+
+    // If no categori is sent, return all videogames
+    // For example http://localhost:3000/videogames
+  } else {
+    res.json(videogames);
+  };
+});
+
+// Endpoint that returns one single videogame
+// For example with id=3 http://localhost:3000/videogames/3
+app.get("/videogames/:id", (req, res) => {
+  const id = req.params.id;
+  const videogame = videogames.find(game => game.id === +id);
+
+  if (videogame) {
+    res.json(videogame);
+  } else {
+    res.send("No videogame found with that ID");
+  }
+});
+
+// Endpoint that returns a list sorted by highest rating
+// For example http://localhost:3000/videogames/sorted/ratings
+app.get("/videogames/sorted/ratings", (req, res) => {
+  const sortedGames = videogames.sort((a, b) => b.rating - a.rating);
+  res.json(sortedGames);
 });
 
 // Start the server
