2a5b2f5f3611f7f86c88348d65b3d115de09d64b
Anna <annah80@gmail.com>
2024-12-05 17:36:44 +0100

    added array method for find a specific breed and changed the other filters to one route with if/else statements for personality, fur length and commonality

--- server.js
+++ server.js
@@ -1,74 +1,79 @@
-import express from "express";
-import cors from "cors";
+import express from "express"
+import cors from "cors"
+import listEndpoints from "express-list-endpoints"
 import cats from "./data/cats.json"
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import topMusicData from "./data/top-music.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 9000;
-const app = express();
+
+const port = process.env.PORT || 9000
+const app = express()
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 // Start defining your routes here
-app.get("/", (request, response) => {
-  response.send("Miao miao!");
-});
 
-// Combined cats route filtering on all cats and cats with category query filter for breed.
-app.get("/cats", (request, response) => {  // http://localhost:9000/cats- route for displaying all cat breeds. 
-  const breed = request.query.breed;
+// Documentation Route
+app.get("/", (request, response) => {
+  const endpoints = listEndpoints(app) // Fetch all available routes
+  response.json({
+    message: "Miao Miao! Welcome to the Cats API!  Here you can find all types of cats. Below are the available endpoints",
+    endpoints: endpoints // Send the list of endpoints as JSON
+  })
+})
+
+// Combined cats route filtering on all cats and cats with category query filter for personality, fur length and commonality. 
+
+app.get("/cats", (request, response) => {  // http://localhost:9000/cats -route for displaying all cat breeds. 
+  const {personality, fur_length, commonality}= request.query
+
+  let filteredCats = cats
   
-  if (breed) {
-    const filteredCats = cats.filter(
-      cat => cat.breed.toLowerCase() === breed.toLowerCase()  // ex. http://localhost:9000/cats?breed=bengal
-    );
+  if (personality) {
+    filteredCats = cats.filter(
+      cat => cat.personality.toLowerCase() === personality.toLowerCase()  // ex. http://localhost:9000/cats?personality=calm  //Filtered on personality in query param. 
+    )
+  }
+
+    if (fur_length) {
+     filteredCats= cats.filter (
+      cat => cat.fur_length.toLowerCase() === fur_length.toLowerCase()
+    )
+  }
+
+  if (commonality) {
+    filteredCats= cats.filter (
+      cat=> cat.commonality.toLowerCase() === commonality.toLowerCase ()
+    )
+  }
 
     if (filteredCats.length>0) {
       response.json (filteredCats) //Return matching cats
     } else {
-      response.status(404).send("No cat breed found with that name ") //if no matches when writing a cat breed that does not exist, this message will show. 
-    }
-    } else {
-      response.json (cats) //Return all cats if no name of breed is provided
-
-    }
-  })
-
-app.get("/cats/personality/:personality", (request, response) => {
-  const personality = request.params.personality
-  
-  const catFilter = cats.filter (cats => cats.personality.toLowerCase() === personality.toLowerCase())
-  if (catFilter.length >0) {  // checks if any cats matched the personality.
-    response.status(200).json(catFilter)
-    } else {
-    response.status(404).send("No personality found with that name ") //if no matches when writing a personality that does not exist, this message will show. 
+      response.status(404).send("No cats found with the specified criteria") //if no matches when writing a cat personality, fur_length or commonality, this message will show. 
     }
   })
 
 
-app.get ("/cats/fur_length/:fur_length", (request, response) => {
-  const fur_length = request.params.fur_length
+  app.get ("/cats/:breed", (request, response) => {
+    const breed = request.params.breed.trim().toLowerCase()
 
-  const catFilter = cats.filter (cats => cats.fur_length.toLowerCase() === fur_length.toLowerCase())
-  if (catFilter.length >0){
-    response.status (200).json (catFilter)
+    const cat = cats.find (cat => cat.breed.toLowerCase() === breed)  //Find specific cat breed.
+  if (cat) {
+    response.status (200).json (cat)
   } else {
-    response.status (404).send ("No fur length found with that type")
+    response.status (400).send ("No breed found with that name")
   }
 })
-
   
   app.get("/cats/:id", (request, response) => {
-  const id = request.params.id
+    const id = request.params.id
   
-  const cat = cats.find(cat => cat.id === +id)
+    const cat = cats.find(cat => cat.id === +id)   //Find specific ID for a cat.
   if (cat) {
   response.status(200).json(cat)
   } else {
@@ -81,6 +86,7 @@ app.get ("/cats/fur_length/:fur_length", (request, response) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
 
 
 
