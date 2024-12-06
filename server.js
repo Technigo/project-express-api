1d45fd0928e3bc696b92713f55da5c34f353c3ed
Jenny <jennyandersen182@gmail.com>
2024-12-06 20:41:11 +0100

    added new dog breed dataset

--- server.js
+++ server.js
@@ -1,30 +1,24 @@
-import express from "express";
-import cors from "cors";
+const express = require("express");
+const cors = require("cors");
+const dogs = require("./data/dogs.json");
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.get("/dogs/:id", (req, res) => {
+  const id = req.params.id;
+  console.log("Requested ID:", id, "Type:", typeof id);
+  
+  const dog = dogs.find(dog => {
+    return dog.id === parseInt(id);
+  });
+  
+  if (!dog) {
+    res.status(404).json({ error: "Dog breed not found" });
+  } else {
+    res.json(dog);
+  }
 });
