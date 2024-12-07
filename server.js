0174836829f9c80cb12e2a1da24b75a63d31eaa3
Johanna Eriksson <johanna.eriksson@schibsted.com>
2024-12-07 20:46:31 +0100

    Add first route that shows the API documentation

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
 import express from "express";
 import cors from "cors";
 import expressListEndpoints from 'express-list-endpoints';
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-//import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
 import elves from "./data/elves.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=1224 npm start
+// Defines the port the app will run on. Defaults to 8080, but can be overridden when starting the server. Example command to overwrite PORT env variable value: PORT=1224 npm start
 const port = process.env.PORT || 1224; // Hoho
 const app = express();
 
@@ -21,9 +11,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Ho Ho!");
+// Documentation endpoint
+app.get("/", (request, response) => {
+  const endpoints = expressListEndpoints(app);
+  response.json({
+    message: "Welcome to the Elves API! Here are the available endpoints:",
+    endpoints: endpoints
+  });
+});
+
+// Test endpoint
+app.get("/test", (reqesst, response) => {
+  res.send("Testing testing!");
+  console.log("Testing testing");
 });
 
 // Start the server
