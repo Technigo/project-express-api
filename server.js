ddffbbea02dcb0006f16130ecbe796ff25cd32f8
Johanna Eriksson <johanna.eriksson@schibsted.com>
2024-12-07 20:30:38 +0100

    Import import expressListEndpoints from 'express-list-endpoints' to the server.js file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import expressListEndpoints from 'express-list-endpoints';
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
